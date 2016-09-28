6071ebd8cd85633da5306b45e2ccad73321220a3
Kerry Su <i_sukai@live.com>
2016-09-28 17:58:55 +0800

    AC

--- VIJOS/1097.cpp
+++ VIJOS/1097.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int heap[N],htop=1;
 inline void insert(int x){
 	heap[htop]=x;
-	for(int i=htop++;i>1&&heap[i>>1]>heap[i];i>>=1){//i>0 => i>1
+	for(int i=htop++;heap[i>>1]>heap[i];i>>=1){//i>0 => i>1
 		swap(heap[i>>1],heap[i]);
 	}
 }
 inline int pop(){
 	swap(heap[--htop],heap[1]);
-	for(int i=1,p=0;i<htop&&p!=i;i=p){
-		p=i;
+	for(int i=0,p=1;p!=i;){//p!=i;i=p    =>   while(p!=i)  i=p
+		i=p;
 		if((i<<1)<htop&&heap[i<<1]<heap[p]){
 			p=i<<1;
 		}
@@ -30,12 +30,13 @@ int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	int n,a,cnt=0,tmp;
+	heap[0]=0;
 	cin>>n;
 	for(int i=0;i<n;i++){
 		cin>>a;
 		insert(a);
 	}
-	while(htop>2){
+	for(int i=1;i<n;i++){
 		tmp=pop()+pop();
 		cnt+=tmp;
 		insert(tmp);
