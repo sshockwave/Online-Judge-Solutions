3b2bfbb4fa18c9fc6ce06d3f55ccf73d86687a1c
Kerry Su <i_sukai@live.com>
2016-09-28 18:13:59 +0800

    Init

---
+++ VIJOS/1304.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#define M 10000
+using namespace std;
+int digit[M],rev[M],n,dtop;
+char s[M];
+inline void getrev(){
+	for(int i=0;i<dtop;i++){
+		rev[dtop-i-1]=digit[i];
+	}
+}
+inline bool judge(){
+	for(int i=0;i<dtop;i++){
+		if(digit[i]!=rev[i]){
+			return false;
+		}
+	}
+	return true;
+}
+inline void addup(){
+	for(int i=0;i<dtop;i++){
+		digit[i]+=rev[i];
+		digit[i+1]+=digit[i]/n;//=  =>  +=
+		digit[i]%=n;
+	}
+	for(;digit[dtop];dtop++);
+}
+int main(){
+	memset(digit,0,sizeof(digit));
+	cin>>n>>s;
+	dtop=strlen(s);
+	int cnt=0;
+	for(int i=0;s[i];i++){
+		if(s[i]>='A'){
+			digit[dtop-i-1]=s[i]-'A'+10;
+		}else{
+			digit[dtop-i-1]=s[i]-'0';
+		}
+	}
+	while(getrev(),!judge()){
+		addup();
+		cnt++;
+	}
+	if(cnt>30){
+		cout<<"Impossible!";
+	}else{
+		cout<<"STEP="<<cnt;
+	}
+}
