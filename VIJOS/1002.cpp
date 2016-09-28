6f9eacce49332ebef02a3ffaac27bb11a9898f1c
Kerry Su <i_sukai@live.com>
2016-09-28 17:55:21 +0800

    2

--- VIJOS/1002.cpp
+++ VIJOS/1002.cpp
@@ -9,16 +9,38 @@ bool stone[N];
 int gcd(int a,int b){
 	return b?gcd(b,a%b):a;
 }
+inline int mins(int a,int b){
+	int i=max(a,0);
+	int minn=f[i];
+	for(;i<=b;i++){
+		if(minn==-1||(f[i]<minn&&f[i]!=-1)){
+			minn=f[i];
+		}
+	}
+	return minn;
+}
 int main(){
 	int l,s,t,m,last=0,space;
 	cin>>l>>s>>t>>m;
+	if(s==t){
+		int cnt=0;
+		for(int i=1;i<=m;i++){
+			cin>>stonepos[i];
+			if(stonepos[i]%s==0){
+				cnt++;
+			}
+		}
+		cout<<cnt;
+		return 0;
+	}
 	space=s/gcd(s,t)*t;
-	memset(f,0,sizeof(f));
+	memset(f,-1,sizeof(f));
 	memset(stone,0,sizeof(stone));
 	for(int i=1;i<=m;i++){
 		cin>>stonepos[i];
 	}
 	sort(stonepos,stonepos+m);
+//	cout<<"Disperse!"<<endl;
 	for(int i=1;i<=m;i++){
 		if(stonepos[i]-last>space){
 			last+=space;
@@ -26,9 +48,12 @@ int main(){
 			last+=stonepos[i]-stonepos[i-1];
 		}
 		stone[last]=true;
+//		cout<<"Stone #"<<i<<" :"<<last<<endl;
 	}
-	for(int i=0;i<=last;i++){
-		
+	f[0]=0;
+	for(int i=s;i<=last+space;i++){
+		f[i]=mins(i-t,i-s)+stone[i];
+//		cout<<"f["<<i<<"]="<<f[i]<<endl;
 	}
-	cout<<f[last];
+	cout<<f[last+space];
 }
