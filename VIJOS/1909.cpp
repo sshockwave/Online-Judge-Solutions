e1be2c53aaf0537b0d2ab0bf339a0fd83b23e2bd
Kerry Su <i_sukai@live.com>
2016-09-29 13:40:47 +0800

    Init

---
+++ VIJOS/1909.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#define N 10010
+#define M 200010
+using namespace std;
+int to[M],bro[M],head[N],s,t;
+bool vis[N],valid[N];
+void dfs(int x){
+	vis[x]=true;
+	valid[x]=true;
+	bool con=false;
+	for(int i=head[x];~i;i=bro[i]){
+		if(!vis[to[i]]){
+			if(dfs(to[i])){
+				
+			}
+		}
+	}
+	vis[x]=false;
+}
+int main(){
+	memset(head,-1,sizeof(head));
+	int n,m,x;
+	scanf("%d%d",&n,&m);
+	for(int i=0;i<m;i++){
+		scanf("%d%d",x,to[i]);
+		bro[i]=head[x];
+		head[x]=i;
+	}
+	scanf("%d%d",&s,&t);
+	memset(vis,0,sizeof(vis));
+	dfs(s);
+}
