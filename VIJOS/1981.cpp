c675cdbd6fc1e6033d3eeecba6d41aac770c7ee0
Kerry Su <i_sukai@live.com>
2016-09-30 14:18:01 +0800

    Init

---
+++ VIJOS/1981.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#define N 50010
+using namespace std;
+int dis[N];
+int main(){
+	int len,m,n;
+	scanf("%d%d%d",&len,&n,&m);
+	for(int i=0;i<n;i++){
+		scanf("%d",dis+i);
+	}
+	int l=0,r=len,mid,last,cnt;
+	while(l<r){
+		mid=l+r>>1;
+		last=cnt=0;
+		for(int i=0;i<n;i++){
+			if(dis[i]-last>=mid){
+				cnt++;
+				last=dis[i];
+			}
+		}
+		if(len-last<mid){
+			cnt--;
+		}
+		if(n-cnt<=m){
+			l=mid;
+		}else{
+			r=mid-1;
+		}
+	}
+	printf("%d",l);
+}
