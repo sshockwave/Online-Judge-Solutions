03e2d38da62250d09d0cd21ba9b5112737cee0f7
Kerry Su <i_sukai@live.com>
2016-10-02 14:29:15 +0800

    little improve

--- Vijos/1739.cpp
+++ Vijos/1739.cpp
@@ -6,21 +6,21 @@
 using namespace std;
 long long c[K];
 int main(){
-	int a,b,k,m,n,upper;
+	int a,b,k,m,n,lower;
 	long long ans;
 	scanf("%d%d%d%d%d",&a,&b,&k,&n,&m);
 	upper=m<n?m:n;
 	//C(m,k)*a^n*b^m
 	c[0]=1;
-	for(int i=1;i<=upper;i++){
+	for(int i=1;i<=lower;i++){
 		c[i]=(c[i-1]*(k-i+1)/i)%MOD;
 	}
-	ans=c[upper];
+	ans=c[lower];
 	for(int i=1;i<=n;i++){
 		ans=(ans*a)%MOD;
 	}
 	for(int i=1;i<=m;i++){
 		ans=(ans*b)%MOD;
 	}
-	printf("%d",ans);
+	printf("%lld",ans);
 }
