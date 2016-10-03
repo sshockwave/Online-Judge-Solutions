947963e687a95f4ac1c247567233f919dd507039
Kerry Su <i_sukai@live.com>
2016-10-03 19:59:08 +0800

    Add return to cal(int x).

--- Vijos/1740.cpp
+++ Vijos/1740.cpp
@@ -18,6 +18,7 @@ inline long long cal(int x){
 	for(int i=1;i<=m;i++){
 		cur+=(sum[rend[i]]-sum[lend[i]-1])*(num[rend[i]]-num[lend[i]-1]);
 	}
+	return cur;//Forgot to return cur
 }
 inline long long absl(long long a){
 	return a>0?a:-a;
@@ -26,7 +27,7 @@ inline long long minl(long long a,long long b){
 	return a<b?a:b;
 }
 int main(){
-	long long s,l=0,r=0,mid;
+	long long s,l=2147483647,r=0,mid;
 	scanf("%d%d",&n,&m);
 	cin>>s;
 	for(int i=1;i<=n;i++){
@@ -34,13 +35,17 @@ int main(){
 		if(w[i]>r){
 			r=w[i];
 		}
+		if(w[i]<l){
+			l=w[i];
+		}
 	}
-	r++;
+	l--,r++;
 	for(int i=1;i<=m;i++){
 		scanf("%d%d",lend+i,rend+i);
 	}
 	sum[0]=num[0]=0;
 	while(l+1<r){
+		cout<<"("<<l<<","<<r<<")"<<endl;
 		mid=(l+r)>>1;
 		if(cal(mid)>s){
 			l=mid;
