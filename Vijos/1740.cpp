56114b19952694dccd9782f3c7c46ad627376d1a
Kerry Su <i_sukai@live.com>
2016-10-03 19:54:53 +0800

    Binary chop + prefix. Initialize.

---
+++ Vijos/1740.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#define N 200010
+using namespace std;
+int w[N],v[N],lend[N],rend[N],num[N],n,m;
+long long sum[N];
+inline long long cal(int x){
+	for(int i=1;i<=n;i++){
+		sum[i]=sum[i-1];
+		num[i]=num[i-1];
+		if(w[i]>=x){
+			sum[i]+=v[i];
+			num[i]++;
+		}
+	}
+	long long cur=0;
+	for(int i=1;i<=m;i++){
+		cur+=(sum[rend[i]]-sum[lend[i]-1])*(num[rend[i]]-num[lend[i]-1]);
+	}
+}
+inline long long absl(long long a){
+	return a>0?a:-a;
+}
+inline long long minl(long long a,long long b){
+	return a<b?a:b;
+}
+int main(){
+	long long s,l=0,r=0,mid;
+	scanf("%d%d",&n,&m);
+	cin>>s;
+	for(int i=1;i<=n;i++){
+		scanf("%d%d",w+i,v+i);
+		if(w[i]>r){
+			r=w[i];
+		}
+	}
+	r++;
+	for(int i=1;i<=m;i++){
+		scanf("%d%d",lend+i,rend+i);
+	}
+	sum[0]=num[0]=0;
+	while(l+1<r){
+		mid=(l+r)>>1;
+		if(cal(mid)>s){
+			l=mid;
+		}else{
+			r=mid;
+		}
+	}
+	cout<<min(absl(cal(l)-s),absl(cal(r)-s));
+}
