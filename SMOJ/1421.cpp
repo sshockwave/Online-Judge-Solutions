b66ce91a4ca5af76ce3e23571fb2960cac7df492
Kerry Su <i_sukai@live.com>
2016-10-04 20:33:59 +0800

    Change to segment tree.

--- SMOJ/1421.cpp
+++ SMOJ/1421.cpp
@@ -1,57 +1,94 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <algorithm>
 //#include <ctime>
 #define N 50010
 #define INF 2147483647
+#define SEG 600100
+#define F 100000
 using namespace std;
-int l[N],r[N],f[N][2];
+int l[N],r[N],f[N][2],drop[N][2],
+	val[SEG],lend[SEG],rend[SEG],mid[SEG],lson[SEG],rson[SEG],ntop=0,root;
+bool delta[N];
 inline int abs(int a){
 	return a>0?a:-a;
 }
 inline void apmin(int &a,int b){
-	if(a>b){
+	if(a>b||a==-1){
 		a=b;
+	}
+}
+void build(int &x,int left,int right){
+	x=ntop++;
+	lend[x]=left,rend[x]=right,mid[x]=(left+right)/2,val[x]=0;
+	if(left!=right){
+		build(lson[x],left,mid[x]);
+		build(rson[x],mid[x]+1,right);
+	}
+}
+void cover(int x,int a,int b,int v){
+	if(lend[x]==rend[x]){
+		val[x]=v;
+		return;
+	}
+	if(delta[x]){
+		val[lson[x]]=val[x],delta[lson[x]]=true;
+		val[rson[x]]=val[x],delta[rson[x]]=true;
+		delta[x]=false;
+	}
+	if(a==lend[x]&&b==rend[x]){
+		delta[x]=true;
+		val[x]=v;
+	}else if(a>mid[x]){
+		cover(rson[x],a,b,v);
+	}else if(b<=mid[x]){
+		cover(lson[x],a,b,v);
+	}else{
+		cover(lson[x],a,mid[x],v);
+		cover(rson[x],mid[x]+1,b,v);
+	}
+}
+int query(int x,int p){
+	if(lend[x]==rend[x]||delta[x]){
+		return val[x];
+	}
+	if(p>mid[x]){
+		return query(rson[x],p);
+	}else{
+		return query(lson[x],p);
 	}
 }
 int main(){
 	freopen("1421.in","r",stdin);
-	freopen("1421.out","w",stdout);
-	int n,s,ans=INF;
+//	freopen("1421.out","w",stdout);
+	memset(delta,0,sizeof(delta));
+	int n,s,lm=INF,rm=-INF;
 	scanf("%d%d",&n,&s);
 	for(int i=1;i<=n;i++){
 		scanf("%d%d",l+i,r+i);
+		if(lm>l[i]){
+			lm=l[i];
+		}
+		if(rm<r[i]){
+			rm=r[i];
+		}
 	}
-	l[n+1]=r[n+1]=s,f[n+1][0]=f[n+1][1]=0;
+	build(root,0,rm-lm);
+	for(int i=1;i<=n;i++){
+		drop[i][0]=query(root,l[i]-lm);
+		drop[i][1]=query(root,r[i]-lm);
+		cover(root,l[i]-lm,r[i]-rm,i);
+	}
+	memset(f,-1,sizeof(f));
+	l[0]=r[0]=0;
+	f[n][0]=abs(s-l[n]),f[n][1]=abs(s-r[n]);
 	for(int i=n;i>0;i--){
-		f[i][0]=f[i][1]=INF;
-		for(int j=n+1;j>i;j--){
-			if(l[j]!=INF){
-				apmin(f[i][0],abs(l[j]-l[i])+f[j][0]);
-				apmin(f[i][1],abs(l[j]-r[i])+f[j][0]);
-				if(l[i]<=l[j]&&l[j]<=r[i]){
-					l[j]=INF;
-				}
-			}
-			if(r[j]!=INF){
-				apmin(f[i][0],abs(r[j]-l[i])+f[j][1]);
-				apmin(f[i][1],abs(r[j]-r[i])+f[j][1]);
-				if(l[i]<=r[j]&&r[j]<=r[i]){
-					r[j]=INF;
-				}
-			}
-		}
+		apmin(f[drop[i][0]][0],abs(l[i]-l[drop[i][0]]));
+		apmin(f[drop[i][0]][1],abs(l[i]-r[drop[i][0]]));
+		apmin(f[drop[i][1]][0],abs(r[i]-l[drop[i][1]]));
+		apmin(f[drop[i][1]][1],abs(r[i]-r[drop[i][1]]));
 	}
-	for(int i=n+1;i>0;i--){
-		if(l[i]!=INF){
-			apmin(ans,abs(l[i])+f[i][0]);
-			apmin(ans,abs(l[i])+f[i][0]);
-		}
-		if(r[i]!=INF){
-			apmin(ans,abs(r[i])+f[i][1]);
-			apmin(ans,abs(r[i])+f[i][1]);
-		}
-	}
-	printf("%d",ans);
+	printf("%d",max(f[0][0],f[0][1]));
 //	printf("\n%dms",clock());
 }
