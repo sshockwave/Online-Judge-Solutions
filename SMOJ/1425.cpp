b9e486597a7ca7256b9abd4729fe2c7e909ad547
Kerry Su <i_sukai@live.com>
2016-10-05 21:33:15 +0800

    Expand limit of f. 90/100

--- SMOJ/1425.cpp
+++ SMOJ/1425.cpp
@@ -3,11 +3,12 @@
 #include <cstring>
 #define N 35
 #define G 20
+#define bnum (i-n+b)
 using namespace std;
-double f[G][G][G][N];//win rate
+double f[N][G][G][N];//win rate
 int main(){
-//	freopen("1425.in","r",stdin);
-//	freopen("1425.out","w",stdout);
+	freopen("1425.in","r",stdin);
+	freopen("1425.out","w",stdout);
 	int tot,n,g,r,b,ans;
 	double rate;
 	for(scanf("%d",&tot);tot--;){
@@ -22,29 +23,29 @@ int main(){
 							f[0][0][0][l]=0;
 						}
 					}else{
-						if(i-n+b>0){//black
-							rate=(double)(i-n+b)/(double)(i-n+b+j+k);
+						if(bnum>0){//black
+							rate=(double)bnum/(double)(bnum+j+k);
 							for(int l=1;l<i;l++){
 								f[i][j][k][l]+=f[i-1][j][k][l-1]*rate;
 							}
 						}
 						if(j>0){//green
-							rate=(double)j/(double)(i-n+b+j+k);
+							rate=(double)j/(double)(bnum+j+k);
 							f[i][j][k][0]+=f[i][j-1][k][i-1]*rate;
 							for(int l=1;l<i;l++){
 								f[i][j][k][l]+=f[i][j-1][k][l-1]*rate;
 							}
 						}
 						if(k>0){//red
-							rate=(double)k/(double)(i-n+b+j+k);
+							rate=(double)k/(double)(bnum+j+k);
 							for(int l=0;l<i;l++){
 								f[i][j][k][l]+=f[i][j][k-1][i-l-1]*rate;
 							}
 						}
 					}
-					for(int l=0;l<i;l++){
-						cout<<"f["<<i<<"]["<<j<<"]["<<k<<"]["<<l<<"]="<<f[i][j][k][l]<<endl;
-					}
+//					for(int l=0;l<i;l++){
+//						cout<<"f["<<i<<"]["<<j<<"]["<<k<<"]["<<l<<"]="<<f[i][j][k][l]<<endl;
+//					}
 				}
 			}
 		}
@@ -56,4 +57,5 @@ int main(){
 		}
 		printf("%d\n",ans);
 	}
+	return 0;
 }
