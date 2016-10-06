986380dcb8164c46abbabe1735dac5f41d28838b
Kerry Su <i_sukai@live.com>
2016-10-06 21:39:07 +0800

    Switch to mod equations and spfa.

--- SMOJ/1424.cpp
+++ SMOJ/1424.cpp
@@ -2,46 +2,67 @@
 #include <cstdio>
 #include <cstring>
 #define N 60
+#define C 10010
+#define INF 2147483647
 using namespace std;
-int to[N<<1],bro[N<<1],val[N<<1],head[N],ntop=0,
-	n,tm;
-inline void add_edge(int u,int v,int w){
-	to[ntop]=v;
-	bro[ntop]=head[u];
-	val[ntop]=w;
-	head[u]=ntop++;
-}
-bool dfs(int x,long long sum){
-//	cout<<"At "<<x<<"\tsum="<<sum<<endl;
-	if(sum==tm){
-//		cout<<"OK!"<<endl;
-		return x==n-1;
-	}else if(sum>tm){
-		return false;
-	}
-	for(int i=head[x];~i;i=bro[i]){
-		if(dfs(to[i],sum+val[i])){
-			return true;
-		}
-	}
-	return false;
+int to[N<<1],bro[N<<1],val[N<<1],head[N],etop=0,qhead,qtail,quei[N*C*2],quec[N*C*2];
+long long reach[N][C];
+bool inq[N][C];
+inline int add_edge(int u,int v,int w){
+	to[etop]=v;
+	val[etop]=w;
+	bro[etop]=head[u];
+	head[u]=etop++;
 }
 int main(){
-	freopen("travel.in","r",stdin);
-	freopen("travel.out","w",stdout);
-	int tot,m,u,v,w;
+	freopen("1424.in","r",stdin);
+	freopen("1424.out","w",stdout);
+	int tot,n,m,u,v,w,modder,x,hash,cur,chash;
+	long long t;
 	for(scanf("%d",&tot);tot--;){
 		memset(head,-1,sizeof(head));
-		scanf("%d%d%d",&n,&m,&tm);
+		memset(reach,-1,sizeof(reach));
+		memset(inq,0,sizeof(inq));
+		cin>>n>>m>>t;
 		while(m--){
 			scanf("%d%d%d",&u,&v,&w);
 			add_edge(u,v,w);
 			add_edge(v,u,w);
 		}
-		if(dfs(0,0)){
+		modder=INF;
+		for(int i=head[0];~i;i=bro[i]){
+			if(val[i]<modder){
+				modder=val[i];
+			}
+		}
+		if(modder==INF){
+			printf("Impossible\n");
+			continue;
+		}
+		modder<<=1;
+		qhead=qtail=0;
+		reach[0][0]=0;
+		inq[0][0]=true;
+		quei[qtail]=0,quec[qtail]=0,qtail++;
+		while(qhead!=qtail){
+			x=quei[qhead],hash=quec[qhead],qhead++;
+			inq[x][hash]=false;
+			for(int i=head[x];~i;i=bro[i]){
+				cur=to[i],chash=(hash+val[i])%modder;
+				if(reach[cur][chash]==-1||reach[cur][chash]>reach[x][hash]+val[i]){
+					reach[cur][chash]=reach[x][hash]+val[i];
+					if(!inq[cur][chash]){
+						inq[cur][chash]=true;
+						quei[qtail]=cur,quec[qtail]=chash,qtail++;
+					}
+				}
+			}
+		}
+		if(reach[n-1][t%modder]!=-1&&reach[n-1][t%modder]<=t){
 			printf("Possible\n");
 		}else{
 			printf("Impossible\n");
 		}
 	}
+	return 0;
 }
