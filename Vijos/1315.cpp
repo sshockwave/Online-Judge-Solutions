b1e77545cd34f9fd343fd14317d541cf0251c2d1
sshockwave <i_sukai@live.com>
2016-10-09 17:55:01 +0800

    Accepted

--- Vijos/1315.cpp
+++ Vijos/1315.cpp
@@ -18,7 +18,7 @@ struct bigint{
 		bigint c;
 		c.dtop=max(dtop,b.dtop);
 		for(int i=0;i<c.dtop;i++){
-			c.digit[i]=digit[i]+b.digit[i];
+			c.digit[i]+=digit[i]+b.digit[i];
 			c.digit[i+1]+=c.digit[i]/10;
 			c.digit[i]%=10;
 		}
@@ -31,12 +31,13 @@ struct bigint{
 		for(int i=0;i<c.dtop;i++){
 			c.digit[i]+=digit[i]-b.digit[i];
 			if(c.digit[i]<0){
-				c.digit[i+1]-=c.digit[i]/10;
-				c.digit[i]=(c.digit[i]+10)%10;
+				c.digit[i+1]--;
+				c.digit[i]+=10;
 			}
 		}
 		for(;digit[dtop]==0&&dtop>0;dtop--);
 		dtop++;
+		return c;
 	}
 	void print(){
 		for(int i=dtop-1;i>=0;i--){
@@ -52,19 +53,13 @@ int main(){
 		c[0][i].digit[0]=1,c[0][i].dtop=1;
 		for(int j=1;j<=i;j++){
 			c[j][i]=c[j][i-1]+c[j-1][i-1];
-			cout<<"c["<<j<<"]["<<i<<"]=";
-			c[j][i].print();
-			cout<<endl;
 		}
 	}
 	for(int i=2;i<=w/k&&i<=(1<<k);i++){
-		ans=ans+c[i][1<<k];
-		cout<<"ANS+=c["<<i<<"]["<<(1<<k)<<"]=";
-		c[i][1<<k].print();
-		cout<<endl;
+		ans=ans+c[i][(1<<k)-1];
 	}
 	if(w%k&&w/k<(1<<k)){
-		ans=ans+c[w/k+1][1<<k]-c[w/k+1][(1<<k)-(1<<(w%k))];
+		ans=ans+c[w/k+1][(1<<k)-1]-c[w/k+1][(1<<k)-(1<<(w%k))];
 	}
 	ans.print();
 }
