b830335d011153e8be165ccda6fa8f24b6f6a5e3
sshockwave <i_sukai@live.com>
2016-10-09 13:42:47 +0800

    Switch to tarjan

--- SMOJ/1419.cpp
+++ SMOJ/1419.cpp
@@ -1,63 +1,53 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-//#include <ctime>
 #define N 10010
 #define M 50010
 using namespace std;
-int to[M],bro[M],head[N],que[N],qhead,qtail,etop=0,n;
-bool vis[N],valid[N];
+int to[M],bro[M],head[N],n,dfn[N],low[N],stk[N],ring[N],rtop=0,stop=0,dtop=0;
+bool instk[N];
 inline void add_edge(int u,int v){
 	to[etop]=v;
 	bro[etop]=head[u];
 	head[u]=etop++;
 }
-inline bool judge(){
-	for(int i=1;i<=n;i++){
-		if(!vis[i]){
-			return false;
+void dfs(int x){
+	dfn[x]=low[x]=dtop++;
+	instk[x]=true;
+	stk[stop++]=x;
+	int cur;
+	for(int i=head[x];~i;i=bro[i]){
+		cur=to[i];
+		if(dfn[cur]==-1){
+			dfs(cur);
+			if(low[cur]<low[x]){
+				low[x]=low[cur];
+			}
+		}else if(instk[cur]&&dfn[cur]<low[x]){
+			low[x]=cur;
 		}
 	}
-	return true;
+	if(dfn[x]==low[x]){
+		ring[rtop++]=x;
+		
+	}
+	instk[x]=false;
 }
 int main(){
-	freopen("1419.in","r",stdin);
-	freopen("1419.out","w",stdout);
+//	freopen("1419.in","r",stdin);
+//	freopen("1419.out","w",stdout);
 	memset(head,-1,sizeof(head));
-	memset(valid,0,sizeof(valid));
-	int m,u,v,x,ans=0;
+	memset(instk,0,sizeof(instk));
+	memset(dfn,-1,sizeof(dfn));
+	int u,v;
 	scanf("%d%d",&n,&m);
 	for(int i=0;i<m;i++){
 		scanf("%d%d",&u,&v);
-		add_edge(v,u);
+		add_edge(u,v);
 	}
 	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			vis[j]=false;
-		}
-		qhead=qtail=0;
-		que[qtail++]=i;
-		vis[i]=true;
-		while(qhead!=qtail){
-			x=que[qhead++];
-			if(valid[x]){
-				valid[i]=true;
-				break;
-			}
-			for(int i=head[x];~i;i=bro[i]){
-				if(!vis[to[i]]){
-					vis[to[i]]=true;
-					que[qtail++]=to[i];
-				}
-			}
-		}
-		if(!valid[i]){
-			valid[i]=judge();
-		}
-		if(valid[i]){
-			ans++;
+		if(dfn[i]==-1){
+			dfs(i);
 		}
 	}
-	printf("%d",ans);
-//	printf("\n%dms",clock());
 }
