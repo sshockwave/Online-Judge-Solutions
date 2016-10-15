a20921c47c7ff0762e27827665063c40fb18fe34
Kerry Su <i_sukai@live.com>
2016-10-15 13:45:41 +0800

    Include iostream.

--- Vijos/1843.cpp
+++ Vijos/1843.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <algorithm>
 #define clr(x) memset(x,0,sizeof(x))
 #define N 10010
 #define M 50010
@@ -8,16 +10,23 @@ using namespace std;
 struct edge{
 	int u,v,w;
 }e[M];
+bool found[N];
 inline bool edgecmp(const edge &a,const edge &b){
 	return a.w>b.w;
+}
+inline void apmin(int &a,int b){
+	if(a>b){
+//		cout<<"Apply min"<<endl; 
+		a=b;
+	}
 }
 namespace unif{
 	int fa[N],rank[N];
 	int getroot(int x){
-		if(fa[x]){
-			return fa[x]=getroot(fa[x]);
+		if(fa[x]==x){//fa[x]==x:return fa[x]=getroot(fa[x]) => now
+			return x;
 		}
-		return x;
+		return fa[x]=getroot(fa[x]);
 	}
 	void join(int a,int b){
 		a=getroot(a),b=getroot(b);
@@ -40,9 +49,12 @@ namespace tree{
 		head[u]=ntop++;
 	}
 	void dfs(int x){
+//		cout<<"DFS:"<<x<<endl;
+//		cout<<"\twalk 1 to "<<walk[x][0]<<"\tmin="<<f[x][0]<<endl;
 		for(int i=1;walk[x][i-1];i++){
 			walk[x][i]=walk[walk[x][i-1]][i-1];
 			f[x][i]=min(f[x][i-1],f[walk[x][i-1]][i-1]);
+//			cout<<"\twalk "<<(1<<i)<<" to "<<walk[x][i]<<"\tmin="<<f[x][i]<<endl;
 		}
 		for(int i=head[x];i;i=bro[i]){
 			if(to[i]!=walk[x][0]){
@@ -57,35 +69,61 @@ namespace tree{
 		if(dep[u]<dep[v]){
 			swap(u,v);
 		}
+//		cout<<"Query:("<<u<<","<<v<<")"<<endl;
 		int ans=INF;
 		for(int i=14;i>=0;i--){
-			if(dep[walk[u][i]]>=dep[v]){
-				ans=min(ans,f[u][i]);
+			if(walk[u][i]&&dep[walk[u][i]]>=dep[v]){
+//				cout<<"\tWalk up to "<<walk[u][i]<<endl;
+				apmin(ans,f[u][i]);
 				u=walk[u][i];
 			}
 		}
 		if(u==v){
 			return ans;
 		}
-		
+		for(int i=14;i>=0;i--){
+			if(walk[u][i]&&walk[v][i]&&walk[u][i]!=walk[v][i]){
+				apmin(ans,f[u][i]);
+				apmin(ans,f[v][i]);
+				u=walk[u][i],v=walk[v][i];
+			}
+		}
+		apmin(ans,f[u][0]);
+		apmin(ans,f[v][0]);
+		return ans;
 	}
 }
 int main(){
-	int n,m;
+	int n,m,q,a,b;
 	scanf("%d%d",&n,&m);
 	for(int i=0;i<m;i++){
 		scanf("%d%d%d",&e[i].u,&e[i].v,&e[i].w);
 	}
-	sort(edge,edge+m,edgecmp);
-	clr(unif::fa),clr(unif::rank),clr(tree:head);
+	sort(e,e+m,edgecmp);
+	for(int i=1;i<=n;i++){
+		unif::fa[i]=i;
+	}
+	clr(unif::rank),clr(tree::head);
 	for(int i=0;i<m;i++){
 		if(unif::getroot(e[i].u)!=unif::getroot(e[i].v)){
-			tree::add_edge(e[i].u,e[i].v);
-			tree::add_edge(e[i].v,e[i].u);
+			tree::add_edge(e[i].u,e[i].v,e[i].w);
+			tree::add_edge(e[i].v,e[i].u,e[i].w);
 			unif::join(e[i].u,e[i].v);
 		}
 	}
-	clr(tree::walk),clr(tree::f),tree::dep[1]=tree::dep[0]=0;
-	tree::dfs(1);
-	
+	clr(tree::walk),clr(tree::f),clr(tree::dep),clr(found);
+	for(int i=1;i<=n;i++){
+		if(!found[unif::getroot(i)]){
+			found[unif::fa[i]]=true;
+			tree::dfs(unif::fa[i]);//i  =>  fa[i]
+		}
+	}
+	for(scanf("%d",&q);q--;){
+		scanf("%d%d",&a,&b);
+		if(unif::fa[a]==unif::fa[b]){
+			cout<<tree::query(a,b)<<endl;
+		}else{
+			cout<<-1<<endl;
+		}
+	}
 }
