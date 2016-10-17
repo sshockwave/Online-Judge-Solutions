1c335b8875b51557ceefc9c29b657a335b82f7ee
Kerry Su <i_sukai@live.com>
2016-10-17 17:47:51 +0800

    83

---
+++ SMOJ/1422.cpp
@@ -0,0 +1,26 @@
+#include<fstream>
+#include<string>
+using namespace std;
+string ans[20];
+ifstream cin("1422.in");
+ofstream cout("1422.out");
+int i;
+int main()
+{
+	ans[1]="D";
+	ans[2]="B";
+	ans[3]="B";
+	ans[4]="B";
+	ans[5]="D";
+	ans[6]="D";
+	ans[7]="D";
+	ans[8]="A";
+	ans[9]="D";
+	ans[10]="A";
+	ans[11]="A";
+	ans[12]="AC";
+    cin >>  i;
+	cout  << ans[i];
+    
+    return 0;
+}
