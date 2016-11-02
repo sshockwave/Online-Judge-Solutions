b90c7389a4fd75dea515975e3f9b676bfa1ddfb4
Kerry Su <i_sukai@live.com>
2016-11-02 17:57:54 +0800

    WA

--- Vijos/1980.cpp
+++ Vijos/1980.cpp
@@ -11,7 +11,7 @@ inline void apmin(int &a,int b){
 }
 inline void getf(int cnt){
 	int num[5]={0},a,b,c,d;
-	for(int i=0;i<=13;i++){
+	for(int i=1;i<=13;i++){
 		num[card[i]]++;
 	}
 	a=num[1],b=num[2],c=num[3],d=num[4];
@@ -27,7 +27,7 @@ void dfs(int cnt){
 		return;
 	}
 	getf(cnt);
-	for(int amt=3;amt>=1;amt++){
+	for(int amt=3;amt>=1;amt--){
 		for(int i=2;i+len[amt]-1<=13;i++){
 			for(int j=i;j<=13;j++){
 				card[j]-=amt;
@@ -59,6 +59,6 @@ int main(){
 		}
 		ans=INF;
 		dfs(0);
-		printf("%d\n",ans);
+		printf("%d\n",ans+(card[0]>0));
 	}
 }
