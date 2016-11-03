c8cec1c54b99d1bdfc2b6d4b9aaeaf174eeb723b
Kerry Su <i_sukai@live.com>
2016-11-03 16:52:59 +0800

    WA

---
+++ Vijos/1100.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#define N 33
+using namespace std;
+int choose[N][N];
+long long f[N][N];
+void dfs(int l,int r){
+	if(l>r){
+		return;
+	}
+	printf("%d ",choose[l][r]);
+	dfs(l,choose[l][r]-1);
+	dfs(choose[l][r]+1,r);
+}
+int main(){
+	int n;
+	long long cur;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		scanf("%d",&f[i][i]);
+		choose[i][i]=i;
+	}
+	for(int i=1;i<n;i++){
+		for(int j=1;j+i<=n;j++){
+			f[j][j+i]=0;
+			choose[j][j+i]=j;
+			for(int k=j;k<=j+i;k++){
+				cur=1;
+				if(j<=k-1){
+					cur*=f[j][k-1];
+				}
+				if(k+1<=j+i){
+					cur*=f[k+1][j+i];
+				}
+				if(cur>f[j][j+i]){
+					f[j][j+i]=cur;
+					choose[j][j+i]=k;
+				}
+			}
+		}
+	}
+	printf("%d\n",f[1][n]);
+	dfs(1,n);
+}
