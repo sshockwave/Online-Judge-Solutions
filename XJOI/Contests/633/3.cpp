fc7d022dabe15034c0b8bd12f892bd4b01a61c46
Kerry Su <i_sukai@live.com>
2016-11-05 18:24:51 +0800

    Init new dp.

--- XJOI/Contests/633/3.cpp
+++ XJOI/Contests/633/3.cpp
@@ -2,37 +2,35 @@
 #include <cstdio>
 #include <cstring>
 #define N 3010
-#define INF 10000000
+#define INF 2147483647
 using namespace std;
-int n,lim,f[N][N],val[N],size[N],fa[N],dep[N],
+int n,lim,f[N][N],val[N],
 	to[N<<1],bro[N<<1],head[N],etop=0;
+inline void apmax(int &a,int b){
+	if(a<b){
+		a=b;
+	}
+}
 inline void add_edge(int u,int v){
 	to[etop]=v;
 	bro[etop]=head[u];
 	head[u]=etop++;
 }
-void dfs(int x){
-	size[x]=1;
-	dep[x]=dep[fa[x]]+1;
+void dfs(int x,int fa){
 	for(int i=head[x];~i;i=bro[i]){
-		if(to[i]!=fa[x]){
-			fa[to[i]]=x;
-			dfs(to[i]);
-			size[x]+=size[to[i]];
-		}
-	}
-	for(int i=0;i<=size[x];i++){
-		f[x][i]=-INF;
-	}
-	f[x][1]=val[x];
-	for(int i=head[x];~i;i=bro[i]){
-		if(to[i]!=fa[x]){
-			for(int j=size[x];j>1;j--){
-				for(int k=min(size[to[i]],j-1);k>=0;k--){
-					f[x][j]=max(f[x][j],f[x][j-k]+f[to[i]][k]);
-				}
+		int v=to[i];
+		if(v!=fa){
+			for(int j=lim;j>=0;j--){
+				f[v][j]=f[x][j];
+			}
+			dfs(v,x);
+			for(int j=lim;j>0;j--){
+				apmax(f[x][j],f[v][j-1]+val[v]);
 			}
 		}
+	}
+	for(int i=0;i<=lim;i++){
+		cout<<"f["<<x<<"]["<<i<<"]="<<f[x][i]<<endl;
 	}
 }
 int main(){
@@ -41,14 +39,16 @@ int main(){
 		scanf("%d",val+i);
 	}
 	memset(head,-1,sizeof(head));
-	int u,v;
+	int u,v,ans=-INF;
 	for(int i=1;i<n;i++){
 		scanf("%d%d",&u,&v);
 		add_edge(u,v);
 		add_edge(v,u);
 	}
-	fa[1]=dep[0]=0;
 	memset(f,0,sizeof(f));
-	dfs(1);
-	printf("%d",f[1][lim]);
+	dfs(1,0);
+	for(int i=0;i<=lim;i++){
+		apmax(ans,f[1][i]);
+	}
+	printf("%d",ans+val[1]);
 }
