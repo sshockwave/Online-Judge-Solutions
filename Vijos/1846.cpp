9b8be39b7c20f57dcaa08832e30ca54185422390
Kerry Su <i_sukai@live.com>
2016-11-08 18:39:07 +0800

    Initialize bfs+spfa.

--- Vijos/1846.cpp
+++ Vijos/1846.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <queue>
 #define N 35
-#define S 810000
-#define INF 2147483647
+#define INF 2139062143
 using namespace std;
-bool mat[N][N];
-int tx,ty,ans,dis[S],mx[4]={0,1,0,-1},my[4]={1,0,-1,0},n,m,q,qtop=1,qpos[S];
-inline int abs(int a){
-	return a>0?a:-a;
-}
+int mx[4]={0,0,1,-1},my[4]={1,-1,0,0},n,m,bdis[N][N],jx,jy,bro[N*N*16],val[N*N*16],head[N][N][4],etop=0,dis[N][N][4];
+string direction[4]={"Right","Left","Down","Up"};
+bool mat[N][N],inq[N][N][4];
 inline void apmin(int &a,int b){
 	if(a>b){
 		a=b;
@@ -18,91 +16,110 @@ inline void apmin(int &a,int b){
 inline bool valid(int x,int y){
 	return x>=1&&x<=n&&y>=1&&y<=m&&mat[x][y];
 }
+struct coor{
+	int x,y;
+	coor(int a,int b){
+		a=x,b=y;
+	}
+};
 struct state{
-	int ex,ey,sx,sy,hash,exp;
+	int x,y,d;
 	state(){}
-	state(int a,int b,int c,int d){
-		ex=a,ey=b,sx=c,sy=d;
-		hash=sy-1+30*(sx-1+30*(ey-1+30*(ex-1)));
-		exp=abs(tx-sx)+abs(ty-sy);
+	state(int a,int b,int c){
+		x=a,y=b,d=c;
 	}
-}cur,que[S];
-bool operator < (state a,state b){
-	return dis[a.hash]+a.exp<dis[b.hash]+b.exp;
-}
-inline void maintain(int pos){
-	for(;pos>1&&que[pos]<que[pos>>1];pos>>=1){
-		qpos[que[pos>>1].hash]=pos;
-		qpos[que[pos].hash]=pos>>1;
-		swap(que[pos],que[pos>>1]);
-	}
-}
-inline void push(state x){
-	que[qtop]=x;
-	qpos[x.hash]=qtop;
-	qtop++;
-}
-inline state pop(){
-	qtop--;
-	int p=1,v=qtop;
-	while(p!=v){
-		qpos[que[p].hash]=v;
-		qpos[que[v].hash]=q;
-		swap(que[p],que[v]);
-		v=p;
-		if(que[p<<1]<que[v]){
-			v=p<<1;
-		}
-		if(que[(p<<1)+1]<que[v]){
-			v=(p<<1)+1;
+}to[N*N*16];
+queue<coor>q;
+queue<state>qs;
+inline void bfs(int sx,int sy){
+	cout<<"New bfs from ("<<sx<<","<<sy<<")"<<endl;
+	q.push(coor(sx,sy));
+	memset(bdis,-1,sizeof(bdis));
+	bdis[sx][sy]=0;
+	int x,y;
+	while(!q.empty()){
+		coor cur=q.front();
+		q.pop();
+		cout<<"\t("<<cur.x<<","<<cur.y<<")="<<bdis[cur.x][cur.y]<<endl;
+		for(int i=0;i<4;i++){
+			x=cur.x+mx[i];
+			y=cur.y+my[i];
+			if(valid(x,y)&&(x!=jx||y!=jy)&&bdis[x][y]==-1){
+				bdis[x][y]=bdis[cur.x][cur.y]+1;
+				q.push(coor(x,y));
+			}
 		}
 	}
-	qpos[que[qtop].hash]=0;
-	return que[qtop];
+}
+inline void add_edge(state u,state v,int w){
+	cout<<"Add edge:("<<u.x<<","<<u.y<<")"<<direction[u.d]<<"\t==>\t("<<v.x<<","<<v.y<<")->"<<direction[v.d]<<"\tlength="<<w<<endl;
+	to[etop]=v;
+	bro[etop]=head[u.x][u.y][u.d];
+	val[etop]=w;
+	head[u.x][u.y][u.d]=etop++;
 }
 int main(){
-	int qry,ex,ey,sx,sy,shash,tmp;
-	scanf("%d%d%d",&n,&m,&qry);
+	memset(head,-1,sizeof(head));
+	int tot,tmp,ex,ey,sx,sy,tx,ty;
+	scanf("%d%d%d",&n,&m,&tot);
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=m;j++){
 			scanf("%d",&tmp);
 			mat[i][j]=(tmp==1);
 		}
 	}
-	while(qry--){
-		scanf("%d%d%d%d%d%d",&ex,&ey,&sx,&sy,&tx,&ty);
-		memset(dis,127,sizeof(dis));
-		memset(qpos,0,sizeof(qpos));
-		dis[sy-1+30*(sx-1+30*(ey-1+30*(ex-1)))]=0;
-		ans=-1;
-		push(state(ex,ey,sx,sy));
-		while(qtop>1){
-			cur=pop();
-			cout<<"ex="<<cur.ex<<"\tey="<<cur.ey<<"\tsx="<<cur.sx<<"\tsy="<<cur.sy<<"\tdis="<<dis[cur.hash]<<endl;
-			if(sx==tx&&sy==ty){
-				ans=dis[cur.hash];
-				break;
-			}
-			for(int i=0;i<4;i++){
-				ex=cur.ex+mx[i],ey=cur.ey+my[i];
-				if(!valid(ex,ey)){
-					continue;
-				}
-				if(ex==cur.sx&&ey==cur.sy){
-					sx=cur.ex,sy=cur.ey;
-				}else{
-					sx=cur.sx,sy=cur.sy;
-				}
-				shash=sy-1+30*(sx-1+30*(ey-1+30*(ex-1)));
-				if(dis[shash]>dis[cur.hash]+1){
-					dis[shash]=dis[cur.hash]+1;
-					if(qpos[shash]==0){
-						push(state(ex,ey,sx,sy));
+	for(jx=1;jx<=n;jx++){
+		for(jy=1;jy<=m;jy++){
+			if(mat[jx][jy]){
+				for(int k=0;k<4;k++){
+					if(valid(jx+mx[k],jy+my[k])){
+						add_edge(state(jx,jy,k),state(jx+mx[k],jy+my[k],k^1),1);
+						bfs(jx+mx[k],jy+my[k]);
+						for(int l=0;l<4;l++){
+							if(l!=k&&bdis[jx+mx[l]][jy+my[l]]!=-1){
+								add_edge(state(jx+mx[k],jy+my[k],k),state(jx+mx[l],jy+my[l],l),bdis[jx+mx[l]][jy+my[l]]);
+							}
+						}
 					}
-					maintain(qpos[shash]);
 				}
 			}
 		}
-		printf("%d\n",ans);
+	}
+	while(tot--){
+		memset(dis,127,sizeof(dis));
+		memset(inq,0,sizeof(inq));
+		scanf("%d%d%d%d%d%d",&ex,&ey,&sx,&sy,&tx,&ty);
+		cout<<"SPFA"<<endl;
+		bfs(ex,ey);
+		for(int i=0;i<4;i++){
+			if(bdis[sx+mx[i]][sy+my[i]]!=-1){
+				dis[sx][sy][i]=bdis[sx+mx[i]][sy+my[i]];
+				qs.push(state(sx,sy,i));
+				inq[sx][sy][i]=true;
+			}
+		}
+		while(!qs.empty()){
+			state cur=qs.front();
+			qs.pop();
+			cout<<"Current:("<<cur.x<<","<<cur.y<<")->"<<direction[cur.d]<<"\tdis="<<dis[cur.x][cur.y][cur.d]<<endl;
+			for(int i=head[cur.x][cur.y][cur.d];~i;i=bro[i]){
+				if(dis[to[i].x][to[i].y][to[i].d]>dis[cur.x][cur.y][cur.d]+val[i]){
+					dis[to[i].x][to[i].y][to[i].d]=dis[cur.x][cur.y][cur.d]+val[i];
+					if(!inq[to[i].x][to[i].y][to[i].d]){
+						inq[to[i].x][to[i].y][to[i].d]=true;
+						qs.push(to[i]);
+					}
+				}
+			}
+		}
+		int ans=INF;
+		for(int i=0;i<4;i++){
+			apmin(ans,dis[tx][ty][i]);
+		}
+		if(ans==INF){
+			printf("-1\n");
+		}else{
+			printf("%d\n",ans);
+		}
 	}
 }
