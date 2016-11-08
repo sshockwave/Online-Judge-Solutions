534c246bafd3ce3054a729238d2fc752827a000e
Kerry Su <i_sukai@live.com>
2016-11-08 18:56:46 +0800

    WA 60/100

--- Vijos/1846.cpp
+++ Vijos/1846.cpp
@@ -5,8 +5,8 @@
 #define N 35
 #define INF 2139062143
 using namespace std;
-int mx[4]={0,0,1,-1},my[4]={1,-1,0,0},n,m,bdis[N][N],jx,jy,bro[N*N*16],val[N*N*16],head[N][N][4],etop=0,dis[N][N][4];
-string direction[4]={"Right","Left","Down","Up"};
+int mx[4]={0,0,1,-1},my[4]={1,-1,0,0},n,m,bdis[N][N],bro[N*N*16],val[N*N*16],head[N][N][4],etop=0,dis[N][N][4];
+//string direction[4]={"Right","Left","Down","Up"};
 bool mat[N][N],inq[N][N][4];
 inline void apmin(int &a,int b){
 	if(a>b){
@@ -32,7 +32,7 @@ struct state{
 queue<coor>q;
 queue<state>qs;
 inline void bfs(int sx,int sy){
-	cout<<"New bfs from ("<<sx<<","<<sy<<")"<<endl;
+//	cout<<"New bfs from ("<<sx<<","<<sy<<")"<<endl;
 	q.push(coor(sx,sy));
 	memset(bdis,-1,sizeof(bdis));
 	bdis[sx][sy]=0;
@@ -40,11 +40,11 @@ inline void bfs(int sx,int sy){
 	while(!q.empty()){
 		coor cur=q.front();
 		q.pop();
-		cout<<"\t("<<cur.x<<","<<cur.y<<")="<<bdis[cur.x][cur.y]<<endl;
+//		cout<<"\t("<<cur.x<<","<<cur.y<<")="<<bdis[cur.x][cur.y]<<endl;
 		for(int i=0;i<4;i++){
 			x=cur.x+mx[i];
 			y=cur.y+my[i];
-			if(valid(x,y)&&(x!=jx||y!=jy)&&bdis[x][y]==-1){
+			if(valid(x,y)&&bdis[x][y]==-1){
 				bdis[x][y]=bdis[cur.x][cur.y]+1;
 				q.push(coor(x,y));
 			}
@@ -52,7 +52,7 @@ inline void bfs(int sx,int sy){
 	}
 }
 inline void add_edge(state u,state v,int w){
-	cout<<"Add edge:("<<u.x<<","<<u.y<<")"<<direction[u.d]<<"\t==>\t("<<v.x<<","<<v.y<<")->"<<direction[v.d]<<"\tlength="<<w<<endl;
+//	cout<<"Add edge:("<<u.x<<","<<u.y<<")"<<direction[u.d]<<"\t==>\t("<<v.x<<","<<v.y<<")->"<<direction[v.d]<<"\tlength="<<w<<endl;
 	to[etop]=v;
 	bro[etop]=head[u.x][u.y][u.d];
 	val[etop]=w;
@@ -68,20 +68,22 @@ int main(){
 			mat[i][j]=(tmp==1);
 		}
 	}
-	for(jx=1;jx<=n;jx++){
-		for(jy=1;jy<=m;jy++){
-			if(mat[jx][jy]){
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			if(mat[i][j]){
+				mat[i][j]=false;
 				for(int k=0;k<4;k++){
-					if(valid(jx+mx[k],jy+my[k])){
-						add_edge(state(jx,jy,k),state(jx+mx[k],jy+my[k],k^1),1);
-						bfs(jx+mx[k],jy+my[k]);
+					if(valid(i+mx[k],j+my[k])){
+						add_edge(state(i,j,k),state(i+mx[k],j+my[k],k^1),1);
+						bfs(i+mx[k],j+my[k]);
 						for(int l=0;l<4;l++){
-							if(l!=k&&bdis[jx+mx[l]][jy+my[l]]!=-1){
-								add_edge(state(jx+mx[k],jy+my[k],k),state(jx+mx[l],jy+my[l],l),bdis[jx+mx[l]][jy+my[l]]);
+							if(l!=k&&bdis[i+mx[l]][j+my[l]]!=-1){
+								add_edge(state(i,j,k),state(i,j,l),bdis[i+mx[l]][j+my[l]]);
 							}
 						}
 					}
 				}
+				mat[i][j]=true;
 			}
 		}
 	}
@@ -89,8 +91,10 @@ int main(){
 		memset(dis,127,sizeof(dis));
 		memset(inq,0,sizeof(inq));
 		scanf("%d%d%d%d%d%d",&ex,&ey,&sx,&sy,&tx,&ty);
-		cout<<"SPFA"<<endl;
+//		cout<<"SPFA"<<endl;
+		mat[sx][sy]=false;
 		bfs(ex,ey);
+		mat[sx][sy]=true;
 		for(int i=0;i<4;i++){
 			if(bdis[sx+mx[i]][sy+my[i]]!=-1){
 				dis[sx][sy][i]=bdis[sx+mx[i]][sy+my[i]];
@@ -101,10 +105,11 @@ int main(){
 		while(!qs.empty()){
 			state cur=qs.front();
 			qs.pop();
-			cout<<"Current:("<<cur.x<<","<<cur.y<<")->"<<direction[cur.d]<<"\tdis="<<dis[cur.x][cur.y][cur.d]<<endl;
+//			cout<<"Current:("<<cur.x<<","<<cur.y<<")->"<<direction[cur.d]<<"\tdis="<<dis[cur.x][cur.y][cur.d]<<endl;
 			for(int i=head[cur.x][cur.y][cur.d];~i;i=bro[i]){
 				if(dis[to[i].x][to[i].y][to[i].d]>dis[cur.x][cur.y][cur.d]+val[i]){
 					dis[to[i].x][to[i].y][to[i].d]=dis[cur.x][cur.y][cur.d]+val[i];
+//					cout<<"\tUpdate ("<<to[i].x<<","<<to[i].y<<")->"<<direction[to[i].d]<<" dis="<<dis[to[i].x][to[i].y][to[i].d]<<endl;
 					if(!inq[to[i].x][to[i].y][to[i].d]){
 						inq[to[i].x][to[i].y][to[i].d]=true;
 						qs.push(to[i]);
