3690559564573d51fa803e2604e15b124cb20751
Kerry Su <i_sukai@live.com>
2016-11-11 22:01:31 +0800

    AC 63/100

--- SMOJ/1468.cpp
+++ SMOJ/1468.cpp
@@ -3,15 +3,18 @@
 #include <cstring>
 #define N 100010 
 using namespace std;
-int a[N],val[N*3],pos[N];
+int val[N*3],pos[N];
 void build(int x,int l,int r){
 	if(l==r){
 		pos[l]=x;
-		val[x]=a[l];
 	}else{
 		int mid=l+r>>1;
 		build(x<<1,l,mid);
 		build((x<<1)+1,mid+1,r);
+	}
+}
+inline void update(int x){
+	for(;x;x>>=1){
 		val[x]=max(val[x<<1],val[(x<<1)+1]);
 	}
 }
@@ -20,23 +23,19 @@ int main(){
 	freopen("1468.out","w",stdout);
 	int n,m,flag,x,y;
 	scanf("%d%d",&n,&m);
-	for(int i=1;i<=n;i++){
-		scanf("%d",a+i);
-	}
 	build(1,1,n);
+	for(int i=1;i<=n;i++){
+		scanf("%d",&x);
+		val[pos[i]]=x;
+		update(pos[i]>>1);
+	}
 	while(m--){
 		scanf("%d",&flag);
 		if(flag==1){
 			scanf("%d%d",&x,&y);
 			x=pos[x];
 			val[x]=y;
-			for(;x;x>>=1){
-				if(val[x>>1]<val[x]){
-					val[x>>1]=val[x];
-				}else{
-					break;
-				}
-			}
+			update(x>>1);
 		}else{
 			printf("%d\n",val[1]);
 		}
