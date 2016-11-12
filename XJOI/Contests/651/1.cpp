89d2cbee1cf45b3b8cf521e6b7221a6d2eb9275a
Kerry Su <i_sukai@live.com>
2016-11-12 18:42:08 +0800

    Accepted

--- XJOI/Contests/651/1.cpp
+++ XJOI/Contests/651/1.cpp
@@ -1,42 +1,34 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <vector>
 #define P 100010
 #define K 1000010
 #define MOD 998244353
 using namespace std;
-int a[K+P],start[P],w;
-vector<int>ext[P];
-vector<int>::iterator status[P];
-long long fac=1,ans=0;
-void dfs(int x){
-	if(status[x]==ext[x].end()){
-		ans=(ans+fac*x)%MOD;
-		fac=(fac*w)%MOD;
-		return;
-	}
-	int p=start[*status[x]];
-	status[x]++;
-	for(;a[p];p++){
-		dfs(a[p]);
-	}
-	status[x]--;
-}
+int seq[K],*start[P],len[P],color[P],
+	f[P],fac[P];
 int main(){
-	int p,q,k,c;
-	scanf("%d%d%d",&p,&q,&w);
-	for(int i=1,j=0;i<=q;i++,j++){
-		scanf("%d%d",&k,&c);
-		ext[c].push_back(i);
-		for(start[i]=j;j-start[i]<k;j++){
-			scanf("%d",a+j);
+	int p,q,l,k,c;
+	scanf("%d%d%d",&p,&q,&l);
+	for(int i=0,j=0;i<q;i++){
+		scanf("%d%d",len+i,color+i);
+		for(start[i]=seq+j;seq+j!=start[i]+len[i];j++){
+			scanf("%d",seq+j);
 		}
-		a[j]=0;
 	}
 	for(int i=1;i<=p;i++){
-		status[i]=ext[i].begin();
+		f[i]=i;
+		fac[i]=l;
 	}
-	dfs(1);
-	printf("%d",ans);
+	for(int i=q-1;i>=0;i--){
+		int *s=start[i];
+		long long sum=0,mul=1;
+		for(int j=0;j<len[i];j++){
+			(sum+=f[s[j]]*mul)%=MOD;
+			(mul*=fac[s[j]])%=MOD;
+		}
+		f[color[i]]=sum;
+		fac[color[i]]=mul;
+	}
+	printf("%d",f[1]);
 }
