a6f3b188c3119707153948464001abb27a2ce5c2
Kerry Su <i_sukai@live.com>
2016-11-13 15:11:20 +0800

    Accepted.

--- XJOI/Contests/652/1.cpp
+++ XJOI/Contests/652/1.cpp
@@ -24,13 +24,11 @@ inline double sqr(double a){
 inline double abs(double a){
 	return a>0?a:-a;
 }
-inline double process(double a){
-	return (double)round(a*100)/100;
-}
 int main(){
 	int n,m1,m2,d,ans=0;
-	scanf("%d%d%d%d%d%d",&n,&m1,&m2,&d,&b[0].x,&b[0].y);
-	memset(f,0,sizeof(f));
+	scanf("%d%d%d%d%lf%lf",&n,&m1,&m2,&d,&b[0].x,&b[0].y);
+	memset(f,-1,sizeof(f));
+	f[0]=0;
 	b[0].t=0;
 	for(int i=1;i<=n;i++){
 		scanf("%lf%lf%lf",&b[i].x,&b[i].y,&b[i].t);
@@ -38,19 +36,19 @@ int main(){
 	sort(b+1,b+n+1);
 	for(int i=1;i<=n;i++){
 		for(int j=0;j<i;j++){
-			if(sqr(b[i].x-b[j].x)+sqr(b[i].y-b[j].y)-sqr(b[i].t-b[j].t)<=EPS){
+			if(f[j]!=-1&&sqr(b[i].x-b[j].x)+sqr(b[i].y-b[j].y)-sqr(b[i].t-b[j].t)<=EPS){
 				apmax(f[i],f[j]+1);
 			}
 		}
-		if(f[i]*d>=m2){
-			printf("Y %.2f",process(b[i].t));
-			return 0;
-		}
-		if((i-f[i])*d>=m1){
-			printf("N %.2f",process(b[i].t));
-			return 0;
-		}
 		apmax(ans,f[i]);
+		if(f[i]*d>=m2){
+			printf("Y %.2f",b[i].t);
+			return 0;
+		}
+		if((i-ans)*d>=m1){
+			printf("N %.2f",b[i].t);
+			return 0;
+		}
 	}
 	printf("%d",ans);
 }
