75e4ea209f0cfd406c4cf019f13c285e92828986
Kerry Su <i_sukai@live.com>
2016-11-15 19:29:35 +0800

    Accepted. Check point's ways from spfa to priority_queue.

--- XJOI/Contests/654/2.cpp
+++ XJOI/Contests/654/2.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include  <queue>
+#include <queue>
 #define MOD 1000000009
 #define N 105
 #define INF 2147483647
 using namespace std;
-int mat[N][N],ans,n,cnt,mx[4]={0,0,1,-1},my[4]={1,-1,0,0};
-struct state{
-	bool mat[N][N];
-	int x,y,dis;
-	state(){
-		memset(mat,0,sizeof(mat));
-	}
+int mat[N][N],ans,n,mx[4]={0,0,1,-1},my[4]={1,-1,0,0},dis[N][N],ways[N][N];
+bool inq[N][N];
+struct coor{
+	int x,y;
 };
-queue<state>q;
+bool operator < (coor a,coor b){
+	return dis[a.x][a.y]>dis[b.x][b.y];
+}
+queue<coor>q;
+priority_queue<coor>pq;
 inline bool valid(int x,int y){
 	return x>=1&&x<=n&&y>=1&&y<=n&&x+y<=n+1;
 }
+inline void apmin(int &a,int b){
+	if(a>b){
+		a=b;
+	}
+}
 int main(){
+	memset(inq,0,sizeof(inq));
 	while(scanf("%d",&n),n){
 		for(int i=1;i<=n;i++){
 			for(int j=1;j<=n;j++){
@@ -30,38 +37,56 @@ int main(){
 				mat[i][j]+=mat[n+1-j][n+1-i];
 			}
 		}
-		ans=INF;
-		state cur;
-		cur.mat[1][1]=true;
-		cur.x=1,cur.y=1;
-		cur.dis=0;
-		q.push(cur);
+		memset(dis,127,sizeof(dis));
+		dis[1][1]=mat[1][1];
+		inq[1][1]=true;
+		q.push((coor){1,1});
 		while(!q.empty()){
-			cur=q.front();
+			int x=q.front().x,y=q.front().y;
 			q.pop();
-			if(cur.dis>ans){
-				continue;
-			}
-			if(cur.x+cur.y==n+1){
-				if(cur.dis<ans){
-					cnt=1;
-					ans=cur.dis;
-				}else{
-					cnt=(cnt+1)%MOD;
-				}
-				continue;
-			}
-			for(int i=0,x=cur.x,y=cur.y;i<4;i++){
-				cur.x=x+mx[i],cur.y=y+my[i];
-				if(valid(cur.x,cur.y)&&!cur.mat[cur.x][cur.y]){
-					cur.mat[cur.x][cur.y]=true;
-					cur.dis+=mat[cur.x][cur.y];
-					q.push(cur);
-					cur.dis-=mat[cur.x][cur.y];
-					cur.mat[cur.x][cur.y]=false;
+			inq[x][y]=false;
+			for(int i=0,tx,ty;i<4;i++){
+				tx=x+mx[i],ty=y+my[i];
+				if(valid(tx,ty)){
+					if(dis[tx][ty]>dis[x][y]+mat[tx][ty]){
+						dis[tx][ty]=dis[x][y]+mat[tx][ty];
+						if(!inq[tx][ty]){
+							inq[tx][ty]=true;
+							q.push((coor){tx,ty});
+						}
+					}
 				}
 			}
 		}
-		printf("%d\n",cnt);
+		memset(ways,0,sizeof(ways));
+		ways[1][1]=1;
+		pq.push((coor){1,1});
+		while(!pq.empty()){
+			int x=pq.top().x,y=pq.top().y;
+			pq.pop();
+			inq[x][y]=false;
+			for(int i=0,tx,ty;i<4;i++){
+				tx=x+mx[i],ty=y+my[i];
+				if(valid(tx,ty)){
+					if(dis[tx][ty]==dis[x][y]+mat[tx][ty]){
+						(ways[tx][ty]+=ways[x][y])%=MOD;
+						if(!inq[tx][ty]){
+							inq[tx][ty]=true;
+							pq.push((coor){tx,ty});
+						}
+					}
+				}
+			}
+		}
+		int minv=INF,ans=0;
+		for(int i=1;i<=n;i++){
+			apmin(minv,dis[i][n+1-i]);
+		}
+		for(int i=1;i<=n;i++){
+			if(dis[i][n+1-i]==minv){
+				(ans+=ways[i][n+1-i])%=MOD;
+			}
+		}
+		printf("%d\n",ans);
 	}
 }
