daa897634d2214677eaf4d9ca2f334931589f880
Kerry Su <i_sukai@live.com>
2016-11-17 20:39:11 +0800

    SPFA+SFL. Accepted.

--- XJOI/Contests/660/3.cpp
+++ XJOI/Contests/660/3.cpp
@@ -1,33 +1,57 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <algorithm>
+#define N 200010
 #define M 100010
+#define V (N+M)
 using namespace std;
-struct interval{
-	int l,r;
-}area[M];
-bool icmp(interval a,interval b){
-	return a.l<b.l;
+int to[V<<1],bro[V<<1],val[V<<1],head[N],etop=0,dis[N],que[N],qhead,qtail;
+bool inq[N];
+inline void add_edge(int u,int v,int w){
+	to[etop]=v;
+	bro[etop]=head[u];
+	val[etop]=w;
+	head[u]=etop++;
 }
 int main(){
-	int n,m;
+	memset(head,-1,sizeof(head));
+	memset(inq,0,sizeof(inq));
+	memset(dis,127,sizeof(dis));
+	int n,m,u,v;
 	scanf("%d%d",&n,&m);
-	for(int i=0;i<m;i++){
-		scanf("%d%d",&area[i].l,&area[i].r);
+	for(int i=1;i<=n;i++){
+		add_edge(i,i+1,1);
+		add_edge(i+1,i,0);
 	}
-	sort(area,area+m,icmp);
-	int reach=0,least=n,ans=area[0].l-1;
-	for(int i=0;i<m;i++){
-		if(area[i].l>least){
-			ans+=1+max(0,area[i].l-reach-1);
-			least=area[i].r;
-		}else{
-			least=min(least,area[i].r);
+	while(m--){
+		scanf("%d%d",&u,&v);
+		add_edge(u,v+1,1);
+		add_edge(v+1,u,-1);
+	}
+	dis[1]=0;
+	inq[1]=true;
+	qhead=qtail=0;
+	que[qtail++]=1;
+	while(qhead!=qtail){
+		int x=que[qhead++];
+		qhead%=N;
+		inq[x]=false;
+		for(int i=head[x];~i;i=bro[i]){
+			if(dis[to[i]]>dis[x]+val[i]){
+				dis[to[i]]=dis[x]+val[i];
+				if(!inq[to[i]]){
+					inq[to[i]]=true;
+					if(dis[to[i]]<dis[que[qhead]]){
+						(qhead+=N-1)%=N;
+						que[qhead]=to[i];
+					}else{
+						que[qtail++]=to[i];
+						qtail%=N;
+					}
+				}
+			}
 		}
-		reach=max(reach,area[i].r);
 	}
-	ans+=n-reach+1;
-	cout<<ans;
+	printf("%d",dis[n+1]);
 	return 0;
 }
