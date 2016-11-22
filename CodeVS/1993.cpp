3b234347f81df6d6dd3b5015a14d70b2ceec1a09
Kerry Su <i_sukai@live.com>
2016-11-22 16:54:54 +0800

    Accepted. Dinic.

--- CodeVS/1993.cpp
+++ CodeVS/1993.cpp
@@ -3,56 +3,48 @@
 #include <cstring>
 #include <queue>
 #define N 250
+#define INF 2147483647
 using namespace std;
-int c[N][N],f[N][N],to[N],dis[N],m;
-inline bool bfs(){
+int c[N][N],n,que[N],qhead,qtail,dis[N];
+inline void bfs(){
 	memset(dis,-1,sizeof(dis));
-	queue<int>point;
-	point.push(m);
-	dis[m]=0;
-	int cur;
-	while(!point.empty()){
-		cur=point.front();
-		point.pop();
-		for(int i=1;i<=m;i++){
-			if(c[i][cur]>f[i][cur]&&dis[i]==-1){
-				point.push(i);
-				dis[i]=dis[cur]+1;
+	dis[n]=0;
+	qhead=qtail=0;
+	que[qtail++]=n;
+	while(qhead<qtail){
+		int x=que[qhead++];
+		for(int i=1;i<=n;i++){
+			if(dis[i]==-1&&c[i][x]){
+				dis[i]=dis[x]+1;
+				que[qtail++]=i;
 			}
 		}
 	}
-	return dis[1]!=-1;
 }
-inline int dfs(int x,int high){
-	if(x==m){
-		return high;
+int dfs(int x,int allo){
+	if(x==n){
+		return allo;
 	}
-	int tmp,sum=0;
-	for(int i=1;i<=m;i++){
-		if(dis[i]==dis[x]-1&&c[x][i]>f[x][i]){
-			tmp=dfs(i,min(high,c[x][i]-f[x][i]));
-			f[x][i]+=tmp;
-			f[i][x]-=tmp;//ADD THIS
-			high-=tmp;
-			sum+=tmp;
-			if(high==0){
-				break;
-			}
+	int flow=0;
+	for(int i=1,delta;i<=n&&allo;i++){
+		if(dis[i]==dis[x]-1){
+			delta=dfs(i,min(allo,c[x][i]));
+			c[x][i]-=delta,c[i][x]+=delta;
+			flow+=delta,allo-=delta;
 		}
 	}
-	return sum;
+	return flow;
 }
 int main(){
-	int n,a,b,flow=0;
-	cin>>n>>m;
+	int m,u,v,w,delta,flow=0;
+	scanf("%d%d",&m,&n);
 	memset(c,0,sizeof(c));
-	for(int i=0;i<n;i++){
-		cin>>a>>b;
-		cin>>c[a][b];
+	while(m--){
+		scanf("%d%d%d",&u,&v,&w);
+		c[u][v]+=w;
 	}
-	memset(f,0,sizeof(f));
-	while(bfs()){
-		flow+=dfs(1,2147482647);
+	while(bfs(),delta=dfs(1,INF)){
+		flow+=delta;
 	}
-	cout<<flow;
+	printf("%d",flow);
 }
