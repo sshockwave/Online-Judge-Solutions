bcebd146381a031d9a8b13b6d88fe03cbdb3d996
Kerry Su <i_sukai@live.com>
2016-11-27 11:24:10 +0800

    Accepted. Chain.

--- SMOJ/1518.cpp
+++ SMOJ/1518.cpp
@@ -1,72 +1,68 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <queue>
 #include <algorithm>
-#include <ctime>
 #define N 100010
 #define M 7000010
-#define NODE 14200039
 #define INF 2147483647
 using namespace std;
 long long u,v;
-int val[NODE],lson[NODE],rson[NODE],fa[NODE],etop,root,delta=0;
-void build(int &x,int l,int r){
-	if(l==r){
-		x=l;
-		return;
-	}
-	x=++etop;
-	int mid=l+r>>1;
-	build(lson[x],l,mid);
-	build(rson[x],mid+1,r);
-	fa[lson[x]]=fa[rson[x]]=x;
-}
-inline void update(int x){
-	while(x!=root){
-		x=fa[x];
-		val[x]=max(val[lson[x]],val[rson[x]]);
-	}
-}
-inline bool valcmp(const int &a,const int &b){
+int origin[N],que[2][N+M],qhead[3],qtail[3];
+bool oricmp(const int &a,const int &b){
 	return a>b;
 }
+inline bool empty(int x){
+	return qhead[x]==qtail[x];
+}
+inline int pop(){
+	int ret=-INF,q;
+	if(!empty(0)&&ret<origin[qhead[0]]){
+		ret=origin[qhead[0]];
+		q=0;
+	}
+	if(!empty(1)&&ret<que[0][qhead[1]]){
+		ret=que[0][qhead[1]];
+		q=1;
+	}
+	if(!empty(2)&&ret<que[1][qhead[2]]){
+		ret=que[1][qhead[2]];
+		q=2;
+	}
+	(++qhead[q])%=(N+M);
+	return ret;
+}
+inline void push(int q,int x){
+	que[q-1][qtail[q]++]=x;
+	qtail[q]%=(N+M);
+}
 int main(){
-//	freopen("earthworm3.in","r",stdin);
-	freopen("earthworm.in","r",stdin);
-	freopen("earthworm.out","w",stdout);
-	int n,m,q,t;
-	memset(val,128,sizeof(val));
-	scanf("%d%d%d%lld%lld%d",&n,&m,&q,&u,&v,&t);
-	etop=n+m;
-	build(root,1,n+m);
-	for(int i=1;i<=n;i++){
-		scanf("%d",val+i);
-		update(i);
+	freopen("1518.in","r",stdin);
+	freopen("1518.out","w",stdout);
+	int m,q,t,delta=0;
+	scanf("%d%d%d%lld%lld%d",qtail,&m,&q,&u,&v,&t);
+	for(int i=0;i<qtail[0];i++){
+		scanf("%d",origin+i);
 	}
-	for(int i=1,x;i<=m;i++){
-		x=root;
-		while(x>n+m){
-			if(val[lson[x]]==val[x]){
-				x=lson[x];
-			}else{
-				x=rson[x];
-			}
-		}
-		val[x]+=delta;
-		if(i%t==0){
-			printf("%d ",val[x]);
-		}
-		val[n+i]=val[x]-(u*val[x]/v);
-		val[x]=u*val[x]/v;
+	sort(origin,origin+qtail[0],oricmp);
+	memset(qhead,0,sizeof(qhead));
+	for(int i=1,val;i<=m;i++){
+		val=pop()+delta;
 		delta+=q;
-		val[x]-=delta,val[n+i]-=delta;
-		update(x),update(n+i);
+		if(i%t==0){
+			printf("%d ",val);
+		}
+		push(1,u*val/v-delta);
+		push(2,val-(u*val/v)-delta);
 	}
-	printf("\n");
-	sort(val+1,val+n+m+1,valcmp);
-	for(int i=1,j=(n+m)/t;i<=j;i++){
-		printf("%d ",val[i*t]+delta);
+	putchar('\n');
+	for(int i=1,val;i<=qtail[0]+m;i++){
+		val=pop();
+		if(i%t==0){
+			printf("%d ",val+delta);
+		}
 	}
 	fclose(stdin);
 	fclose(stdout);
+	return 0;
 }
