eb3a39882a5a2165df53d474b49dc7f23e5db692
Kerry Su <i_sukai@live.com>
2017-01-11 20:58:03 +0800

    Modify

--- XJOI/Contests/651/2.cpp
+++ XJOI/Contests/651/2.cpp
@@ -19,3 +19,44 @@ int main(){
 	}
 	printf("%d",f[k]);
 }
+#include <bits/stdc++.h>
+#define ll long long
+#define inf 0x7fffffff
+#define mod 998244353
+using namespace std;
+  
+ll n,k,ans;
+ll a[30];
+inline ll kusumi(ll a,ll b){ 
+    ll ans=1;for(;a;a/=2,(b*=b)%=mod)
+    if(a&1) (ans*=b)%=mod;return ans;
+}
+inline ll cal(ll n,ll x){
+    ll an=1,tmp=1;
+    for(ll i=1;i<=x;i++){
+        (tmp*=i)%=mod;
+        (an*=(n-i+1)%mod)%=mod;
+    }
+	tmp=kusumi(mod-2,tmp);
+    (an*=tmp)%=mod;
+}
+void dfs(ll x,ll sum,ll t){
+    if(sum>k) return;
+    if(x==n+1){
+        ll tmp=k-sum;
+        if(t&1)ans-=cal(n-1+tmp,n-1);
+        else ans+=cal(n-1+tmp,n-1);
+        return;
+    }
+    dfs(x+1,sum,t);
+    dfs(x+1,sum+a[x]+1,t+1);
+}
+int main(){
+    scanf("%lld%lld",&n,&k);
+    for(ll i=1;i<=n;i++){
+        scanf("%lld",&a[i]);
+    }
+	dfs(1,0,0);
+    cout<<(ans%mod+mod)%mod<<endl;
+    return 0;
+}
