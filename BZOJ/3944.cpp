4fbc35a4d0b86e99a7790acc5971a9561386896d
Kerry Su <i_sukai@live.com>
2017-02-27 18:32:01 +0800

    TLE

---
+++ BZOJ/3944.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#define N 4000000
+using namespace std;
+inline bool is_num(char c){
+	return c>='0'&&c<='9';
+}
+inline int ni(){
+	int i=0;char c;
+	while(!is_num(c=getchar()));
+	for(;is_num(c);i=i*10-'0'+c,c=getchar());
+	return i;
+}
+int phi[N],mu[N],prephi[N],premu[N],prime[N],ptop=0;
+bool np[N];
+int prem(int n){
+	if(n<10){
+		return premu[n];
+	}
+	long long ret=1;
+	for(int i=2,l,r;i<=n;i++){
+		l=i,r=n/(n/l);
+		ret-=prem(n/i)*(r-l+1);
+		i=r;
+	}
+	return ret;
+}
+int prep(int n){
+	if(n<10){
+		return prephi[n];
+	}
+	long long ret=1ll*n*(n+1)/2;
+	for(int i=2,l,r;i<=n;i++){
+		l=i,r=n/(n/l);
+		ret-=1ll*prep(n/i)*(r-l+1);
+		i=r;
+	}
+	return ret;
+}
+int main(){
+	prephi[1]=phi[1]=premu[1]=mu[1]=1;
+	memset(np,0,sizeof(np));
+	for(int i=2;i<N;i++){
+		if(!np[i]){
+			prime[ptop++]=i;
+			phi[i]=i-1;
+			mu[i]=-1;
+		}
+		prephi[i]=phi[i]+prephi[i-1];
+		premu[i]=mu[i]+premu[i-1];
+		for(int j=0;j<ptop&&i*prime[j]<N;j++){
+			np[i*prime[j]]=true;
+			if(i%prime[j]==0){
+				mu[i*prime[j]]=0;
+				phi[i*prime[j]]=phi[i]*prime[j];
+			}else{
+				mu[i*prime[j]]=-mu[i];
+				phi[i*prime[j]]=phi[i]*phi[prime[j]];
+			}
+		}
+	}
+	for(int tot=ni(),n;tot--;){
+		n=ni();
+		printf("%d %d\n",prep(n),prem(n));
+	}
+}
