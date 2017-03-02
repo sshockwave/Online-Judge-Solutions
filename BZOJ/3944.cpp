d3624c52be2a64bb514540c21bda750eb8253399
Kerry Su <i_sukai@live.com>
2017-03-02 17:20:50 +0800

    Accepted.

--- BZOJ/3944.cpp
+++ BZOJ/3944.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 #include <map>
 #include <cassert>
-#define N 4000000
+#define N 5000000
 using namespace std;
 inline bool is_num(char c){
 	return c>='0'&&c<='9';
@@ -14,20 +14,14 @@ inline int ni(){
 	for(;is_num(c);i=i*10-'0'+c,c=getchar());
 	return i;
 }
-inline long long nl(){
-	long long i=0;char c;
-	while(!is_num(c=getchar()));
-	for(;is_num(c);i=i*10-'0'+c,c=getchar());
-	return i;
-}
-int phi[N],mu[N],premu[N],prime[N],ptop=0;
+int mu[N],prime[N],ptop=0;
 map<int,int>pm;
 map<int,long long>pp;
-long long prephi[N];
+long long phi[N];
 bool np[N];
 int prem(int n){
 	if(n<N){
-		return premu[n];
+		return mu[n];
 	}
 	map<int,int>::iterator it=pm.find(n);
 	if(it!=pm.end()){
@@ -39,29 +33,27 @@ int prem(int n){
 		ret-=prem(n/i)*(r-l+1);
 		i=r;
 	}
-	pm[n]=ret;
-	return ret;
+	return pm[n]=ret;
 }
-long long prep(int n){
+long long prep(long long n){
 	if(n<N){
-		return prephi[n];
+		return phi[n];
 	}
 	map<int,long long>::iterator it=pp.find(n);
 	if(it!=pp.end()){
 		return it->second;
 	}
-	long long ret=(long long)n*((long long)n+1)/2;
+	long long ret=n*(n+1)/2;
 	int l,r;
 	for(unsigned int i=2;i<=n;i++){
 		l=i,r=n/(n/l);
 		ret-=prep(n/i)*(r-l+1);
 		i=r;
 	}
-	pp[n]=ret;
-	return ret;
+	return pp[n]=ret;
 }
 int main(){
-	prephi[1]=phi[1]=premu[1]=mu[1]=1;
+	phi[1]=mu[1]=1;
 	memset(np,0,sizeof(np));
 	for(int i=2;i<N;i++){
 		if(!np[i]){
@@ -69,18 +61,19 @@ int main(){
 			phi[i]=i-1;
 			mu[i]=-1;
 		}
-		prephi[i]=prephi[i-1]+phi[i];
-		premu[i]=mu[i]+premu[i-1];
 		for(int j=0;j<ptop&&i*prime[j]<N;j++){
 			np[i*prime[j]]=true;
 			if(i%prime[j]==0){
 				mu[i*prime[j]]=0;
 				phi[i*prime[j]]=phi[i]*prime[j];
+				break;
 			}else{
 				mu[i*prime[j]]=-mu[i];
-				phi[i*prime[j]]=phi[i]*phi[prime[j]];
+				phi[i*prime[j]]=phi[i]*(prime[j]-1);
 			}
 		}
+		phi[i]+=phi[i-1];
+		mu[i]+=mu[i-1];
 	}
 	for(int tot=ni(),n;tot--;){
 		n=ni();
