a5e4e6d3d3dc2a0e0665ef5e7a8818cd5005122a
Kerry Su <i_sukai@live.com>
2017-03-05 17:57:17 +0800

    Accepted.

--- BZOJ/1053.cpp
+++ BZOJ/1053.cpp
@@ -12,27 +12,20 @@ inline int ni(){
 	while(i=i*10-'0'+c,is_num(c=getchar()));
 	return i;
 }
-struct state{
-	int num;
-	long long g;
-};
-inline void apmax(state &a,state b){
-	if(a.g<b.g||(a.g==b.g&&a.num<b.num)){
-		a=b;
-	}
-}
-int prime[]={2,3,5,7,11,13,17,19,23,29,31},n;
-state dfs(int p,long long cur,long long g){
+int prime[]={2,3,5,7,11,13,17,19,23,29,31},n,num=1,gx=1;
+void dfs(int p,long long cur,long long g){
 	if(p>10){
-		return (state){cur,g};
+		if(gx<g){
+			gx=g,num=cur;
+		}else if(gx==g&&cur<num){
+			num=cur;
+		}
+	}else for(int i=0;cur<=n;i++,cur*=prime[p]){
+		dfs(p+1,cur,g*(i+1));
 	}
-	state ret=(state){1,1};
-	for(int i=0;cur<=n;i++,cur*=prime[p]){
-		apmax(ret,dfs(p+1,cur,g*(i+1)));
-	}
-	return ret;
 }
 int main(){
 	n=ni();
-	printf("%d",dfs(0,1,1).num);
+	dfs(0,1,1);
+	printf("%d",num);
 }
