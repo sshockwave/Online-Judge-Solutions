8858892dc7b23a1e014c1feffc7bb8428d81bf11
Kerry Su <i_sukai@live.com>
2017-03-11 20:57:52 +0800

    Output limited exceeded.

--- HDU/1402.cpp
+++ HDU/1402.cpp
@@ -25,28 +25,29 @@ inline void bitrev(int n){
 	for(int i=1;i<n;i++){
 		rev[i]=rev[i>>1]>>1;
 		if(i&1){
-			rev[i]|=n;
+			rev[i]|=n>>1;
 		}
 	}
 }
-inline void bitrev(int n,complex *num){
+inline void bitrev(int n,complex<double> *num){
 	for(int i=0;i<n;i++){
 		if(i<rev[i]){
 			swap(num[i],num[rev[i]]);
 		}
 	}
 }
-inline void fft(int n,complex *num,int d){
+inline void fft(int n,complex<double> *num,int d){
 	bitrev(n,num);
-	for(int i=2,half;i<n;i<<=1){
+	for(int i=2,half;i<=n;i<<=1){
 		half=i>>1;
-		complex<double>w=unit(i*d),p,q;
+		complex<double>wn=unit(i*d),w=1,p,q;
 		for(int j=0;j<half;j++){
 			for(int k=j;k<n;k+=i){
 				p=num[k],q=w*num[k+half];
 				num[k]=p+q;
 				num[k+half]=p-q;
 			}
+			w*=wn;
 		}
 	}
 	if(d==-1){
@@ -55,7 +56,7 @@ inline void fft(int n,complex *num,int d){
 		}
 	}
 }
-inline int process(char *s,complex *num){
+inline int process(char *s,complex<double> *num){
 	int n=0;
 	for(;s[n];n++){
 		num[n]=s[n]-'0';
@@ -67,6 +68,8 @@ inline int process(char *s,complex *num){
 }
 int main(){
 	while(scanf("%s%s",s1,s2)){
+		memset(a,0,sizeof(a));
+		memset(b,0,sizeof(b));
 		int n=process(s1,a)+process(s2,b),shift=0,r=0;
 		for(;(1<<shift)<n;shift++);
 		n=(1<<shift);
@@ -83,7 +86,14 @@ int main(){
 			digit[i]%=10;
 		}
 		while(r){
-			digit[n++]=r;
+			digit[n++]=r%10;
+			r/=10;
+		}
+		while(n&&digit[n-1]==0){
+			n--;
+		}
+		if(n==0){
+			n++;
 		}
 		for(int i=n-1;i>=0;i--){
 			putchar(digit[i]+'0');
