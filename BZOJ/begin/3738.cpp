1f8fe7c37fac44c4d1df96d050904a84f180083d
Kerry Su <i_sukai@live.com>
2017-03-11 16:31:17 +0800

    Accepted.

--- BZOJ/begin/3738.cpp
+++ BZOJ/begin/3738.cpp
@@ -27,24 +27,24 @@ inline int c(int n,int m){//c(up,down)
 int main(){
 	memset(f,0,sizeof(f));
 	int n=ni(),m=ni(),k=ni(),w,sum=0;
-	long long ans=0;
 	f[0]=1;
 	for(int i=1;i<=m;i++){
-		w=ni();
+		w=ni()+1;
 		sum+=w;
 		for(int j=sum;j>=w;j--){
-			f[j]-=f[j-w];
+			f[j]=(((long long)f[j]-f[j-w])%MOD+MOD)%MOD;
 		}
 	}
 	fac[0]=fac[1]=inv[1]=invfac[0]=invfac[1]=1;
 	for(int i=2,top=k+n-1;i<=top;i++){
 		fac[i]=(long long)fac[i-1]*i%MOD;
-		inv[i]=(((long long)MOD-(long long)inv[MOD%i]*(MOD/i))%MOD+MOD)%MOD;
+		inv[i]=((-(long long)inv[MOD%i]*(MOD/i))%MOD+MOD)%MOD;
 		invfac[i]=(long long)invfac[i-1]*inv[i]%MOD;
 	}
+	long long ans=0;
 	apmin(sum,k);
 	for(int i=0;i<=sum;i++){
-		ans=((ans+f[i]*c(n-1,k+n-1-i)%MOD)%MOD+MOD)%MOD;
+		ans=(ans+(long long)f[i]*c(n-1,k+n-1-i)%MOD)%MOD;
 	}
 	printf("%d",ans);
 }
