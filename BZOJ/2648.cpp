dbb5446e301bc6365e9c83eccec85f84aab58b0f
Kerry Su <i_sukai@live.com>
2017-03-24 15:54:39 +0800

    WA

---
+++ BZOJ/2648.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+using namespace std;
+inline bool is_num(char c){
+	return c>='0'&&c<='9';
+}
+inline int ni(){
+	int i=0;char c;
+	while(!is_num(c=getchar()));
+	while(i=i*10-'0'+c,is_num(c=getchar()));
+	return i;
+}
+int main(){
+	int 
+}
