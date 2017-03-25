119f61ddad981bf102a62ba1aac38b6f2fcdfe24
Kerry Su <i_sukai@live.com>
2017-03-25 21:29:52 +0800

    3439.cpp

--- BZOJ/3439.cpp
+++ BZOJ/3439.cpp
@@ -16,46 +16,46 @@ inline int ni(){
 	while(i=i*10-'0'+c,is_num(c=getchar()));
 	return i;
 }
-const int N=100010,L=300010,D=1700000;
+const int N=100010,L=300010,D=2000000;
 int start[N],last[N],n;
-char pool[L],*s[N];
+char s[L];
 struct SegmentTree{
 	#define lson(x) son[x][0]
 	#define rson(x) son[x][1]
-	int lend[D],rend[D],mid[D],son[D][2],sum[D],root[N],ntop;
+	int son[D][2],sum[D],root[N],ntop;
 	SegmentTree(){
 		sum[0]=root[0]=0;
 		ntop=1;
 		memset(son,0,sizeof(son));
 	}
-	void alter(int &x,int l,int r,int p){
+	int alter(int org,int l,int r,int p){
 		assert(l<=p&&p<=r);
-		lson(ntop)=lson(x),rson(ntop)=rson(x);
-		sum[ntop]=sum[x]+1;
-		x=ntop++;
-		lend[x]=l,rend[x]=r,mid[x]=(l+r)>>1;
+		int x=ntop++,mid=(l+r)>>1;
+		lson(x)=lson(org),rson(x)=rson(org);
+		sum[x]=sum[org]+1;
 		if(l==r){
-			return;
+			return x;
 		}
-		if(p<=mid[x]){
-			alter(lson(x),l,mid[x],p);
+		if(p<=mid){
+			lson(x)=alter(lson(x),l,mid,p);
 		}else{
-			alter(rson(x),mid[x]+1,r,p);
+			rson(x)=alter(rson(x),mid+1,r,p);
 		}
+		return x;
 	}
-	int query(int x1,int x2,int k){
-		assert(x2);
-		assert(k<=rend[x2]-lend[x2]+1);
-		if(lend[x2]==rend[x2]){
+	int query(int x1,int x2,int l,int r,int k){
+		assert(x2!=0);
+		assert(k<=r-l+1);
+		if(l==r){
 			assert(k==1);
-			return lend[x2];
+			return l;
 		}
-		int diff=sum[lson(x2)]-sum[lson(x1)];
+		int diff=sum[lson(x2)]-sum[lson(x1)],mid=(l+r)>>1;
 		assert(diff>=0);
 		if(diff>=k){
-			return query(lson(x1),lson(x2),k);
+			return query(lson(x1),lson(x2),l,mid,k);
 		}else{
-			return query(rson(x1),rson(x2),k-diff);
+			return query(rson(x1),rson(x2),mid+1,r,k-diff);
 		}
 	}
 }seg;
@@ -71,21 +71,22 @@ struct Trie{
 		bro[etop]=head[u];
 		head[u]=etop++;
 	}
-	void insert(int x,char *s,int id){//root=0
-		if((*s)){
-			int &c=son[x][(*s)-'a'];
-			if(c==-1){
-				c=ntop++;
+	inline void insert(int id){//rev!
+		int x=0,i=-1;
+		while(s[++i]);
+		while((--i)>=0){
+			int &to=son[x][s[i]-'a'];
+			if(to==-1){
+				to=ntop++;
 			}
-			insert(c,s+1,id);
-		}else{
-			add_edge(x,id);
+			x=to;
 		}
+		add_edge(x,id);
 	}
 	void dfs(int x){
 		int st=dfn;
 		for(int i=head[x];~i;i=bro[i]){
-			seg.alter(seg.root[dfn]=seg.root[dfn-1],1,n,to[i]);
+			seg.root[dfn]=seg.alter(seg.root[dfn-1],1,n,to[i]);
 			dfn++;
 		}
 		for(int i=0;i<26;i++){
@@ -102,22 +103,14 @@ inline int work(int i,int k){
 	if(k<=0||last[i]-start[i]<k){
 		return -1;
 	}
-	return seg.query(seg.root[start[i]-1],seg.root[last[i]-1],k);
+	return seg.query(seg.root[start[i]-1],seg.root[last[i]-1],1,n,k);
 }
 int main(){
 	n=ni();
 	char c;
-	memset(pool,0,sizeof(pool));
-	for(int i=1,p=0;i<=n;i++){
-		s[i]=pool+p;
-		while(!is_letter(c=getchar()));
-		while(pool[p++]=c,is_letter(c=getchar()));
-		for(char *j=s[i],*k=pool+(p++)-1;j<k;j++,k--){
-			swap(*j,*k);
-		}
-	}
-	for(int i=n;i>0;i--){
-		trie.insert(0,s[i],i);
+	for(int i=1,s;i<=n;i++){
+		scanf("%s",s);
+		trie.insert(i);
 	}
 	trie.dfs(0);
 	for(int i=1;i<=n;i++){
