63574f360374fb6c0b3ceb1b38e2effc0a632d55
Kerry Su <i_sukai@live.com>
2017-04-04 21:16:53 +0800

    WA.50/100

--- EZOJ/Contests/1273/B.cpp
+++ EZOJ/Contests/1273/B.cpp
@@ -27,6 +27,9 @@ struct Point{
 	Point(int a=0,int b=0){
 		x=a,y=b;
 	}
+	inline bool iszero(){
+		return x==0&&y==0;
+	}
 }pt[N],base;
 inline Point operator - (Point a,Point b){
 	return Point(a.x-b.x,a.y-b.y);
@@ -37,49 +40,53 @@ inline int det(Point a,Point b){
 inline int dot(Point a,Point b){
 	return a.x*b.x+a.y*b.y;
 }
-inline ostream & operator << (ostream & out,Point a){
-	out<<"("<<a.x<<","<<a.y<<")";
-	return out;
-}
 inline bool turn_left(Point a,Point b,Point c){
-	cout<<"Ask turn:"<<a<<b<<c;
 	int d=det(b-a,c-b);
 	if(d==0){
-		cout<<(dot(b-a,c-b)>0)<<endl;
 		return dot(b-a,c-b)>0;
 	}
-	cout<<(d>0)<<endl;
 	return d>0;
+}
+inline bool verticalcmp(Point a,Point b){
+	if(a.y==b.y){
+		return a.x<b.x;
+	}
+	return a.y<b.y;
 }
 inline bool polarcmp(int a,int b){
 	Point pa=pt[a]-base,pb=pt[b]-base;
-	if(pa.y*pb.y<0){
-		return pa.y>pb.y;
+	if(pa.y>=0&&pb.y<0){
+		return true;
+	}
+	if(pa.y<0&&pb.y>=0){
+		return false;
+	}
+	if(pa.y==0&&pb.y==0){
+		int d=dot(pa,pb);
+		if(dot>0){
+			return dot(pa,pa)<dot(pb,pb);
+		}else if(dot<0){
+			return pa.x>0;
+		}
+		return pa.x==0&&pb.x;
 	}
 	int d=det(pa,pb);
-	if(d==0){
-		if(pa.x*pb.x>0){
-			assert(pa.y==0&&pb.y==0);
-			return dot(pa,pa)<dot(pb,pb);
-		}else if(pa.x*pb.x<0){
-			assert(pa.y==0&&pb.y==0);
-			return pa.x>pb.x;
-		}
-		
+	if(d!=0){
+		return d>0;
 	}
-	return d>0;
+	return dot(pa,pa)<dot(pb,pb);
 }
 int rlist[N][N],head[N][N],f[N][N];
 int main(){
-	freopen("B.in","r",stdin);
 	int n=ni();
-	if(n<=3){
+	if(n<3){
 		printf("%d",n);
 		return 0;
 	}
 	for(int i=1;i<=n;i++){
 		scanf("%d%d",&pt[i].x,&pt[i].y);
 	}
+	sort(pt+1,pt+n+1,verticalcmp);
 	memset(rlist,0,sizeof(rlist));
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=n;j++){
@@ -87,42 +94,44 @@ int main(){
 		}
 		base=pt[i];
 		sort(rlist[i]+1,rlist[i]+n+1,polarcmp);
-		cout<<"Sort of "<<i<<endl;
-		cout<<"\tbase="<<base<<endl<<"\t";
-		for(int j=1;j<=n;j++){
-			cout<<pt[rlist[i][j]];
-		}
-		cout<<endl<<"\t=>"<<endl<<"\t";
-		for(int j=1;j<=n;j++){
-			cout<<pt[rlist[i][j]]-base;
-		}
-		cout<<endl;
 		assert(rlist[i][1]==i);
-		for(int j=2,k=3;j<=n;j++){
-			while(k<=n&&!turn_left(pt[rlist[i][j]],pt[i],pt[rlist[i][k]])){
+		for(int j=2,k=2;j<=n;j++){
+			int u=rlist[i][j];
+			while(!turn_left(pt[u],base,pt[rlist[i][k]])){
 				k++;
+				if(k>n){
+					k=2;
+				}
+				if(k==j){
+					break;
+				}
 			}
-			head[rlist[i][j]][i]=rlist[i][k];
-			cout<<"head["<<rlist[i][j]<<"]["<<i<<"]="<<rlist[i][k]<<endl;
+			if(k!=j){
+				head[u][i]=rlist[i][k];
+			}
 		}
 	}
 	int ans=0;
 	for(int i=1;i<=n;i++){
 		memset(f,0,sizeof(f));
-		for(int j=1;j<=n;j++){
-			int u=rlist[i][j];
-			f[u][head[i][u]]=2;
+		for(int j=i+1;j<=n;j++){
+			f[i][j]=1;
 		}
-		for(int j=1;j<=n;j++){
-			int u=rlist[i][j],mx=0;
-			for(int k=1;k<=n;k++){
-				int v=rlist[u][k];
-				apmax(mx,f[u][v]);
-				apmax(f[u][v],mx);
+		for(int j=i,u;j<=n;j++){
+			u=rlist[i][j];
+			if(u<i){
+				break;
+			}
+			for(int k=1,mx=0,v;k<=n;k++){
+				v=rlist[u][k];
+				if(v<i){
+					break;
+				}
+				mx=f[u][v]=max(mx,f[u][v]);
 				apmax(f[v][head[u][v]],mx+1);
 			}
 		}
-		for(int j=1;j<=n;j++){
+		for(int j=i+1;j<=n;j++){
 			apmax(ans,f[j][i]);
 		}
 	}
