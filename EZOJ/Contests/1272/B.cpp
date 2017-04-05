5feb6740a9c6b529e95982e967420ca299a819b1
Kerry Su <i_sukai@live.com>
2017-04-05 20:41:36 +0800

    Accepted.79ms

--- EZOJ/Contests/1272/B.cpp
+++ EZOJ/Contests/1272/B.cpp
@@ -12,74 +12,54 @@ inline int ni(){
 	while(i=i*10-'0'+c,is_num(c=getchar()));
 	return i;
 }
+inline void apmax(int &a,int b){
+	if(a<b){
+		a=b;
+	}
+}
 const int N=100010;
-long long f[N][2][2];//f[i][len[i]-len[i-1]][len[i+1]-len[i]]
+long long f[N][8];
 char s[N];
-int bef[N],nf[N];
-inline void pr(int i,int j,int k,int w){
-	cout<<"("<<i<<","<<i-1<<")="<<i-1<<",("<<i<<","<<i<<")="<<i-1+j<<",("<<i<<","<<i+1<<")="<<i-1+j+k<<"     "<<w<<endl;
-}
-inline void dp(int i,char c,long long delta){
-	for(int t=i;t<=i+2;t++){
-		if(c==s[t]){
-			nf[t]=bef[t-1]+1;
-		}else{
-			nf[t]=max(nf[t-1],bef[t]);
-		}
-	}
-	if(nf[i]<i){
-		return;
-	}
-	cout<<"\tChar=";
-	if(c){
-		cout<<c;
-	}else{
-		cout<<"E";
-	}
-	cout<<" Add to:";
-	pr(i+1,nf[i+1]-nf[i],nf[i+2]-nf[i+1],delta);
-	f[i+1][nf[i+1]-nf[i]][nf[i+2]-nf[i+1]]+=delta;
-}
 int main(){
-//	freopen("B.in","r",stdin);
-	int n=ni(),m=ni();
-	if(n==1){
-		printf("%d",m-1);
-		return 0;
-	}
+	int n=ni();
+	char m=ni();
+	memset(s,0,sizeof(s));
+	memset(f,0,sizeof(f));
 	scanf("%s",s+1);
 	if(s[1]==s[2]){
-		f[1][0][0]=m-1;
-		f[1][0][1]=0;
-		f[1][1][0]=1;
-		f[1][1][1]=0;
+		f[1][6]=1;//0 1 1:+1 +1 0
+		f[1][4]=m-1;//0 0 0:+1 0 0
 	}else{
-		f[1][0][0]=m-2;
-		f[1][0][1]=1;
-		f[1][1][0]=1;
-		f[1][1][1]=0;
+		f[1][6]=1;//0 1 1:+1 +1 0
+		f[1][5]=1;//0 0 1:+1 0 +1
+		f[1][4]=m-2;//0 0 0:+1 0 0
 	}
-	for(int i=1;i<=n;i++){
-		for(int j=0;j<2;j++){
-			for(int k=0;k<2;k++){
-				if(f[i][j][k]==0){
+	m+='a';
+	for(int i=1;i<n;i++){
+		for(int v=0;v<8;v++){
+			if(f[i][v]==0){
+				continue;
+			}
+			int j=((v>>2)&1)+i-2,k=((v>>1)&1)+j,l=(v&1)+k,nj,nk,nl;
+			for(char t='a';t<m;t++){
+				nj=k;
+				if(t==s[i]){
+					apmax(nj,j+1);
+				}
+				if(nj<i-1){
 					continue;
 				}
-				pr(i,j,k,f[i][j][k]);
-				bef[i-1]=i-1,bef[i]=i-1+j,bef[i+1]=i-1+j+k,bef[i+2]=0;
-				nf[i-1]=0;
-				if(s[i-1]){
-					dp(i,s[i-1],f[i][j][k]);
+				nk=max(nj,l);
+				if(t==s[i+1]){
+					apmax(nk,k+1);
 				}
-				if(s[i]!=s[i-1]){
-					dp(i,s[i],f[i][j][k]);
+				nl=nk;
+				if(t==s[i+2]){
+					apmax(nl,l+1);
 				}
-				if(s[i+1]!=s[i]&&s[i+1]!=s[i-1]){
-					dp(i,s[i+1],f[i][j][k]);
-				}
-				dp(i,0,f[i][j][k]*max(0,m-(s[i-1]!=0)-(s[i]!=s[i-1])-(s[i+1]!=s[i-1]&&s[i+1]!=s[i])));
+				f[i+1][((nj>i-1)<<2)|((nk>nj)<<1)|(nl>nk)]+=f[i][v];
 			}
 		}
 	}
-	printf("%lld",f[n][0][0]+f[n][0][1]);
+	printf("%lld",f[n][2]+f[n][3]+f[n][4]+f[n][5]);
 }
