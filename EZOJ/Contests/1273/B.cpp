477e9f628d888ed8f1675283dc4b46653f9d5d50
Kerry Su <i_sukai@live.com>
2017-04-05 14:22:22 +0800

    Accepted.

--- EZOJ/Contests/1273/B.cpp
+++ EZOJ/Contests/1273/B.cpp
@@ -13,9 +13,6 @@ inline int ni(){
 	while(i=i*10-'0'+c,is_num(c=getchar()));
 	return i;
 }
-inline int abs(int a){
-	return a>0?a:-a;
-}
 inline void apmax(int &a,int b){
 	if(a<b){
 		a=b;
@@ -40,12 +37,19 @@ inline int det(Point a,Point b){
 inline int dot(Point a,Point b){
 	return a.x*b.x+a.y*b.y;
 }
-inline bool turn_left(Point a,Point b,Point c){
-	int d=det(b-a,c-b);
+inline ostream & operator << (ostream & out,Point a){
+	out<<"("<<a.x<<","<<a.y<<")";
+	return out;
+}
+inline bool turn_left(int a,int b,int c){
+	int d=det(pt[b]-pt[a],pt[c]-pt[b]);
 	if(d==0){
-		return dot(b-a,c-b)>0;
+		return dot(pt[b]-pt[a],pt[c]-pt[b])>0;
 	}
 	return d>0;
+}
+inline bool same_direc(Point a,Point b){
+	return det(a-base,b-base)==0&&dot(a-base,b-base)>0;
 }
 inline bool verticalcmp(Point a,Point b){
 	if(a.y==b.y){
@@ -63,9 +67,9 @@ inline bool polarcmp(int a,int b){
 	}
 	if(pa.y==0&&pb.y==0){
 		int d=dot(pa,pb);
-		if(dot>0){
+		if(d>0){
 			return dot(pa,pa)<dot(pb,pb);
-		}else if(dot<0){
+		}else if(d<0){
 			return pa.x>0;
 		}
 		return pa.x==0&&pb.x;
@@ -76,7 +80,8 @@ inline bool polarcmp(int a,int b){
 	}
 	return dot(pa,pa)<dot(pb,pb);
 }
-int rlist[N][N],head[N][N],f[N][N];
+int rlist[N],f[N][N],head[N][N],nxt[N][N],ind[N][N],qu[N*N],qv[N*N];
+bool vis[N][N];
 int main(){
 	int n=ni();
 	if(n<3){
@@ -90,43 +95,79 @@ int main(){
 	memset(rlist,0,sizeof(rlist));
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=n;j++){
-			rlist[i][j]=j;
+			rlist[j]=j;
 		}
 		base=pt[i];
-		sort(rlist[i]+1,rlist[i]+n+1,polarcmp);
-		assert(rlist[i][1]==i);
-		for(int j=2,k=2;j<=n;j++){
-			int u=rlist[i][j];
-			while(!turn_left(pt[u],base,pt[rlist[i][k]])){
-				k++;
-				if(k>n){
-					k=2;
-				}
-				if(k==j){
-					break;
-				}
-			}
+		sort(rlist+1,rlist+n+1,polarcmp);
+		int j=1,k=2,u;
+		while(u=rlist[++j],u>i){
+			nxt[i][u]=rlist[j+1];
+			for(;k<=n&&!turn_left(u,i,rlist[k]);k++);
+			head[u][i]=rlist[k];
+		}
+		for(k=2;j<=n;j++){
+			u=rlist[j];
+			nxt[i][u]=rlist[j+1];
+			for(;k<=n&&k<j&&!turn_left(u,i,rlist[k]);k++);
 			if(k!=j){
-				head[u][i]=rlist[i][k];
+				head[u][i]=rlist[k];
 			}
 		}
 	}
 	int ans=0;
-	for(int i=1;i<=n;i++){
+	for(int i=1;i+ans<=n;i++){
+		base=pt[i];
 		memset(f,0,sizeof(f));
+		memset(ind,0,sizeof(ind));
+		memset(vis,0,sizeof(vis));
+		int qhead=0,qtail=0;
 		for(int j=i+1;j<=n;j++){
 			f[i][j]=1;
+			vis[i][j]=true;
+			qu[qtail]=i,qv[qtail]=j,qtail++;
 		}
-		for(int j=1,u;j<=n;j++){
-			u=rlist[i][j];
-			if(u<i){
-				break;
+		while(qhead<qtail){
+			int u=qu[qhead],v=qv[qhead];
+			qhead++;
+			int H=head[u][v],X=nxt[u][v];
+			if(H&&v>=i){
+				ind[v][H]++;
+				if(!vis[v][H]){
+					vis[v][H]=true;
+					qu[qtail]=v,qv[qtail]=H,qtail++;
+				}
 			}
-			for(int k=1,mx=0,v;k<=n;k++){
-				v=rlist[u][k];
-				mx=f[u][v]=max(mx,f[u][v]);
-				if(head[u][v]){
-					apmax(f[v][head[u][v]],mx+1);
+			if(X){
+				ind[u][X]++;
+				if(!vis[u][X]){
+					vis[u][X]=true;
+					qu[qtail]=u,qv[qtail]=X,qtail++;
+				}
+			}
+		}
+		qhead=qtail=0;
+		for(int j=i+1;j<=n;j++){
+			if(ind[i][j]==0){
+				qu[qtail]=i,qv[qtail]=j,qtail++;
+			}
+		}
+		while(qhead<qtail){
+			int u=qu[qhead],v=qv[qhead];
+			qhead++;
+			if(f[u][v]==0){
+				continue;
+			}
+			int H=head[u][v],X=nxt[u][v];
+			if(v>=i&&H){
+				apmax(f[v][H],f[u][v]+1);
+				if((--ind[v][H])==0){
+					qu[qtail]=v,qv[qtail]=H,qtail++;
+				}
+			}
+			if(X){
+				apmax(f[u][X],f[u][v]);
+				if((--ind[u][X])==0){
+					qu[qtail]=u,qv[qtail]=X,qtail++;
 				}
 			}
 		}
