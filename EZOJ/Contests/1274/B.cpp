72411eb76905cd132705900581ecfcd78e244642
Kerry Su <i_sukai@live.com>
2017-04-06 15:07:42 +0800

    WA&&RE

--- EZOJ/Contests/1274/B.cpp
+++ EZOJ/Contests/1274/B.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cmath>
+#include <complex>
 using namespace std;
 inline bool is_num(char c){
 	return c>='0'&&c<='9';
@@ -13,99 +14,107 @@ inline int ni(){
 	while(i=i*10-'0'+c,is_num(c=getchar()));
 	return i;
 }
-const int N=27,MOD=998244353;
-int n,q[N];
-inline int isqrt(int x){
-	if(x<0){
-		return -1;
-	}
-	int rt=round(sqrt(x));
-	if(rt*rt!=x){
-		return -1;
-	}else{
-		return rt;
-	}
+inline int sqr(int x){
+	return x*x;
 }
-inline bool is_palin(){
+const int N=27,MOD=998244353;
+const double EPS=1e-6;
+typedef complex<double> comp;
+int n,p1[N],p2[N],ans[N];
+inline bool is_zero(double x){
+	return x>=-EPS&&x<=EPS;
+}
+inline bool palin(int *p){
 	for(int i=1,j=n-1;i<j;i++,j--){
-		if(q[i]!=q[j]){
+		if(p[i]!=p[j]){
 			return false;
 		}
 	}
 	return true;
 }
+comp w[N],tmp[N],rt[2][N],cur[N];
+inline void dft(comp *a){
+	memcpy(tmp,a,sizeof(comp)*n);
+	memset(a,0,sizeof(comp)*n);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			a[i]+=tmp[j]*w[(i*j)%n];
+		}
+	}
+}
+inline bool check(){
+	for(int i=0;i<n;i++){
+		int t=0;
+		for(int j=0;j<n;j++){
+			t+=p1[(i-j+n)%n]*p1[j];
+			if(t>p2[i]){
+				return false;
+			}
+		}
+		if(t!=p2[i]){
+			return false;
+		}
+	}
+	return true;
+}
+inline bool les(int *a,int *b){
+	for(int i=0;i<n;i++){
+		if(a[i]!=b[i]){
+			return a[i]<b[i];
+		}
+	}
+	return false;
+}
 int main(){
 	for(int tot=ni();tot--;){
 		n=ni();
-		int sum=0,rt;
-		bool flag=true;
+		int sum=0,cnt=0;
 		for(int i=0;i<n;i++){
-			q[i]=ni();
-			sum+=q[i];
-			if(q[i]!=q[0]){
-				flag=false;
-			}
+			p2[i]=ni();
+			sum+=p2[i];
 		}
-		rt=isqrt(sum);
-		if(rt<0||!is_palin()){
+		if(!is_zero(sum-sqr((int)sqrt(sum)))||!palin(p2)){
 			puts("0");
-		}else if(n==1){
-			printf("1 %d\n",rt);
-		}else if(flag){
-			if(rt%n){
-				puts("0");
-			}else{
-				putchar('1');
-				rt/=n;
-				for(int i=0;i<n;i++){
-					printf(" %d",rt);
-				}
-				putchar('\n');
+			continue;
+		}
+		for(int i=0;i<n;i++){
+			double angle=(2*M_PI*i)/n;
+			w[i]=comp(cos(angle),sin(angle));
+			rt[0][i]=p2[i];
+		}
+		dft(rt[0]);
+		for(int i=0;i<n;i++){
+			w[i]=comp(w[i].real(),-w[i].imag());
+			rt[0][i]=sqrt(rt[0][i]);
+			rt[1][i]=-rt[0][i];
+		}
+		for(int state=(1<<(n>>1))-1;state>=0;state--){
+			for(int i=0;i<n;i++){
+				cur[i]=rt[(state>>min(i,n-i))&1][i];
 			}
-		}else if(n==2){
-			int rt2=isqrt(q[0]-q[1]);
-			if(rt2<0){
-				puts("0");
-			}else if((rt+rt2)&1){
-				puts("0");
-			}else if(rt2==0){
-				printf("1 %d %d\n",rt/2,rt/2);
-			}else{
-				printf("2 %d %d\n",(rt-rt2)/2,(rt+rt2)/2);
+			dft(cur);
+			bool flag=true;
+			for(int i=0;i<n;i++){
+				cur[i]/=n;
+				p1[i]=round(cur[i].real());
+				if(!is_zero(cur[i].real()-p1[i])||p1[i]<0){
+					flag=false;
+					break;
+				}
 			}
-		}else if(n==3){
-			int rt2=isqrt(q[0]-q[1]);
-			if(rt2<0){
-				puts("0");
-			}else if(rt2==0){
-				if((q[0]+q[1])%6){
-					puts("0");
-				}else{
-					int rt3=isqrt((q[0]+q[1])/6);
-					if(rt3<0){
-						puts("0");
-					}else{
-						printf("1 %d %d %d\n",rt3,rt3,rt3);
-					}
+			if(flag&&palin(p1)&&check()){
+				if(cnt==0||les(p1,ans)){
+					memcpy(ans,p1,sizeof(comp)*n);
 				}
-			}else{
-				int rt3=isqrt(q[0]+2*q[1]);
-				if(rt3<0){
-					puts("0");
-				}else if((rt2+rt3)%3){
-					if((rt3+2*rt2)%3){
-						puts("0");
-					}else if(rt3<rt2){
-						puts("0");
-					}else{
-						printf("1 %d %d %d\n",(rt3+2*rt2)/3,(rt3-rt2)/3,(rt3-rt2)/3);
-					}
-				}else if(rt3<2*rt2){
-					puts("0");
-				}else{
-					printf("1 %d %d %d\n",(rt3-2*rt2)/3,(rt2+rt3)/3,(rt2+rt3)/3);
-				}
+				cnt++;
 			}
 		}
+		printf("%d",cnt);
+		if(cnt){
+			for(int i=0;i<n;i++){
+				printf(" %d",ans[i]);
+			}
+		}
+		putchar('\n');
 	}
 }
