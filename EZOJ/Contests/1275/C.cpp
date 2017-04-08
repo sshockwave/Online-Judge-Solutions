a7e2388d1169107be41fc20d344985dc47ed0330
Kerry Su <i_sukai@live.com>
2017-04-09 07:56:21 +0800

    Rewrite

--- EZOJ/Contests/1275/C.cpp
+++ EZOJ/Contests/1275/C.cpp
@@ -12,80 +12,261 @@ inline int ni(){
 	while(i=i*10-'0'+c,is_num(c=getchar()));
 	return i;
 }
-const int N=1010,E=2010,INF=0x7f7f7f7f;
-int to[E],bro[E],cap[E],head[N],etop=0;
-inline void add_edge(int u,int v,int c){
-	to[etop]=v;
-	bro[etop]=head[u];
-	cap[etop]=c;
-	head[u]=etop++;
+inline void apmin(int &a,int b){
+	if(a>b){
+		a=b;
+	}
 }
-int que[N],dis[N];
-inline bool bfs(int s,int t){
-	int qhead=0,qtail=0;
-	memset(dis,-1,sizeof(dis));
-	dis[s]=0;
-	que[qtail++]=s;
-	while(qhead<qtail){
-		int x=que[qhead++];
+const int N=100010,E=200010,D=E,INF=0x7f7f7f7f;
+struct Edge{
+	int u,v,w;
+	inline bool contain(int b){
+		return u==b||v==b;
+	}
+	inline int other(int b){
+		return u^v^b;
+	}
+}e[E];
+#define lson(x) son[x][0]
+#define rson(x) son[x][1]
+struct SegmentTree{
+	int lend[D],rend[D],mid[D],son[D][2],val[D],ntop;//root=1
+	SegmentTree(){
+		ntop=1;
+		memset(val,127,sizeof(val));
+	}
+	int build(int l,int r){
+		int x=ntop++;
+		lend[x]=l,rend[x]=r,mid[x]=(l+r)>>1;
+		if(l!=r){
+			lson(x)=build(l,mid[x]);
+			rson(x)=build(mid[x]+1,r);
+		}
+		return x;
+	}
+	void alter(int p,int v,int x=1){
+		if(lend[x]==rend[x]){
+			assert(lend[x]==p);
+			val[x]=v;
+		}else{
+			alter(p,v,son[x][p>mid[x]]);
+			val[x]=min(val[lson(x)],val[rson(x)]);
+		}
+	}
+	int ask_min(int l,int r,int x=1){
+		assert(l<=r);
+		assert(lend[x]<=l&&r<=rend[x]);
+		if(l==lend[x]&&r==rend[x]){
+			return val[x];
+		}
+		if(r<=mid[x]){
+			return ask_min(l,r,lson(x));
+		}
+		if(l>mid[x]){
+			return ask_min(l,r,rson(x));
+		}
+		return min(ask_min(l,mid[x],lson(x)),ask_min(mid[x]+1,r,rson(x)));
+	}
+}rseg,tseg;
+struct Graph{
+	int to[E*2],bro[E*2],head[N];
+	Graph(){
+		memset(head,-1,sizeof(head));
+	}
+	inline void add_edge(int id,int u,int v){
+		to[id]=v;
+		bro[id]=head[u];
+		head[u]=id;
+	}
+};
+struct Undirected:Graph{
+	int stk[N],stop,dfn[N],low[N],tim,bln[N],fa[N],con[N];
+	bool instk[N];
+	int pos[N],ptop,st[N],end[N];//pos[eid]
+	Undirected(){
+		stop=0;
+		ptop=tim=1;
+		memset(dfn,0,sizeof(dfn));
+		memset(con,0,sizeof(con));
+		memset(instk,0,sizeof(instk));
+	}
+	void tarjan(int x){
+		dfn[x]=low[x]=tim;
+		stk[stop++]=x;
+		instk[x]=true;
 		for(int i=head[x],v;~i;i=bro[i]){
 			v=to[i];
-			if(dis[v]==-1&&cap[i]){
-				dis[v]=dis[x]+1;
-				if(v==t){
-					return true;
+			if(instk[v]){
+				if(v!=fa[x]){
+					assert(low[x]>dfn[v]);
+					low[x]=dfn[v];
+					con[v]=i>>1;
 				}
-				que[qtail++]=v;
+			}else{
+				assert(dfn[v]==0);
+				fa[v]=x;
+				con[v]=i>>1;
+				tarjan(v);
+				apmin(low[x],low[v]);
+			}
+		}
+		if(dfn[x]==low[x]){
+			st[x]=ptop;
+			int v;
+			do{
+				assert(stop>0);
+				v=stk[--stop];
+				bln[v]=x;
+				rseg.alter(ptop,e[con[v]].w);
+				pos[con[v]]=ptop++;
+			}while(x!=v);
+			end[x]=ptop-1;
+			assert(st[x]<=end[x]);
+		}
+	}
+	inline int ring(int u,int v){
+		assert(bln[u]==bln[v]);
+		if(u==v){
+			return INF;
+		}
+		int b=bln[u],s=st[b],t=end[b];
+		u=pos[con[u]],v=pos[con[v]];
+		if(u>v){
+			swap(u,v);
+		}
+		int ans=INF;
+		if(u!=s){
+			apmin(ans,rseg.ask_min(s,u-1));
+		}
+		if(v!=t){
+			apmin(ans,rseg.ask_min(v,t));
+		}
+		return ans+rseg.ask_min(u,v-1);
+	}
+}g;
+struct Tree:Graph{
+	int fa[N],son[N],size[N],dep[N],con[N];
+	int top[N],dfn[N],rdfn[N],tim;
+	Tree(){
+		memset(head,-1,sizeof(head));
+		memset(son,0,sizeof(son));
+		fa[1]=size[0]=son[0]=0;
+		tim=top[1]=dep[1]=1;
+	}
+	inline void renew(int x){
+		assert(son[x]);
+		tseg.alter(dfn[son[x]],min(e[con[son[x]]].w,g.ring(x,fa[son[x]])));
+	}
+	void dfs1(int x){
+		assert(x==g.bln[x]);
+		size[x]=1;
+		int bf=g.bln[fa[x]];
+		for(int i=head[x],v;~i;i=bro[i]){
+			v=to[i];
+			if(v!=bf){
+				fa[v]=e[i>>1].other(v);
+				dep[v]=dep[x]+1;
+				con[v]=i>>1;
+				dfs1(v);
+				size[x]+=size[v];
+				if(size[v]>size[son[x]]){
+					son[x]=v;
+				}
 			}
 		}
 	}
-	return false;
-}
-int aug(int x,int alloc,int t){
-	if(x==t){
-		return alloc;
-	}
-	int rest=alloc,delta;
-	for(int i=head[x],v;rest&&(~i);i=bro[i]){
-		v=to[i];
-		if(cap[i]&&dis[v]==dis[x]+1){
-			if(delta=aug(v,min(rest,cap[i]),t)){
-				rest-=delta;
-				cap[i]-=delta,cap[i^1]+=delta;
+	void dfs2(int x){
+		dfn[x]=tim;
+		rdfn[tim++]=x;
+		if(son[x]){
+			top[son[x]]=x;
+			dfs2(son[x]);
+			renew(x);
+			int bf=g.bln[fa[x]];
+			for(int i=head[x],v;~i;i=bro[i]){
+				v=to[i];
+				if(v!=bf&&v!=son[x]){
+					top[v]=v;
+					dfs2(v);
+				}
 			}
 		}
 	}
-	if(rest==0){
-		dis[x]=INF;
+	inline int approach(int &u,int v){
+		int ans=INF;
+		if(g.bln[u]==g.bln[v]){
+			ans=g.ring(u,v);
+			u=v;
+			return ans;
+		}
+		assert(dep[g.bln[u]]>dep[g.bln[v]]);
+		if(u!=g.bln[u]){
+			apmin(ans,g.ring(u,g.bln[u]));
+			u=g.bln[u];
+		}
+		int to=dfn[g.bln[v]]+1;
+		if(dfn[u]>to){
+			apmin(ans,tseg.ask_min(dfn[u],to+1));
+			u=rdfn[to];
+		}
+		assert(dfn[u]==to);
+		apmin(ans,e[con[u]].w);
+		u=fa[u];
+		apmin(ans,g.ring(u,v));
+		u=v;
+		return ans;
 	}
-	return alloc-rest;
-}
-inline int dinic(int s,int t){
-	int flow=0;
-	while(bfs(s,t)){
-		flow+=aug(s,INF,t);
+	inline int query(int u,int v){
+		int ans=INF;
+		while(top[g.bln[u]]!=top[g.bln[v]]){
+			if(dep[top[g.bln[u]]]<dep[top[g.bln[v]]]){
+				swap(u,v);
+			}
+			apmin(ans,approach(u,fa[top[g.bln[u]]]));
+		}
+		if(dep[g.bln[u]]<dep[g.bln[v]]){
+			swap(u,v);
+		}
+		apmin(ans,approach(u,v));
+		return ans;
 	}
-	return flow;
-}
+}t;
 int main(){
-	int n=ni(),e=ni();
-	memset(head,-1,sizeof(head));
-	for(int i=1;i<=e;i++){
-		int u=ni(),v=ni(),w=ni();
-		add_edge(u,v,w);
-		add_edge(v,u,0);
+	int n=ni(),m=ni();
+	for(int i=1,u,v,w;i<=m;i++){
+		e[i]=(Edge){ni(),ni(),ni()};
+		g.add_edge(i<<1,e[i].u,e[i].v);
+		g.add_edge((i<<1)|1,e[i].v,e[i].u);
 	}
-	for(int tot=ni();tot--;){
+	rseg.build(1,m);
+	g.fa[1]=0,g.tarjan(1);
+	for(int i=1,u,v;i<=m;i++){
+		u=g.bln[e[i].u];
+		v=g.bln[e[i].v];
+		if(u!=v){
+			t.add_edge(i<<1,u,v);
+			t.add_edge((i<<1)|1,v,u);
+		}
+	}
+	assert(g.bln[1]==1);
+	t.dfs1(1),t.dfs2(1);
+	tseg.build(1,t.tim-1);
+	for(int tot=ni(),op,u,v;tot--;){
 		if(ni()){
-			int id=(ni()-1)<<1;
-			cap[id]=ni();
-		}else{
-			int s=ni(),t=ni();
-			printf("%d\n",dinic(s,t));
-			for(int i=0;i<e;i++){
-				cap[i<<1]+=cap[(i<<1)|1];
-				cap[(i<<1)|1]=0;
+			int eid=ni();
+			e[eid].w=ni();
+			int u=g.bln[e[eid].u],v=g.bln[e[eid].v];
+			if(u==v){
+				rseg.alter(g.pos[eid],e[eid].w);
+				assert(g.pos[eid]);
+				t.renew(u);
+			}else if(t.dep[u]<t.dep[v]){
+				t.renew(u);
+			}else{
+				t.renew(v);
 			}
+		}else{
+			printf("%d\n",t.query(ni(),ni()));
 		}
 	}
 }
