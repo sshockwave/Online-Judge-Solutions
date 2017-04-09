1e37525d8bb8fab2c96bbdd058350b155a515806
Kerry Su <i_sukai@live.com>
2017-04-09 14:09:12 +0800

    New way for segment tree.

--- EZOJ/Contests/1275/C.cpp
+++ EZOJ/Contests/1275/C.cpp
@@ -17,59 +17,56 @@ inline void apmin(int &a,int b){
 		a=b;
 	}
 }
-const int N=100010,E=200010,D=E,INF=0x7f7f7f7f;
+const int N=100010,E=200010,INF=0x7f7f7f7f;
 struct Edge{
 	int u,v,w;
-	inline bool contain(int b){
-		return u==b||v==b;
-	}
 	inline int other(int b){
 		return u^v^b;
 	}
 }e[E];
-#define lson(x) son[x][0]
-#define rson(x) son[x][1]
 struct SegmentTree{
-	int lend[D],rend[D],mid[D],son[D][2],val[D],ntop;//root=1
+	typedef SegmentTree node;
+	int lend,rend,mid,val;
+	node *lson,*rson;
 	SegmentTree(){
-		ntop=1;
-		memset(val,127,sizeof(val));
+		val=INF;
 	}
-	int build(int l,int r){
-		int x=ntop++;
-		lend[x]=l,rend[x]=r,mid[x]=(l+r)>>1;
+	void build(int l,int r){
+		static node pool[E*4],*new_node=pool+1;
+		lend=l,rend=r,mid=(l+r)>>1;
 		if(l!=r){
-			lson(x)=build(l,mid[x]);
-			rson(x)=build(mid[x]+1,r);
+			(lson=new_node++)->build(l,mid);
+			(rson=new_node++)->build(mid+1,r);
 		}
-		return x;
 	}
-	void alter(int p,int v,int x=1){
-		if(lend[x]==rend[x]){
-			assert(lend[x]==p);
-			val[x]=v;
+	void alter(int x,int v){
+		if(lend==rend){
+			assert(x==lend);
+			val=v;
 		}else{
-			alter(p,v,son[x][p>mid[x]]);
-			val[x]=min(val[lson(x)],val[rson(x)]);
+			(x<=mid?lson:rson)->alter(x,v);
+			val=min(lson->val,rson->val);
 		}
 	}
-	int ask_min(int l,int r,int x=1){
-		assert(l<=r);
-		assert(lend[x]<=l&&r<=rend[x]);
-		if(l==lend[x]&&r==rend[x]){
-			return val[x];
+	int ask(int l,int r){
+		assert(lend<=l&&r<=rend);
+		if(lend==l&&rend==r){
+			return val;
 		}
-		if(r<=mid[x]){
-			return ask_min(l,r,lson(x));
+		if(r<=mid){
+			return lson->ask(l,r);
 		}
-		if(l>mid[x]){
-			return ask_min(l,r,rson(x));
+		if(l>mid){
+			return rson->ask(l,r);
 		}
-		return min(ask_min(l,mid[x],lson(x)),ask_min(mid[x]+1,r,rson(x)));
+		return min(lson->ask(l,mid),rson->ask(mid+1,r));
 	}
 }rseg,tseg;
+int bln[N];
 struct Graph{
-	int to[E*2],bro[E*2],head[N];
+	typedef int info[N];
+	int to[E*2],bro[E*2];
+	info head;
 	Graph(){
 		memset(head,-1,sizeof(head));
 	}
@@ -80,96 +77,80 @@ struct Graph{
 	}
 };
 struct Undirected:Graph{
-	int stk[N],stop,dfn[N],low[N],tim,bln[N],fa[N],con[N];
-	bool instk[N];
-	int pos[N],ptop,st[N],end[N],ntop;//pos[eid]
+	info con,pos,st,end;
 	Undirected(){
-		stop=ntop=0;
-		ptop=tim=1;
-		memset(dfn,0,sizeof(dfn));
-		memset(con,0,sizeof(con));
-		memset(instk,0,sizeof(instk));
+		memset(bln,0,sizeof(bln));
 	}
-	void tarjan(int x){
-		dfn[x]=low[x]=tim++;
+	void dfs(int x,int fa){
+		static bool vis[N]={0};
+		static int stk[N],stop=0;
+		vis[x]=true;
 		stk[stop++]=x;
-		instk[x]=true;
 		for(int i=head[x],v;~i;i=bro[i]){
 			v=to[i];
-			if(instk[v]){
-				if(v!=fa[x]&&dfn[v]<dfn[x]){
-					low[x]=dfn[v];
-					con[v]=i>>1;
-				}
+			if(v==fa){
+				continue;
+			}
+			con[v]=i>>1;
+			if(vis[v]){
+				assert(bln[v]==0);
+				static int ptop=1;
+				st[v]=ptop;
+				int u;
+				do{
+					u=stk[--stop];
+					bln[u]=v;
+					rseg.alter(ptop,e[con[u]].w);
+					pos[con[u]]=ptop++;
+				}while(u!=v);
+				end[v]=ptop-1;
 			}else{
-				assert(dfn[v]==0);
-				fa[v]=x;
-				con[v]=i>>1;
-				tarjan(v);
-				apmin(low[x],low[v]);
+				dfs(v,x);
 			}
 		}
-		if(dfn[x]==low[x]){
-			ntop++;
-			st[x]=ptop;
-			int v;
-			do{
-				assert(stop>0);
-				v=stk[--stop];
-				bln[v]=x;
-				if(con[v]){
-					rseg.alter(ptop,e[con[v]].w);
-				}
-				pos[con[v]]=ptop++;
-			}while(x!=v);
-			end[x]=ptop-1;
-			assert(st[x]<=end[x]);
+		if(bln[x]==0){
+			stop--;
+			assert(stk[stop]==x);
+			bln[x]=x;
 		}
 	}
-	inline int ring(int u,int v){
+	int ring(int u,int v){
 		assert(bln[u]==bln[v]);
 		if(u==v){
 			return INF;
 		}
-		int b=bln[u],s=st[b],t=end[b];
+		int ans=INF,s=st[bln[u]],t=end[bln[v]];
 		u=pos[con[u]],v=pos[con[v]];
 		if(u>v){
 			swap(u,v);
 		}
-		int ans=INF;
-		if(u!=s){
-			assert(u>s);
-			apmin(ans,rseg.ask_min(s,u-1));
+		if(u>s){
+			apmin(ans,rseg.ask(s,u-1));
 		}
-		apmin(ans,rseg.ask_min(v,t));
-		assert(u<v);
-		return ans+rseg.ask_min(u,v-1);
+		apmin(ans,rseg.ask(v,t));
+		return ans+rseg.ask(u,v-1);
 	}
-}g;
+}G;
 struct Tree:Graph{
-	int fa[N],son[N],size[N],dep[N],con[N];
-	int top[N],dfn[N],rdfn[N],tim;
+	info fa,son,size,dep,con,dfn,top;
 	Tree(){
-		memset(head,-1,sizeof(head));
+		size[0]=0;
+		dep[1]=top[1]=1;
 		memset(son,0,sizeof(son));
-		fa[1]=size[0]=son[0]=0;
-		tim=top[1]=dep[1]=1;
 	}
-	inline void renew(int x){
-		assert(son[x]);
-		tseg.alter(dfn[son[x]],min(e[con[son[x]]].w,g.ring(x,fa[son[x]])));
+	inline int ask(int x){
+		return min(e[con[x]].v,G.ring(fa[x],bln[fa[x]]));
 	}
-	void dfs1(int x){
-		assert(x==g.bln[x]);
+	void dfs1(int x,int f){
+		assert(bln[x]==x);
 		size[x]=1;
-		int bf=g.bln[fa[x]];
 		for(int i=head[x],v;~i;i=bro[i]){
 			v=to[i];
-			if(v!=bf){
+			if(v!=f){
+				con[v]=i>>1;
 				fa[v]=e[i>>1].other(v);
 				dep[v]=dep[x]+1;
-				con[v]=i>>1;
-				dfs1(v);
+				dfs1(v,x);
 				size[x]+=size[v];
 				if(size[v]>size[son[x]]){
 					son[x]=v;
@@ -177,99 +158,98 @@ struct Tree:Graph{
 			}
 		}
 	}
-	void dfs2(int x){
-		dfn[x]=tim;
-		rdfn[tim++]=x;
+	void dfs2(int x,int f){
+		assert(bln[x]==x);
+		static int tim=1;
+		dfn[x]=tim++;
 		if(son[x]){
-			top[son[x]]=x;
-			dfs2(son[x]);
-			renew(x);
-			int bf=g.bln[fa[x]];
+			top[son[x]]=top[x];
+			dfs2(son[x],x);
+			tseg.alter(dfn[son[x]],ask(son[x]));
 			for(int i=head[x],v;~i;i=bro[i]){
 				v=to[i];
-				if(v!=bf&&v!=son[x]){
+				if(v!=son[x]&&v!=f){
 					top[v]=v;
-					dfs2(v);
+					dfs2(v,x);
 				}
 			}
 		}
 	}
-	inline int approach(int &u,int v,int w){
+	int query(int u,int v){
+		assert(u!=v);
+		if(bln[u]==bln[v]){
+			return G.ring(u,v);
+		}
 		int ans=INF;
-		if(g.bln[u]==g.bln[w]){
-			ans=g.ring(u,w);
-			u=w;
-			return ans;
-		}
-		assert(dep[g.bln[u]]>dep[g.bln[w]]);
-		if(u!=g.bln[u]){
-			apmin(ans,g.ring(u,g.bln[u]));
-			u=g.bln[u];
-		}
-		assert(w==g.bln[w]);
-		int to=dfn[v];
-		if(dfn[u]>to){
-			apmin(ans,tseg.ask_min(to,dfn[u]));
-			u=v;
-		}
-		assert(dfn[u]==to);
-		apmin(ans,e[con[u]].w);
-		u=fa[u];
-		apmin(ans,g.ring(u,w));
-		u=w;
-		return ans;
-	}
-	inline int query(int u,int v){
-		int ans=INF;
-		while(top[g.bln[u]]!=top[g.bln[v]]){
-			if(dep[top[g.bln[u]]]<dep[top[g.bln[v]]]){
+		while(top[bln[u]]!=top[bln[v]]){
+			if(dep[top[bln[u]]]<dep[top[bln[v]]]){
 				swap(u,v);
 			}
-			apmin(ans,approach(u,top[g.bln[u]],fa[top[g.bln[u]]]));
+			assert(dep[top[bln[u]]]>dep[top[bln[v]]]);
+			if(u!=bln[u]){
+				apmin(ans,G.ring(u,bln[u]));
+				u=bln[u];
+			}
+			if(u!=top[u]){
+				apmin(ans,tseg.ask(dfn[top[u]]+1,dfn[u]));
+				u=top[u];
+			}
+			apmin(ans,e[con[u]].w);
+			u=fa[u];
 		}
-		if(dep[g.bln[u]]<dep[g.bln[v]]){
+		if(bln[u]==bln[v]){
+			return min(ans,G.ring(u,v));
+		}
+		if(dep[bln[u]]<dep[bln[u]]){
 			swap(u,v);
 		}
-		apmin(ans,approach(u,rdfn[dfn[v]+1],v));
-		return ans;
+		if(u!=bln[u]){
+			apmin(ans,G.ring(u,bln[u]));
+			u=bln[u];
+		}
+		if(dfn[u]>dfn[v]+1){
+			apmin(ans,tseg.ask(dfn[v]+2,dfn[u]));
+			u=son[v];
+		}
+		assert(u==son[v]);
+		apmin(ans,e[con[u]].w);
+		u=fa[u];
+		return min(ans,G.ring(u,v));
 	}
-}t;
+}T;
 int main(){
 	int n=ni(),m=ni();
-	for(int i=1,u,v,w;i<=m;i++){
+	rseg.build(1,n),tseg.build(1,n);
+	for(int i=1;i<=m;i++){
 		e[i]=(Edge){ni(),ni(),ni()};
-		g.add_edge(i<<1,e[i].u,e[i].v);
-		g.add_edge((i<<1)|1,e[i].v,e[i].u);
+		G.add_edge(i<<1,e[i].u,e[i].v);
+		G.add_edge((i<<1)|1,e[i].v,e[i].u);
 	}
-	rseg.build(1,m);
-	g.fa[1]=0,g.tarjan(1);
+	G.dfs(1,0);
 	for(int i=1,u,v;i<=m;i++){
-		u=g.bln[e[i].u];
-		v=g.bln[e[i].v];
+		u=bln[e[i].u];
+		v=bln[e[i].v];
 		if(u!=v){
-			t.add_edge(i<<1,u,v);
-			t.add_edge((i<<1)|1,v,u);
+			T.add_edge(i<<1,u,v);
+			T.add_edge((i<<1)|1,v,u);
 		}
 	}
-	assert(g.bln[1]==1);
-	tseg.build(1,g.ntop);
-	t.dfs1(1),t.dfs2(1);
-	for(int tot=ni(),op,u,v;tot--;){
+	T.dfs1(1,0),T.dfs2(1,0);
+	for(int tot=ni();tot--;){
 		if(ni()){
-			int eid=ni();
+			int eid=ni(),u,v;
 			e[eid].w=ni();
-			int u=g.bln[e[eid].u],v=g.bln[e[eid].v];
+			u=bln[e[eid].u];
+			v=bln[e[eid].v];
 			if(u==v){
-				rseg.alter(g.pos[eid],e[eid].w);
-				assert(g.pos[eid]);
-				t.renew(u);
-			}else if(t.dep[u]<t.dep[v]){
-				t.renew(u);
-			}else{
-				t.renew(v);
+				assert(G.pos[eid]);
+				rseg.alter(G.pos[eid],e[eid].w);
+				if(v=T.son[u]){
+					tseg.alter(T.dfn[v],T.ask(v));
+				}
 			}
 		}else{
-			printf("%d\n",t.query(ni(),ni()));
+			printf("%d\n",T.query(ni(),ni()));
 		}
 	}
 }
