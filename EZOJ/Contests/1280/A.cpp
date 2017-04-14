9f770d2ae7af4c10ec505b1b0bd79ade4d99dfb2
Kerry Su <i_sukai@live.com>
2017-04-14 15:10:52 +0800

    Init.

--- EZOJ/Contests/1280/A.cpp
+++ EZOJ/Contests/1280/A.cpp
@@ -13,100 +13,110 @@ inline int ni(){
 	while(i=i*10-'0'+c,is_num(c=getchar()));
 	return i;
 }
-const int N=2010;
-int perm[N],pos[N];
-bool valid[N][N];
-void dfs1(int,int,int,int);
-void dfs2(int l,int r,int a,int b,int c,int d){
-	if(l+1>=r||valid[l][r]){
-		return;
+const int N=300010,INF=0x7f7f7f7f;
+struct SegmentTree{
+	typedef SegmentTree node;
+	int lend,rend,mid,mn,mn2,mnc,mnc2,delta;
+	node *lson,*rson;
+	inline void up(){
+		assert(lend!=rend);
+		mn=lson->mn,mn2=lson->mn2,mnc=lson->mnc,mnc2=lson->mnc2;
+		if(rson->mn<mn){
+			mn2=mn,mnc2=mnc;
+			mn=rson->mn,mnc=rson->mnc;
+		}else if(rson->mn==mn){
+			mnc+=rson->mnc;
+		}else if(rson->mn<mn2){
+			mn2=rson->mn,mnc2=rson->mnc;
+		}else if(rson->mn==mn2){
+			mnc2+=rson->mnc;
+		}
+		if(rson->mn2<mn2){
+			mn2=rson->mn2,mnc2=rson->mnc2;
+		}else if(rson->mn2==mn2){
+			mnc2+=rson->mnc2;
+		}
 	}
-	if(a>b){
-		dfs1(l,r,c,d);
-		return;
+	inline void down(){
+		if(delta){
+			lson->add(lend,mid,delta);
+			rson->add(mid+1,rend,delta);
+			delta=0;
+		}
 	}
-	if(c>d){
-		dfs1(l,r,a,b);
-		return;
+	void build(int l,int r){
+		static node *n=new node[N*2];
+		lend=l,rend=r,mid=(l+r)>>1;
+		delta=mn=0,mn2=INF,mnc=mnc2=1;
+		if(l!=r){
+			(lson=n++)->build(l,mid);
+			(rson=n++)->build(mid+1,r);
+			up();
+		}
 	}
-	valid[l][r]=true;
-	if(pos[l]==a){
-		dfs2(l+1,r,a+1,b,c,d);
-	}else if(pos[l]==b){
-		dfs2(l+1,r,a,b-1,c,d);
-	}else if(pos[l]==c){
-		dfs2(l+1,r,a,b,c+1,d);
-	}else if(pos[l]==d){
-		dfs2(l+1,r,a,b,c,d-1);
+	void add(int l,int r,int v){
+		assert(lend<=l&&r<=rend);
+		if(lend==l&&rend==r){
+			delta+=v,mn+=v,mn2+=v;
+			return;
+		}
+		down();
+		if(r<=mid){
+			lson->add(l,r,v);
+		}else if(l>mid){
+			rson->add(l,r,v);
+		}else{
+			lson->add(l,mid,v);
+			rson->add(mid+1,r,v);
+		}
+		up();
 	}
-	if(pos[r]==a){
-		dfs2(l,r-1,a+1,b,c,d);
-	}else if(pos[r]==b){
-		dfs2(l,r-1,a,b-1,c,d);
-	}else if(pos[r]==c){
-		dfs2(l,r-1,a,b,c+1,d);
-	}else if(pos[r]==d){
-		dfs2(l,r-1,a,b,c,d-1);
-	}
-}
-void dfs1(int l,int r,int a,int b){
-	if(l+1>=r||valid[l][r]){
-		return;
-	}
-	assert(a<=b);
-	valid[l][r]=true;
-	dfs2(l+1,r,a,pos[l]-1,pos[l]+1,b);
-	dfs2(l,r-1,a,pos[r]-1,pos[r]+1,b);
-}
-bool isset[N];
-inline int brute(int n){
+}seg;
+inline int ask(){
 	int ans=0;
-	isset[0]=false;
-	for(int i=1;i<=n;i++){
-		for(int j=i+1;j<=n;j++){
-			memset(isset+1,0,n<<2);
-			for(int k=i;k<=j;k++){
-				isset[pos[k]]=true;
-			}
-			int ent=0;
-			for(int k=1;k<=n;k++){
-				if(!isset[k-1]&&isset[k]){
-					ent++;
-				}
-			}
-			if(ent<=2){
-				ans++;
-			}
+	if(seg.mn<=2){
+		ans+=seg.mnc;
+		if(seg.mn2<=2){
+			ans+=seg.mnc2;
 		}
 	}
 	return ans;
 }
+int perm[N],pos[N];
 int main(){
 	int n=ni();
-	bool flag=true;
+	if(n==1){
+		puts("0");
+		return 0;
+	}
+	memset(perm,0,sizeof(perm));
+	memset(pos,0,sizeof(pos));
 	for(int i=1;i<=n;i++){
 		pos[perm[i]=ni()]=i;
-		if(perm[i]!=i){
-			flag=false;
-		}
 	}
-	if(flag){
-		printf("%lld\n",(lint)n*(n-1)/2);
-		return 0;
-	}
-	if(n<=20){
-		printf("%d\n",brute(n));
-		return 0;
-	}
-	memset(valid,0,sizeof(valid));
-	dfs1(1,n,1,n);
 	lint ans=0;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			if(valid[i][j]){
-				ans++;
-			}
+	for(int i=n;i>=1;i--){//tocheck
+		int p1=perm[pos[i]-1],p2=perm[pos[i]+1];
+		if(p1>p2){
+			swap(p1,p2);
 		}
+		if(p1==0){
+			if(p2<i){
+				seg.add(i,n,1);
+			}else{
+				seg.add(i,p2-1,1);
+			}
+		}else if(p1<i){
+			if(p2<i){
+				seg.add(i,n,1);
+			}else{
+				seg.add(i,p2-1,1);
+			}
+		}else{
+			seg.add(i,p1-1,1);
+			seg.add(p2,n,-1);
+		}
+		ans+=ask();
 	}
-	printf("%lld",ans+n-1);
+	printf("%d",ans);
 }
