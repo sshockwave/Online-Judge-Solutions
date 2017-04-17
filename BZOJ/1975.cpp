941e9a1ab583e0b8a021a8ff99f4b89e13b36c00
Kerry Su <i_sukai@live.com>
2017-04-17 21:22:49 +0800

    Accepted.

--- BZOJ/1975.cpp
+++ BZOJ/1975.cpp
@@ -15,19 +15,23 @@ inline int ni(){
 	return i;
 }
 const int N=5010,E=200010*2;
-int to[E],bro[E],val[E],head[N],rhead[N];
-inline void add_edge(int u,int v,int w,int *head){
+const double EPS=1e-5;
+int to[E],bro[E],head[N],rhead[N];
+double val[E];
+inline void add_edge(int u,int v,double w,int *head){
 	static int etop=0;
 	to[etop]=v;
 	bro[etop]=head[u];
 	val[etop]=w;
 	head[u]=etop++;
 }
-int dis[N];
-inline void spfa(int s,int *head){
-	memset(dis,127,sizeof(dis));
+double dis[N];
+inline void spfa(int s,int t,int *head){
+	for(int i=1;i<=s;i++){
+		dis[i]=1e100;
+	}
 	bool *inque=new bool[N];
-	memset(inque,0,N<<2);
+	memset(inque,0,N);
 	queue<int>q;
 	q.push(s);
 	dis[s]=0;
@@ -37,7 +41,7 @@ inline void spfa(int s,int *head){
 		q.pop();
 		for(int i=head[x],v;~i;i=bro[i]){
 			v=to[i];
-			if(dis[v]>dis[x]+val[i]){
+			if(dis[v]>dis[x]+val[i]+EPS){
 				dis[v]=dis[x]+val[i];
 				if(!inque[v]){
 					inque[v]=true;
@@ -49,25 +53,26 @@ inline void spfa(int s,int *head){
 	}
 }
 struct state{
-	int x,dis;
+	int x;
+	double dis;
 };
 inline bool operator < (const state &a,const state &b){
-	return a.dis+dis[a.x]>b.dis+dis[b.x];
+	return a.dis+dis[a.x]>b.dis+dis[b.x]+EPS;
 }
-inline int astar(int t,int sum){
+inline int astar(int t,double sum){
 	priority_queue<state>q;
-	q.push(1);
+	q.push((state){1,0});
 	int cnt=0;
 	while(!q.empty()){
 		state s=q.top();
 		q.pop();
 		if(s.x==t){
-			if(s.dis>sum){
+			if(s.dis>sum+EPS){
 				return cnt;
 			}
 			cnt++;
 			sum-=s.dis;
-			if(sum==0){
+			if(sum>=-EPS&&sum<=EPS){
 				return cnt;
 			}
 		}
@@ -78,12 +83,16 @@ inline int astar(int t,int sum){
 	return cnt;
 }
 int main(){
-	int n=ni(),m=ni(),e=ni();
-	for(int i=1,u,v,w;i<=m;i++){
-		u=ni(),v=ni(),w=ni();
+	int n=ni(),m=ni();
+	double e,w;
+	scanf("%lf",&e);
+	memset(head,-1,sizeof(head));
+	memset(rhead,-1,sizeof(rhead));
+	for(int i=1,u,v;i<=m;i++){
+		scanf("%d%d%lf",&u,&v,&w);
 		add_edge(u,v,w,head);
 		add_edge(v,u,w,rhead);
 	}
-	spfa(n,rhead);
+	spfa(n,1,rhead);
 	printf("%d\n",astar(n,e));
 }
