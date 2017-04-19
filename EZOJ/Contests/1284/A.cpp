752fb291d3d675f69243ea621f6a669d7606a062
Kerry Su <i_sukai@live.com>
2017-04-19 14:14:40 +0800

    Init.

--- EZOJ/Contests/1284/A.cpp
+++ EZOJ/Contests/1284/A.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <utility>
 #include <algorithm>
 using namespace std;
 typedef long long lint;
@@ -17,41 +18,51 @@ inline void apmax(int &a,const int &b){
 		a=b;
 	}
 }
-const int N=12;
-bool choose[N];
-int n,pval[N];
-inline bool valid(){
-	for(int i=2;i<=n;i++){
-		if(choose[i]&&!choose[i>>1]){
-			return false;
-		}
+typedef pair<int,int> pii;
+const int N=200010;
+int n,f[N],g[N],mid,mx,ans[N],val[N];
+pii pval[N];
+inline void up(int x){
+	f[x]=val[x];
+	if((x<<1)<=n&&f[x<<1]>0){
+		f[x]+=f[x<<1];
 	}
-	return true;
+	if(((x<<1)|1)<=n&&f[(x<<1)|1]>0){
+		f[x]+=f[(x<<1)|1];
+	}
+	g[x]=f[x];
+	if((x<<1)<=n){
+		apmax(g[x],f[x<<1]);
+	}
+	if(((x<<1)|1)<=n){
+		apmax(g[x],f[(x<<1)|1]);
+	}
 }
-int seq[N];
-int main(){
-	n=ni();
-	for(int i=1;i<=n;i++){
-		pval[i]=ni();
+inline void update(int x){
+	assert(val[x]==-1);
+	val[x]=1;
+	for(;x>=1;x>>=1){
+		up(x);
+		apmax(mx,g[x]);
 	}
-	for(int a=0;a<n;a++){
-		int ans=0;
-		for(int state=0;state<(1<<(n-1));state++){
-			for(int i=1;i<=n;i++){
-				choose[i]=(state>>(i-1))&1;
-			}
-			if(!valid()){
-				continue;
-			}
-			int stop=0;
-			for(int i=1;i<=n;i++){
-				if(choose[i]){
-					seq[++stop]=pval[i];
-				}
-			}
-			sort(seq+1,seq+stop+1);
-			apmax(ans,seq[(stop-a+1)/2]);
+}
+int main(){
+	int n=ni();
+	for(int i=1;i<=n;i++){
+		pval[i]=make_pair(ni(),i);
+	}
+	sort(pval+1,pval+n+1);
+	memset(f+1,-1,n<<2);
+	memset(g+1,-1,n<<2);
+	memset(val+1,-1,n<<2);
+	for(int i=n,a=0;i>=1;i--){
+		mid=pval[i].first;
+		update(pval[i].second);
+		for(;a<=mx;a++){
+			ans[a]=mid;
 		}
-		printf("%d ",ans);
+	}
+	for(int i=1;i<=n;i++){
+		printf("%d ",ans[i]);
 	}
 }
