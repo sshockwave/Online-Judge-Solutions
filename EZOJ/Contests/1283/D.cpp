878ec328e46e8f39fcd484fb7591bfef791fc4ab
Kerry Su <i_sukai@live.com>
2017-04-21 15:43:11 +0800

    WA

--- EZOJ/Contests/1283/D.cpp
+++ EZOJ/Contests/1283/D.cpp
@@ -2,66 +2,63 @@
 #include <cstdio>
 #include <cstring>
 #include <cassert>
+#include <cctype>
+#include <queue>
 using namespace std;
 typedef long long lint;
-inline bool is_num(char c){
-	return c>='0'&&c<='9';
-}
-inline int ni(){
-	int i=0;char c;
-	while(!is_num(c=getchar()));
-	while(i=i*10-'0'+c,is_num(c=getchar()));
+#define ni (next_num<int>())
+#define nl (next_num<lint>())
+template<class T>inline T next_num(){
+	T i=0;char c;
+	while(!isdigit(c=getchar()));
+	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return i;
 }
-inline lint nl(){
-	lint i=0;char c;
-	while(!is_num(c=getchar()));
-	while(i=i*10-'0'+c,is_num(c=getchar()));
-	return i;
-}
-inline void apmin(lint &a,lint b){
+template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
 	if(a>b){
 		a=b;
 	}
 }
-inline void apmax(lint &a,lint b){
+template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 	if(a<b){
 		a=b;
 	}
 }
-const int N=1010;
-lint val[N],_val[N],f[N];
-inline void brute(int n){
-	lint mx=nl(),sum=0,ans=0,curmx=0;
-	for(int i=1;i<=n;i++){
-		lint cur=nl();
-		if(sum+cur>mx){
-			ans+=curmx;
-			curmx=sum=0;
-		}
-		sum+=cur;
-		apmax(curmx,cur);
+const int N=300010;
+int f[N],w[N],_w[N],nxt[N];
+struct Value{
+	int i,j,v;
+	Value(int _i,int _j):i(_i),j(_j),v(f[j]+w[i]){}
+	inline friend bool operator < (const Value &a,const Value &b){
+		return a.v>b.v;
 	}
-	printf("%lld\n",ans+curmx);
-}
+};
+queue<int>q;
+priority_queue<Value>h;
 int main(){
-	int n=ni();
-	if(n>3000){
-		brute(n);
-		return 0;
-	}
-	lint lim=nl(),sum;
-	_val[0]=0;
-	memset(f,127,sizeof(f));
-	f[0]=0;
-	for(int i=1;i<=n;i++){
-		val[i]=nl();
-		_val[i]=_val[i-1]+val[i];
-		lint mx=val[i];
-		for(int j=i-1;j>=0&&_val[i]-_val[j]<=lim;j--){
-			apmin(f[i],mx+f[j]);
-			apmax(mx,val[j]);
+	int n=ni,lim=ni;
+	f[0]=w[0]=_w[0]=0;
+	for(int i=1,j=0;i<=n;i++){
+		_w[i]=_w[i-1]+(w[i]=ni);
+		for(;_w[i]-_w[j]>lim;j++);
+		for(;!q.empty()&&q.front()<j;q.pop()){
+			nxt[q.front()]=0;
+		}
+		for(;!q.empty()&&q.back()<=w[i];q.pop()){
+			nxt[q.front()]=0;
+		}
+		if(!q.empty()){
+			h.push(Value(i,q.back()));
+			nxt[q.back()]=i;
+		}
+		q.push(i);
+		f[i]=f[j]+w[q.front()];
+		for(;!h.empty();h.pop()){
+			if(nxt[h.top().j]==h.top().i){
+				apmin(f[i],h.top().v);
+				break;
+			}
 		}
 	}
-	printf("%lld\n",f[n]);
+	printf("%d\n",f[n]);
 }
