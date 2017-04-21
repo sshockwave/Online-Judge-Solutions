ef545b8a6ef9fa492bc004e6dcf2ad84fdc81e8a
Kerry Su <i_sukai@live.com>
2017-04-21 16:01:48 +0800

    Accepted.

--- EZOJ/Contests/1283/D.cpp
+++ EZOJ/Contests/1283/D.cpp
@@ -25,7 +25,7 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 	}
 }
 const int N=300010;
-int f[N],w[N],_w[N],nxt[N];
+int f[N],w[N],_w[N],q[N],nxt[N];
 struct Value{
 	int i,j,v;
 	Value(int _i,int _j):i(_i),j(_j),v(f[j]+w[i]){}
@@ -33,26 +33,21 @@ struct Value{
 		return a.v>b.v;
 	}
 };
-queue<int>q;
 priority_queue<Value>h;
 int main(){
-	int n=ni,lim=ni;
+	int n=ni,lim=ni,qhead=0,qtail=0;
 	f[0]=w[0]=_w[0]=0;
 	for(int i=1,j=0;i<=n;i++){
 		_w[i]=_w[i-1]+(w[i]=ni);
 		for(;_w[i]-_w[j]>lim;j++);
-		for(;!q.empty()&&q.front()<j;q.pop()){
-			nxt[q.front()]=0;
+		for(;qhead<qtail&&q[qhead]<j;nxt[q[qhead++]]=0);
+		for(;qhead<qtail&&w[q[qtail-1]]<=w[i];nxt[q[--qtail-1]]=0);
+		if(qhead<qtail){
+			nxt[q[qtail-1]]=i;
+			h.push(Value(i,q[qtail-1]));
 		}
-		for(;!q.empty()&&q.back()<=w[i];q.pop()){
-			nxt[q.front()]=0;
-		}
-		if(!q.empty()){
-			h.push(Value(i,q.back()));
-			nxt[q.back()]=i;
-		}
-		q.push(i);
-		f[i]=f[j]+w[q.front()];
+		q[qtail++]=i;
+		f[i]=f[j]+w[q[qhead]];
 		for(;!h.empty();h.pop()){
 			if(nxt[h.top().j]==h.top().i){
 				apmin(f[i],h.top().v);
