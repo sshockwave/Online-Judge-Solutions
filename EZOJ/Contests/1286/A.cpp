f67552ee44674a2864c38fa6c3bcfd643a4361e4
Kerry Su <i_sukai@live.com>
2017-04-22 14:33:01 +0800

    Accepted. 400ms

--- EZOJ/Contests/1286/A.cpp
+++ EZOJ/Contests/1286/A.cpp
@@ -19,6 +19,9 @@ inline int add(const int &a,const int &b){
 	assert(b<MOD&&b>=0);
 	return (a+b)%MOD;
 }
+inline int sub(int &a,int b){
+	return b==0?a:add(a,MOD-b);
+}
 inline int mul(const int &a,const int &b){
 	assert(a<MOD&&a>=0);
 	assert(b<MOD&&b>=0);
@@ -26,6 +29,11 @@ inline int mul(const int &a,const int &b){
 }
 inline void apadd(int &a,const int &b){
 	a=add(a,b);
+}
+inline void apsub(int &a,const int &b){
+	if(b){
+		apadd(a,MOD-b);
+	}
 }
 inline void apmul(int &a,const int &b){
 	a=mul(a,b);
@@ -39,9 +47,9 @@ inline int fpow(int x,int n){
 	}
 	return ret;
 }
-int a[N],b[N];
-const int lessN=110;
-int f[lessN][lessN][lessN];
+int a[N],b[N],w[N];
+const int N2=2010;
+int f[N2],g[N2],c[N2][N2],fac[N];
 int main(){
 	int n=ni,k=ni;
 	for(int i=1;i<=n;i++){
@@ -53,32 +61,50 @@ int main(){
 	if(k==1){
 		int ans=0;
 		for(int i=1,j=1;i<=n;i++){
-			for(;b[j]<=a[i];j++);
+			for(;j<=n&&b[j]<=a[i];j++);
 			apadd(ans,j-1);
 		}
 		printf("%d\n",mul(ans,fpow(n,MOD-2)));
+	}else if(k==2){
+		int sum=0,ans=0;
+		fac[0]=1;
+		for(int i=1,j=0;i<=n;i++){
+			for(;j<=n&&b[j]<=a[i];j++);
+			w[i]=j-1;
+			fac[i]=mul(fac[i-1],i);
+		}
+		for(int i=n;i>=1;i--){
+			apadd(ans,mul(w[i],sub(sum,n-i)));
+			apadd(sum,w[i]);
+		}
+		apmul(ans,mul(fac[n-2],2));
+		apadd(ans,mul(fac[n-1],sum));
+		printf("%d\n",mul(ans,fpow(fac[n],MOD-2)));
 	}else{
 		memset(f,0,sizeof(f));
-		for(int i=0;i<=n;i++){
-			f[i][0][0]=1;
-			f[0][i][0]=1;
-		}
+		memset(c,0,sizeof(c));
+		fac[0]=f[0]=c[0][0]=1;
 		for(int i=1;i<=n;i++){
-			for(int j=1;j<=n;j++){
-				for(int k=1;k<=i&&k<=j;k++){
-					f[i][j][k]=f[i-1][j][k];
-					for(int l=1;l<=j;l++){
-						if(a[i]>=b[l]){
-							apadd(f[i][j][k],f[i-1][l-1][k-1]);
-						}
-					}
-				}
+			fac[i]=mul(fac[i-1],i);
+			c[i][0]=1;
+			for(int j=1;j<=i;j++){
+				c[i][j]=add(c[i-1][j-1],c[i-1][j]);
+			}
+		}
+		for(int i=1,j=1;i<=n;i++){
+			for(;j<=n&&b[j]<=a[i];j++);
+			for(int k=j-1;k;k--){
+				apadd(f[k],mul(f[k-1],j-k));
 			}
 		}
 		int ans=0;
-		for(int i=1;i<=n;i++){
-			apadd(ans,mul(f[n][n][i],fpow(i,k)));
+		for(int i=n;i>=1;i--){
+			g[i]=mul(f[i],fac[n-i]);
+			for(int j=i+1;j<=n;j++){
+				apsub(g[i],mul(g[j],c[j][i]));
+			}
+			apadd(ans,mul(g[i],fpow(i,k)));
 		}
-		printf("%d\n",mul(ans,fpow(n,MOD-2)));
+		printf("%d\n",mul(ans,fpow(fac[n],MOD-2)));
 	}
 }
