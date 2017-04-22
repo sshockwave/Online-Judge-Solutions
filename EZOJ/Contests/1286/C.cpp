c75a78f3859af1f981b4e1d0a94d351a34f1a0bf
Kerry Su <i_sukai@live.com>
2017-04-22 17:12:54 +0800

    Accepted. 13486ms

--- EZOJ/Contests/1286/C.cpp
+++ EZOJ/Contests/1286/C.cpp
@@ -17,16 +17,16 @@ template<class T>inline T next_num(){
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return i;
 }
-const int SHIFT=18,N=1<<SHIFT;
+const int SHIFT2=18,N2=1<<SHIFT2;
 const double EPS=1e-3;
-int n,MOD,rev[N];
+int n,MOD,rev[N2],N,SHIFT;
 inline int add(const int &a,const int &b){
 	return (a+b)%MOD;
 }
 inline void apadd(int &a,const int &b){
 	a=add(a,b);
 }
-comp w1[N>>1][SHIFT+1],w2[N>>1][SHIFT+1];
+comp w1[N2>>1][SHIFT2+1],w2[N2>>1][SHIFT2+1];
 inline comp omega(int i,int n){
 	double angle=M_PI*2*i/n;
 	return comp(cos(angle),sin(angle));
@@ -55,34 +55,47 @@ inline void dft(complex<double>a[],bool inv){
 	}
 }
 struct Poly{
-	int x[N];
-	void operator *=(const Poly&);
+	int x[N2];
+	inline void operator *= (const Poly &b){
+		static comp *ca=new comp[N2],*cb=new comp[N2];
+		for(int i=0;i<n;i++){
+			ca[i]=x[i],cb[i]=b.x[i];
+		}
+		for(int i=n;i<N;i++){
+			ca[i]=cb[i]=0;
+		}
+		dft(ca,false),dft(cb,false);
+		for(int i=0;i<N;i++){
+			ca[i]*=cb[i];
+		}
+		dft(ca,true);
+		memset(x,0,n<<2);
+		for(int i=0;i<N;i++){
+			apadd(x[i%n],ca[i].real()+EPS);
+		}
+	}
+	inline void sqr(){
+		static comp *ca=new comp[N];
+		for(int i=0;i<n;i++){
+			ca[i]=x[i];
+		}
+		for(int i=n;i<N;i++){
+			ca[i]=0;
+		}
+		dft(ca,false);
+		for(int i=0;i<N;i++){
+			ca[i]*=ca[i];
+		}
+		dft(ca,true);
+		memset(x,0,n<<2);
+		for(int i=0;i<N;i++){
+			apadd(x[i%n],ca[i].real()+EPS);
+		}
+	}
 }trans,a;
-inline ostream & operator << (ostream & out,const Poly &p){
-	for(int i=0;i<n;i++){
-		out<<p.x[i]<<" ";
-	}
-	return out;
-}
-inline void Poly::operator *= (const Poly &b){
-	static comp *ca=new comp[N],*cb=new comp[N];
-	for(int i=0;i<n;i++){
-		ca[i]=x[i],cb[i]=b.x[i];
-	}
-	for(int i=n;i<N;i++){
-		ca[i]=cb[i]=0;
-	}
-	dft(ca,false),dft(cb,false);
-	for(int i=0;i<N;i++){
-		ca[i]*=cb[i];
-	}
-	dft(ca,true);
-	memset(x,0,n<<2);
-	for(int i=0;i<N;i++){
-		apadd(x[i%n],ca[i].real()+EPS);
-	}
-}
 int main(){
+	n=ni,MOD=ni;
+	for(N=1,SHIFT=0;N<(n<<1);N<<=1,SHIFT++);
 	for(int i=1;i<=SHIFT;i++){
 		for(int j=0;j<(1<<(i-1));j++){
 			double angle=M_PI*2*j/(1<<i);
@@ -97,13 +110,12 @@ int main(){
 			rev[i]|=s;
 		}
 	}
-	n=ni,MOD=ni;
 	int t=ni;
 	for(int i=0;i<n;i++){
 		a.x[i]=ni-1;
 	}
 	trans.x[0]=MOD-1,trans.x[n-1]=2;
-	for(;t;t>>=1,trans*=trans){
+	for(;t;t>>=1,trans.sqr()){
 		if(t&1){
 			a*=trans;
 		}
