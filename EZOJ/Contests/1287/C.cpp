7e069032dc013812376d7fe4851799653a1f7df1
Kerry Su <i_sukai@live.com>
2017-04-23 15:16:26 +0800

    Add part 1.

--- EZOJ/Contests/1287/C.cpp
+++ EZOJ/Contests/1287/C.cpp
@@ -17,56 +17,145 @@ template<class T>inline T next_num(){
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return flag?-i:i;
 }
-const int N=55;
-char s[N][N*2],forbid[N][N*2];
-char stk[N*2];
-int n,m,l,len[N];
-inline bool match(char s[],char t[]){
-	for(int i=0;t[i];i++){
-		if(s[i]!=t[i]){
-			return false;
-		}
+inline void apmax(int &a,const int &b){
+	if(a<b){
+		a=b;
 	}
-	return true;
 }
-inline bool findmatch(char s[]){
-	for(int i=0;i<l;i++){
-		if(match(stk+i,s)){
-			return true;
-		}
-	}
-	return false;
+const int N=55,L=110,D=L*2,SIGMA=26,MOD=1000000007;
+inline int add(const int &a,const int &b){
+	return (a+b)%MOD;
 }
-inline bool checkvalid(){
-	for(int i=1;i<=m;i++){
-		if(findmatch(forbid[i])){
-			return false;
-		}
-	}
-	return true;
+inline int mul(const int &a,const int &b){
+	return (lint)a*b%MOD;
 }
-int dfs(char cur[],int rest){
-	if(rest==0){
-		cur[0]=0;
-		return checkvalid();
+inline void apadd(int &a,const int &b){
+	a=add(a,b);
+}
+struct ACAutomaton{
+	struct node{
+		int son[SIGMA],fail;
+		bool end;
+		node(){
+			memset(son,0,sizeof(son));
+		}
+	}pol[D];
+	int pool;
+	ACAutomaton():pool(1){}
+	void insert(int id,char s[]){
+		node &x=pol[id];
+		if((*s)==0){
+			x.end=true;
+			return;
+		}
+		int c=(*s)-'a';
+		if(x.son[c]==0){
+			x.son[c]=++pool;
+		}
+		insert(x.son[c],s+1);
 	}
-	int ans=0;
-	for(int i=1;i<=n;i++){
-		if(len[i]<=rest){
-			memcpy(cur,s[i],len[i]);
-			ans+=dfs(cur+len[i],rest-len[i]);
+	void build(int id){//assert fail ok
+		node &x=pol[id];
+		for(int i=0;i<26;i++){
+			if(x.son[i]){
+				int p=x.fail;
+				for(;p&&pol[p].son[i]==0;p=pol[p].fail);
+				pol[x.son[i]].fail=pol[p].son[i];
+				build(x.son[i]);
+			}
 		}
 	}
-	return ans;
+	void build(){
+		for(int i=0;i<SIGMA;i++){
+			pol[0].son[i]=1;
+		}
+		pol[1].fail=0;
+		build(1);
+	}
+	int digest(int p,char c){
+		node &x=pol[p];
+		if(x.end){
+			return -1;
+		}
+		c-='a';
+		for(;p&&pol[p].son[c]==0;p=pol[p].fail);
+		return pol[p].son[c];
+	}
+}ac;
+int n,m,lim;
+int len1[N],len2[N];
+char s[N][L],t[N][L];//t:forbidden
+namespace solve1{
+	int f[L*2][D];
+	int to[D][N];//to[at][str]
+	inline int work(){
+		for(int i=1;i<=ac.pool;i++){
+			for(int j=1;j<=n;j++){
+				to[i][j]=i;
+				for(int k=1;s[j][k];k++){
+					to[i][j]=ac.digest(to[i][j],s[j][k]);
+					if(to[i][j]==-1){
+						break;
+					}
+				}
+			}
+		}
+		f[0][1]=1;
+		for(int i=0;i<lim;i++){
+			for(int j=1;j<=ac.pool;j++){
+				for(int k=1;k<=n;k++){
+					if(~to[j][k]){
+						apadd(f[i+len1[k]][to[j][k]],f[i][j]);
+					}
+				}
+			}
+		}
+		int ans=0;
+		for(int i=1;i<=ac.pool;i++){
+			apadd(ans,f[lim][i]);
+		}
+		return ans;
+	}
+}
+namespace solve2{
+	struct Matrix{
+		int num[D][D];
+		Matrix(){
+			memset(num,0,sizeof(num));
+		}
+		inline int* operator [] (int i){
+			return num[i];
+		}
+	}trans;
+	inline int work(){
+		for(int i=1;i<=ac.pool;i++){
+			for(int j=1;j<=n;j++){
+				if(len2[j]==1){
+					
+				}else{
+					
+				}
+			}
+		}
+	}
 }
 int main(){
-	n=ni,m=ni,l=ni;
+	n=ni,m=ni,lim=ni;
 	for(int i=1;i<=n;i++){
 		scanf("%s",s[i]);
-		len[i]=strlen(s[i]);
+		len1[i]=strlen(s[i]);
 	}
+	int mxlen=0;
 	for(int i=1;i<=m;i++){
-		scanf("%s",forbid[i]);
+		scanf("%s",t[i]);
+		ac.insert(1,t[i]);
+		len2[i]=strlen(t[i]);
+		apmax(mxlen,len2[i]);
 	}
-	printf("%d\n",dfs(stk,l));
+	ac.build();
+	if(mxlen<=2){
+		printf("%d\n",solve2::work());
+	}else{
+		printf("%d\n",solve1::work());
+	}
 }
