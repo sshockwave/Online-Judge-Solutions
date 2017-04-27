5ed671640a66707070a3fb0ae70745ec0e032917
Kerry Su <i_sukai@live.com>
2017-04-27 13:16:58 +0800

    RE

--- EZOJ/Contests/1289/C.cpp
+++ EZOJ/Contests/1289/C.cpp
@@ -5,107 +5,91 @@
 #include <cctype>
 using namespace std;
 typedef long long lint;
+typedef long double ld;
 #define ni (next_num<int>())
 #define nl (next_num<lint>())
 template<class T>inline T next_num(){
-	T i=0;char c;bool flag=false;
+	T i=0;char c;
 	while(!isdigit(c=getchar())&&c!='-');
-	if(c=='-'){
-		flag=true;
-		c=getchar();
-	}
+	bool flag=c=='-';
+	flag?(c=getchar()):0;
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return flag?-i:i;
 }
-inline lint _sqr(lint n){
-	return n*(n+1)/2*((n<<1)+1)/3;
-}
 const int N=100010;
 int inix[N],iniy[N];
+inline lint _sqr(lint x){
+	return x*(x+1)*(x*2+1)/6;
+}
 struct SegmentTree{
 	typedef SegmentTree node;
 	struct Info{
-		lint x,y,xy,xx;
-		bool tag2,tag3;
-		int s2,t2,s3,t3;
-		inline friend Info operator + (Info a,Info b){
-			return (Info){a.x+b.x,a.y+b.y,a.xy+b.xy,a.xx+b.xx};
+		ld x,y,xx,xy;
+		inline friend Info operator + (const Info &a,const Info &b){
+			return (Info){a.x+b.x,a.y+b.y,a.xx+b.xx,a.xy+b.xy};
 		}
-		inline Info trans2(int l,int r,int s,int t){
-			//x+=s,y+=t
-			lint len=r-l+1;
-			Info tmp=(Info){x+len*s,y+len*t,xy+y*s+x*t+len*s*t,xx+x*s*2+len*s*s};
-			tmp.tag2=true;
-			tmp.tag3=false;
-			tmp.s2=s;
-			tmp.t2=t;
-			return tmp;
+		inline void trans2(int l,int r,int s,int t){
+			lint n=r-l+1;
+			ld tx=x,ty=y;
+			x+=n*s;
+			y+=n*t;
+			xx+=tx*s*2+n*s*s;
+			xy+=ty*s+tx*t+n*s*t;
 		}
-		inline Info trans3(int l,int r,int s,int t){
-			//x=s+i,y=t+i
-			lint len=r-l+1,sigi=(l+r)*len/2,sigi2=_sqr(r)-_sqr(l-1);
-			Info tmp=(Info){len*s+sigi,len*t+sigi,len*s*t+sigi*(s+t)+sigi2,len*s*s+sigi*s*2+sigi2};
-			tmp.tag2=false;
-			tmp.tag3=true;
-			tmp.s3=s;
-			tmp.t3=t;
-			return tmp;
+		inline void trans3(int l,int r,int s,int t){
+			lint n=r-l+1,sigi=n*(l+r)/2,sigi2=_sqr(r)-_sqr(l-1);
+			x=n*s+sigi;
+			y=n*t+sigi;
+			xx=n*s*s+sigi*s*2+sigi2;
+			xy=n*s*t+sigi*(s+t)+sigi2;
 		}
-		inline double cal(int l,int r){
+		inline ld cal(int l,int r){
 			int n=r-l+1;
-			return (xy-(double)x/n*y)/(xx-(double)x/n*x);
+			return (xy*n-x*y)/(xx*n-x*x);
 		}
 	}info;
-	int lend,rend,mid;
+	int lend,rend,mid,s,t;
+	bool tag2,tag3;
 	node *lson,*rson;
 	inline void up(){
 		assert(lend!=rend);
 		info=lson->info+rson->info;
 	}
 	inline void down(){
-		if(lend==rend){
-			info.tag2=info.tag3=false;
+		if(lend!=rend&&(tag2||tag3)){
+			assert((tag2&&!tag3)||(!tag2&&tag3));
+			lson->alter(lend,mid,s,t,3-tag2);
+			rson->alter(mid+1,rend,s,t,3-tag2);
 		}
-		if(info.tag2){
-			assert(!info.tag3);
-			lson->cover2(info.s2,info.t2);
-			rson->cover2(info.s2,info.t2);
-			info.tag2=false;
-		}
-		if(info.tag3){
-			assert(!info.tag2);
-			lson->cover3(info.s3,info.t3);
-			rson->cover3(info.s3,info.t3);
-			info.tag3=false;
-		}
+		tag2=tag3=false;
 	}
 	void build(int l,int r){
 		static node *n=new node[N];
 		lend=l,rend=r,mid=(l+r)>>1;
+		tag2=tag3=false;
 		if(l==r){
-			info=(Info){inix[l],iniy[l],(lint)inix[l]*iniy[l],(lint)inix[l]*inix[l],0,0};
+			info.x=inix[l];
+			info.y=iniy[l];
+			info.xx=info.x*info.x;
+			info.xy=info.x*info.y;
 		}else{
 			(lson=n++)->build(l,mid);
 			(rson=n++)->build(mid+1,r);
 			up();
 		}
 	}
-	inline void cover2(int s,int t){
-		info=info.trans2(lend,rend,s,t);
-	}
-	inline void cover3(int s,int t){
-		info=info.trans3(lend,rend,s,t);
-	}
 	void alter(int l,int r,int s,int t,int type){
 		assert(lend<=l&&r<=rend);
 		down();
 		if(lend==l&&rend==r){
 			if(type==2){
-				cover2(s,t);
+				tag2=true;
+				info.trans2(l,r,s,t);
 			}else{
-				assert(type==3);
-				cover3(s,t);
+				tag3=true;
+				info.trans3(l,r,s,t);
 			}
+			this->s=s,this->t=t;
 			return;
 		}
 		if(r<=mid){
@@ -145,7 +129,7 @@ int main(){
 	while(tot--){
 		int op=ni,l=ni,r=ni;
 		if(op==1){
-			printf("%.10lf\n",seg.ask(l,r).cal(l,r));
+			printf("%.10lf\n",(double)seg.ask(l,r).cal(l,r));
 		}else{
 			int s=ni,t=ni;
 			seg.alter(l,r,s,t,op);
