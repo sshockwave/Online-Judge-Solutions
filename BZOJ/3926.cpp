f343a4e13e13f718a0e8f6a72755ece44c051fc3
Kerry Su <i_sukai@live.com>
2017-06-03 09:34:54 +0800

    Accepted.

--- BZOJ/3926.cpp
+++ BZOJ/3926.cpp
@@ -48,84 +48,51 @@ inline ostream & operator << (ostream & out,SAM*p){
 	return out;
 }
 inline SAM* SAM::ext(int c){
-	if(go[c]){
-		return go[c];
-	}
-	go[c]=nn();
-	go[c]->val=val+1;
-	node p=lnk;
+	node p=this,np=nn();
+	np->val=val+1;
 	for(;p&&p->go[c]==NULL;p=p->lnk){
-		p->go[c]=go[c];
+		p->go[c]=np;
 	}
-	if(p==NULL){
-		go[c]->lnk=&sam;
-		return go[c];
+	if(p==0){
+		np->lnk=&sam;
+		return np;
 	}
 	node q=p->go[c];
 	assert(q!=NULL);
 	if(q->val==p->val+1){
-		go[c]->lnk=q;
-		return go[c];
+		np->lnk=q;
+		return np;
 	}
 	assert(q->val>p->val+1);
 	node nq=nn();
 	*nq=*q;
 	nq->val=p->val+1;
-	q->lnk=go[c]->lnk=nq;
+	q->lnk=np->lnk=nq;
 	for(;p&&p->go[c]==q;p=p->lnk){
 		p->go[c]=nq;
 	}
-	return go[c];
+	return np;
 }
-struct Trie{
-	typedef Trie* node;
-	node son[SIGMA];
-	Trie(){
-		memset(son,0,sizeof(son));
-	}
-	inline node nn(){
-		static node n=new Trie[N*20];
-		return n++;
-	}
-	inline node go(int c){
-		if(son[c]==NULL){
-			son[c]=nn();
-		}
-		return son[c];
-	}
-	void dfs(SAM *node){
-		for(int i=0;i<SIGMA;i++){
-			if(son[i]){
-				son[i]->dfs(node->ext(i));
-			}
-		}
-	}
-}trie;
 namespace G{
 	const int E=N*2;
-	int deg[N],head[N],to[E],bro[E],etop=0;
+	int deg[N],head[N],to[E],bro[E],e=0;
 	int c[N];
 	inline void init(){
 		memset(head,-1,sizeof(head));
 		memset(deg,0,sizeof(deg));
 	}
-	inline void add_edge(int u,int v){
-		to[etop]=v;
-		bro[etop]=head[u];
-		head[u]=etop++;
-		deg[u]++;
+	inline void ae(int u,int v){
+		to[e]=v,bro[e]=head[u],head[u]=e++;
 	}
 	inline void add(int u,int v){
-		add_edge(u,v);
-		add_edge(v,u);
+		ae(u,v),ae(v,u);
+		deg[u]++,deg[v]++;
 	}
-	void dfs(int x,int f,Trie& fa){
-		Trie &cur=*fa.go(c[x]);
+	void dfs(int x,int f,SAM *node){
+		node=node->ext(c[x]);
 		for(int i=head[x],v;~i;i=bro[i]){
 			v=to[i];
-			if(v!=f){
-				dfs(v,x,cur);
-			}
+			if(v!=f){dfs(v,x,node);}
 		}
 	}
 }
@@ -138,13 +105,12 @@ int main(){
 	for(int i=1;i<n;i++){
 		G::add(ni,ni);
 	}
+	sam.val=0;
 	for(int i=1;i<=n;i++){
 		if(G::deg[i]==1){
-			G::dfs(i,0,trie);
+			G::dfs(i,0,&sam);
 		}
 	}
-	sam.val=0;
-	trie.dfs(&sam);
 	lint ans=0;
 	for(SAM *i=pool;i<ptop;i++){
 		ans+=i->val-i->lnk->val;
