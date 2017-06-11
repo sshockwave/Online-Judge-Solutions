46a78e321883e970c7d79aca8bd4f0f49f49ab03
Kerry Su <i_sukai@live.com>
2017-06-11 12:58:49 +0800

    70pts. TLE&&RE

--- EZOJ/Contests/1304/C.cpp
+++ EZOJ/Contests/1304/C.cpp
@@ -23,9 +23,12 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
 	if(a>b){a=b;}
 }
-const int N=110,MOD=1000000000;
+const int N=100010,MOD=1000000000,PHI=400000000;
 inline int add(const int &a,const int &b){
 	return (a+b)%MOD;
+}
+inline int sub(const int &a,const int &b){
+	return add(a,MOD-b);
 }
 inline int mul(const int &a,const int &b){
 	return (lint)a*b%MOD;
@@ -40,32 +43,29 @@ inline int fpow(int x,int n){
 	}
 	return ret;
 }
+inline int rev(int x){
+	return fpow(x,PHI-1);
+}
 struct giant{
-	int p[N];
-	inline int eval(){
-		int ret=1;
-		for(int i=2;i<N;i++){
-			ret=mul(ret,fpow(i,p[i]));
-		}
-		return ret;
-	}
-	inline friend giant operator * (const giant &a,int x){
-		giant b;
-		for(int i=2;i<N;i++){
-			b.p[i]=a.p[i];
-			for(;x%i==0;x/=i,b.p[i]++);
-		}
-		return b;
-	}
-	inline friend giant operator / (const giant &a,const giant &b){
-		giant c;
-		for(int i=0;i<N;i++){
-			c.p[i]=a.p[i]-b.p[i];
-		}
+	int x,cnt2,cnt5;
+	inline friend giant operator * (const giant &a,int b){
+		giant c=a;
+		for(;(b&1)==0;b>>=1,c.cnt2++);
+		for(;b%5==0;b/=5,c.cnt5++);
+		c.x=mul(c.x,b);
 		return c;
 	}
-}fac[N];
-int c[N][N];
+	inline friend giant operator * (const giant &a,const giant &b){
+		return (giant){mul(a.x,b.x),a.cnt2+b.cnt2,a.cnt5+b.cnt5};
+	}
+	inline int eval(){
+		assert(cnt2>=0&&cnt5>=0);
+		return mul(x,mul(fpow(2,cnt2),fpow(5,cnt5)));
+	}
+}fac[N],refac[N];
+inline giant rev(const giant &x){
+	return (giant){rev(x.x),-x.cnt2,-x.cnt5};
+}
 void putint(int x,int k){
 	if(k==0){
 		return;
@@ -73,25 +73,29 @@ void putint(int x,int k){
 	putint(x/10,k-1);
 	putchar('0'+x%10);
 }
+int S[N];
+inline int c(int n,int k){
+	return (fac[n]*refac[k]*refac[n-k]).eval();
+}
+inline int work(int a,int b){
+	S[0]=1;
+	for(int i=1;i<=a;i++){
+		S[i]=add(S[i-1],c(a,i));
+	}
+	int ans=0;
+	for(int i=0;i<=b;i++){
+		ans=add(ans,mul(S[i],c(b,i)));
+	}
+	return sub(fpow(2,a+b),ans);
+}
 int main(){
+	fac[0]=(giant){1,0,0};
 	for(int i=1;i<N;i++){
 		fac[i]=fac[i-1]*i;
 	}
-	c[0][0]=1;
-	for(int i=1;i<N;i++){
-		for(int j=0;j<=i;j++){
-			c[i][j]=(fac[i]/fac[j]/fac[i-j]).eval();
-		}
+	refac[N-1]=rev(fac[N-1]);
+	for(int i=N-1;i;i--){
+		refac[i-1]=refac[i]*i;
 	}
-	for(int a,b,k;scanf("%d%d%d",&a,&b,&k)!=EOF;){
-		int ans=0;
-		for(int i=0;i<=b;i++){
-			int cur=0;
-			for(int j=i+1;j<=a;j++){
-				cur=add(cur,c[a][j]);
-			}
-			ans=add(ans,mul(cur,c[b][i]));
-		}
-		putint(ans,k),putchar('\n');
-	}
+	for(int a,b,k;scanf("%d%d%d",&a,&b,&k)!=EOF;putint(work(a,b),k),putchar('\n'));
 }
