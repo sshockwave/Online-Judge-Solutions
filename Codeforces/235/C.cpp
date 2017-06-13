f645f54eaa1c3a348fa010d3430f54835826ab4e
Kerry Su <i_sukai@live.com>
2017-06-13 20:27:32 +0800

    Accepted.

--- Codeforces/235/C.cpp
+++ Codeforces/235/C.cpp
@@ -25,25 +25,24 @@ const int N=1000010,SIGMA=26;
 struct SAM{
 	typedef SAM* node;
 	node lnk,go[SIGMA];
-	int val,right,head;
+	int val,right,head,tag;
 	SAM(){
 		memset(this,0,sizeof(SAM));
 		head=-1;
 	}
 	inline void add(node);
 	inline node ext(int);
-	inline void dfs(){
-		for(int i=0;i<SIGMA;i++){
-			if(go[i]){
-				go[i]->dfs();
-				right+=go[i]->right;
-			}
-		}
-	}
+	void dfs();
 }sam,*tail=&sam,pol[N*3],*pool=pol,*to[N*3];
 int bro[N*3],e=0;
 inline void SAM::add(SAM* v){
 	to[e]=v,bro[e]=head,head=e++;
+}
+void SAM::dfs(){
+	for(int i=head;~i;i=bro[i]){
+		to[i]->dfs();
+		right+=to[i]->right;
+	}
 }
 inline SAM* SAM::ext(int c){
 	node p=this,np=pool++;
@@ -63,6 +62,7 @@ inline SAM* SAM::ext(int c){
 	}
 	node nq=pool++;
 	*nq=*q;
+	nq->right=0;
 	nq->val=p->val+1;
 	q->lnk=np->lnk=nq;
 	for(;p&&p->go[c]==q;p=p->lnk){
@@ -80,6 +80,7 @@ int main(){
 		i->lnk->add(i);
 	}
 	sam.dfs();
+	int tim=0;
 	for(int tot=ni;tot--;){
 		scanf("%s",s);
 		int n=strlen(s),ans=0;
@@ -87,18 +88,24 @@ int main(){
 			s[j]=s[i];
 		}
 		SAM *p=&sam;
-		for(int i=0,nn=n<<1;i<nn;i++){
+		tim++;
+		int len=0;
+		for(int i=0,nn=(n<<1)-1;i<nn;i++){
 			int c=s[i]-'a';
 			for(;p&&p->go[c]==NULL;p=p->lnk);
 			if(p==NULL){
 				break;
 			}
+			apmin(len,p->val);
+			len++;
 			p=p->go[c];
+			for(;n<=p->lnk->val;p=p->lnk);
 			if(p->val<=i-n+1){
 				break;
 			}
-			if(p->val>=n){
+			if(len>=n&&p->tag<tim){
 				ans+=p->right;
+				p->tag=tim;
 			}
 		}
 		printf("%d\n",ans);
