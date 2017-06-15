f4390f4202691e2c65c5b2901a74dd038b0a4519
Kerry Su <i_sukai@live.com>
2017-06-15 21:13:17 +0800

    Runtime error.

--- Codeforces/238/D.cpp
+++ Codeforces/238/D.cpp
@@ -21,7 +21,7 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
 	if(b<a){a=b;}
 }
-const int N=100010,D=10;
+const int N=100010,D=10,INF=0x7f7f7f7f;
 struct State{
 	int cnt[D];
 	State(){
@@ -48,7 +48,66 @@ struct State{
 		}
 		putchar('\n');
 	}
-}f[N],g[N];
+	inline bool vis(){
+		return cnt[0]<INF;
+	}
+}ff[N],gg[N];
+struct SegmentTree{
+	typedef SegmentTree* node;
+	int lend,rend,mid;
+	node lson,rson;
+	bool tag;
+	State val;
+	inline void up(){
+		val=max(lson->val,rson->val);
+	}
+	void down(){
+		assert(lend!=rend);
+		if(tag){
+			lson->apmin(lend,mid,val);
+			rson->apmin(mid+1,rend,val);
+			tag=false;
+		}
+	}
+	void build(int l,int r){
+		static node n=new SegmentTree[N*2];
+		lend=l,rend=r,mid=(l+r)>>1;
+		tag=false;
+		if(l!=r){
+			(lson=n++)->build(l,mid);
+			(rson=n++)->build(mid+1,r);
+		}
+	}
+	void apmin(int l,int r,const State &v){
+		if(val<v){
+			return;
+		}
+		if(l==lend&&r==rend){
+			val=v;
+			tag=true;
+			return;
+		}
+		down();
+		if(r<=mid){
+			lson->apmin(l,r,v);
+		}else if(l>mid){
+			rson->apmin(l,r,v);
+		}else{
+			lson->apmin(l,mid,v);
+			rson->apmin(mid+1,r,v);
+		}
+		up();
+	}
+	void cache(State put[]){
+		if(lend==rend){
+			put[lend]=val;
+		}else{
+			down();
+			lson->cache(put);
+			rson->cache(put);
+		}
+	}
+}f,g;
 char s[N];
 int pre[N],nxt[N];
 inline void del(int x){
@@ -57,7 +116,7 @@ inline void del(int x){
 }
 int main(){
 	int n=ni,tot=ni;
-	scanf("%s",s+1);
+	s[0]='>',scanf("%s",s+1);
 	for(int i=1;i<=n;i++){
 		pre[i]=i-1,nxt[i]=i+1;
 	}
@@ -65,7 +124,8 @@ int main(){
 	int *pt=nxt;
 	State cur;
 	memset(&cur,0,sizeof(cur));
-	for(int i=0;i<=n;apmin((pt==nxt?f:g)[i],cur),i=pt[i],apmin(pt==nxt?f[i-1]:g[i+1],cur)){
+	f.build(1,n),g.build(1,n);
+	for(int i=1;i<=n;i=pt[i]){
 		if(i==0){
 			pt=nxt;
 			continue;
@@ -83,15 +143,17 @@ int main(){
 				del(i);
 			}
 		}
+		if(pt==nxt){
+			f.apmin(i,pt[i]-1,cur);
+		}else{
+			g.apmin(pt[i]+1,i,cur);
+		}
 	}
-	for(int i=n;i>1;i--){
-		apmin(f[i-1],f[i]);
-	}
-	for(int i=1;i<n;i++){
-		apmin(g[i+1],g[i]);
-	}
+	f.cache(ff),g.cache(gg);
+	memset(ff,0,sizeof(State));
+	memset(&cur,127,sizeof(cur));
 	while(tot--){
 		int l=ni,r=ni;
-		(min(f[r],g[l])-f[l-1]).print();
+		(min(ff[r],gg[l])-ff[l-1]).print();
 	}
 }
