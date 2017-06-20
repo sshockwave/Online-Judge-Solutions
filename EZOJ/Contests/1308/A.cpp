55422e765cacda56f9b8a703e3f7223ebc7655de
Kerry Su <i_sukai@live.com>
2017-06-20 19:54:12 +0800

    Accepted.

--- EZOJ/Contests/1308/A.cpp
+++ EZOJ/Contests/1308/A.cpp
@@ -23,150 +23,85 @@ template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
 }
 const int N=100010,E=N*2;
 namespace G{
-	int to[E],bro[E],head[N],e;
+	int to[E],bro[E],head[N],deg[N],e;
 	inline void ae(int u,int v){
 		to[e]=v,bro[e]=head[u],head[u]=e++;
 	}
 	inline void add(int u,int v){
+		deg[u]++,deg[v]++;
 		ae(u,v),ae(v,u);
 	}
+	bool vis[E];
 	inline void build(int n){
 		memset(head+1,-1,n<<2);
+		memset(deg+1,0,n<<2);
+		memset(vis,0,sizeof(vis));
 		e=0;
 		for(int i=1;i<n;i++){
 			add(ni,ni);
 		}
 	}
-}
-bool vis[N];
-namespace task0{
-	int f[N][3][2],g[N][3];
-	void dfs(int x){
-		int deg=0;
-		vis[x]=true;
-		for(int i=G::head[x];~i;i=G::bro[i]){
-			if(!vis[G::to[i]]){
-				deg++;
+	int f[E],g[E],h[E];//s->t:from t,pass t,ignore t
+	void dfs(int e){
+		int x=to[e];
+		vis[e]=true;
+		f[e]=0,g[e]=deg[x]-1,h[e]=0;
+		for(int i=head[x];~i;i=bro[i]){
+			if((i^1)!=e){
+				if(!vis[i]){
+					dfs(i);
+				}
+				apmax(h[e],max(g[i],h[i]));
+				apmax(g[e],f[e]+f[i]-1);
+				apmax(f[e],f[i]+deg[x]-2);
 			}
 		}
-		for(int i=G::head[x],v;~i;i=G::bro[i]){
-			v=G::to[i];
-			if(!vis[v]){
-				dfs(v);
-				//dp
-				//todo
-			}
-		}
+		apmax(f[e],deg[x]-1);
+		apmax(g[e],f[e]);
 	}
-	inline int work(){
-		int n=ni;
-		G::build(n);
-		memset(vis+1,0,n);
-		dfs(1);
+	inline int work(int n){
 		int ans=0;
-		apmax(ans,f[1][1][0]);
-		apmax(ans,f[1][1][1]);
-		apmax(ans,f[1][2][0]);
-		apmax(ans,f[1][2][1]);
-		apmax(ans,g[1][0]);
-		apmax(ans,g[1][1]);
-		apmax(ans,g[1][2]);
-		return ans;
-	}
-}
-namespace task2{
-	int fa[N],dep[N];
-	inline void mark(int u,int v){
-		while(u!=v){
-			if(dep[u]<dep[v]){
-				swap(u,v);
-			}
-			vis[u]=true;
-			u=fa[u];
+		for(int i=0;i<e;i+=2){
+			apmax(ans,g[i]+g[i^1]);
+			apmax(ans,h[i]+h[i^1]+1);
 		}
-		vis[u]=true;
-	}
-	void dfs(int x){
-		for(int i=G::head[x],v;~i;i=G::bro[i]){
-			v=G::to[i];
-			if(v!=fa[x]){
-				fa[v]=x;
-				dep[v]=dep[x]+1;
-				dfs(v);
+		for(int x=1;x<=n;x++){
+			int mxg=0,f1=0,f2=0,f3=0;
+			for(int i=head[x];~i;i=bro[i]){
+				apmax(ans,mxg+g[i]+1);
+				apmax(mxg,g[i]);
+				apmax(ans,f1+f2+f3+f[i]+deg[x]-4);
+				if(f[i]>f1){
+					f3=f2,f2=f1,f1=f[i];
+				}else if(f[i]>f2){
+					f3=f2,f2=f[i];
+				}else if(f[i]>f3){
+					f3=f[i];
+				}
 			}
-		}
-	}
-	void mark(int x){
-		vis[x]=true;
-		for(int i=G::head[x],v;~i;i=G::bro[i]){
-			v=G::to[i];
-			if(!vis[v]){
-				mark(v);
-			}
-		}
-	}
-	inline int work(){
-		int n=ni,ps=ni,pt=ni,hs=ni,ht=ni;
-		G::build(n);
-		fa[1]=dep[1]=0;
-		dfs(1);
-		memset(vis+1,0,n);
-		mark(ps,pt),mark(hs,ht);
-		int cnt=0;
-		for(int i=1;i<=n;i++){
-			if(!vis[i]){
-				cnt++;
-				mark(i);
-			}
-		}
-		return cnt;
-	}
-}
-namespace task1{
-	int f[N],g[N],h[N];
-	void dfs(int x){
-		vis[x]=true;
-		int deg=0;
-		for(int i=G::head[x],v;~i;i=G::bro[i]){
-			v=G::to[i];
-			if(!vis[v]){
-				deg++;
-			}
-		}
-		h[x]=1;
-		f[x]=0;
-		g[x]=deg;
-		for(int i=G::head[x],v;~i;i=G::bro[i]){
-			v=G::to[i];
-			if(!vis[v]){
-				dfs(v);
-				apmax(h[x],max(f[v],g[v])+1);
-				apmax(h[x],h[v]);
-				apmax(g[x],f[x]-1+f[v]);
-				apmax(f[x],f[v]+deg-1);
-			}
-		}
-		apmax(f[x],deg);
-	}
-	inline int work(){
-		int n=ni,ps=ni,pt=ni;
-		G::build(n);
-		task2::fa[1]=task2::dep[1]=0;
-		task2::dfs(1);
-		memset(vis+1,0,n);
-		task2::mark(ps,pt);
-		int ans=0;
-		for(int i=1;i<=n;i++){
-			if(!vis[i]){
-				dfs(i);
-				ans+=max(f[i],max(g[i],h[i]));
-			}
+			apmax(ans,deg[x]);
+			apmax(ans,f1+deg[x]-1);
+			apmax(ans,f1+f2+deg[x]-2);
+			apmax(ans,f1+f2+f3+deg[x]-3);
 		}
 		return ans;
 	}
 }
 int main(){
 	for(int tot=ni,x=ni;tot--;){
-		printf("%d\n",x==0?task0::work():(x==1?task1::work():task2::work()));
+		int n=ni;
+		if(x){
+			ni,ni;
+			if(x==2){
+				ni,ni;
+			}
+		}
+		G::build(n);
+		for(int i=0;i<G::e;i++){
+			if(!G::vis[i]){
+				G::dfs(i);
+			}
+		}
+		printf("%d\n",G::work(n));
 	}
 }
