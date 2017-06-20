10120c59209f2f4d1e7fcc830750934e861ffae1
Kerry Su <i_sukai@live.com>
2017-06-20 13:06:44 +0800

    Use '<' instead of '>'

--- template.cpp
+++ template.cpp
@@ -19,7 +19,7 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 	if(a<b){a=b;}
 }
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
-	if(a>b){a=b;}
+	if(b<a){a=b;}
 }
 int main(){
 	
