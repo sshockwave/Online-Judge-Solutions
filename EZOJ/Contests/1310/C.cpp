43b6695f23a6ae88102c863dd79a0855c6b36592
Kerry Su <i_sukai@live.com>
2017-06-23 17:22:59 +0800

    Accepted.

--- EZOJ/Contests/1310/C.cpp
+++ EZOJ/Contests/1310/C.cpp
@@ -36,20 +36,68 @@ namespace G{
 	inline void add(int u,int v,int w){
 		ae(u,v,w),ae(v,u,w);
 	}
-	void dfs(int e){
-		vis[e]=true;
-		int x=to[e];
+	void dfs1(int x,int e){//pos
 		for(int i=head[x];~i;i=bro[i]){
-			if((e^i)!=1){
-				if(!vis[i]){
-					dfs(i);
-				}
-				apmax(g[e],g[i]);
-				apmax(g[e],f[e]+f[i]+val[i]);
+			if(i^e^1){
+				dfs1(to[i],i);
+				apmax(g[e],max(g[i],f[e]+f[i]+val[i]));
 				apmax(f[e],f[i]+val[i]);
 			}
 		}
-		apmax(g[e],f[e]);
+	}
+	void dfs2(int x,int e,lint f1,lint g1){//rev
+		lint f2=0,f3=0,f1f=e^1,f2f=-1,f3f=-1;
+		lint g2=0,g1f=e^1,g2f=-1;
+		for(int i=head[x];~i;i=bro[i]){
+			if(i^e^1){
+				lint nf=f[i]+val[i];
+				if(nf>f1){
+					f3=f2,f3f=f2f;
+					f2=f1,f2f=f1f;
+					f1=nf,f1f=i;
+				}else if(nf>f2){
+					f3=f2,f3f=f2f;
+					f2=nf,f2f=i;
+				}else if(nf>f3){
+					f3=nf,f3f=i;
+				}
+				if(g[i]>g1){
+					g2=g1,g2f=g1f;
+					g1=g[i],g1f=i;
+				}else if(g[i]>g2){
+					g2=g[i],g2f=i;
+				}
+			}
+		}
+		for(int i=head[x];~i;i=bro[i]){
+			if(i^e^1){
+				lint curf,curg;
+				if(f1f!=i){
+					curf=f1;
+				}else{
+					assert(f2f!=i);
+					curf=f2;
+				}
+				if(g1f!=i){
+					curg=g1;
+				}else{
+					assert(g2f!=i);
+					curg=g2;
+				}
+				if(f1f==i){
+					assert(f2f!=i&&f3f!=i);
+					apmax(curg,f2+f3);
+				}else if(f2f==i){
+					assert(f1f!=i&&f3f!=i);
+					apmax(curg,f1+f3);
+				}else{
+					assert(f1f!=i&&f2f!=i);
+					apmax(curg,f1+f2);
+				}
+				f[i^1]=curf,g[i^1]=curg;
+				dfs2(to[i],i,curf+val[i],curg);
+			}
+		}
 	}
 }
 int main(){
@@ -59,11 +107,7 @@ int main(){
 		int u=ni,v=ni;
 		G::add(u,v,ni);
 	}
-	for(int i=0;i<G::e;i++){
-		if(!G::vis[i]){
-			G::dfs(i);
-		}
-	}
+	G::dfs1(1,G::e),G::dfs2(1,G::e,0,0);
 	lint sum=0;
 	for(int i=1;i<n;i++){
 		lint p=g[(i<<1)-2],q=g[(i<<1)-1];
