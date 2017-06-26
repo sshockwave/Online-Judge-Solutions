58bb4a39b1819b346e05af6c48b7366e95c51284
Kerry Su <i_sukai@live.com>
2017-06-26 14:03:49 +0800

    Accepted.

--- EZOJ/Contests/1310/B.cpp
+++ EZOJ/Contests/1310/B.cpp
@@ -3,9 +3,9 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
-#include <ctime>
 using namespace std;
 typedef long long lint;
+#define cout cerr
 #define ni (next_num<int>())
 #define nl (next_num<lint>())
 template<class T>inline T next_num(){
@@ -22,317 +22,319 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
 	if(b<a){a=b;}
 }
-const int N=100010,TN=N*40,INF=0x7f7f7f7f;//tocheck
+const int n=ni,N=100010,TN=N*40;
 int tim=0;
-#define lson(x) son[x][0]
-#define rson(x) son[x][1]
-#define clr(x) memset((x),0,sizeof(x))
+#define getter(fn)\
+inline int fn(int x,int p){\
+	q=p;\
+	return fn(x,1,n);\
+}
+#define setter(fn,set)\
+inline int fn(int x,int p,int v){\
+	q=p,qv=v;\
+	return set(x,1,n);\
+}
 namespace W{
-	int son[TN][2],tag[TN],val[TN],n=0;
+	struct node{
+		int lson,rson,tag,cnt;
+	}pt[TN];
+	int nn=1;
 	inline void init(){
-		clr(son),clr(val);
-		memset(tag,-1,sizeof(tag));
+		pt[0]=(node){0,0,-1,0};
 	}
-	inline void up(int x){
-		assert(tag[x]==tim);
-		val[x]=val[lson(x)]+val[rson(x)];
+	inline node* renew(int x){
+		return pt+(pt[x].tag<tim?(pt[nn]=pt[x],pt[nn].tag=tim,nn++):x);
 	}
-	inline int renew(int x){
-		if(tag[x]<tim){
-			n++,lson(n)=lson(x),rson(n)=rson(x),tag[n]=tim,val[n]=x,x=n;
-		}
-		return x;
-	}
-	int set(int x,int p,int lend,int rend){
-		x=renew(x);
-		if(lend==rend){
-			assert(p==lend);
-			val[x]=1;
+	int q;
+	int set(int xx,int l,int r){
+		node* x=renew(xx);
+		if(l==r){
+			x->cnt=1;
 		}else{
-			int mid=(lend+rend)>>1;
-			if(p<=mid){
-				lson(x)=set(lson(x),p,lend,mid);
+			int mid=(l+r)>>1;
+			if(q<=mid){
+				x->lson=set(x->lson,l,mid);
 			}else{
-				rson(x)=set(rson(x),p,mid+1,rend);
+				x->rson=set(x->rson,mid+1,r);
 			}
-			up(x);
+			x->cnt++;
 		}
-		return x;
+		return x-pt;
 	}
-	int del(int x,int p,int lend,int rend){
-		assert(x&&val[x]);
-		if(val[x]==1){
+	int del(int xx,int l,int r){
+		assert(pt[xx].cnt);
+		if(pt[xx].cnt==1){
 			return 0;
 		}
-		x=renew(x);
-		int mid=(lend+rend)>>1;
-		if(p<=mid){
-			lson(x)=del(lson(x),p,lend,mid);
+		node* x=renew(xx);
+		int mid=(l+r)>>1;
+		if(q<=mid){
+			x->lson=del(x->lson,l,mid);
 		}else{
-			rson(x)=del(rson(x),p,mid+1,rend);
+			x->rson=del(x->rson,mid+1,r);
 		}
-		up(x);
-		return x;
+		x->cnt--;
+		return x-pt;
 	}
-	int pre(int x,int p,int lend,int rend){
-		if(val[x]==0){
+	int pre(int xx,int l,int r){
+		node* x=pt+xx;
+		if(x->cnt==0){
 			return 0;
 		}
-		int mid=(lend+rend)>>1;
-		if(p<=mid){
-			return pre(lson(x),p,lend,mid);
+		if(l==r){
+			return l<q?l:0;
 		}
-		int res=pre(rson(x),p,mid+1,rend);
-		return res?res:pre(lson(x),p,lend,mid);
+		int mid=(l+r)>>1;
+		if(q<=mid){
+			return pre(x->lson,l,mid);
+		}
+		int res=pre(x->rson,mid+1,r);
+		return res?res:pre(x->lson,l,mid);
 	}
-	int mxval;
-	int nxt(int x,int p,int lend,int rend){
-		if(val[x]==0){
-			return mxval;
+	int nxt(int xx,int l,int r){
+		node* x=pt+xx;
+		if(x->cnt==0){
+			return n+1;
 		}
-		int mid=(lend+rend)>>1;
-		if(p>mid){
-			return nxt(rson(x),p,mid+1,rend);
+		if(l==r){
+			return l>q?l:n+1;
 		}
-		int res=nxt(lson(x),p,lend,mid);
-		return res==mxval?nxt(rson(x),p,mid+1,rend):res;
+		int mid=(l+r)>>1;
+		if(q>mid){
+			return nxt(x->rson,mid+1,r);
+		}
+		int res=nxt(x->lson,l,mid);
+		return res<=n?res:nxt(x->rson,mid+1,r);
 	}
+	getter(set);getter(del);getter(pre);getter(nxt);
 }
 namespace R{
-	int son[TN][2],tag[TN],val[TN],n=0;
-	int root[N];
+	struct node{
+		int lson,rson,tag,val;
+	}pt[TN];
+	int root[N],nn=1;
 	inline void init(){
-		clr(son),clr(val),clr(root);
-		memset(tag,-1,sizeof(tag));
+		pt[0]=(node){0,0,-1,0};
+		memset(root,0,sizeof(root));
 	}
-	inline int renew(int x){
-		if(tag[x]<tim){
-			n++,lson(n)=lson(x),rson(n)=rson(x),tag[n]=tim,val[n]=x,x=n;
-		}
-		return x;
+	inline node* renew(int x){
+		return pt+(pt[x].tag<tim?(pt[nn]=pt[x],pt[nn].tag=tim,nn++):x);
 	}
-	int set(int x,int p,int v,int lend,int rend){
-		x=renew(x);
-		if(lend==rend){
-			assert(lend==p);
-			val[x]=v;
-			return x;
+	int q,qv;
+	int _set(int xx,int l,int r){
+		node* x=renew(xx);
+		if(l==r){
+			x->val=qv;
+			return x-pt;
 		}
-		int mid=(lend+rend)>>1;
-		if(p<=mid){
-			lson(x)=set(lson(x),p,v,lend,mid);
+		int mid=(l+r)>>1;
+		if(q<=mid){
+			x->lson=_set(x->lson,l,mid);
 		}else{
-			rson(x)=set(rson(x),p,v,mid+1,rend);
+			x->rson=_set(x->rson,mid+1,r);
 		}
-		return x;
+		return x-pt;
 	}
-	int ask(int x,int p,int lend,int rend){
-		if(lend==rend){
-			return val[x];
+	int ask(int x,int l,int r){
+		if(x==0){
+			return 0;
 		}
-		int mid=(lend+rend)>>1;
-		if(p<=mid){
-			return ask(lson(x),p,lend,mid);
+		if(l==r){
+			return pt[x].val;
+		}
+		int mid=(l+r)>>1;
+		if(q<=mid){
+			return ask(pt[x].lson,l,mid);
 		}else{
-			return ask(rson(x),p,mid+1,rend);
+			return ask(pt[x].rson,mid+1,r);
 		}
 	}
+	setter(set,_set);getter(ask);
 }
 namespace T{
-	int son[TN][2],tag[TN],color[TN],nxt[TN],n=0;
-	lint sum[TN],diff[TN];
-	int root[N];
-	inline void init(){
-		clr(son),clr(color),clr(nxt),clr(nxt),clr(root);
-		memset(tag,-1,sizeof(tag));
-	}
-	lint ask(int x,int v,int lend,int rend){//get sum after limit
-		if(lend==rend){
-			assert(diff[x]==0);
-			return min(sum[x],(lint)v);
-		}
-		int mid=(lend+rend)>>1;
-		if(nxt[rson(x)]<=v){
-			assert(nxt[x]==nxt[rson(x)]);
-			return ask(rson(x),v,mid+1,rend)+sum[lson(x)]+diff[x];
-		}else{
-			assert(nxt[x]==nxt[lson(x)]);
-			return ask(lson(x),v,lend,mid)+(((v<<1)-rend-(mid+1))*(rend-mid)>>1);
-		}
-	}
-	inline void up(int x,int lend,int rend){
-		assert(lson(x)&&rson(x));
-		assert(tag[x]==tim);
-		nxt[x]=min(nxt[lson(x)],nxt[rson(x)]);
-		int mid=(lend+rend)>>1;
-		if(nxt[lson(x)]<=nxt[rson(x)]){
-			nxt[x]=nxt[lson(x)];
-			sum[x]=ask(lson(x),nxt[rson(x)],lend,mid)+sum[rson(x)];
-		}else{
-			nxt[x]=nxt[rson(x)];
-			sum[x]=sum[rson(x)]+((((lint)nxt[x]<<1)-lend-mid)*(mid-lend+1)>>1);
-		}
-		diff[x]=sum[x]-sum[lson(x)]-sum[rson(x)];
-	}
-	inline int renew(int x){
-		if(tag[x]<tim){
-			n++;
-			lson(n)=lson(x),rson(n)=rson(x);
-			tag[n]=tim;
-			color[n]=color[x];
-			nxt[n]=nxt[x];
-			sum[n]=sum[x],diff[n]=diff[x];
-			x=n;
-		}
-		return x;
-	}
-	int set(int x,int p,int v,int val[],int lend,int rend){
-		x=renew(x);
-		if(lend==rend){
-			assert(lend==p);
-			val[x]=v;
-			if(val==nxt){
-				diff[x]=0;
-				sum[x]=v-lend;
-			}
-		}else{
-			int mid=(lend+rend)>>1;
-			if(p<=mid){
-				lson(x)=set(lson(x),p,v,val,lend,mid);
-			}else{
-				rson(x)=set(rson(x),p,v,val,mid+1,rend);
-			}
-			if(val==nxt){
-				up(x,lend,rend);
-			}
-		}
-		return x;
-	}
-	int build(int x,int lend,int rend,int a[],int _nxt[]){
-		x=renew(x);
-		if(lend==rend){
-			color[x]=a[lend];
-			nxt[x]=_nxt[lend];
-			diff[x]=0;
-			sum[x]=nxt[x]-lend;
-		}else{
-			int mid=(lend+rend)>>1;
-			lson(x)=build(lson(x),lend,mid,a,_nxt);
-			rson(x)=build(rson(x),mid+1,rend,a,_nxt);
-			up(x,lend,rend);
-		}
-		return x;
-	}
-	int col(int x,int p,int lend,int rend){
-		if(lend==rend){
-			return color[x];
-		}
-		int mid=(lend+rend)>>1;
-		if(p<=mid){
-			return col(lson(x),p,lend,mid);
-		}else{
-			return col(rson(x),p,mid+1,rend);
-		}
-	}
-}
-namespace Q{
-	struct Info{
-		int nxt;
+	struct node{
+		int lson,rson,tag,nxt,color;
 		lint sum,diff;
-	}info[TN];
-	lint lim(int x,int v,int l,int lend,int rend){
-		assert(l<=rend&&l>=lend);
-		if(lend==rend){
-			return min(info[x].sum,(lint)v);
+		inline void up(int,int);
+	}pt[TN];
+	int root[N],nn=1;
+	inline void init(){
+		pt[0]=(node){0,0,-1,0,0,0ll,0ll};
+		memset(root,0,sizeof(root));
+	}
+	inline node* renew(int x){
+		return pt+(pt[x].tag<tim?(pt[nn]=pt[x],pt[nn].tag=tim,nn++):x);
+	}
+	int q,qv,ql,qr;
+	inline lint all(int l,int r){
+		return ((((lint)qv<<1)-l-r)*(r-l+1))>>1;
+	}
+	lint lim(int xx,int l,int r){
+		assert(xx);
+		node* x=pt+xx;
+		if(x->nxt>=qv){
+			return all(l,r);
 		}
-		int mid=(lend+rend)>>1;
-		if(l>mid){
-			return lim(T::rson(x),v,l,mid+1,rend);
+		if(l==r){
+			return x->nxt-l;
 		}
-		if(info[T::rson(x)].nxt<=v){
-			return lim(T::rson(x),v,mid+1,mid+1,rend)+info[T::lson(x)].sum+info[x].diff;
+		int mid=(l+r)>>1;
+		if(pt[x->rson].nxt<=qv){
+			return lim(x->rson,mid+1,r)+pt[x->lson].sum+x->diff;
 		}else{
-			return lim(T::lson(x),v,l,lend,mid)+((((lint)v<<1)-rend-(mid+1))*(rend-mid)>>1);
+			return lim(x->lson,l,mid)+all(mid+1,r);
 		}
 	}
-	int mxnxt;
-	void ask(int x,int l,int r,int lend,int rend){
-		if(lend==rend){
-			assert(l==lend&&r==rend);
-			assert(T::diff[x]==0);
-			info[x].nxt=min(T::nxt[x],mxnxt);
-			info[x].sum=info[x].nxt-lend;
-			info[x].diff=0;
-			return;
+	lint ask(int xx,int l,int r){
+		node *x=pt+xx;
+		if(ql<=l&&r<=qr){
+			lint ans=lim(xx,l,r);
+			apmin(qv,x->nxt);
+			return ans;
 		}
-		int mid=(lend+rend)>>1,lc=T::lson(x),rc=T::rson(x);
-		if(r<=mid){
-			ask(lc,l,r,lend,mid);
-			info[x]=info[lc];
-			info[x].diff=0;
-		}else if(l>mid){
-			ask(rc,l,r,mid+1,rend);
-			info[x]=info[rc];
-			info[x].diff=0;
+		int mid=(l+r)>>1;
+		lint ans=0;
+		if(qr>mid){
+			ans+=ask(x->rson,mid+1,r);
+		}
+		if(ql<=mid){
+			ans+=ask(x->lson,l,mid);
+		}
+		return ans;
+	}
+	inline lint ask(int x,int l,int r,int lim){
+		qv=lim,ql=l,qr=r;
+		return ask(x,1,n);
+	}
+	inline void node::up(int l,int r){
+		assert(tag==tim);
+		assert(lson&&rson);
+		int mid=(l+r)>>1;
+		if(pt[lson].nxt>=pt[rson].nxt){
+			nxt=pt[rson].nxt;
+			qv=pt[rson].nxt;
+			sum=all(l,mid);
 		}else{
-			ask(lc,l,mid,lend,mid);
-			ask(rc,mid+1,r,mid+1,rend);
-			if(info[lc].nxt<=info[rc].nxt){
-				info[x].sum=lim(lc,info[rc].nxt,l,lend,mid);
+			nxt=pt[lson].nxt;
+			qv=pt[rson].nxt;
+			sum=lim(lson,l,mid);
+		}
+		diff=sum-pt[lson].sum;
+		sum+=pt[rson].sum;
+	}
+	int _setc(int xx,int l,int r){
+		node* x=renew(xx);
+		if(l==r){
+			assert(q==l);
+			x->color=qv;
+		}else{
+			int mid=(l+r)>>1;
+			if(q<=mid){
+				x->lson=_setc(x->lson,l,mid);
 			}else{
-				info[x].sum=((lint)(info[rc].nxt<<1)-l-mid)*(mid-l+1)>>1;
+				x->rson=_setc(x->rson,mid+1,r);
 			}
-			info[x].nxt=min(info[lc].nxt,info[rc].nxt);
-			info[x].diff=info[x].sum-info[lc].sum;
-			info[x].sum+=info[rc].sum;
+		}
+		return x-pt;
+	}
+	int askc(int x,int l,int r){
+		assert(x);
+		if(l==r){
+			assert(q==l);
+			return pt[x].color;
+		}
+		int mid=(l+r)>>1;
+		if(q<=mid){
+			return askc(pt[x].lson,l,mid);
+		}else{
+			return askc(pt[x].rson,mid+1,r);
 		}
 	}
+	int _setn(int xx,int l,int r){
+		node* x=renew(xx);
+		if(l==r){
+			assert(q==l);
+			x->nxt=qv,x->sum=x->nxt-l;
+			assert(x->diff==0);
+		}else{
+			int mid=(l+r)>>1;
+			if(q<=mid){
+				x->lson=_setn(x->lson,l,mid);
+			}else{
+				x->rson=_setn(x->rson,mid+1,r);
+			}
+			x->up(l,r);
+		}
+		return x-pt;
+	}
+	int *nxt,*color;
+	int build(int l,int r){
+		node* x=renew(0);
+		if(l==r){
+			x->color=color[l];
+			x->nxt=nxt[l],x->sum=x->nxt-l;
+			assert(x->diff==0);
+		}else{
+			int mid=(l+r)>>1;
+			x->lson=build(l,mid);
+			x->rson=build(mid+1,r);
+			x->up(l,r);
+		}
+		return x-pt;
+	}
+	setter(setc,_setc);setter(setn,_setn);getter(askc);
+}
+lint ans=0;
+inline int nd(){
+	return (ans+ni)%n+1;
 }
 int main(){
 	W::init(),R::init(),T::init();
-	int n=ni;
-	W::mxval=n+1;
 	{
-		int a[N],nxt[N],root[N];
+		int color[N],nxt[N],root[N];
 		memset(root,0,sizeof(root));
 		for(int i=1;i<=n;i++){
-			a[i]=ni;
-			root[a[i]]=W::set(root[a[i]],i,1,n);
+			color[i]=ni;
+			root[color[i]]=W::set(root[color[i]],i);
 		}
+		R::root[0]=0;
 		for(int i=1;i<=n;i++){
 			if(root[i]){
-				R::root[tim]=R::set(R::root[tim],i,root[i],1,n);
+				R::root[0]=R::set(R::root[0],i,root[i]);
 			}
 			root[i]=n+1;
 		}
 		for(int i=n;i>=1;i--){
-			nxt[i]=root[a[i]];
-			root[a[i]]=i;
+			nxt[i]=root[color[i]];
+			root[color[i]]=i;
 		}
-		T::root[tim]=T::build(T::root[tim],1,n,a,nxt);
+		T::color=color,T::nxt=nxt;
+		T::root[0]=T::build(1,n);
 	}
-	lint ans=0;
 	for(int tot=ni;tot--;){
-		int id=ni,t=ni,a=(ni+ans)%n+1,b=(ni+ans)%n+1;
-//		int l=ni,r=ni;
+		int id=ni,t=ni;
 		if(id==1){//alt
 			tim++;
-			int oldc=T::col(T::root[t],a,1,n),newc=b;
-			if(oldc==newc){
+			int x=nd(),newc=nd(),oldc=T::askc(T::root[t],x);
+			if(newc==oldc){
+				T::root[tim]=T::root[t];
+				R::root[tim]=R::root[t];
 				continue;
 			}
-			int oldr=R::ask(R::root[t],oldc,1,n),newr=R::ask(R::root[t],newc,1,n);
-			//start setting
-			T::root[tim]=T::set(T::root[t],a,W::nxt(newr,a,1,n),T::nxt,1,n);
-			R::root[tim]=R::set(R::set(R::root[t],newc,W::set(newr,a,1,n),1,n),oldc,W::del(oldr,a,1,n),1,n);
-			int pre=W::pre(oldr,a,1,n);
+			int oldr=W::del(R::ask(R::root[t],oldc),x),newr=W::set(R::ask(R::root[t],newc),x);
+			R::root[tim]=R::set(R::set(R::root[t],oldc,oldr),newc,newr);
+			T::root[tim]=T::setn(T::setc(T::root[t],x,newc),x,W::nxt(newr,x));
+			int pre=W::pre(oldr,x);
 			if(pre){
-				T::root[tim]=T::set(T::root[tim],a,W::nxt(oldr,pre,1,n),T::nxt,1,n);
+				T::root[tim]=T::setn(T::root[tim],pre,W::nxt(oldr,pre));
+			}
+			pre=W::pre(newr,x);
+			if(pre){
+				T::root[tim]=T::setn(T::root[tim],pre,x);
 			}
 		}else{//ask
-			Q::mxnxt=b;
-			Q::ask(T::root[t],a,b,1,n);
-			printf("%lld\n",Q::info[T::root[t]].sum);
+			int l=nd(),r=nd();
+			printf("%lld\n",ans=T::ask(T::root[t],l,r,r+1));
 		}
 	}
 }
