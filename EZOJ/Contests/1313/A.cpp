3408c4247ce03867dfd6e12bc99a4f3595706ddc
Kerry Su <i_sukai@live.com>
2017-06-28 14:41:37 +0800

    WA. Sometimes cannot get answer

---
+++ EZOJ/Contests/1313/A.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+typedef long long lint;
+#define ni (next_num<int>())
+#define nl (next_num<lint>())
+template<class T>inline T next_num(){
+	T i=0;char c;
+	while(!isdigit(c=getchar())&&c!='-');
+	bool flag=c=='-';
+	flag?(c=getchar()):0;
+	while(i=i*10-'0'+c,isdigit(c=getchar()));
+	return flag?-i:i;
+}
+template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
+	if(a<b){a=b;}
+}
+template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
+	if(b<a){a=b;}
+}
+const int n=ni,k=ni,N=10010,K=5,INF=0x7f7f7f7f;
+int col[N];
+namespace T{
+	const int E=N*2;
+	int to[E],bro[E],head[N],e=0;
+	inline void init(){
+		memset(head,-1,sizeof(head));
+	}
+	inline void ae(const int &u,const int &v){
+		to[e]=v,bro[e]=head[u],head[u]=e++;
+	}
+	inline void add(const int &u,const int &v){
+		ae(u,v),ae(v,u);
+	}
+	int f[N][1<<K],cmap[N];
+	inline void dfs(int x,int e){
+		int *F=f[x];
+		F[0]=0,F[cmap[col[x]]]=1;
+		for(int i=head[x],v;~i;i=bro[i]){
+			if(i!=e){
+				dfs(v=to[i],i^1);
+				int *G=f[v];
+				const static int tot=1<<k;
+				for(int s=0;s<tot;s++){
+					for(int t=s;t;t=(t-1)&s){
+						if(F[t]<INF&&G[s^t]<INF){
+							apmin(F[s],F[t]+G[s^t]);
+						}
+					}
+				}
+			}
+		}
+	}
+	inline int work(){
+		for(int i=1;i<=n;i++){
+			cmap[i]=1<<rand()%k;
+		}
+		memset(f,127,sizeof(f));
+		dfs(1,-1);
+		int ans=INF;
+		for(int i=1,top=(1<<k)-1;i<=n;i++){
+			apmin(ans,f[i][top]);
+		}
+		return ans;
+	}
+}
+int main(){
+	T::init();
+	for(int i=1;i<=n;i++){
+		col[i]=ni;
+	}
+	for(int i=1;i<n;i++){
+		T::add(ni,ni);
+	}
+	int ans=INF;
+	for(int tot=30;tot--;){
+		apmin(ans,T::work());
+	}
+	printf("%d\n",ans);
+}
