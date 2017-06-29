d253cc8a831b7e88096f9f365678bf1be7f04fd1
Kerry Su <i_sukai@live.com>
2017-06-29 19:46:21 +0800

    Accpeted. 3302ms

--- EZOJ/Contests/1313/C.cpp
+++ EZOJ/Contests/1313/C.cpp
@@ -7,6 +7,9 @@ using namespace std;
 typedef long long lint;
 #define ni (next_num<int>())
 #define nl (next_num<lint>())
+#define cout cerr
+#undef assert
+#define assert
 template<class T>inline T next_num(){
 	T i=0;char c;
 	while(!isdigit(c=getchar())&&c!='-');
@@ -21,45 +24,240 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
 	if(b<a){a=b;}
 }
-const int N=100000000;
-bool np[N];
-int prime[N],ps=0;
-inline bool isprime(lint n){
-	for(int i=0,cur=2;i<ps&&(lint)cur*cur<=n;cur=prime[++i]){
-		if(n%cur==0){
+const int R=100010,MOD=1000000007;
+int p,r;
+lint n;
+inline int sqrt(lint n){
+	lint i=0;
+	for(;i*i<n;i++);
+	return i;
+}
+inline int add(const int &a,const int &b){
+	return (a+b)%MOD;
+}
+inline int sub(const int &a,const int &b){
+	return add(a,MOD-b);
+}
+inline int mul(const int &a,const int &b){
+	return (lint)a*b%MOD;
+}
+inline void apadd(int &a,const int &b){
+	a=add(a,b);
+}
+inline void apsub(int &a,const int &b){
+	a=sub(a,b);
+}
+inline void apmul(int &a,const int &b){
+	a=mul(a,b);
+}
+inline int _n1(lint n){
+	const static int rev2=(MOD+1)>>1;
+	n%=MOD;
+	return mul(mul(n,n+1),rev2);
+}
+namespace MillerRabin{
+	inline void apmul(lint &a,const lint &b,const lint &p){
+		a=a*b-(lint)((double)a/p*b+0.5)*p;
+		if(a<0){
+			a+=p;
+		}
+	}
+	inline lint fpow(lint x,lint n,const lint &p=MOD){
+		lint ans=1;
+		for(;n;n>>=1,apmul(x,x,p)){
+			if(n&1){
+				apmul(ans,x,p);
+			}
+		}
+		return ans;
+	}
+	inline bool isp(lint p){//accurate only to 4034k-1
+		if(p==3493072871ll){
 			return false;
 		}
-	}
-	return true;
-}
-int main(){
-	memset(np,0,sizeof(np));
-	int n=ni,p=ni;
-	if(n==1000000){
-		if(p==2){
-			puts("430567427");
-		}else{
-			puts("150850429");
+		lint n=p-1;
+		int cnt=0;
+		for(;(n&1)==0;n>>=1,cnt++);
+		n=fpow(3,n,p);
+		if(n==1){
+			return true;
 		}
-		return 0;
+		for(;cnt--;apmul(n,n,p)){
+			if(n==p-1){
+				return true;
+			}
+		}
+		return false;
 	}
-	const lint maxn=10000000010ll;
-	for(int i=2;i<N;i++){
-		if(!np[i]){
-			prime[ps++]=i;
-			lint x=i;
-			for(int j=1;x<maxn;j++,x*=i){
-				if((x+1)/(i-1)%p==0){
-					cout<<x<<"="<<i<<"^"<<j<<endl;
+}
+namespace sieve{
+	bool np[R];
+	int prime[R],_prime[R],_fprime[R],ps=0;
+	int wu[R],mnfac[R],mnp[R];//wu%=p
+	inline void work(){
+		memset(np,0,sizeof(np));
+		_prime[0]=_fprime[0]=0,wu[1]=1;
+		for(int i=2;i<=r;i++){
+			if(!np[i]){
+				prime[++ps]=i;
+				_prime[ps]=add(_prime[ps-1],i);
+				wu[i]=(i+1)%p;
+				_fprime[ps]=_fprime[ps-1];
+				if(wu[i]){
+					apadd(_fprime[ps],i);
+				}
+				mnfac[i]=mnp[i]=i;
+			}else if(mnfac[i]==i){
+				wu[i]=(wu[i/mnp[i]]*mnp[i]+1)%p;
+			}else{
+				wu[i]=wu[mnfac[i]]*wu[i/mnfac[i]]%p;
+			}
+			assert(i%mnp[i]==0&&i%mnfac[i]==0&&i/mnfac[i]%mnp[i]);
+			for(int j=1,cur=2;j<=ps&&i*cur<=r;cur=prime[++j]){
+				np[i*cur]=true,	mnp[i*cur]=cur;
+				if(i%cur==0){
+					assert(mnp[i]==cur);
+					mnfac[i*cur]=mnfac[i]*cur;
+					break;
+				}else{
+					mnfac[i*cur]=cur;
 				}
 			}
 		}
-		for(int j=0,cur=2;j<ps&&i*cur<N;cur=prime[++j]){
-			np[i*cur]=true;
-			if(i%cur==0){
-				break;
+	}
+}
+namespace mapper{
+	lint id[R<<1];
+	int mxp[R<<1],gs=0;
+	inline int dfn(lint x){
+		assert(id[x<=r?x:gs-(n/x)+1]==x);
+		return x<=r?x:gs-(n/x)+1;
+	}
+	inline void init(){
+		int i=1;
+		for(;i<=r;i++){
+			id[++gs]=i;
+		}
+		for(;i&&id[gs]>=n/i;i--);
+		for(;i>=1;i--){
+			id[++gs]=n/i;
+		}
+		for(int i=gs,j=sieve::ps;i>=1;i--){//get mxp
+			for(;sieve::prime[j]>id[i];j--);
+			mxp[i]=j;
+		}
+	}
+}
+namespace G{
+	int f[R<<1],g[R<<1],tag[R<<1];
+	inline int getg(int i,int j){
+		assert(tag[j]<=i);
+		apmin(i,mapper::mxp[j]);
+		if(tag[j]<i){
+			using namespace sieve;
+			apsub(g[j],sub(_prime[i],_prime[tag[j]])),tag[j]=i;
+		}
+		return g[j];
+	}
+	inline void init(){
+		if(p==2){
+			return;
+		}
+		using namespace mapper;
+		{//get f
+			memset(f,0,sizeof(f));
+#define gap 4034
+			int j=1;
+			lint i=gap;
+			for(;i<=r;i+=gap);
+			for(lint _=n+1;i<=_;i+=gap){
+				if(MillerRabin::isp(i-1)){
+					assert(i%2017==0);
+					assert(id[gs]>=i-1);
+					for(;id[j]<i-1;j++);
+					assert(j<=gs);
+					apadd(f[j],(i-1)%MOD);
+				}
+			}
+			for(int i=1;i<=gs;i++){
+				apadd(f[i],f[i-1]);
+			}
+#undef gap
+		}
+		{//get g
+			using namespace sieve;
+			memset(tag,0,sizeof(tag));
+			for(int i=1;i<=gs;i++){
+				g[i]=_n1(id[i]);
+			}
+			for(int i=1,cur=2;i<=ps;cur=prime[++i]){
+				lint p2=(lint)cur*cur;
+				for(int j=gs;id[j]>=p2;j--){
+					assert(tag[j]==i-1);
+					apsub(g[j],mul(cur,getg(i-1,dfn(id[j]/cur)))),tag[j]=i;
+				}
 			}
 		}
 	}
-	cout<<"ps="<<ps<<endl;
+	inline int get(lint n){
+		if(p==2){
+			if(n<=1){
+				return 0;
+			}
+			return r>=2?0:2;
+		}
+		int j=mapper::dfn(n);
+		return sub(getg(sieve::ps,j),add(f[j],1));
+	}
+}
+namespace F{
+	int f[R<<1];
+	bool tag[R<<1];
+	inline int getf(int i,int j){
+		if(tag[j]){
+			return f[j];
+		}
+		using namespace sieve;
+		using namespace mapper;
+		return i>mxp[j]?1:add(1,sub(_fprime[mxp[j]],_fprime[i-1]));
+	}
+	inline void init(){
+		using namespace mapper;
+		using namespace sieve;
+		memset(tag,0,sizeof(tag));
+		for(int i=1;i<=gs;i++){
+			f[i]=1;
+		}
+		for(int i=ps,cur=prime[i];i>=1;cur=prime[--i]){
+			lint p2=(lint)cur*cur;
+			for(int j=gs;id[j]>=p2;j--){
+				if(!tag[j]){
+					assert(f[j]==1);
+					f[j]=getf(i+1,j);
+					tag[j]=true;
+				}
+				lint pw=cur;
+				for(int wu=(cur+1)%p;pw<=id[j];pw*=cur,wu=(wu*cur+1)%p){
+					if(wu){
+						apadd(f[j],mul(pw%MOD,getf(i+1,dfn(id[j]/pw))));
+					}
+				}
+			}
+		}
+	}
+}
+int main(){
+#ifndef ONLINE_JUDGE
+	freopen("bu.in","r",stdin);
+	freopen("bu.out","w",stdout);
+#endif
+	n=nl,p=ni,r=sqrt(n);
+	sieve::work(),mapper::init(),G::init(),F::init();
+	int ans=F::getf(sieve::ps,mapper::gs);
+	for(int i=1;i<=r;i++){
+		if(sieve::wu[i]){
+			apadd(ans,mul(i,G::get(n/i)));
+		}
+	}
+	printf("%d\n",sub(_n1(n),ans));
 }
