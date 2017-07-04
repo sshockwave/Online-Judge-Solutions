727a5069f6b78b4aa4004b6122372795d4630907
Kerry Su <i_sukai@live.com>
2017-07-04 19:07:25 +0800

    Accepted. 271ms

--- EZOJ/Contests/1316/A.cpp
+++ EZOJ/Contests/1316/A.cpp
@@ -26,70 +26,48 @@ int stat[N];
 namespace T{
 	const int E=N;
 	int to[E],bro[E],head[N],e;
+	bool swi[N],mark[N];//swi:within one step stat change to -1
 	inline void init(int n){
 		memset(head+1,-1,n<<2);
+		memset(mark+1,0,n);
 		e=0;
 	}
 	inline void ae(const int &u,const int &v){
 		to[e]=v,bro[e]=head[u],head[u]=e++;
 	}
-	bool f[N][2];//0:me 1:oppo f:who wins?
 	void dfs(int x){
-		if(~stat[x]){
-			f[x][0]=f[x][1]=stat[x];
-			return;
-		}else if(head[x]==-1){
-			f[x][0]=0,f[x][1]=1;
+		swi[x]=false;
+		if(head[x]==-1){
 			return;
 		}
-		int cnt[2]={0},tie=0;
+		int cnt=0;
 		for(int i=head[x],v;~i;i=bro[i]){
-			assert(to[i]!=x);
 			dfs(v=to[i]);
-			if(f[v][0]==f[v][1]){
-				cnt[f[v][0]]++;
-			}else{
-				assert(f[v][0]==0&&f[v][1]==1);
-				tie++;
+			if(stat[v]==0){
+				cnt++;
+			}else if(stat[v]==1){
+				cnt--;
 			}
 		}
-		tie&=1;
-		f[x][0]=cnt[0]+tie>cnt[1]?0:1;
-		f[x][1]=cnt[1]+tie>cnt[0]?1:0;
-	}
-	bool mark[N];
-	void markall(int x){
-		mark[x]=stat[x]==-1;
-		for(int i=head[x];~i;i=bro[i]){
-			mark[x]=false;
-			markall(to[i]);
+		if(cnt){
+			stat[x]=cnt<0;
+			swi[x]=cnt==-1;
 		}
 	}
 	void dfs2(int x){
-		if(f[x][0]==f[x][1]){
-			assert(f[x][0]==0);
-			markall(x);
+		if(stat[x]==0||(stat[x]==1&&!swi[x])){
+			mark[x]=false;
 			return;
 		}
-		if(head[x]==-1){
-			assert(stat[x]==-1);
-			mark[x]=true;
-			return;
+		mark[x]=head[x]==-1&&stat[x]==-1;
+		for(int i=head[x];~i;i=bro[i]){
+			dfs2(to[i]);
 		}
-		bool flag=false;//debug
-		for(int i=head[x],v;~i;i=bro[i]){
-			v=to[i];
-			if(f[v][0]!=f[v][1]){
-				flag=true;
-				dfs2(v);
-			}
-		}
-		assert(flag);
 	}
 }
 int main(){
 #ifndef ONLINE_JUDGE
-	freopen("ah1.in","r",stdin);
+	freopen("ah.in","r",stdin);
 	freopen("ah.out","w",stdout);
 #endif
 	for(int tot=ni;tot--;){
@@ -97,15 +75,27 @@ int main(){
 		T::init(n);
 		ni;//fa 1
 		for(int i=2;i<=n;i++){
-			int tmp;
-			T::ae(tmp=ni,i);
+			T::ae(ni,i);
 		}
 		for(int i=1;i<=n;i++){
 			stat[i]=ni;
 		}
 		T::dfs(1);
-		if(T::f[1][0]){
-			puts("-1");
+		if(stat[1]==1){
+			putchar('-'),putchar('1');
+		}else if(stat[1]==0){
+			int cnt=0;
+			for(int i=1;i<=n;i++){
+				if(T::head[i]==-1&&stat[i]==-1){
+					cnt++;
+				}
+			}
+			printf("%d",cnt);
+			for(int i=1;i<=n;i++){
+				if(T::head[i]==-1&&stat[i]==-1){
+					printf(" %d",i);
+				}
+			}
 		}else{
 			T::dfs2(1);
 			int cnt=0;
@@ -120,7 +110,7 @@ int main(){
 					printf(" %d",i);
 				}
 			}
-			putchar('\n');
 		}
+		putchar('\n');
 	}
 }
