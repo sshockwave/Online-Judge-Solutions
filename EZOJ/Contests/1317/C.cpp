304896887da2b2e89777186aac76bed91546d779
Kerry Su <i_sukai@live.com>
2017-07-05 16:55:54 +0800

    Accepted. 594ms

--- EZOJ/Contests/1317/C.cpp
+++ EZOJ/Contests/1317/C.cpp
@@ -64,8 +64,8 @@ struct Edge{
 struct Query{
 	int k,x;
 	lint ans;
-	inline void ae(const set<int>&s);
 }q[Q];
+int tot;
 struct Node{
 	int fa,size,dep,tag;
 };
@@ -98,7 +98,6 @@ namespace uni1{
 		}
 	}
 	inline lint solve(const vector<int>&g){
-		tim++;
 		lint ans=0;
 		for(int i=0,n=g.size();i<n;i++){
 			int u=root(e[g[i]].u),v=root(e[g[i]].v);
@@ -144,15 +143,21 @@ namespace uni2{
 	struct Graph{
 		Query *q;
 		vector<int>e;
-		inline lint run();
 	};
 	vector<Graph>g[W];
 	inline void work(){
 		for(int i=2;i<W;i++){
 			if(!g[i].empty()){
-				lint ans=uni1::solve(g[i][0].e);
-				for(int j=1,tn=g[i].size();j<tn;j++){
-					g[i][j].q->ans+=(solve(g[i][j].e)+ans)*mu[i];
+				uni1::tim++;
+				for(int j=0,tn=g[i].size();j<tn;j++){
+					if(g[i][j].q){
+						g[i][j].q->ans+=solve(g[i][j].e)*mu[i];
+					}else{
+						lint ans=uni1::solve(g[i][j].e);
+						for(int k=0;k<=tot;k++){
+							q[k].ans+=ans*mu[i];
+						}
+					}
 				}
 			}
 		}
@@ -161,7 +166,7 @@ namespace uni2{
 inline void pushe(int w,Query *q,int e){
 	using namespace uni2;
 	int x=g[w].size();
-	if(g[w][x-1].q!=q){
+	if(x==0||g[w][x-1].q!=q){
 		g[w].push_back(Graph());
 		g[w][x].q=q;
 	}else{
@@ -170,7 +175,7 @@ inline void pushe(int w,Query *q,int e){
 	g[w][x].e.push_back(e);
 }
 inline void ae(Query *q,const set<int>&s){
-	for(set<int>::iterator it=s.begin(),ed=s.end();it!=s.end();it++){
+	for(set<int>::iterator it=s.begin();it!=s.end();it++){
 		vector<int>&vec=e[*it].vec;
 		for(int i=1,n=vec.size();i<n;i++){
 			pushe(vec[i],q,*it);
@@ -187,17 +192,17 @@ int main(){
 	static int last[N];
 	for(int i=1;i<n;i++){
 		e[i]=(Edge){ni,ni,ni};
+		e[i].fact();
 		last[i]=i;
 	}
 	static bool vis[N+Q];
 	memset(vis,0,sizeof(vis));
-	int tot=ni,es=n;
+	tot=ni;
 	for(int i=1;i<=tot;i++){
-		q[i]=(Query){ni,ni};
+		q[i]=(Query){ni,ni,0ll};
 		vis[q[i].k]=vis[n+i-1]=true;
-		e[es]=e[q[i].k];
-		e[es].w=q[i].x;
-		e[es].fact();
+		e[n+i-1]=(Edge){e[q[i].k].u,e[q[i].k].v,q[i].x};
+		e[n+i-1].fact();
 	}
 	set<int>s,ini;
 	for(int i=1;i<n;i++){
@@ -210,8 +215,7 @@ int main(){
 	ae(0,ini),ae(q,s);
 	for(int i=1;i<=tot;i++){
 		s.erase(last[q[i].k]);
-		s.insert(n-i+1);
-		last[q[i].k]=n-i+1;
+		s.insert(last[q[i].k]=n+i-1);
 		ae(q+i,s);
 	}
 	uni2::work();
