5cae3f3ad1cfef746d741d23de085474b57e2eb6
Kerry Su <i_sukai@live.com>
2017-07-05 17:40:18 +0800

    WA.

--- EZOJ/Contests/1317/A.cpp
+++ EZOJ/Contests/1317/A.cpp
@@ -24,52 +24,29 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 	if(a<b){a=b;}
 }
 const int N=1500010;
-lint a[N],b[N];
-vector<int>vec[N];
-inline int lowbit(int x){
-	return x&(-x);
+void solve(lint a[],int n){
+	if(n==1){
+		return;
+	}
+	int half=n>>1;
+	lint s0=a[half-1],s1=a[n-1]-s0;
+	for(int i=0;i<half;i++){
+		a[i]=(a[i+half]+a[i]-s1)>>1;
+		a[i+half]-=a[i];
+	}
+	solve(a,half),solve(a+half,half);
 }
-inline int cntbit(int x){
-	int cnt=0;
-	for(;x;cnt++,x^=lowbit(x));
-	return cnt;
-}
-inline lint sum(int i,int j){
-	return b[i|j]-b[i&j];
-}
-inline void solve(int i,int j,int k){
-	lint A=sum(i,j),B=sum(j,k),C=sum(k,i),all=((lint)A+B+C)/2;
-	a[i]=all-B,a[j]=all-C,a[k]=all-A;
-}
+lint a[N];
 int main(){
 #ifndef ONLINE_JUDGE
-	freopen("spy1.in","r",stdin);
+	freopen("spy.in","r",stdin);
 	freopen("spy.out","w",stdout);
 #endif
-	int n=ni,sh=cntbit(n-1);
+	int n=ni;
 	for(int i=0;i<n;i++){
-		b[i]=nl;
-		vec[cntbit(i)].push_back(i);
+		a[i]=ni;
 	}
-	for(int i=1;i<sh;i++){
-		int j=0;
-		int n=vec[i].size();
-		for(;j+3<=n;j+=3){
-			solve(vec[i][j],vec[i][j+1],vec[i][j+2]);
-		}
-		for(;j<n;j++){
-			a[vec[i][j]]=sum(vec[i][j],vec[i][0])-a[vec[i][0]];
-		}
-	}
-	a[0]=b[0],a[n-1]=b[n-1];
-	for(int i=1;i<n;i++){
-		if((cntbit(i)&1)==0){
-			a[0]-=a[i];
-		}
-	}
-	for(int i=0,tn=n-1;i<tn;i++){
-		a[n-1]-=a[i];
-	}
+	solve(a,n);
 	for(int i=0;i<n;i++){
 		printf("%lld ",a[i]);
 	}
