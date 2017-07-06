1099d8a7e7802060a6a0f4b4355311f5fe719ef3
Kerry Su <i_sukai@live.com>
2017-07-06 15:06:10 +0800

    TLE. 20pts.

--- EZOJ/Contests/1318/B.cpp
+++ EZOJ/Contests/1318/B.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
-#include <map>
+#include <cmath>
 using namespace std;
 typedef long long lint;
 typedef unsigned int uint;
@@ -23,88 +23,78 @@ template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 	if(a<b){a=b;}
 }
-const int N=10000010;
-uint prime[N],mnfac[N],mnexp[N],d[N],ps=0;
+const int N=5010,sqrtN=75;
+uint prime[N],mnexp[N],d[N],ps=0;
 bool np[N];
-inline void sieve(){
+inline void sieve(uint n){
 	memset(np,0,sizeof(np));
 	d[1]=1;
-	for(int i=2;i<N;i++){
+	for(uint i=2;i<=n;i++){
 		if(!np[i]){
 			prime[ps++]=i;
-			mnfac[i]=i;
-			mnexp[i]=1;
+			d[i]=mnexp[i]=1;
 		}
-		d[i]=d[i/mnfac[i]]*(mnexp[i]+1);
-		for(uint j=0,cur=2;j<ps&&i*cur<N;cur=prime[++j]){
+		for(uint j=0,cur=2;j<ps&&i*cur<=n;cur=prime[++j]){
 			np[i*cur]=true;
 			if(i%cur==0){
-				mnfac[i*cur]=mnfac[i]*cur;
 				mnexp[i*cur]=mnexp[i]+1;
+				d[i*cur]=d[i];
 				break;
 			}else{
-				mnfac[i*cur]=cur;
 				mnexp[i*cur]=1;
+				d[i*cur]=d[i]*(mnexp[i]+1);
 			}
 		}
+		d[i]*=mnexp[i]+1;
 	}
 }
-struct pti{
-	int a,b,c;
-	pti(int _a,int _b,int _c):a(_a),b(_b),c(_c){
-		if(a>b){
-			swap(a,b);
-		}
-		if(b>c){
-			swap(b,c);
-		}
-		if(a>b){
-			swap(a,b);
-		}
+struct mapper{
+	int n,rt,ds,id[sqrtN<<1];
+	inline int dfn(int x){
+		assert(id[x<=rt?x:ds-n/x+1]==x);
+		return x<=rt?x:ds-n/x+1;
 	}
-	inline friend bool operator < (const pti &a,const pti &b){
-		return a.a==b.a?(a.b==b.b?(a.c<b.c):a.b<b.b):a.a<b.a;
-	}
-};
-map<pti,uint>cache[N];
-inline uint f(int a,int b,int c,int id){
-	int p=prime[id];
-	if(a<p&&b<p&&c<p){
-		return 1;
-	}
-	pti hush(a,b,c);
-	map<pti,uint>::iterator it=cache[id].find(hush);
-	if(it!=cache[id].end()){
-		return it->second;
-	}
-	uint ans=0;
-	for(int i=1;i<=a;i*=p){
-		for(int j=1;j<=b;j*=p){
-			for(int k=1;k<=c;k*=p){
-				ans+=f(a/i,b/j,c/k,id+1);
-			}
+	inline void map(int _n){
+		n=_n,ds=rt=0;
+		for(;rt*rt<n;rt++);
+		for(int l=1;l<=n;l++){
+			l=n/(n/l);
+			id[++ds]=l;
 		}
 	}
-	return cache[id][hush]=ans;
-}
+}A,B,C;
+uint f[sqrtN<<1][sqrtN<<1][sqrtN<<1];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("skyfall.in","r",stdin);
 	freopen("skyfall.out","w",stdout);
 #endif
-	sieve();
-	int A=ni,B=ni,C=ni;
-	if(A*B*C<N){
-		uint ans=0;
-		for(int i=1;i<=A;i++){
-			for(int j=1;j<=B;j++){
-				for(int k=1;k<=C;k++){
-					ans+=d[i*j*k];
+	A.map(ni),B.map(ni),C.map(ni);
+	sieve(max(max(A.n,B.n),C.n));
+	for(int i=1;i<=A.ds;i++){
+		for(int j=1;j<=B.ds;j++){
+			for(int k=1;k<=C.ds;k++){
+				f[i][j][k]=1;
+			}
+		}
+	}
+	for(int p=ps-1;p>=0;p--){
+		int cur=prime[p];
+		for(int i=A.ds;i>=1;i--){
+			for(int j=B.ds;j>=1;j--){
+				for(int k=B.ds;k>=1;k--){
+					uint tmp=0;
+					for(uint cnta=0,a=A.id[i];a>=1;cnta++,a/=cur){
+						for(uint cntb=0,b=B.id[j];b>=1;cntb++,b/=cur){
+							for(uint cntc=0,c=C.id[k];c>=1;cntc++,c/=cur){
+								tmp+=(cnta+cntb+cntc+1)*f[A.dfn(a)][B.dfn(b)][C.dfn(c)];
+							}
+						}
+					}
+					f[i][j][k]=tmp;
 				}
 			}
 		}
-		printf("%u\n",ans&((1u<<30)-1));
-		return 0;
 	}
-	printf("%u\n",f(A,B,C,0)&((1u<<30)-1));
+	printf("%u\n",f[A.ds][B.ds][C.ds]&((1u<<30)-1));
 }
