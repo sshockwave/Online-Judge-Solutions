ed5598f09fe5bf215506a4bc54ccf3dbf56dc3a7
Kerry Su <i_sukai@live.com>
2017-07-10 14:41:37 +0800

    Accepted.

--- EZOJ/Contests/1321/B.cpp
+++ EZOJ/Contests/1321/B.cpp
@@ -3,7 +3,6 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
-#include <map>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -23,57 +22,13 @@ inline void apmin(int &a,const int &b){
 }
 const int N=4010,D=26,INF=0x7f7f7f7f;
 char s[N],t[N];
-int f[N][N];
-struct incque{
-	struct rint{
-		int j;
-		rint(int _j):j(_j){}
-		inline friend bool operator < (const rint &a,const rint &b){
-			return a.j>b.j;
-		}
-	};
-	typedef map<rint,int>mii;
-	mii m;
-	inline void push(int i,int v){
-		mii::iterator it=m.find(rint(i));
-		if(it==m.end()){
-			m[rint(i)]=v;
-			it=m.find(rint(i));
-		}else{
-			if(it->second<=v){
-				return;
-			}
-			it->second=v;
-		}
-		while(true){
-			assert(it!=m.end());
-			if(it==m.begin()){
-				return;
-			}
-			it--;
-			if(it->second>=v){
-				m.erase(it);
-			}else{
-				return;
-			}
-			it=m.find(rint(i));
-		}
-	}
-	inline int ask(int i){//find j<i
-		mii::iterator it=m.upper_bound(rint(i));
-		if(it==m.end()){
-			return INF;
-		}
-		assert(it->first.j<i);
-		return it->second;
-	}
-}g[D][D];
+int f[N][N],_s[D],_t[D];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("string.in","r",stdin);
 	freopen("string.out","w",stdout);
 #endif
-	int a=ni,b=ni,c=ni,d=ni,ab=a+b;
+	int a=ni,b=ni,c=ni,d=ni;
 	scanf("%s%s",s+1,t+1);
 	int lens=strlen(s+1),lent=strlen(t+1);
 	memset(f,127,sizeof(f));
@@ -83,7 +38,9 @@ int main(){
 	for(int i=0;i<=lent;i++){
 		f[0][i]=i*a;
 	}
+	memset(_s,0,sizeof(_s));
 	for(int i=1;i<=lens;i++){
+		memset(_t,0,sizeof(_t));
 		for(int j=1;j<=lent;j++){
 			int &F=f[i][j];
 			apmin(F,f[i][j-1]+a);
@@ -93,24 +50,13 @@ int main(){
 			}else{
 				apmin(F,f[i-1][j-1]+c);
 			}
-			if(d>=ab){
-				continue;
+			int p=_s[t[j]-'a'],q=_t[s[i]-'a'];
+			if(p&&q){
+				apmin(F,f[p-1][q-1]+(j-q-1)*a+(i-p-1)*b+d);
 			}
-			int G=g[t[j]-'a'][s[i]-'a'].ask(j);
-			if(G<INF){
-				apmin(F,(lint)G+a*j+b*i+d-ab);
-			}
-			assert(F>=0);
+			_t[t[j]-'a']=j;
 		}
-		if(d>=ab){
-			continue;
-		}
-		if(i==lens){
-			break;
-		}
-		for(int j=1;j<=lent;j++){
-			g[s[i]-'a'][t[j]-'a'].push(j,f[i-1][j-1]-a*j-b*i);
-		}
+		_s[s[i]-'a']=i;
 	}
 	printf("%d\n",f[lens][lent]);
 }
