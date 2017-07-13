70cc6aed3a1181acb554ec78c6e2f1120a2f3bd3
Kerry Su <i_sukai@live.com>
2017-07-13 11:47:36 +0800

    WA. 50pts.

--- EZOJ/1619.cpp
+++ EZOJ/1619.cpp
@@ -5,65 +5,88 @@
 #include <cctype>
 using namespace std;
 typedef long long lint;
+#define cout cerr
 #define ni (next_num<int>())
-#define nl (next_num<lint>())
-template<typename T>inline T next_num(){
+template<class T>inline T next_num(){
 	T i=0;char c;
-	while(!isdigit(c=getchar()));
+	while(!isdigit(c=getchar())&&c!='-');
+	bool flag=c=='-';
+	flag?(c=getchar()):0;
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
-	return i;
+	return flag?-i:i;
 }
-const int N=100010;
-struct State{
-	State *lnk,*go[26];
-	int val;
-	State():lnk(0),val(0){
-		memset(go,0,sizeof(go));
-	}
-}pol[N*3],*pool;
+const int N=100010,D=26;
 struct SAM{
-	State ini,*tail;
-	SAM():tail(&ini){
-		pool=pol;
+	typedef SAM* node;
+	static SAM pol[N<<1],*pool,ini;
+	node lnk,go[D];
+	int val;
+	static inline void init(){
 		memset(pol,0,sizeof(pol));
 	}
-	inline void extend(int c){
-		State *p=tail;
-		tail=pool++;
-		tail->val=p->val+1;
+	inline node ext(int c){
+		node p=this,np=pool++;
+		np->val=val+1;
 		for(;p&&p->go[c]==0;p=p->lnk){
-			p->go[c]=tail;
+			p->go[c]=np;
 		}
 		if(p==0){
-			tail->lnk=&ini;
-			return;
+			np->lnk=&ini;
+			return np;
 		}
-		State *q=p->go[c];
+		node q=p->go[c];
 		if(q->val==p->val+1){
-			tail->lnk=q;
-			return;
+			np->lnk=q;
+			return np;
 		}
-		State *nq=pool++;
+		assert(q->val>p->val+1);
+		node nq=pool++;
 		*nq=*q;
 		nq->val=p->val+1;
-		tail->lnk=q->lnk=nq;
+		q->lnk=np->lnk=nq;
 		for(;p&&p->go[c]==q;p=p->lnk){
 			p->go[c]=nq;
 		}
+		return np;
 	}
 };
+SAM SAM::pol[N<<1],*SAM::pool=SAM::pol,SAM::ini;
+namespace T{
+	int son[N][D],n=1;
+	inline void init(){
+		memset(son,0,sizeof(son));
+	}
+	inline void ins(char s[]){
+		int x=1;
+		for(;*s;x=son[x][(*s++)-'a']){
+			if(son[x][(*s)-'a']==0){
+				son[x][(*s)-'a']=++n;
+			}
+		}
+	}
+	void dfs(int x,SAM* p){
+		for(int i=0;i<D;i++){
+			if(son[x][i]){
+				dfs(son[x][i],p->ext(i));
+			}
+		}
+	}
+}
 char s[N];
 int main(){
-	int ans=0;
+#ifndef ONLINE_JUDGE
+	freopen("substring.in","r",stdin);
+	freopen("substring.out","w",stdout);
+#endif
+	T::init(),SAM::init(),memset(&SAM::ini,0,sizeof(SAM));
 	for(int tot=ni;tot--;){
 		scanf("%s",s);
-		SAM sam;
-		for(int i=0;s[i];i++){
-			sam.extend(s[i]-'a');
-		}
-		for(State *p=pol;p<pool;p++){
-			ans+=p->val-p->lnk->val;
-		}
+		T::ins(s);
+	}
+	T::dfs(1,&SAM::ini);
+	int ans=0;
+	for(SAM* i=SAM::pol;i<SAM::pool;i++){
+		ans+=i->val-i->lnk->val;
 	}
 	printf("%d\n",ans);
 }
