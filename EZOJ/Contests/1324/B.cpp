e77cad908eb481687098feb711aff632a322b67d
Kerry Su <i_sukai@live.com>
2017-07-14 14:40:15 +0800

    Accepted. 1120ms

--- EZOJ/Contests/1324/B.cpp
+++ EZOJ/Contests/1324/B.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <queue>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -21,14 +22,66 @@ inline void apmax(int &a,const int &b){
 	}
 }
 const int N=100010;
-lint ans=0;
+struct SegmentTree{
+	typedef SegmentTree* node;
+	int lend,rend,mid;
+	node lson,rson;
+	int val,cnt,delta;
+	void build(int l,int r){
+		lend=l,rend=r,mid=(l+r)>>1;
+		val=0,cnt=r-l+1;
+		if(l!=r){
+			static node n=new SegmentTree[N<<1];
+			(lson=n++)->build(l,mid);
+			(rson=n++)->build(mid+1,r);
+		}
+	}
+	inline void up(){
+		if(lson->val<rson->val){
+			val=lson->val,cnt=lson->cnt;
+		}else if(lson->val>rson->val){
+			val=rson->val,cnt=rson->cnt;
+		}else{
+			val=lson->val,cnt=lson->cnt+rson->cnt;
+		}
+	}
+	inline void down(){
+		if(delta){
+			lson->val+=delta,lson->delta+=delta;
+			rson->val+=delta,rson->delta+=delta;
+			delta=0;
+		}
+	}
+	void add(int l,int r,int v){
+		if(lend==l&&rend==r){
+			val+=v,delta+=v;
+			return;
+		}
+		down();
+		if(r<=mid){
+			lson->add(l,r,v);
+		}else if(l>mid){
+			rson->add(l,r,v);
+		}else{
+			lson->add(l,mid,v);
+			rson->add(mid+1,r,v);
+		}
+		up();
+	}
+	inline int ask(){
+		return val?0:cnt;
+	}
+}seg;
 int n,color[N];
-bool vis[N];
+lint ans=0;
+queue<int>q[N];
 namespace T{
 	const int E=N<<1;
 	int to[E],bro[E],head[N],e=0;
+	int _c[N];
 	inline void init(){
 		memset(head,-1,sizeof(head));
+		memset(_c,0,sizeof(_c));
 	}
 	inline void ae(const int &u,const int &v){
 		to[e]=v,bro[e]=head[u],head[u]=e++;
@@ -36,31 +89,43 @@ namespace T{
 	inline void add(const int &u,const int &v){
 		ae(u,v),ae(v,u);
 	}
-	int dfs(int x){
-		if(vis[color[x]]){
-			return 0;
+	int dfn[N]={0},dfe[N],tim=0;
+	void dfs(int x,int e){
+		static int stk[N],ss=0;
+		int c=color[x],old=_c[c],pre=0;
+		dfn[x]=++tim,stk[++ss]=x,_c[c]=ss;
+		if(old){
+			pre=stk[old+1];
+			seg.add(1,dfn[pre]-1,1);
 		}
-		vis[color[x]]=true;
-		int ans=1;
+		queue<int>tmp;
+		for(int i;!q[c].empty();tmp.push(i),q[c].pop()){
+			i=q[c].front();
+			if(dfn[i]>=dfn[pre]){
+				seg.add(dfn[i],dfe[i],1);
+			}
+		}
+		ans+=seg.ask()-(n-tim);
 		for(int i=head[x];~i;i=bro[i]){
-			ans+=dfs(to[i]);
+			if(i!=e){
+				dfs(to[i],i^1);
+				for(int u;!q[c].empty();q[c].pop()){
+					u=q[c].front();
+					seg.add(dfn[u],dfe[u],1);
+				}
+			}
 		}
-		vis[color[x]]=false;
-		return ans;
-	}
-}
-namespace task3{
-	int pre[N];
-	inline lint solve(int n){
-		memset(pre,0,sizeof(pre));
-		lint ans=0;
-		int cur=0;
-		for(int i=1;i<=n;i++){
-			apmax(cur,pre[color[i]]);
-			ans+=i-cur;
-			pre[color[i]]=i;
+		dfe[x]=tim,ss--,_c[c]=old;
+		if(old){
+			seg.add(1,dfn[pre]-1,-1);
 		}
-		return ans;
+		for(int i;!tmp.empty();q[c].push(i),tmp.pop()){
+			i=tmp.front();
+			if(dfn[i]>=dfn[pre]){
+				seg.add(dfn[i],dfe[i],-1);
+			}
+		}
+		q[c].push(x);
 	}
 }
 int main(){
@@ -76,15 +141,7 @@ int main(){
 	for(int i=1;i<n;i++){
 		T::add(ni,ni);
 	}
-	if(n>=5000){
-		printf("%lld\n",task3::solve(n));
-		return 0;
-	}
-	lint ans=0;
-	memset(vis,0,sizeof(vis));
-	for(int i=1;i<=n;i++){
-		ans+=T::dfs(i);
-	}
-	printf("%lld\n",(ans-n)/2+n);
+	seg.build(1,n),T::dfs(1,-1);
+	printf("%lld\n",ans);
 	return 0;
 }
