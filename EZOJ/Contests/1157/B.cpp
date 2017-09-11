a62332c6c47fdcf5a22c6cbc4527c59f01f62368
Kerry Su <i_sukai@live.com>
2017-09-11 15:42:50 +0800

    Accepted.

--- EZOJ/Contests/1157/B.cpp
+++ EZOJ/Contests/1157/B.cpp
@@ -16,41 +16,62 @@ template<class T>inline T next_num(){
 	return flag?-i:i;
 }
 const int N=500010;
-int a[N];
-inline int work(){
-	int l=ni,r=ni;
-	if(l>r){
+namespace seg{
+	struct Node{
+		Node *lson,*rson;
+		int cnt;
+	}pool[N*20];
+	typedef Node* node;
+	inline void init(){
+		pool[0]=(Node){pool,pool,0};
+	}
+	inline node renew(node x){
+		static node n=pool;
+		*(++n)=*x;
+		return n;
+	}
+	inline node alt(node x,int l,int r,int p){
+		x=renew(x);
+		x->cnt++;
+		if(l!=r){
+			int mid=(l+r)>>1;
+			if(p<=mid){
+				x->lson=alt(x->lson,l,mid,p);
+			}else{
+				x->rson=alt(x->rson,mid+1,r,p);
+			}
+		}
+		return x;
+	}
+	inline int sub(node x,node y,int l,int r,int v){
+		if(l==r){
+			return y->cnt-x->cnt>v?l:0;
+		}
+		int mid=(l+r)>>1;
+		if(y->lson->cnt-x->lson->cnt>v){
+			return sub(x->lson,y->lson,l,mid,v);
+		}
+		if(y->rson->cnt-x->rson->cnt>v){
+			return sub(x->rson,y->rson,mid+1,r,v);
+		}
 		return 0;
 	}
-	int num=-1,cnt=1;
-	for(int i=l;i<=r;i++){
-		if(num!=a[i]){
-			cnt--;
-			if(cnt==0){
-				num=a[i];
-				cnt=1;
-			}
-		}else{
-			cnt++;
-		}
-	}
-	cnt=0;
-	for(int i=l;i<=r;i++){
-		cnt+=a[i]==num;
-	}
-	return cnt>(r-l+1)/2?num:0;
 }
+seg::node rt[N];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("kur.in","r",stdin);
 	freopen("kur.out","w",stdout);
 #endif
 	int n=ni,tot=ni;
+	seg::init();
+	rt[0]=seg::pool;
 	for(int i=1;i<=n;i++){
-		a[i]=ni;
+		rt[i]=seg::alt(rt[i-1],1,n,ni);
 	}
 	while(tot--){
-		printf("%d\n",work());
+		int l=ni,r=ni;
+		printf("%d\n",seg::sub(rt[l-1],rt[r],1,n,(r-l+1)>>1));
 	}
 	return 0;
 }
