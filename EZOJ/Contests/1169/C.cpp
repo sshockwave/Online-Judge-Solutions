d5bebf96884f2e93644611988b701154299342aa
Kerry Su <i_sukai@live.com>
2017-09-14 07:28:16 +0800

    Accepted. 84ms

--- EZOJ/Contests/1169/C.cpp
+++ EZOJ/Contests/1169/C.cpp
@@ -34,8 +34,12 @@ struct stat{
 		}
 		return ret;
 	}
-	inline int sum(){
-		return a[0]+a[1]+a[2]+a[3];
+	inline friend stat operator + (const stat &a,const stat &b){
+		stat c;
+		for(int i=0;i<4;i++){
+			c.a[i]=a.a[i]+b.a[i];
+		}
+		return c;
 	}
 	inline friend stat operator - (const stat &a,const stat &b){
 		stat c;
@@ -56,14 +60,12 @@ struct stat{
 			a[i]+=b.a[i];
 		}
 	}
-}st[N];
+};
 namespace T{
 	const int E=N<<1;
 	int to[E],bro[E],len[E],head[N],e;
-	bool vis[N];
 	inline void init(int n){
 		memset(head+1,-1,n<<2);
-		memset(vis+1,0,n);
 		e=0;
 	}
 	inline void ae(int u,int v,int w){
@@ -72,54 +74,22 @@ namespace T{
 	inline void add(int u,int v,int w){
 		ae(u,v,w),ae(v,u,w);
 	}
-	stat dfs(int x){
-		vis[x]=true;
-		stat ans=(stat){1,0,0,0};
+	stat size[N];
+	void dfs1(int x,int f){
+		size[x]=(stat){1,0,0,0};
 		for(int i=head[x],v;~i;i=bro[i]){
-			if(!vis[v=to[i]]){
-				ans+=dfs(v).shift(len[i]);
+			if((v=to[i])!=f){
+				dfs1(v,x);
+				size[x]+=size[v].shift(len[i]);
 			}
 		}
-		vis[x]=false;
-		return ans;
 	}
-	int bal[N],g;
-	int gg(int x,int n){
-		bal[x]=0;
-		vis[x]=true;
-		int sz=1;
+	void dfs2(int x,int f,stat oth){
+		oth+=size[x];
+		ans+=oth.a[0];
 		for(int i=head[x],v;~i;i=bro[i]){
-			if(!vis[v=to[i]]){
-				int t=gg(v,n);
-				sz+=t;
-				apmax(bal[x],t);
-			}
-		}
-		apmax(bal[x],n-sz);
-		if(bal[x]<bal[g]){
-			g=x;
-		}
-		vis[x]=false;
-		return sz;
-	}
-	void solve(int x,int n){
-		g=x,gg(x,n),x=g;
-		vis[x]=true;
-		st[x]=(stat){1,0,0,0};
-		for(int i=head[x],v;~i;i=bro[i]){
-			if(!vis[v=to[i]]){
-				st[x]+=st[v]=dfs(v).shift(len[i]);
-			}
-		}
-		for(int i=head[x],v;~i;i=bro[i]){
-			if(!vis[v=to[i]]){
-				ans+=(st[x]-st[v])*st[v];
-			}
-		}
-		ans+=st[x].a[0];
-		for(int i=head[x],v;~i;i=bro[i]){
-			if(!vis[v=to[i]]){
-				solve(v,st[v].sum());
+			if((v=to[i])!=f){
+				dfs2(v,x,(oth-size[v].shift(len[i])).shift(len[i]));
 			}
 		}
 	}
@@ -136,9 +106,9 @@ int main(){
 			T::add(u,v,ni%4);
 		}
 		ans=0;
-		T::solve(1,n);
-		int t=n*n,d=gcd(ans,t);
-		printf("%d/%d\n",ans/d,t/d);
+		T::dfs1(1,0),T::dfs2(1,0,(stat){0,0,0,0});
+		int g=n*n,d=gcd(g,ans);
+		printf("%d/%d\n",ans/d,g/d);
 	}
 	return 0;
 }
