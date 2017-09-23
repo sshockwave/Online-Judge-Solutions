023ade3b62671fcd2c3ed0560ec0f2505fbb791a
Kerry Su <i_sukai@live.com>
2017-09-23 13:43:25 +0800

    Accepted. 260ms

--- EZOJ/Contests/1043/A.cpp
+++ EZOJ/Contests/1043/A.cpp
@@ -22,19 +22,11 @@ template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
 		a=b;
 	}
 }
+template<class T>inline T sqr(T x){
+	return x*x;
+}
 const int N=25;
-const double EPS=1e-8;
-struct Point{
-	double x,y;
-	inline friend Point operator - (const Point &a,const Point &b){
-		return (Point){a.x-b.x,a.y-b.y};
-	}
-	inline double d2(){
-		return x*x+y*y;
-	}
-}pt[N];
 int n,es=0;
-double dis[N][N],dis2[N][N];
 int fa[N];
 inline int root(int x){
 	if(fa[x]==0){
@@ -45,132 +37,51 @@ inline int root(int x){
 struct Edge{
 	int u,v;
 	double w;
-	bool vis;
 	inline friend bool operator < (const Edge &a,const Edge &b){
 		return a.w<b.w;
 	}
 }e[N*N];
-inline double calc(double sumx){
+double dis[N][N],dis2[N][N];
+inline double work(double avg){
 	for(int i=1;i<=es;i++){
-		e[i].w=dis2[e[i].u][e[i].v]*(n-1)-dis[e[i].u][e[i].v]*sumx;
-		e[i].vis=false;
+		e[i].w=sqr(dis[e[i].u][e[i].v]-avg);
 	}
 	sort(e+1,e+es+1);
 	memset(fa+1,0,n<<2);
-	double asum=0;
-	for(int i=1;i<=es;i++){
-		int u=root(e[i].u),v=root(e[i].v);
-		if(u!=v){
-			e[i].vis=true;
-			asum+=dis[e[i].u][e[i].v];
-			fa[u]=v;
-		}
-	}
-	return asum;
-}
-namespace task1{
-	double ans,val1,val2;
-	bool con[N][N];
-	int fa[N];
-	inline int root(int x){
-		if(fa[x]==0){
-			return x;
-		}
-		return fa[x]=root(fa[x]);
-	}
-	inline bool check(){
-		memset(fa+1,0,n<<2);
-		for(int i=1;i<=n;i++){
-			for(int j=1;j<i;j++){
-				if(con[i][j]){
-					int u=root(i),v=root(j);
-					if(u==v){
-						return false;
-					}
-					fa[u]=v;
-				}
-			}
-		}
-		for(int i=1;i<=n;i++){
-			if(root(i)!=root(1)){
-				return false;
-			}
-		}
-		return true;
-	}
-	void dfs(int x,int y,int cnt){
-		if(x==y){
-			x++,y=1;
-		}
-		if(x>n){
-			if(cnt==0&&check()){
-				apmin(ans,val1*(n-1)-val2*val2);
-			}
-			return;
-		}
-		if(cnt){
-			con[x][y]=con[y][x]=true,val1+=dis2[x][y],val2+=dis[x][y];
-			dfs(x,y+1,cnt-1);
-			con[x][y]=con[y][x]=false,val1-=dis2[x][y],val2-=dis[x][y];
-		}
-		dfs(x,y+1,cnt);
-	}
-	inline double Main(){
-		for(int i=1;i<=n;i++){
-			for(int j=1;j<=n;j++){
-				if(i!=j){
-					dis2[i][j]=(pt[i]-pt[j]).d2();
-					dis[i][j]=sqrt(dis2[i][j]);
-				}
-			}
-		}
-		ans=1e100;
-		memset(con,0,sizeof(con));
-		val1=0,val2=0;
-		dfs(1,1,n-1);
-		return sqrt(ans)/(n-1);
-	}
-}
-inline double Main(){
-	n=ni,es=0;
-	for(int i=1;i<=n;i++){
-		scanf("%lf",&pt[i].x);
-	}
-	for(int i=1;i<=n;i++){
-		scanf("%lf",&pt[i].y);
-	}
-	if(n<=5){
-		return task1::Main();
-	}
-	double l=0,r=0,m;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			dis2[i][j]=(pt[i]-pt[j]).d2();
-			dis[i][j]=sqrt(dis2[i][j]);
-			r+=dis[i][j];
-			if(i<j){
-				e[++es]=(Edge){i,j};
-			}
-		}
-	}
-	assert(es==n*(n-1)/2);
-	while(r-l>EPS){
-		m=(l+r)/2;
-		if(calc(m)<m){//tocheck
-			l=m;
-		}else{
-			r=m;
-		}
-	}
-	calc(l);
 	double val1=0,val2=0;
-	for(int i=1;i<=n;i++){
-		if(e[i].vis){
+	for(int i=1;i<=es;i++){
+		if(root(e[i].u)!=root(e[i].v)){
+			fa[root(e[i].u)]=root(e[i].v);
 			val1+=dis2[e[i].u][e[i].v];
 			val2+=dis[e[i].u][e[i].v];
 		}
 	}
-	return sqrt(max((n-1)*val1-val2*val2,0.0))/(n-1);
+	return val1*(n-1)-val2*val2;
+}
+double x[N],y[N];
+inline double Main(){
+	n=ni,es=0;
+	for(int i=1;i<=n;i++){
+		scanf("%lf",x+i);
+	}
+	for(int i=1;i<=n;i++){
+		scanf("%lf",y+i);
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<i;j++){
+			dis2[i][j]=sqr(x[i]-x[j])+sqr(y[i]-y[j]);
+			dis[i][j]=sqrt(dis2[i][j]);
+			e[++es]=(Edge){i,j};
+		}
+	}
+	assert(es==n*(n-1)/2);
+	double ans=1e100;
+	for(int i=1;i<=es;i++){
+		for(int j=1;j<=i;j++){
+			apmin(ans,work((dis[e[i].u][e[i].v]+dis[e[j].u][e[j].v])/2));
+		}
+	}
+	return sqrt(ans)/(n-1);
 }
 int main(){
 #ifndef ONLINE_JUDGE
