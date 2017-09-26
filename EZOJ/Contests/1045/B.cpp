4a72eb6278621bd8f6176025c9cef155d8c8dd52
Kerry Su <i_sukai@live.com>
2017-09-26 18:50:10 +0800

    Accepted. 86ms

--- EZOJ/Contests/1045/B.cpp
+++ EZOJ/Contests/1045/B.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <vector>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -15,7 +16,7 @@ template<class T>inline T next_num(){
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return flag?-i:i;
 }
-const int MOD=1000000007;
+const int N=1000110,MOD=1000000007;
 inline int add(const int &a,const int &b){
 	return (a+b)%MOD;
 }
@@ -53,32 +54,63 @@ struct Mat{
 			putchar('\n');
 		}
 	}
-}T[111],ans;
-int a[1000100];
+}T[111][20],ans,id;
+int a[N],b[N],x[111];
+inline void pushmat(int n){
+	static int cnt[11]={0};
+	if(1<=n&&n<=10){
+		cnt[n]++;
+	}else{
+		for(int i=1;i<=10;i++){
+			for(int j=0;j<20;j++){
+				if((cnt[i]>>j)&1){
+					ans=ans*T[i][j];
+				}
+			}
+			cnt[i]=0;
+		}
+		ans=ans*T[n][0];
+	}
+}
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("matrix0.in","r",stdin);
 	freopen("matrix.out","w",stdout);
 #endif
+	memset(&id,0,sizeof(id));
+	for(int i=0;i<10;i++){
+		id.a[i][i]=1;
+	}
 	for(int i=1;i<=10;i++){
-		T[i].in();
+		T[i][0].in();
+		for(int j=0;j<19;j++){
+			T[i][j+1]=T[i][j]*T[i][j];
+		}
 	}
 	int n=ni;
 	for(int i=1;i<=n;i++){
 		a[i]=ni;
 	}
-	for(int i=1;i<=100;i++,n++){
-		int x=ni+1;
-		for(int j=n;j>=x;j--){
-			a[j+1]=a[j];
-		}
-		a[x]=i+10;
-		T[i+10].in();
+	memset(b,0,sizeof(b));
+	for(int i=11;i<=110;i++){
+		x[i]=ni;
+		T[i][0].in();
 	}
-	ans=T[a[1]];
-	for(int i=2;i<=n;i++){
-		ans=ans*T[a[i]];
+	for(int i=110;i>=11;i--){
+		x[i]++;
+		for(int j=i+1;j<=110&&x[j]<=x[i];x[i]++,j++);
+		b[x[i]]=i;
+		for(int j=i;j<110&&x[j]>x[j+1];swap(x[j],x[j+1]),j++);
 	}
+	for(int i=1,j=1;i<=n;i++){
+		for(;b[j];j++);
+		b[j]=a[i];
+	}
+	ans=id;
+	for(int i=1,ti=n+100;i<=ti;i++){
+		pushmat(b[i]);
+	}
+	T[0][0]=id,pushmat(0);
 	ans.print();
 	return 0;
 }
