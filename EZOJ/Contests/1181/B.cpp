79308a8dc8d647093dc05a8b349e8b5c05f42d8a
Kerry Su <i_sukai@live.com>
2017-10-03 12:44:23 +0800

    WA. 60pts

--- EZOJ/Contests/1181/B.cpp
+++ EZOJ/Contests/1181/B.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <algorithm>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -15,28 +16,62 @@ template<class T>inline T next_num(){
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return flag?-i:i;
 }
-const int N=100010;
-int a[N];
+const int N=100010,K=1000010;
+int a[N],rt;
+struct Query{
+	int l,r;
+	lint ans;
+}q[N];
+inline bool lcmp(int a,int b){
+	int lb=q[a].l/rt,rb=q[b].l/rt;
+	return lb!=rb?lb<rb:q[a].r<q[b].r;
+}
+lint ans=0;
+int lst[N];
+int org[K],trs[K];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("xor.in","r",stdin);
 	freopen("xor.out","w",stdout);
 #endif
 	int n=ni,tot=ni,k=ni;
+	for(rt=0;rt*rt<n;rt++);
+	a[0]=0;
 	for(int i=1;i<=n;i++){
-		a[i]=ni;
+		a[i]=ni^a[i-1];
 	}
-	while(tot--){
-		int l=ni,r=ni;
-		lint cnt=0;
-		for(int i=l;i<=r;i++){
-			int sum=0;
-			for(int j=i;j<=r;j++){
-				sum^=a[j];
-				cnt+=sum==k;
-			}
+	for(int i=1;i<=tot;i++){
+		q[i]=(Query){ni-1,ni};
+		lst[i]=i;
+	}
+	sort(lst+1,lst+tot+1,lcmp);
+	int l=1,r=0;
+	memset(org,0,sizeof(org));
+	memset(trs,0,sizeof(trs));
+	for(int i=1;i<=tot;i++){
+		int L=q[lst[i]].l,R=q[lst[i]].r;
+		for(;l<L;l++){
+			ans-=trs[a[l]];
+			org[a[l]]--,trs[a[l]^k]--;
 		}
-		printf("%lld\n",cnt);
+		for(;r>R;r--){
+			ans-=org[a[r]^k];
+			org[a[r]]--,trs[a[r]^k]--;
+		}
+		for(;l>L;){
+			l--;
+			ans+=trs[a[l]];
+			org[a[l]]++,trs[a[l]^k]++;
+		}
+		for(;r<R;){
+			r++;
+			ans+=org[a[r]^k];
+			org[a[r]]++,trs[a[r]^k]++;
+		}
+		q[lst[i]].ans=ans;
+	}
+	for(int i=1;i<=tot;i++){
+		printf("%lld\n",q[i].ans);
 	}
 	return 0;
 }
