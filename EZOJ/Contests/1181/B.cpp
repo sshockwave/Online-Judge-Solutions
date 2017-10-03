29ba3028fcf33c57a28476ee3642d0a050c63a7b
Kerry Su <i_sukai@live.com>
2017-10-03 13:05:40 +0800

    Accepted. 543ms

--- EZOJ/Contests/1181/B.cpp
+++ EZOJ/Contests/1181/B.cpp
@@ -16,8 +16,8 @@ template<class T>inline T next_num(){
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return flag?-i:i;
 }
-const int N=100010,K=1000010;
-int a[N],rt;
+const int N=100010,K=2000010;
+int a[N],rt,k;
 struct Query{
 	int l,r;
 	lint ans;
@@ -27,14 +27,20 @@ inline bool lcmp(int a,int b){
 	return lb!=rb?lb<rb:q[a].r<q[b].r;
 }
 lint ans=0;
-int lst[N];
-int org[K],trs[K];
+int lst[N],cnt[K];
+inline void inc(int x){
+	ans+=cnt[x^k],cnt[x]++;
+}
+inline void dec(int x){
+	cnt[x]--,ans-=cnt[x^k];
+}
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("xor.in","r",stdin);
 	freopen("xor.out","w",stdout);
 #endif
-	int n=ni,tot=ni,k=ni;
+	int n=ni,tot=ni;
+	k=ni;
 	for(rt=0;rt*rt<n;rt++);
 	a[0]=0;
 	for(int i=1;i<=n;i++){
@@ -46,28 +52,13 @@ int main(){
 	}
 	sort(lst+1,lst+tot+1,lcmp);
 	int l=1,r=0;
-	memset(org,0,sizeof(org));
-	memset(trs,0,sizeof(trs));
+	memset(cnt,0,sizeof(cnt));
 	for(int i=1;i<=tot;i++){
 		int L=q[lst[i]].l,R=q[lst[i]].r;
-		for(;l<L;l++){
-			ans-=trs[a[l]];
-			org[a[l]]--,trs[a[l]^k]--;
-		}
-		for(;r>R;r--){
-			ans-=org[a[r]^k];
-			org[a[r]]--,trs[a[r]^k]--;
-		}
-		for(;l>L;){
-			l--;
-			ans+=trs[a[l]];
-			org[a[l]]++,trs[a[l]^k]++;
-		}
-		for(;r<R;){
-			r++;
-			ans+=org[a[r]^k];
-			org[a[r]]++,trs[a[r]^k]++;
-		}
+		for(;l>L;inc(a[--l]));
+		for(;r<R;inc(a[++r]));
+		for(;l<L;dec(a[l++]));
+		for(;r>R;dec(a[r--]));
 		q[lst[i]].ans=ans;
 	}
 	for(int i=1;i<=tot;i++){
