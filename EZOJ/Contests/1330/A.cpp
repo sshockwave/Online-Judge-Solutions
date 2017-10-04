fe77a1edfda7b91fa8388a1228414e9643c8bce6
Kerry Su <i_sukai@live.com>
2017-10-04 14:05:35 +0800

    Accepted. 142ms.

--- EZOJ/Contests/1330/A.cpp
+++ EZOJ/Contests/1330/A.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <queue>
 #include <algorithm>
 using namespace std;
 typedef long long lint;
@@ -16,30 +17,42 @@ template<class T>inline T next_num(){
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return flag?-i:i;
 }
-const int N=1010;
+const int N=100010;
 int gcd(int a,int b){
 	return b?gcd(b,a%b):a;
 }
-struct Itsect{
-	int u,v;
-	int a,b;
-	inline void sani(){
+int pre[N],nxt[N];
+int L,d[N],v[N];
+struct icmp{
+	int x,y,a,b;
+	icmp(int _x){
+		y=nxt[x=_x];
+		if(v[x]<v[y]){
+			a=(d[x]-d[y]+L)%L;
+			b=v[y]-v[x];
+		}else{
+			a=(d[y]-d[x]+L)%L;
+			b=v[x]-v[y];
+		}
 		int g=gcd(a,b);
 		a/=g,b/=g;
 	}
-	inline friend bool operator < (const Itsect &a,const Itsect &b){
-		return (lint)a.a*b.b<(lint)b.a*a.b;
+	inline friend bool operator < (const icmp &a,const icmp &b){
+		return (lint)a.a*b.b>(lint)b.a*a.b;
 	}
-}lst[N*N];
-int d[N],vc[N];
-bool vis[N];
+};
+priority_queue<icmp>q;
+int lst[N];
+inline bool lcmp(int a,int b){
+	return d[a]<d[b];
+}
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("race.in","r",stdin);
 	freopen("race.out","w",stdout);
 #endif
-	memset(vis,0,sizeof(vis));
-	int n=ni,L=ni;
+	int n=ni;
+	L=ni;
 	if(n==1){
 		puts("0");
 		return 0;
@@ -48,26 +61,31 @@ int main(){
 		d[i]=ni;
 	}
 	for(int i=1;i<=n;i++){
-		vc[i]=ni;
+		v[i]=ni;
+		lst[i-1]=i;
 	}
-	int ss=0;
+	sort(lst,lst+n,lcmp);
+	for(int i=0;i<n;i++){
+		pre[nxt[lst[i]]=lst[(i+1)%n]]=lst[i];
+	}
 	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			if(vc[i]>vc[j]){
-				lst[ss]=(Itsect){i,j,(d[j]-d[i]+L)%L,vc[i]-vc[j]};
-				lst[ss++].sani();
-			}
+		q.push(icmp(i));
+	}
+	for(int i=2;i<n;i++){
+		for(;nxt[q.top().x]!=q.top().y;q.pop());
+		int x=q.top().x,y=nxt[x];
+		q.pop();
+		if(x<y){
+			nxt[pre[y]=pre[x]]=y;
+			pre[x]=nxt[x]=0;
+			q.push(icmp(pre[y]));
+		}else{
+			pre[nxt[x]=nxt[y]]=x;
+			pre[y]=nxt[y]=0;
+			q.push(icmp(x));
 		}
 	}
-	sort(lst,lst+ss);
-	int last=0;
-	memset(vis,0,sizeof(vis));
-	for(int i=0;i<ss;i++){
-		if(!vis[lst[i].u]&&!vis[lst[i].v]){
-			vis[min(lst[i].u,lst[i].v)]=true;
-			last=i;
-		}
-	}
-	printf("%d/%d\n",lst[last].a,lst[last].b);
+	for(;nxt[q.top().x]!=q.top().y;q.pop());
+	printf("%d/%d\n",q.top().a,q.top().b);
 	return 0;
 }
