b9636dc39ab72c253542dc2a5d7fb69ee21339cd
Kerry Su <i_sukai@live.com>
2017-10-12 15:03:55 +0800

    Accepted. 182ms

--- EZOJ/Contests/1335/A.cpp
+++ EZOJ/Contests/1335/A.cpp
@@ -16,63 +16,59 @@ template<class T>inline T next_num(){
 	return flag?-i:i;
 }
 const int N=55,L=20;
-char str[N][L];
+char str[N][L+1];
 lint dfi[1<<L];
 int dficnt[1<<L];
 double f[1<<L];
-int cnt[128],tag[128],tim=0;
+int bcnt[1<<L];
+inline int bitcnt(lint x){
+	static const int A=(1<<L)-1,L2=L<<1;
+	return bcnt[x&A]+bcnt[(x>>L)&A]+bcnt[(x>>L2)&A];
+}
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("memory.in","r",stdin);
 	freopen("memory.out","w",stdout);
 #endif
-	int n=ni,l;
+	int n=ni;
 	for(int i=0;i<n;i++){
 		scanf("%s",str[i]);
 	}
-	l=strlen(str[0]);
-	dfi[0]=0;
-	dficnt[0]=0;
-	memset(tag,0,sizeof(tag));
-	for(int s=1,ts=1<<l;s<ts;s++){
-		int x=s&-s,sh=0;
-		lint last=dfi[s^x];
-		for(;(1<<sh)<x;sh++);
-		tim++;
-		for(int i=0;i<n;i++){
-			if(((last>>i)&1)==0){
-				if(tag[str[i][sh]]<tim){
-					tag[str[i][sh]]=tim;
-					cnt[str[i][sh]]=0;
-				}
-				cnt[str[i][sh]]++;
+	int m=strlen(str[0]),shm=1<<m;
+	memset(dfi,0,sizeof(dfi));
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(i==j){
+				continue;
 			}
-		}
-		dfi[s]=last;
-		dficnt[s]=dficnt[s^x];
-		for(int i=0;i<n;i++){
-			if(((last>>i)&1)==0){
-				if(cnt[str[i][sh]]==1){
-					dfi[s]|=1ll<<i;
-					dficnt[s]++;
-				}
+			int tmp=0;
+			for(int k=0;k<m;k++){
+				tmp|=(str[i][k]==str[j][k])<<k;
 			}
+			dfi[tmp]|=1ll<<i;
 		}
 	}
-	for(int s=(1<<l)-1;s>=0;s--){
+	bcnt[0]=0;
+	for(int s=1,ts=1<<L;s<ts;s++){
+		bcnt[s]=bcnt[s^(s&-s)]+1;
+	}
+	for(int s=shm-1;s>=0;s--){
+		for(int t=s;t;t^=t&-t){
+			dfi[s^(t&-t)]|=dfi[s];
+		}
+		dfi[s]^=(1ll<<n)-1;
+		dficnt[s]=bitcnt(dfi[s]);
+	}
+	for(int s=shm-1;s>=0;s--){
 		f[s]=0;
-		if(dficnt[s]!=n){
-			int totunvis=0;
-			for(int i=0;i<l;i++){
-				if(((s>>i)&1)==0){
-					totunvis++;
-					int toS=s|(1<<i);
-					if(dficnt[toS]<n){
-						f[s]+=(double)(n-dficnt[toS])/(n-dficnt[s])*f[toS];
-					}
+		if(dficnt[s]<n){
+			for(int t=s,x;t<shm;t|=x){
+				x=(t+1)&-(t+1);
+				if(dficnt[s|x]<n){
+					f[s]+=(double)(n-dficnt[s|x])/(n-dficnt[s])*f[s|x];
 				}
 			}
-			f[s]=f[s]/totunvis+1;
+			f[s]=f[s]/bcnt[s^(shm-1)]+1;
 		}
 	}
 	printf("%.10lf\n",f[0]);
