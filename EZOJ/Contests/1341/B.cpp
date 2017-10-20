0621e033cbc047284f798966e18812251679f0e5
Kerry Su <i_sukai@live.com>
2017-10-20 12:32:43 +0800

    Accepted. 77ms

--- EZOJ/Contests/1341/B.cpp
+++ EZOJ/Contests/1341/B.cpp
@@ -16,7 +16,7 @@ template<class T>inline T next_num(){
 	return flag?-i:i;
 }
 const int N=200010;
-int vec[2][N],vs=0;
+int stk[N],ss=0;
 int a[N],p[N];
 int main(){
 #ifndef ONLINE_JUDGE
@@ -28,30 +28,22 @@ int main(){
 		a[i]=ni;
 	}
 	a[0]=a[n]=0;
-	for(int i=n;i>=1;i--){
-		a[i]-=a[i-1];
-	}
 	for(int i=1;i<=n;i++){
-		if(a[i]==2){
-			vec[0][vs]=i;
-			vec[1][vs]=i;
-			vs++;
-		}else if(a[i]==-2){
-			if(vs==0){
-				puts("No");
-				return 0;
-			}
-			vs--;
-			p[vec[0][vs]]=i;
-			p[i]=vec[1][vs];
-		}else if(a[i]==0){
+		int d=a[i]-a[i-1];
+		if(d==2){
+			stk[ss++]=i;
+		}else if(d==0){
 			p[i]=i;
+		}else if(ss&&d==-2){
+			ss--;
+			p[stk[ss]]=i;
+			p[i]=stk[ss];
 		}else{
 			puts("No");
 			return 0;
 		}
 	}
-	if(vs){
+	if(ss){
 		puts("No");
 		return 0;
 	}
