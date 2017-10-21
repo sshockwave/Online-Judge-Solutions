ce8a668d0bba00da6df188a157ae4ce5ff19d848
Kerry Su <i_sukai@live.com>
2017-10-21 19:10:07 +0800

    TLE. 50pts

--- EZOJ/Contests/1342/C.cpp
+++ EZOJ/Contests/1342/C.cpp
@@ -20,18 +20,16 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
 		a=b;
 	}
 }
-template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
-	if(b<a){
-		a=b;
-	}
-}
 const int N=200010,INF=0x7f7f7f7f;
+int ans=0;
 namespace T{
 	const int E=N<<2;
-	int to[E],bro[E],dhead[N],mhead[N],e=0;
+	int to[E],bro[E],mhead[N],dhead[N],e=0;
+	int fa[N],son[N],size[N],dep[N],top[N];
+	int dfn[N],idx[N],tim=0;
 	inline void init(){
-		memset(dhead,-1,sizeof(dhead));
 		memset(mhead,-1,sizeof(mhead));
+		memset(dhead,-1,sizeof(dhead));
 	}
 	inline void ae(int u,int v,int *head){
 		to[e]=v,bro[e]=head[u],head[u]=e++;
@@ -39,45 +37,61 @@ namespace T{
 	inline void add(int u,int v,int *head){
 		ae(u,v,head),ae(v,u,head);
 	}
-	int fa[N],dep[N],hei[N];
-	void dfs(int x){
-		hei[x]=0;
+	void dfs1(int x){
+		size[x]=1;
 		for(int i=mhead[x],v;~i;i=bro[i]){
 			if((v=to[i])!=fa[x]){
 				fa[v]=x;
 				dep[v]=dep[x]+1;
-				dfs(v);
-				apmax(hei[x],hei[v]+1);
+				dfs1(v);
+				if(size[v]>size[son[x]]){
+					son[x]=v;
+				}
 			}
 		}
 	}
-}
-namespace Subtask1{
-	const int N=1010;
-	int f[N][N][2];//0:deer 1:man
-	inline void init(){
-		memset(f,-1,sizeof(f));
-	}
-	int F(int x,int y,bool d){
-		if(x==y){
-			return 0;
-		}
-		if(f[x][y][d]>=0){
-			return f[x][y][d];
-		}
-		if(f[x][y][d]==-2){
-			throw -1;
-		}
-		f[x][y][d]=-2;
-		int ans=d?INF:0;
-		for(int i=d?T::mhead[y]:T::dhead[x];~i;i=T::bro[i]){
-			if(d){
-				apmin(ans,F(x,T::to[i],0));
-			}else{
-				apmax(ans,F(T::to[i],y,1));
+	void dfs2(int x){
+		idx[dfn[x]=++tim]=x;
+		top[x]=son[fa[x]]==x?top[fa[x]]:x;
+		if(son[x]){
+			dfs2(son[x]);
+			for(int i=mhead[x],v;~i;i=bro[i]){
+				if((v=to[i])!=fa[x]&&v!=son[x]){
+					dfs2(v);
+				}
 			}
 		}
-		return f[x][y][d]=ans;
+	}
+	inline int lca(int u,int v){
+		for(;top[u]!=top[v];dep[top[u]]>dep[top[v]]?(u=fa[top[u]]):(v=fa[top[v]]));
+		return dep[u]<dep[v]?u:v;
+	}
+	inline int dis(int u,int v){
+		return dep[u]+dep[v]-(dep[lca(u,v)]<<1);
+	}
+	inline int fly(int x,int n){
+		int d=dep[x]-n;
+		for(;dep[top[x]]>d;x=fa[top[x]]);
+		return idx[dfn[x]-(dep[x]-d)];
+	}
+	inline int chase(int u,int v){
+		return u==v?u:lca(u,v)==u?fly(v,dep[v]-dep[u]-1):fa[u];
+	}
+	int rel[N];
+	void dfs3(int x,int fa,int dep){
+		apmax(ans,dep+dis(x,rel[x]));
+		if(rel[x]==x){
+			return;
+		}
+		for(int i=dhead[x],v;~i;i=bro[i]){
+			if((v=to[i])!=fa){
+				if(dis(v,x)>2){
+					throw -1;
+				}
+				rel[v]=chase(rel[x],v);
+				dfs3(v,x,dep+1);
+			}
+		}
 	}
 }
 int main(){
@@ -85,19 +99,17 @@ int main(){
 	freopen("deer.in","r",stdin);
 	freopen("deer.out","w",stdout);
 #endif
-	int n=ni,x=ni,y=ni;
+	int n=ni,rt=ni;
+	T::rel[rt]=ni;
 	T::init();
-	for(int i=1;i<n;i++){
-		T::add(ni,ni,T::dhead);
+	for(int i=1;i<n;T::add(ni,ni,T::dhead),i++);
+	for(int i=1;i<n;T::add(ni,ni,T::mhead),i++);
+	T::dfs1(1),T::dfs2(1);
+	try{
+		T::dfs3(rt,0,0);
+		printf("%d\n",ans<<1);
+	}catch(int ans){
+		printf("%d\n",ans);
 	}
-	for(int i=1;i<n;i++){
-		T::add(ni,ni,T::mhead);
-	}
-	T::dep[y]=1,T::dfs(y);
-	int ans=0;
-	for(int i=x;T::dep[i]-1>T::dep[x]-T::dep[i];i++){
-		apmax(ans,T::dep[x]-T::dep[i]+T::hei[i]+(T::dep[i]-1-(T::dep[x]-T::dep[i])));
-	}
-	printf("%d\n",ans<<1);
 	return 0;
 }
