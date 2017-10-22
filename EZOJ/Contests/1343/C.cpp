e5fdfa08d74d34012719f824d0a0a64501da918e
Kerry Su <i_sukai@live.com>
2017-10-22 17:24:48 +0800

    Accepted. 44ms

--- EZOJ/Contests/1343/C.cpp
+++ EZOJ/Contests/1343/C.cpp
@@ -3,8 +3,6 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
-#include <vector>
-#include <map>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -17,67 +15,28 @@ template<class T>inline T next_num(){
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return flag?-i:i;
 }
-const int N=10,O=1000000007;
-inline int fpow(int x,int n){
-	lint ret=1;
-	for(;n;n>>=1,x=(lint)x*x%O){
-		if(n&1){
-			(ret*=x)%=O;
-		}
-	}
-	return ret;
-}
-inline int inv(int x){
-	return fpow(x,O-2);
-}
-map<int>m;
-vector<int>vec;
-int bitcnt[1<<N];
-int num[N],k,invk;
-int dfs(vector<int>&cur){
-	if(cur.size()==1){
-		m.insert(cur);
-	}
-	lint ans=0;
-	int cnt=0;
-	for(int i=0;i<(int)vec.size();i++){
-		if(vec[i]<(1<<cur.size())){
-			cnt++;
-			vector<int>nxt;
-			lint sum=0;
-			for(int j=0;j<(int)cur.size();j++){
-				if((vec[i]>>j)&1){
-					sum+=cur[j];
-				}else{
-					nxt.push_back(cur[j]);
-				}
-			}
-			nxt.push_back(sum%O);
-			ans+=dfs(nxt);
-		}
-	}
-	return ans%O*inv(cnt)%O;
-}
+const int N=2010,O=1000000007;
+int f[N];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("classroom.in","r",stdin);
 	freopen("classroom.out","w",stdout);
 #endif
-	int n=ni,m=ni;
-	k=ni,invk=inv(k);
-	for(int s=1,ts=1<<n;s<ts;s++){
-		bitcnt[s]=bitcnt[s^(s&-s)]+1;
-		if(bitcnt[s]==k){
-			vec.push_back(s);
+	int n=ni,m=ni,d=ni;
+	f[0]=1;
+	lint ans=0;
+	for(int i=1,ti=(n+m-1)/(d-1);i<=ti;i++){
+		for(int j=min((d-1)*i,n),tj=max((d-1)*i-(m-1),0);j>=tj;j--){
+			lint tmp=0;
+			for(int k=min(d-1,j);k;k--){
+				tmp+=f[j-k];
+			}
+			if((n-j)%(d-1)==0){
+				ans+=tmp%O;
+			}
+			f[j]=(f[j]+tmp)%O;
 		}
 	}
-	vector<int>cur;
-	for(int i=0;i<n;i++){
-		cur.push_back(0);
-	}
-	for(int j=0;j<m;j++){
-		cur.push_back(1);
-	}
-	printf("%d\n",dfs(cur));
+	printf("%lld\n",ans%O);
 	return 0;
 }
