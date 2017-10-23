148ca9309a0ef6a66600bc0ac5c921acff7c5c4c
Kerry Su <i_sukai@live.com>
2017-10-23 09:45:45 +0800

    Accepted. 330ms

--- Codeforces/238/E.cpp
+++ Codeforces/238/E.cpp
@@ -3,10 +3,11 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <queue>
 using namespace std;
 typedef long long lint;
+#define cout cerr
 #define ni (next_num<int>())
-#define nl (next_num<lint>())
 template<class T>inline T next_num(){
 	T i=0;char c;
 	while(!isdigit(c=getchar())&&c!='-');
@@ -15,123 +16,100 @@ template<class T>inline T next_num(){
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return flag?-i:i;
 }
-template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){
-	if(a<b){a=b;}
-}
-template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){
-	if(a>b){a=b;}
-}
+template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b){a=b;}}
+template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a){a=b;}}
 const int N=110,INF=0x7f7f7f7f;
-int n;
-namespace G2{
-	int dis[N][N];
-	inline void init(){
-		memset(dis,127,sizeof(dis));
-		for(int i=1;i<=n;i++){
-			dis[i][i]=0;
+int n,m;
+bool con[N][N];
+int dis[N][N],sline[N],tline[N],reach[N][N];
+bool mark[N][N];
+bool vis[N];
+inline void markkey(int id){
+	int u=sline[id],v=tline[id];
+	queue<int>q;
+	memset(vis+1,0,n);
+	vis[u]=true,q.push(u);
+	while(!q.empty()){
+		int x=q.front();
+		q.pop();
+		if(q.empty()){
+			mark[id][x]=true;
 		}
-	}
-	inline void floyd(){
-		for(int k=1;k<=n;k++){
-			for(int i=1;i<=n;i++){
-				if(dis[i][k]<INF){
-					for(int j=1;j<=n;j++){
-						if(dis[k][j]<INF){
-							apmin(dis[i][j],dis[i][k]+dis[k][j]);
-						}
-					}
-				}
+		for(int i=1;i<=n;i++){
+			if(con[x][i]&&dis[i][v]==dis[x][v]-1&&!vis[i]){
+				vis[i]=true,q.push(i);
 			}
 		}
 	}
 }
-namespace G1{
-	bool con[N][N];
-	inline void init(){
-		memset(con,0,sizeof(con));
-	}
-	int dis[N],que[N];
-	bool vis[N];
-	inline void bfs1(int s){
-		memset(dis,127,sizeof(dis));
-		dis[s]=0;
-		int qh=0,qt=0;
-		que[qt++]=s;
-		while(qh<qt){
-			int x=que[qh++];
-			for(int i=1;i<=n;i++){
-				if(con[x][i]&&dis[i]==INF){
-					dis[i]=dis[x]+1;
-					que[qt++]=i;
+inline void relax(){
+	for(int i=1;i<=n;i++){
+		int allmin=INF;
+		for(int j=1;j<=m;j++){
+			int t=-INF;
+			for(int k=1;k<=n;k++){
+				if(con[i][k]&&dis[i][tline[j]]==dis[k][tline[j]]+1){
+					apmax(t,reach[k][j]);
 				}
 			}
-		}
-	}
-	inline void bfs2(int t){
-		memset(vis,0,sizeof(vis));
-		if(dis[t]==INF){
-			return;
-		}
-		int qh=0,qt=0;
-		vis[t]=true;
-		que[qt++]=t;
-		while(qh<qt){
-			int x=que[qh++];
-			for(int i=1;i<=n;i++){
-				if(!vis[i]&&con[i][x]&&dis[i]+1==dis[x]){
-					vis[i]=true;
-					que[qt++]=i;
-				}
+			if(t>-INF){
+				apmin(reach[i][j],t);
+			}
+			if(mark[j][i]){
+				apmin(allmin,reach[i][j]);
 			}
 		}
-	}
-	int pos[N];
-	inline void work(int s,int t){
-		bfs1(s),bfs2(t);
-		memset(pos,0,sizeof(pos));
-		for(int i=1;i<=n;i++){
-			if(vis[i]){
-				int &pt=pos[dis[i]];
-				if(pt==0){
-					pt=i;
-				}else if(pt>0){
-					pt=-1;
-				}
-			}
-		}
-		for(int i=0;i<n;i++){
-			if(pos[i]>0){
-				for(int j=i+1;j<n;j++){
-					if(pos[j]>0){
-						assert(pos[i]!=pos[j]);
-						G2::dis[pos[i]][pos[j]]=1;
-					}
-				}
-			}
+		allmin++;
+		for(int j=1;j<=m;j++){
+			apmin(reach[i][j],allmin);
 		}
 	}
-}
-inline int sanitize(int x){
-	return x==INF?-1:x;
 }
 int main(){
-	int s,t;
-	{
-		n=ni;
-		int tot=ni;
-		s=ni,t=ni;
-		G1::init();
-		while(tot--){
-			int u=ni,v=ni;
-			G1::con[u][v]=true;
+#ifndef ONLINE_JUDGE
+	freopen("meet.in","r",stdin);
+	freopen("meet.out","w",stdout);
+#endif
+	n=ni;
+	int tot=ni,s=ni,t=ni;
+	memset(dis,127,sizeof(dis));
+	for(int u,v;tot--;u=ni,v=ni,dis[u][v]=con[u][v]=1);
+	for(int i=1;i<=n;i++){
+		dis[i][i]=0;
+	}
+	for(int k=1;k<=n;k++){
+		for(int i=1;i<=n;i++){
+			if(dis[i][k]<INF){
+				for(int j=1;j<=n;j++){
+					if(dis[k][j]<INF){
+						apmin(dis[i][j],dis[i][k]+dis[k][j]);
+					}
+				}
+			}
 		}
 	}
-	{
-		G2::init();
-		for(int tot=ni;tot--;){
-			int s=ni,t=ni;
-			G1::work(s,t);
+	m=ni;
+	for(int i=1;i<=m;i++){
+		sline[i]=ni,tline[i]=ni;
+		if(dis[sline[i]][tline[i]]<INF){
+			markkey(i);
+		}else{
+			i--,m--;
 		}
 	}
-	printf("%d\n",(G2::floyd(),sanitize(G2::dis[s][t])));
+	memset(reach,127,sizeof(reach));
+	for(int i=1;i<=m;i++){
+		reach[t][i]=1;
+	}
+	for(int i=1;i<=n;i++){
+		relax();
+	}
+	int ans=INF;
+	for(int i=1;i<=m;i++){
+		if(mark[i][s]){
+			apmin(ans,reach[s][i]);
+		}
+	}
+	printf("%d\n",ans<INF?ans:-1);
+	return 0;
 }
