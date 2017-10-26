965b89c895acee160a6654746424426b0edaabfe
Kerry Su <i_sukai@live.com>
2017-10-26 14:15:12 +0800

    In competition. A:90; B:100;

---
+++ EZOJ/Contests/1347/A.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <cctype>
+using namespace std;
+typedef long long lint;
+#define cout cerr
+#define ni (next_num<int>())
+template<class T>inline T next_num(){
+	T i=0;char c;
+	while(!isdigit(c=getchar())&&c!='-');
+	bool flag=c=='-';
+	flag?(c=getchar()):0;
+	while(i=i*10-'0'+c,isdigit(c=getchar()));
+	return flag?-i:i;
+}
+const int N=100010;
+int n;
+lint calc(int n){
+	lint tmp=n;
+	tmp*=(n-1);
+	tmp/=2;
+	return tmp+n;
+}
+namespace T{
+	const int E=N<<1;
+	int to[E],bro[E],head[N],e=0;
+	int fa[N],son[N],size[N],dep[N],top[N];
+	int dfn[N],idx[N],tim=0;
+	lint f[N],g[N],h[N],hsum[N];
+	inline void init(){
+		memset(head,-1,sizeof(head));
+		memset(f,0,sizeof(f));
+		memset(son,0,sizeof(son));
+		fa[1]=0,g[1]=0,dep[1]=1;
+	}
+	inline void ae(int u,int v){
+		to[e]=v,bro[e]=head[u],head[u]=e++;
+	}
+	inline void add(int u,int v){
+		ae(u,v),ae(v,u);
+	}
+	void dfs1(int x){
+		dep[x]=dep[fa[x]]+1;
+		size[x]=f[x]=1;
+		h[x]=calc(n);
+		for(int i=head[x],v;~i;i=bro[i]){
+			if((v=to[i])!=fa[x]){
+				fa[v]=x;
+				dfs1(v);
+				size[x]+=size[v];
+				f[x]+=f[v]+size[v];
+				h[x]-=calc(size[v]);
+				if(size[v]>size[son[x]]){
+					son[x]=v;
+				}
+			}
+		}
+		h[x]-=calc(n-size[x]);
+	}
+	void dfs2(int x){
+		idx[dfn[x]=++tim]=x;
+		top[x]=son[fa[x]]==x?top[fa[x]]:x;
+		hsum[x]=h[x];
+		if(son[x]){
+			g[son[x]]=g[x]+(n-size[x])+f[x]-(f[son[x]]+size[son[x]]);
+			dfs2(son[x]);
+			for(int i=head[x],v;~i;i=bro[i]){
+				if((v=to[i])!=fa[x]&&v!=son[x]){
+					g[v]=g[x]+(n-size[x])+f[x]-(f[v]+size[v]);
+					dfs2(v);
+				}
+			}
+			for(int i=head[x],v;~i;i=bro[i]){
+				if((v=to[i])!=fa[x]){
+					hsum[x]+=f[v]*(n-size[v]);
+				}
+			}
+		}
+		hsum[x]+=g[x]*size[x];
+	}
+	inline int lca(int u,int v){
+		for(;top[u]!=top[v];dep[top[u]]>dep[top[v]]?(u=fa[top[u]]):(v=fa[top[v]]));
+		return dep[u]<dep[v]?u:v;
+	}
+	inline int fly(int x,int n){
+		assert(n>=0);
+		for(int d;d=dep[x]-dep[fa[top[x]]],d<=n;n-=d,x=fa[top[x]]);
+		return idx[dfn[x]-n];
+	}
+}
+int main(){
+#ifndef ONLINE_JUDGE
+	freopen("happy.in","r",stdin);
+	freopen("happy.out","w",stdout);
+#endif
+	n=ni;
+	int tot=ni;
+	T::init();
+	for(int i=1;i<n;T::add(ni,ni),i++);
+	T::dfs1(1),T::dfs2(1);
+	while(tot--){
+		using namespace T;
+		int u=ni,v=ni,w=lca(u,v);
+		if(u==v){
+			printf("%.7lf\n",(double)hsum[u]/h[u]);
+			continue;
+		}
+		int dis=dep[u]+dep[v]-(dep[w]<<1);
+		if(dep[u]>dep[v]){
+			swap(u,v);
+		}
+		if(u==w){
+			int s=fly(v,dep[v]-dep[u]-1);
+			assert(fa[s]==u);
+			printf("%.7lf\n",dis-1+(double)g[s]/(n-size[s])+(double)f[v]/size[v]);
+		}else{
+			printf("%.7lf\n",dis-1+(double)f[u]/size[u]+(double)f[v]/size[v]);
+		}
+	}
+	return 0;
+}
---
+++ EZOJ/Contests/1347/B.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <cctype>
+using namespace std;
+typedef long long lint;
+#define cout cerr
+#define ni (next_num<int>())
+template<class T>inline T next_num(){
+	T i=0;char c;
+	while(!isdigit(c=getchar())&&c!='-');
+	bool flag=c=='-';
+	flag?(c=getchar()):0;
+	while(i=i*10-'0'+c,isdigit(c=getchar()));
+	return flag?-i:i;
+}
+const int N=200010,O=924844033;
+inline int fpow(int x,int n){
+	int a=1;
+	for(;n;n>>=1,x=(lint)x*x%O){
+		if(n&1){
+			a=(lint)a*x%O;
+		}
+	}
+	return a;
+}
+inline int inv(int x){
+	return fpow(x,O-2);
+}
+namespace poly{
+	const int SH=19,N=1<<SH;
+	int sh,n,invn;
+	int rev[N],o[SH][N<<1],io[SH][N<<1];
+	inline void init(int _n){//g=5
+		for(sh=0;(1<<sh)<_n;sh++);
+		n=1<<sh,invn=inv(n);
+		rev[0]=0;
+		for(int i=0;i<n;i++){
+			rev[i]=(rev[i>>1]>>1)|((i&1)<<(sh-1));
+		}
+		for(int i=0;i<sh;i++){
+			int half=1<<i,full=half<<1;
+			lint w=1,iw=1,wn=fpow(5,(O-1)/full),iwn=inv(wn);
+			for(int j=0;j<half;j++,(w*=wn)%=O,(iw*=iwn)%=O){
+				o[i][j]=w,io[i][j]=iw;
+			}
+		}
+	}
+	inline void ntt(int a[],int d=1){
+		for(int i=0;i<n;i++){
+			if(rev[i]>i){
+				swap(a[i],a[rev[i]]);
+			}
+		}
+		for(int i=0;i<sh;i++){
+			int half=1<<i,full=half<<1;
+			for(int j=0;j<half;j++){
+				int w=(d==1?o:io)[i][j];
+				for(int k=j;k<n;k+=full){
+					int p=a[k],q=(lint)a[k+half]*w%O;
+					a[k]=(p+q)%O;
+					a[k+half]=(p+O-q)%O;
+				}
+			}
+		}
+		if(d==-1){
+			for(int i=0;i<n;i++){
+				a[i]=(lint)a[i]*invn%O;
+			}
+		}
+	}
+}
+int n,cnt[N];
+namespace T{
+	const int E=N<<1;
+	int to[E],bro[E],head[N],e=0;
+	inline void init(){
+		memset(head,-1,sizeof(head));
+	}
+	inline void ae(int u,int v){
+		to[e]=v,bro[e]=head[u],head[u]=e++;
+	}
+	inline void add(int u,int v){
+		ae(u,v),ae(v,u);
+	}
+	int dfs(int x,int fa){
+		int size=1;
+		for(int i=head[x],v;~i;i=bro[i]){
+			if((v=to[i])!=fa){
+				size+=dfs(v,x);
+			}
+		}
+		if(fa){
+			cnt[size]++;
+			cnt[n-size]++;
+		}
+		return size;
+	}
+}
+int fac[N],invfac[N];
+inline void gmath(int n){
+	fac[0]=1;
+	for(int i=1;i<=n;i++){
+		fac[i]=(lint)fac[i-1]*i%O;
+	}
+	invfac[n]=inv(fac[n]);
+	for(int i=n;i>=1;i--){
+		invfac[i-1]=(lint)invfac[i]*i%O;
+	}
+}
+int a[poly::N],b[poly::N];
+int main(){
+#ifndef ONLINE_JUDGE
+	freopen("simple.in","r",stdin);
+	freopen("simple.out","w",stdout);
+#endif
+	n=ni;
+	gmath(n),T::init();
+	for(int i=1;i<n;T::add(ni,ni),i++);
+	T::dfs(1,0);
+	poly::init(n<<1);
+	memset(a,0,sizeof(a));
+	memset(b,0,sizeof(b));
+	for(int i=0;i<n;i++){
+		a[i]=(lint)fac[n-i]*cnt[n-i]%O;
+		b[i]=invfac[i];
+	}
+	poly::ntt(a),poly::ntt(b);
+	for(int i=0;i<poly::n;i++){
+		a[i]=(lint)a[i]*b[i]%O;
+	}
+	poly::ntt(a,-1);
+	for(int k=1;k<=n;k++){
+		lint ans=(lint)fac[n]*invfac[k]%O*invfac[n-k]%O*n%O;
+		ans-=(lint)a[n-k]*invfac[k]%O;
+		printf("%lld\n",(ans%O+O)%O);
+	}
+	return 0;
+}
