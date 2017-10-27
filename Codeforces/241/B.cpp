66c8483ab733aea82df61f0ee66938f535bfd88f
Kerry Su <i_sukai@live.com>
2017-10-27 16:28:47 +0800

    WA on test 5.

--- Codeforces/241/B.cpp
+++ Codeforces/241/B.cpp
@@ -26,12 +26,14 @@ struct xorsum{
 		n=0;
 	}
 	inline void operator += (int b){
+		n++;
 		for(int i=0;i<D;i++){
-			a[D]+=(b>>i)&1;
+			a[i]+=(b>>i)&1;
 		}
 	}
 	inline friend xorsum operator - (const xorsum &a,const xorsum &b){
 		xorsum c;
+		c.clr(),c.n=a.n-b.n;
 		for(int i=0;i<D;i++){
 			c.a[i]=a.a[i]-b.a[i];
 		}
@@ -40,8 +42,8 @@ struct xorsum{
 	inline friend int operator * (const xorsum &a,const xorsum &b){
 		lint ans=0;
 		for(int i=0;i<D;i++){
-			ans+=a.a[i]*(b.n-b.a[i]);
-			ans+=b.a[i]*(a.n-a.a[i]);
+			ans+=((a.a[i]*(b.n-b.a[i])%O)<<i)%O;
+			ans+=((b.a[i]*(a.n-a.a[i])%O)<<i)%O;
 		}
 		return ans%O;
 	}
@@ -51,7 +53,7 @@ namespace T{
 	struct Node;
 	typedef Node* node;
 	struct Node{
-		int l,m,r;
+		int l,r;
 		node lson,rson;
 		int dep;
 	}*rt;
@@ -65,11 +67,15 @@ struct cros{
 	inline int eval(){
 		return (_a[l->r]-_a[l->l-1])*(_a[r->r]-_a[r->l-1]);
 	}
+	inline friend ostream & operator << (ostream & out,const cros &b){
+		out<<"(("<<b.l->l<<","<<b.l->r<<",d="<<b.l->dep<<"),("<<b.r->l<<","<<b.r->r<<",d="<<b.r->dep<<"))";
+		return out;
+	}
 	inline friend bool operator < (const cros &x,const cros &y){
-		int d=max(x.l->dep,y.l->dep)+1;
+		int d=max(x.l->dep,y.r->dep)+1;
 		int vx=(a[x.l->l]^a[x.r->r])>>d;
 		int vy=(a[y.l->l]^a[y.r->r])>>d;
-		return vx>vy;
+		return vx<vy;
 	}
 	inline friend bool operator == (const cros &x,const cros &y){
 		return !(x<y)&&!(y<x);
@@ -81,6 +87,10 @@ inline void psh(T::node l,T::node r){
 		q.push((cros){l,r});
 	}
 }
+int binsearch(int l,int r,int dep){
+	for(int m;l<r;m=(l+r)>>1,((a[m]>>dep)&1)?(r=m):(l=m+1));
+	return l;
+}
 T::node T::build(int l,int r,int dep){
 	static node n=new Node[N*18];
 	if(l>r){
@@ -88,13 +98,12 @@ T::node T::build(int l,int r,int dep){
 	}
 	node x=n++;
 	x->l=l,x->r=r,x->dep=dep;
-	for(;x->m<=r&&((a[x->m]>>dep)&1)==0;x->m++);
-	x->m--;
-	if(a[l]==a[r]){
+	if(dep<0){
 		x->lson=x->rson=NULL;
 	}else{
-		x->lson=build(l,x->m,dep-1);
-		x->rson=build(x->m+1,r,dep-1);
+		int m=binsearch(l,r+1,dep)-1;
+		x->lson=build(l,m,dep-1);
+		x->rson=build(m+1,r,dep-1);
 		psh(x->lson,x->rson);
 	}
 	return x;
@@ -139,6 +148,6 @@ int main(){
 			psh(vec[i].l->rson,vec[i].r->rson);
 		}
 	}
-	printf("%lld\n",ans);
+	printf("%d\n",(int)ans);
 	return 0;
 }
