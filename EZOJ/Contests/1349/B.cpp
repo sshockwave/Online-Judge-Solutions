b9d22f31648366bbb27f4c23226d1373fa282b5c
Kerry Su <i_sukai@live.com>
2017-10-29 21:19:46 +0800

    Accepted. 487ms

--- EZOJ/Contests/1349/B.cpp
+++ EZOJ/Contests/1349/B.cpp
@@ -17,22 +17,26 @@ template<class T>inline T next_num(){
 }
 const int N=5,M=8,K=10,O=12345678;
 char mat[N][M];
-int sur[N][M];
 int n,m;
 inline bool valid(int x,int y){
 	return x>=1&&x<=n&&y>=1&&y<=m;
 }
 int posx[K],posy[K],ps;
 int vis[N][M],tim=0;
-int rest[1<<K];
+int rest[1<<K],bcnt[1<<K];
 lint f[1<<K];
 inline int dp(){
 	for(int s=0,ts=1<<ps;s<ts;s++){
 		rest[s]=n*m;
+		bcnt[s]=s?bcnt[s^(s&-s)]+1:0;
 		tim++;
 		for(int i=0;i<ps;i++){
-			if(((s>>i)&1)==0){
-				rest[s]++;
+			if((s>>i)&1){
+				if(vis[posx[i]][posy[i]]<tim){
+					vis[posx[i]][posy[i]]=tim;
+					rest[s]--;
+				}
+			}else{
 				for(int x=posx[i]-1;x<=posx[i]+1;x++){
 					for(int y=posy[i]-1;y<=posy[i]+1;y++){
 						if(valid(x,y)&&vis[x][y]<tim){
@@ -47,11 +51,12 @@ inline int dp(){
 	memset(f,0,(1<<ps)<<3);
 	f[0]=1;
 	for(int i=1,ti=n*m;i<=ti;i++){
-		for(int s=0,ts=1<<ps;s<ts;s++){
-			(f[s]*=max(rest[s]-(i-1),0))%=O;
+		for(int s=(1<<ps)-1;s>=0;s--){
+			(f[s]*=max(rest[s]-(i-1-bcnt[s]),0))%=O;
 			for(int t=s;t;t^=t&-t){
 				f[s]+=f[s^(t&-t)];
 			}
+			f[s]%=O;
 		}
 	}
 	return f[(1<<ps)-1];
@@ -80,15 +85,23 @@ int dfs(int x,int y){
 	}
 }
 inline int Main(){
-	n=ni,m=ni;
-	memset(sur,0,sizeof(sur));
+	n=ni,m=ni,ps=0;
 	for(int i=1;i<=n;i++){
 		scanf("%s",mat[i]+1);
 		for(int j=1;j<=m;j++){
-			for(int ti=i-1;ti<=i+1;ti++){
-				for(int tj=j-1;tj<=j+1;tj++){
-					if(valid(ti,tj)&&(ti!=i||tj!=j)){
-						sur[i][j]++;
+			if(mat[i][j]=='X'){
+				posx[ps]=i,posy[ps]=j,ps++;
+			}
+		}
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			if(mat[i][j]=='X'){
+				for(int x=i-1;x<=i+1;x++){
+					for(int y=j-1;y<=j+1;y++){
+						if((x!=i||y!=j)&&valid(x,y)&&mat[x][y]=='X'){
+							return 0;
+						}
 					}
 				}
 			}
