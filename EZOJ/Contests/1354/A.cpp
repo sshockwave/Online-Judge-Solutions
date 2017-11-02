43bcb995ef16e469b6a77d8ee967f7f1bc61f133
Kerry Su <i_sukai@live.com>
2017-11-02 14:17:58 +0800

    Accepted. 426ms

--- EZOJ/Contests/1354/A.cpp
+++ EZOJ/Contests/1354/A.cpp
@@ -15,27 +15,50 @@ template<class T>inline T next_num(){
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
 	return flag?-i:i;
 }
-const int N=10010,O=1000000007;
-bool ban[N];
-lint f[N];
+const int N=1010,O=1000000007;
+struct Mat{
+	const static int D=3;
+	int a[D][D];
+	inline friend Mat operator * (const Mat &a,const Mat &b){
+		Mat c;
+		for(int i=0;i<D;i++){
+			for(int j=0;j<D;j++){
+				lint tmp=0;
+				for(int k=0;k<D;k++){
+					tmp+=(lint)a.a[i][k]*b.a[k][j]%O;
+				}
+				c.a[i][j]=tmp%O;
+			}
+		}
+		return c;
+	}
+	inline void fpow(Mat &res,int n){
+		if(n==0){
+			res=(Mat){1,0,0,0,1,0,0,0,1};
+			return;
+		}
+		fpow(res,n>>1);
+		res=res*res;
+		if(n&1){
+			res=res**this;
+		}
+	}
+}trans1={1,2,1,0,1,1,1,2,2},trans2=(Mat){1,2,1,0,1,1,0,0,1};
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("stick.in","r",stdin);
 	freopen("stick.out","w",stdout);
 #endif
-	memset(ban,0,sizeof(ban));
-	int n=ni;
-	for(int tot=ni;tot--;ban[ni]=true);
-	f[0]=1;
-	for(int i=1;i<=n;i++){
-		f[i]=0;
-		if(!ban[i]){
-			for(int j=0;j<i;j++){
-				f[i]+=f[j]*(i-j)%O*(i-j)%O;
-			}
-			f[i]%=O;
-		}
+	int n=ni,m=ni,x=0;
+	Mat ans,tmp;
+	tmp.fpow(ans,0);
+	for(int i=1,j;i<=m;i++){
+		trans1.fpow(tmp,(j=ni)-x-1);
+		ans=trans2*tmp*ans;
+		x=j;
 	}
-	printf("%lld\n",f[n]);
+	trans1.fpow(tmp,n-x-1);
+	ans=tmp*ans;
+	printf("%lld\n",((lint)ans.a[0][2]+(ans.a[1][2]<<1)+ans.a[2][2])%O);
 	return 0;
 }
