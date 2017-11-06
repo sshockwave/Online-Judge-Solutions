490bab4d45e7385e89f9689b25c94e233e8d90e3
Kerry Su <i_sukai@live.com>
2017-11-06 15:00:55 +0800

    Accepted. 66ms

--- EZOJ/Contests/1358/B.cpp
+++ EZOJ/Contests/1358/B.cpp
@@ -20,39 +20,46 @@ inline char nc(){
 	while(!isalpha(c=getchar()));
 	return c;
 }
-const int N=20,INF=0x7f7f7f7f;
+template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
+template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
+const int N=17,INF=0x7f7f7f7f;
 char tp[N];
-int r[N],b[N],ans=INF;
-bool vis[N];
-int n;
-void dfs(int x,int rcnt,int bcnt,int R,int B,int tim){
-	if(tim>=ans){
-		return;
-	}
-	if(x==0){
-		ans=tim;
-		return;
-	}
-	for(int i=1;i<=n;i++){
-		if(!vis[i]){
-			int dig=max(max(max(r[i]-R,0)-rcnt,max(b[i]-B,0)-bcnt),0);
-			vis[i]=true;
-			dfs(x-1,rcnt+dig-max(r[i]-R,0),bcnt+dig-max(b[i]-R,0),R+(tp[i]=='R'),B+(tp[i]=='B'),tim+dig+1);
-			vis[i]=false;
-		}
-	}
-}
+int r[N],b[N];
+int f[1<<16][16*16/2],bitcnt[1<<N];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("chip.in","r",stdin);
 	freopen("chip.out","w",stdout);
 #endif
-	n=ni;
+	int n=ni,ans=INF;
+	int rmask=0,bmask=0;
+	int rsum=0,bsum=0;
 	for(int i=1;i<=n;i++){
 		tp[i]=nc(),r[i]=ni,b[i]=ni;
+		rsum+=r[i],bsum+=b[i];
+		(tp[i]=='R'?rmask:bmask)|=1<<(i-1);
 	}
-	memset(vis,0,sizeof(vis));
-	dfs(n,0,0,0,0,0);
-	printf("%d\n",ans);
+	bitcnt[0]=0;
+	memset(f,-1,sizeof(f));
+	f[0][0]=0;
+	for(int s=0,ts=1<<n;s<ts;s++){
+		bitcnt[s]=s?bitcnt[s^(s&-s)]+1:0;
+		int rcnt=bitcnt[s&rmask],bcnt=bitcnt[s&bmask];
+		int tj=rcnt*bcnt+((rcnt*(rcnt-1))>>1);
+		for(int i=0;i<n;i++){
+			if(!((s>>i)&1)){
+				int tmp=s|(1<<i),ta=min(rcnt,r[i+1]),tb=min(bcnt,b[i+1]);
+				for(int j=0;j<=tj;j++){
+					if(f[s][j]>=0){
+						apmax(f[tmp][j+ta],f[s][j]+tb);
+					}
+				}
+			}
+		}
+	}
+	for(int i=0,s=(1<<n)-1,rcnt=bitcnt[s&rmask],bcnt=bitcnt[s&bmask],ti=rcnt*bcnt+((rcnt*(rcnt-1))>>1);i<=ti;i++){
+		apmin(ans,max(rsum-i,bsum-f[s][i]));
+	}
+	printf("%d\n",ans+n);
 	return 0;
 }
