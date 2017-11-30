cde56b50b89a72c59494de4b0e1e19fbca4619cc
Kerry Su <i_sukai@live.com>
2017-11-30 09:31:03 +0800

    Accepted. 15ms.

--- AtCoder/Grand/17/E.cpp
+++ AtCoder/Grand/17/E.cpp
@@ -25,6 +25,7 @@ namespace G{
 	int bln[N],dfn[N],low[N],tim=0;
 	int stk[N],ss=0;
 	bool instk[N];
+	bool flag=true;
 	inline void init(){
 		memset(head,-1,sizeof(head));
 		memset(bln,-1,sizeof(bln));
@@ -33,7 +34,9 @@ namespace G{
 	}
 	inline void ae(int u,int v){
 		to[e]=v,bro[e]=head[u],head[u]=e++;
-		oudeg[u]++,indeg[v]++;
+		if(flag){
+			oudeg[u]++,indeg[v]++;
+		}
 	}
 	void tarjan(int x){
 		dfn[x]=low[x]=++tim;
@@ -57,29 +60,22 @@ inline bool Main(){
 	using namespace G;
 	int n=ni,h=ni;
 	init();
-	for(int i=1,j=h+1;i<=h;i++,j++){
-		ae(0,i),ae(j,0);
-		indeg[i]=oudeg[j]=0;
-	}
 	for(int i=1;i<=n;i++){
 		int a=ni,b=ni,c=ni,d=ni;
 		ae(c?c+h:a,d?d:b+h);
 	}
-	tarjan(0);
-	int cntin=0,cntou=0;
+	flag=false;
 	for(int i=1,j=h+1;i<=h;i++,j++){
-		if(indeg[i]||oudeg[i]){
-			if(bln[i])return false;
-			if(indeg[i]>oudeg[i])return false;
-			cntin+=oudeg[i]-indeg[i];
-		}
-		if(indeg[j]||oudeg[j]){
-			if(bln[j])return false;
-			if(indeg[j]<oudeg[j])return false;
-			cntou+=indeg[j]-oudeg[j];
-		}
+		if(indeg[i]>oudeg[i])return false;
+		if(indeg[i]!=oudeg[i])ae(0,i);
+		if(indeg[j]<oudeg[j])return false;
+		if(indeg[j]!=oudeg[j])ae(j,0);
 	}
-	return cntin==cntou;
+	tarjan(0);
+	for(int i=1,ti=h<<1;i<=ti;i++){
+		if((indeg[i]||oudeg[i])&&bln[i])return false;
+	}
+	return true;
 }
 int main(){
 	puts(Main()?"YES":"NO");
