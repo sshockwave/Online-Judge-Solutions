52fbc2745c5970dc9e6c014eb624dfd3a2875949
Kerry Su <i_sukai@live.com>
2017-12-01 08:20:05 +0800

    Accepted. 105ms.

--- AtCoder/Regular/81/F.cpp
+++ AtCoder/Regular/81/F.cpp
@@ -27,32 +27,29 @@ int main(){
 	for(int i=1;i<=n;i++){
 		scanf("%s",mat[i]+1);
 	}
-	for(int j=1;j<=m;j++){
-		ext[1][j]=0;
-	}
+	memset(ext,0,sizeof(ext));
 	int ans=0;
-	for(int i=2;i<=n;i++){
-		ext[i][1]=0;
-		for(int j=2;j<=m;j++){
-			ext[i][j]=(mat[i-1][j-1]^mat[i-1][j])==(mat[i][j-1]^mat[i][j])?ext[i-1][j]+1:1;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<m;j++){
+			ext[i][j]=i!=1&&(mat[i-1][j]^mat[i-1][j+1])==(mat[i][j]^mat[i][j+1])?ext[i-1][j]+1:1;
 		}
 		int ss=0;
 		stk[0]=0;
-		for(int j=1;j<=m;j++){
+		for(int j=1;j<m;j++){
 			for(;ss&&ext[i][stk[ss]]>=ext[i][j];ss--);
-			tol[j]=stk[ss];
+			tol[j]=stk[ss]+1;
 			stk[++ss]=j;
 		}
-		stk[ss=0]=m+1;
-		for(int j=m;j>=1;j--){
+		stk[ss=0]=m;
+		for(int j=m-1;j>=1;j--){
 			for(;ss&&ext[i][stk[ss]]>=ext[i][j];ss--);
 			tor[j]=stk[ss];
 			stk[++ss]=j;
 		}
-		for(int j=1;j<=m;j++){
-			apmax(ans,(tor[j]-tol[j])*ext[i][j]);
+		for(int j=1;j<m;j++){
+			apmax(ans,(tor[j]-tol[j]+1)*ext[i][j]);
 		}
 	}
-	printf("%d\n",ans);
+	printf("%d\n",max(ans,max(n,m)));
 	return 0;
 }
