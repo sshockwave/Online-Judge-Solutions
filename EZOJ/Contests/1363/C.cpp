5df83b8fdbda622045d9ecb3ea1df6a17f15833f
Kerry Su <i_sukai@live.com>
2017-12-12 08:44:00 +0800

    WA.

--- EZOJ/Contests/1363/C.cpp
+++ EZOJ/Contests/1363/C.cpp
@@ -18,87 +18,47 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int N=50010,B=43;
-int rt;
+const int N=50010,B=33;
 inline int grt(lint x){
-	lint ans=sqrt(x);
-	for(;ans*ans<x;ans++);
-	for(;ans*ans>x;ans--);
+	int ans=sqrt(x);
+	for(;(lint)ans*ans<x;ans++);
+	for(;(lint)ans*ans>x;ans--);
 	return ans;
 }
 lint a[N];
-lint cacheval=-1,afterval;
-int idx[N];
-int lst[N],ls;
-bool flag;
-namespace seg{
-	struct Node;
-	typedef Node* node;
-	struct Node{
-		node lson,rson;
-		int l,m,r;
-		bool one;
-		lint sum;
-		inline void up(){
-			one=lson->one&&rson->one;
-			sum=lson->sum+rson->sum;
-		}
-	}*rt[B][B];
-	node build(int l,int r,int m,int d){
-		static node n=new Node[(N*B)<<2];
-		node x=n++;
-		x->l=l,x->m=(l+r)>>1,x->r=r;
-		if(l==r){
-			x->sum=a[idx[l*m+d]];
-			x->one=x->sum==1;
-		}else{
-			x->lson=build(l,x->m,m,d);
-			x->rson=build(x->m+1,r,m,d);
-			x->up();
-		}
-		return x;
+int rt;
+struct BIT{
+	lint *c;
+	int n;
+	inline void init(int _n){
+		n=_n;
+		c=new lint[n+1];
+		memset(c,0,(n+1)<<3);
 	}
-	void app(node x,int l,int r){
-		if(x->one)return;
-		if(x->l==l&&x->r==r){
-			if(cacheval!=x->sum){
-				cacheval=x->sum;
-				afterval=grt(x->sum);
-			}
-			x->sum=afterval;
-			x->one=x->sum==1;
-			if(flag){
-				lst[++ls]=l;
-			}
-			return;
-		}
-		if(r<=x->m){
-			app(x->lson,l,r);
-		}else if(l>x->m){
-			app(x->rson,l,r);
-		}else{
-			app(x->lson,l,x->m);
-			app(x->rson,x->m+1,r);
-		}
-		x->up();
+	inline void add(int x,lint v){
+		for(;x<=n;c[x]+=v,x+=x&-x);
 	}
-	lint ask(node x,int l,int r){
-		if(x->one)return r-l+1;
-		if(x->l==l&&x->r==r)return x->sum;
-		if(r<=x->m)return ask(x->lson,l,r);
-		if(l>x->m)return ask(x->rson,l,r);
-		return ask(x->lson,l,x->m)+ask(x->rson,x->m+1,r);
+	inline lint sum(int x){
+		lint ans=0;
+		for(;x;ans+=c[x],x^=x&-x);
+		return ans;
 	}
-}
+	inline lint ask(int l,int r){
+		return sum(r)-sum(l-1);
+	}
+}bt[B][B];
 namespace T{
 	const int E=N<<1;
 	int to[E],bro[E],head[N],e=0;
 	int fa[N],son[N],size[N],dep[N],top[N];
-	int dfn[N],tim=0;
+	int dfn[N],idx[N],tim=0;
+	int ufa[N][B];
 	inline void init(){
 		memset(head,-1,sizeof(head));
-		size[0]=son[0]=0;
-		dfn[0]=-1;
+		memset(fa,0,sizeof(fa));
+		memset(son,0,sizeof(son));
+		memset(ufa,0,sizeof(ufa));
+		fa[1]=son[0]=size[0]=dep[0]=0;
 	}
 	inline void ae(int u,int v){
 		to[e]=v,bro[e]=head[u],head[u]=e++;
@@ -106,12 +66,25 @@ namespace T{
 	inline void add(int u,int v){
 		ae(u,v),ae(v,u);
 	}
+	inline void act(int x){//x:original
+		if(a[x]==1)return;
+		lint dt=grt(a[x])-a[x];
+		a[x]+=dt;
+		for(int i=1;i<=rt;i++){
+			bt[i][dfn[x]%i].add(dfn[x]/i+1,dt);
+		}
+	}
+	int stk[N],ss=0;
 	void dfs1(int x){
+		for(int i=1;i<=ss;i++){
+			ufa[x][i]=stk[ss-i];
+		}
+		stk[ss++]=x;
+		fa[x]=ufa[x][1];
+		dep[x]=ss;
 		size[x]=1;
-		dep[x]=dep[fa[x]]+1;
 		for(int i=head[x],v;~i;i=bro[i]){
 			if((v=to[i])!=fa[x]){
-				fa[v]=x;
 				dfs1(v);
 				size[x]+=size[v];
 				if(size[v]>size[son[x]]){
@@ -119,11 +92,11 @@ namespace T{
 				}
 			}
 		}
+		ss--;
 	}
 	void dfs2(int x){
 		idx[dfn[x]=tim++]=x;
 		top[x]=son[fa[x]]==x?top[fa[x]]:x;
-		cout<<"top["<<x<<"]="<<top[x]<<endl;
 		if(son[x]){
 			dfs2(son[x]);
 			for(int i=head[x],v;~i;i=bro[i]){
@@ -134,13 +107,7 @@ namespace T{
 		}
 	}
 	inline int lca(int u,int v){
-		while(top[u]!=top[v]){
-			if(dep[top[u]]>dep[top[v]]){
-				u=fa[top[u]];
-			}else{
-				v=fa[top[v]];
-			}
-		}
+		for(;top[u]!=top[v];dep[top[u]]>dep[top[v]]?(u=fa[top[u]]):(v=fa[top[v]]));
 		return dep[u]<dep[v]?u:v;
 	}
 	inline int fly(int x,int n){
@@ -148,85 +115,54 @@ namespace T{
 		for(int d;d=dep[x]-dep[fa[top[x]]],d<=n;n-=d,x=fa[top[x]]);
 		return idx[dfn[x]-n];
 	}
-	int op;
+	inline int uroot(int x,int k){
+		return a[x]==1?ufa[x][k]=uroot(ufa[x][k],k):x;
+	}
+	int op,k;
 	lint ans;
-	inline void workp(int x){//TODO
-		cout<<"Workingp("<<x<<")"<<endl;
-		if(op==1)return ans+=a[x],void();
-		if(a[x]==1)return;
-		for(int i=1;i<=rt;i++){
-			int d=dfn[x]%i,t=dfn[x]/i;
-			seg::app(seg::rt[i][d],t,t);
-		}
-		a[x]=afterval;
-	}
-	inline int realdiv(int a,int b){
-		return (a-a%b)/b;
-	}
-	inline void workintv(int l,int r,int m,int d){//dfn % m == d
-		int nl=l<d?-1:(l-d)/m,nr=r<d?-1:(r-d)/m;
-		if(nl*m+d<l){
-			nl++;
-		}
-		assert(nl*m+d>=l);
-		assert(nr*m+d<=r);
-		if(nl>nr)return;
-		if(m>rt){
-			for(int i=nl*m+d,ti=nr*m+d;i<=ti;i+=m){
-				workp(idx[i]);
-			}
-			return;
-		}
+	inline void workp(int x){
 		if(op){
-			ans+=seg::ask(seg::rt[m][d],nl,nr);
-			cout<<"asking ["<<nl<<","<<nr<<"] from ["<<m<<"]["<<d<<"]"<<endl;
-			cout<<"ans="<<ans<<endl;
+			ans+=a[x];
 		}else{
-			ls=0,flag=true;
-			seg::app(seg::rt[m][d],nl,nr);
-			flag=false;
-			for(int i=1;i<=ls;i++){
-				int x=lst[i]*m+d;
-				for(int j=1;j<=rt;j++){
-					if(j!=m){
-						seg::app(seg::rt[j][x%j],x/j,x/j);
-					}
-				}
-				a[idx[x]]=afterval;
+			act(x);
+		}
+	}
+	inline void workup(int x,int w,int d){//dep[x]%k==d && don't go above w
+		assert(w);
+		if(dep[x]%k!=d){
+			workp(x);
+			if(dep[x]<d||(dep[x]<k&&d==0))return;
+			x=fly(x,(dep[x]%k+k-d)%k);
+		}
+		if(k>rt){
+			for(;dep[x]>=dep[w];x=fly(x,k)){
+				workp(x);
+				if(dep[x]<=k)break;
 			}
+		}else if(op){
+			for(;dep[x]>=dep[w];x=fa[top[x]]){
+				if(dep[x]<d||(dep[x]<k&&d==0))break;
+				x=fly(x,(dep[x]%k+k-d)%k);
+				if(dep[x]<dep[w])break;
+				int y=idx[max(dfn[w],dfn[top[x]])];
+				y=idx[dfn[y]+((d-dep[y])%k+k)%k];
+				assert(dep[y]%k==d);
+				assert(dfn[y]<=dfn[x]&&(dfn[x]-dfn[y])%k==0);
+				ans+=bt[k][dfn[x]%k].ask(dfn[y]/k+1,dfn[x]/k+1);
+			}
+		}else for(;x=uroot(x,k),dep[x]>=dep[w];x=ufa[x][k]){
+			act(x);
 		}
 	}
 	inline void work(){
-		op=ni,ans=0;
-		int u=ni,v=ni,k=ni;
-		int w=lca(u,v);
-		int du=dep[u]%k,dv=(((dep[w]<<1)-dep[u])%k+k)%k;
-		cout<<"working! u="<<u<<" v="<<v<<" du="<<du<<" dv="<<dv<<" k="<<k<<endl;
-		cout<<"\top="<<op<<endl;
-		if(dep[v]%k!=dv){
-			workp(v);
+		op=ni;
+		int u=ni,v=ni,w=lca(u,v);
+		k=ni;
+		ans=0;
+		if(u!=w){
+			workup(u,fly(u,dep[u]-dep[w]-1),dep[u]%k);
 		}
-		cout<<"new lca"<<endl;
-		while(top[u]!=top[v]){
-			cout<<"("<<u<<","<<v<<")"<<endl;
-			if(dep[top[u]]<dep[top[v]]){
-				swap(u,v),swap(du,dv);
-			}
-			workintv(dfn[top[u]],dfn[u],k,((du-dep[top[u]]+dfn[top[u]])%k+k)%k);
-			u=fa[top[u]];
-		}
-		cout<<"out of loop"<<endl;
-		if(u==v){
-			if(dep[w]%k==du){
-				workp(w);
-			}
-		}else{
-			if(dep[u]<dep[v]){
-				swap(u,v),swap(du,dv);
-			}
-			cout<<"("<<u<<","<<v<<")"<<endl;
-			workintv(dfn[v],dfn[u],k,((du-dep[v]+dfn[v])%k+k)%k);
-		}
+		workup(v,w,(((dep[w]<<1)-dep[u])%k+k)%k);
 		if(op){
 			printf("%lld\n",ans);
 		}
@@ -238,7 +174,7 @@ int main(){
 	freopen("down.out","w",stdout);
 #endif
 	int n=ni;
-	rt=min(n,B-1);
+	rt=min(n,B-3);
 	for(int i=1;i<=n;i++){
 		a[i]=next_num<lint>();
 	}
@@ -246,13 +182,21 @@ int main(){
 	for(int i=1;i<n;i++){
 		T::add(ni,ni);
 	}
-	T::fa[1]=0,T::dfs1(1),T::dfs2(1);
+	T::dfs1(1),T::dfs2(1);
+	assert(T::tim==n);
 	for(int i=1;i<=rt;i++){
-		for(int j=0;j<i;j++){//x % i == j
+		for(int j=0;j<i;j++){
 			assert(j<n);
-			seg::rt[i][j]=seg::build(0,(n-1-j)/i,i,j);
+			bt[i][j].init((n-1-j)/i+1);
 		}
 	}
-	for(int tot=ni;tot--;T::work());
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=rt;j++){
+			bt[j][T::dfn[i]%j].add(T::dfn[i]/j+1,a[i]);
+		}
+	}
+	for(int tot=ni;tot--;){
+		T::work();
+	}
 	return 0;
 }
