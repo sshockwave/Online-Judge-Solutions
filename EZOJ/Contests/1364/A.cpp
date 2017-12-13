5eb76576e326ca97825d61b3e71d53dd9bbae977
Kerry Su <i_sukai@live.com>
2017-12-13 15:31:59 +0800

    Accepted. 383ms.

--- EZOJ/Contests/1364/A.cpp
+++ EZOJ/Contests/1364/A.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <algorithm>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -18,54 +19,61 @@ template<class T>inline T next_num(){
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 const int N=60,INF=0x7f7f7f7f;
-int qd[N<<1],qdh,qdt,qu[N<<1],quh,qut;
-int n;
-int a[N],b[N];
-int ans;
-int tp[N];//-1:unvis 0:up 1:down 2:inf
-int stat[N];
+struct BIT{
+	int c[N],n;
+	inline void init(int _n){
+		memset(c+1,0,(n=_n)<<2);
+	}
+	inline void add(int x,int v){
+		for(;x<=n;c[x]+=v,x+=x&-x);
+	}
+	inline int sum(int x){
+		int ans=0;
+		for(;x;ans+=c[x],x^=x&-x);
+		return ans;
+	}
+	inline int ask(int l,int r){
+		return sum(r)-sum(l-1);
+	}
+}B1,B2;
+struct pii{
+	int a,b;
+	inline friend bool operator < (const pii &a,const pii &b){
+		return a.a!=b.a?a.a<b.a:a.b<b.b;
+	}
+}intv[N];
+int ans,its;
 void dfs(int x,int cur){
 	if(cur>ans)return;
-	if(x>n){
-		memset(stat+1,0,n<<2);
-		for(int i=1;i<=n;i++){
-			if(tp[i]!=2)continue;
-			if(stat[b[i]]==0){
-				stat[b[i]]=1;
-			}else{
-				stat[b[i]]=0;
-				for(int j=1;j<i;j++){
-					cur+=stat[b[j]];
-				}
-			}
-		}
-		apmin(ans,cur);
-		return;
-	}
+	if(x>its)return ans=cur,void();
+	int l=intv[x].a,r=intv[x].b;
+	B1.add(r,1);
+	dfs(x+1,cur+min(B1.ask(l+1,r-1),B1.ask(r+1,B1.n)+B2.ask(l+1,B2.n)));
+	B1.add(r,-1);
+	B2.add(r,1);
+	dfs(x+1,cur+min(B2.ask(l+1,r-1),B2.ask(r+1,B2.n)+B1.ask(l+1,B1.n)));
+	B2.add(r,-1);
 }
-int cnt[N];
+int last[N];
 inline int Main(){
-	n=ni;
-	memset(cnt,0,sizeof(cnt));
+	int n=ni;
+	memset(last+1,0,n<<2);
+	B1.init(n),B2.init(n);
+	its=0;
 	for(int i=1;i<=n;i++){
-		cnt[a[i]=ni]++;
-	}
-	int nn=0;
-	for(int i=1;i<=n;i++){
-		if(cnt[a[i]]==2){
-			b[++nn]=a[i];
+		int cur=ni;
+		if(last[cur]){
+			intv[++its]=(pii){last[cur],i};
+		}else{
+			last[cur]=i;
 		}
 	}
-	assert(nn%2==0);
-	if(nn==0)return 0;
-	if(nn==2)return 0;
-	if(nn==4)return 0;
-	return 3;
-	qdh=qdt=quh=qut=0;
-	memset(tp,-1,sizeof(tp));
+	if(its==0)return 0;
+	sort(intv+1,intv+its+1);
+	B1.add(intv[1].b,1);
 	ans=INF;
-	tp[b[1]]=0;
-	qu[quh++]=b[1];
+	dfs(2,0);
+	return ans;
 }
 int main(){
 #ifndef ONLINE_JUDGE
