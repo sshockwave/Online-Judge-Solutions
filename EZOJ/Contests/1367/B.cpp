c5e40646acd69760be600d29112da9e31afa1c00
Kerry Su <i_sukai@live.com>
2017-12-19 17:06:00 +0800

    Accepted. 1589ms.

--- EZOJ/Contests/1367/B.cpp
+++ EZOJ/Contests/1367/B.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <map>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -21,6 +22,32 @@ template<class T>T gcd(T a,T b){
 	return b?gcd(b,a%b):a;
 }
 const int N=100010,O=998244353;
+inline int fpow(int x,int n){
+	assert(n>=0);
+	int a=1;
+	for(;n;n>>=1,x=(lint)x*x%O){
+		if(n&1){
+			a=(lint)a*x%O;
+		}
+	}
+	return a;
+}
+int len,sigma;
+int lst[N],cnt[N],ls=0;
+inline void factor(lint num){
+	memset(cnt,0,sizeof(cnt));
+	for(lint i=2;i*i<=num;i++){
+		lst[ls]=i;
+		for(;num%i==0;cnt[ls]++,num/=i);
+		ls+=cnt[ls]!=0;
+	}
+	if(num>1){
+		lst[ls]=num;
+		cnt[ls]++;
+		ls++;
+	}
+}
+lint lcm=0;
 namespace seg{
 	struct Node;
 	typedef Node* node;
@@ -35,6 +62,7 @@ namespace seg{
 		x->l=l,x->m=(l+r)>>1,x->r=r;
 		if(l==r){
 			x->g=next_num<lint>();
+			lcm=lcm?lcm/gcd(lcm,x->g)*x->g:x->g;
 		}else{
 			x->lson=build(l,x->m);
 			x->rson=build(x->m+1,r);
@@ -49,26 +77,51 @@ namespace seg{
 		return gcd(ask(x->lson,l,x->m),ask(x->rson,x->m+1,r));
 	}
 }
-int pw[N];
+map<lint,int>f,g;
+int rst[N];
+int dfs2(int x,int mu,lint p){
+	if(x==ls)return (lint)mu*fpow(sigma,p%(O-1))%O;
+	lint ans=0;
+	for(int i=0;i<=rst[x];i++,p/=lst[x]){
+		ans+=dfs2(x+1,i==0?mu:i==1?O-mu:0,p);
+	}
+	return ans%O;
+}
+int faccnt=0;
+void dfs1(int x,lint p){
+	if(x==ls)return faccnt++,f[p]=dfs2(0,1,p),void();
+	for(int &i=rst[x]=0;i<=cnt[x];i++,p*=lst[x]){
+		dfs1(x+1,p);
+	}
+}
+int dfs4(int x,lint p){
+	if(x==ls)return (lint)(len/p)*f[p]%O;
+	lint ans=0;
+	for(int i=0;i<=rst[x];i++,p*=lst[x]){
+		ans+=dfs4(x+1,p);
+	}
+	return ans%O;
+}
+void dfs3(int x,lint b){
+	if(x==ls)return g[b]=dfs4(0,1),void();
+	for(int &i=rst[x]=0;i<=cnt[x];i++,b*=lst[x]){
+		dfs3(x+1,b);
+	}
+}
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("heiten.in","r",stdin);
 	freopen("heiten.out","w",stdout);
 #endif
-	int n=ni,len=ni;
-	pw[0]=1,pw[1]=ni%O;
+	int n=ni;
+	len=ni,sigma=ni;
 	int tot=ni;
-	for(int i=2;i<=len;i++){
-		pw[i]=(lint)pw[i-1]*pw[1]%O;
-	}
 	seg::rt=seg::build(1,n);
+	factor(lcm);
+	dfs1(0,1),dfs3(0,1);
 	while(tot--){
 		int l=ni,r=ni;
-		lint ans=0,g=seg::ask(seg::rt,l,r);
-		for(int i=1;i<=len;i++){
-			ans+=pw[gcd((lint)i,g)];
-		}
-		printf("%lld\n",ans%O);
+		printf("%d\n",g[seg::ask(seg::rt,l,r)]);
 	}
 	return 0;
 }
