c015b2c39859ce9284f027baa8aecf46a3e6f8c9
Kerry Su <i_sukai@live.com>
2017-12-19 15:41:10 +0800

    Accepted. 571ms.

--- EZOJ/Contests/1367/C.cpp
+++ EZOJ/Contests/1367/C.cpp
@@ -3,7 +3,6 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
-#include <fstream>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -18,7 +17,7 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int N=200010,O=370137601,INF=0x7f7f7f7f;
+const int N=200010,O=370137601,INF=0x7f7f7f7f,K=10;
 inline int fpow(int x,int n){
 	int a=1;
 	for(;n;n>>=1,x=(lint)x*x%O){
@@ -31,29 +30,20 @@ inline int fpow(int x,int n){
 inline int inv(int x){
 	return fpow(x,O-2);
 }
-int statcnt[4]={0,2,4,9},scnt,m,k;
+int statcnt[4]={0,2,4,9},m,k,sm;
+int str[9][9];
 struct Mat{
-	int *a[90];
-	inline void init(){
-		for(int i=0;i<scnt;i++){
-			a[i]=new int[scnt];
-		}
-	}
+	int a[9][9];
 	inline void clr(){
-		for(int i=0;i<scnt;i++){
-			memset(a[i],0,scnt<<2);
-		}
-	}
-	inline void operator = (const Mat &b){
-		for(int i=0;i<scnt;i++){
-			memcpy(a[i],b.a[i],scnt<<2);
+		for(int i=0;i<sm;i++){
+			memset(a[i],0,sm<<2);
 		}
 	}
 	inline void getmul(const Mat &x,const Mat &y){
-		for(int i=0;i<scnt;i++){
-			for(int j=0;j<scnt;j++){
+		for(int i=0;i<sm;i++){
+			for(int j=0;j<sm;j++){
 				lint tmp=0;
-				for(int k=0;k<scnt;k++){
+				for(int k=0;k<sm;k++){
 					tmp+=(lint)x.a[i][k]*y.a[k][j]%O;
 				}
 				a[i][j]=tmp%O;
@@ -62,35 +52,31 @@ struct Mat{
 	}
 	inline int eval(){
 		lint ans=0;
-		for(int i=0;i<statcnt[m];i++){
+		for(int i=0;i<sm;i++){
 			ans+=a[i][0];
 		}
 		return ans%O;
 	}
-}trans,tmpmat;
-int str[9][9];
-inline void gtrans(Mat &trans,int prob[]){
-	static int statprob[9];
-	for(int i=0,ti=1<<m;i<ti;i++){
-		lint tmp=1;
-		for(int j=0;j<m;j++){
-			(tmp*=(i>>j)&1?O+1-prob[j]:prob[j])%=O;
+	inline void gtrans(int prob[],int w[K]){
+		static int statprob[9];
+		for(int i=0,ti=1<<m;i<ti;i++){
+			lint tmp=1;
+			for(int j=0;j<m;j++){
+				(tmp*=(i>>j)&1?O+1-prob[j]:prob[j])%=O;
+			}
+			statprob[i]=tmp;
 		}
-		statprob[i]=tmp;
-	}
-	statprob[8]=statprob[5];
-	trans.clr();
-	int tmp=statcnt[m];
-	for(int i=0;i<k;i++){
-		for(int j=0;j<tmp;j++){//i*tmp+j
-			for(int l=0;l<tmp;l++){//to->
+		statprob[8]=statprob[5];
+		clr();
+		for(int j=0;j<sm;j++){
+			for(int l=0;l<sm;l++){
 				if(str[j][l]<INF){
-					trans.a[(i+str[j][l]+k)%k*tmp+l][i*tmp+j]=statprob[l];
+					a[l][j]=(lint)statprob[l]*w[str[j][l]]%O;
 				}
 			}
 		}
 	}
-}
+}trans[19][K];
 bool mp[2][3];
 int bln[2][3];
 void dfs(int x,int y,bool flag){
@@ -129,11 +115,11 @@ inline void putstat(bool mp[],int s){
 	}
 }
 inline void gstr(){
-	for(int i=0;i<statcnt[m];i++){
+	for(int i=0;i<sm;i++){
 		memset(mp,0,sizeof(mp));
 		putstat(mp[0],i);
 		int oldv=gcomp(i);
-		for(int j=0;j<statcnt[m];j++){
+		for(int j=0;j<sm;j++){
 			putstat(mp[1],j);
 			int newv=gcomp(i);
 			if(j==5&&bln[1][0]==bln[1][2]){
@@ -141,45 +127,88 @@ inline void gstr(){
 			}else if(j==8&&bln[1][0]!=bln[1][2]){
 				str[i][j]=INF;
 			}else{
-				str[i][j]=newv-oldv;
+				str[i][j]=(newv-oldv+k)%k;
 			}
 		}
 	}
 }
 int prob[N][3];
+int om[K][K],io[K];
 namespace seg{
 	struct Node;
 	typedef Node* node;
 	struct Node{
 		int l,m,r;
 		node lson,rson;
-		Mat mat;
+		Mat *mat[K];
+		int sh;
+		bool vis;
+		inline Mat& gmat(int i){
+			return vis?*mat[i]:trans[sh][i];
+		}
 		inline void up(){
-			mat.getmul(rson->mat,lson->mat);
+			assert(vis);
+			for(int i=0;i<k;i++){
+				mat[i]->getmul(lson->gmat(i),rson->gmat(i));
+			}
 		}
 	}*rt;
 	node build(int l,int r){
 		static node n=new Node[N<<1];
 		node x=n++;
 		x->l=l,x->m=(l+r)>>1,x->r=r;
-		x->mat.init();
+		x->vis=false;
 		if(l==r){
-			x->mat=trans;
+			x->sh=1;
 		}else{
 			x->lson=build(l,x->m);
 			x->rson=build(x->m+1,r);
-			x->up();
+			assert(x->lson->sh==x->rson->sh);
+			x->sh=x->lson->sh+1;
 		}
 		return x;
 	}
 	void alt(node x,int p){
-		if(x->l==x->r){
-			gtrans(x->mat,prob[p]);
-		}else{
+		if(!x->vis){
+			x->vis=true;
+			for(int i=0;i<k;i++){
+				x->mat[i]=new Mat;
+			}
+		}
+		if(x->l<x->r){
 			alt(p<=x->m?x->lson:x->rson,p);
 			x->up();
+		}else for(int i=0;i<k;i++){
+			x->mat[i]->gtrans(prob[p],om[i]);
 		}
 	}
+}
+inline void idft(int a[],int n){
+	static int b[K];
+	for(int i=0;i<n;i++){
+		int w=1,wn=io[i];
+		lint tmp=0;
+		for(int j=0;j<n;j++,w=(lint)w*wn%O){
+			tmp+=(lint)a[j]*w%O;
+		}
+		b[i]=tmp%O;
+	}
+	int iv=inv(n);
+	for(int i=0;i<n;i++){
+		a[i]=(lint)b[i]*iv%O;
+	}
+}
+inline int work(){
+	static int a[K];
+	for(int i=0;i<k;i++){
+		a[i]=seg::rt->gmat(i).eval();
+	}
+	idft(a,k);
+	return a[0];
+}
+inline int nextfrac(){
+	int p=ni,q=ni;
+	return (lint)p*inv(q)%O;
 }
 int main(){
 #ifndef ONLINE_JUDGE
@@ -187,36 +216,33 @@ int main(){
 	freopen("kami.out","w",stdout);
 #endif
 	int n=ni;
-	m=ni,k=ni;
+	m=ni,k=ni,sm=statcnt[m];
 	int tot=ni;
 	gstr();
-	scnt=statcnt[m]*k;
-	trans.init();
+	for(int i=0,w=1,wn=fpow(37,(O-1)/k);i<k;i++,w=(lint)w*wn%O){
+		om[i][0]=1,io[i]=inv(w);
+		for(int j=1;j<k;j++){
+			om[i][j]=(lint)om[i][j-1]*w%O;
+		}
+	}
 	{
-		int p=ni%O;
-		p=(lint)p*inv(ni%O)%O;
-		for(int i=1;i<=n;i++){
+		int p=nextfrac();
+		for(int i=0;i<n;i++){
 			for(int j=0;j<m;j++){
 				prob[i][j]=p;
 			}
 		}
-		gtrans(trans,prob[1]);
 	}
-	if(tot==0){
-		assert((n^(n&-n))==0);
-		tmpmat.init();
-		for(;n>1;n>>=1){
-			tmpmat.getmul(trans,trans);
-			trans=tmpmat;
+	for(int i=0;i<k;i++){
+		trans[1][i].gtrans(prob[0],om[i]);
+		for(int j=2;j<=18;j++){
+			trans[j][i].getmul(trans[j-1][i],trans[j-1][i]);
 		}
-		printf("%d\n",trans.eval());
-		return 0;
 	}
-	seg::rt=seg::build(1,n);
-	while(printf("%d\n",seg::rt->mat.eval()),tot--){
-		int x=ni,y=ni-1;
-		prob[x][y]=ni%O;
-		prob[x][y]=(lint)prob[x][y]*inv(ni%O)%O;
+	seg::rt=seg::build(0,n-1);
+	while(printf("%d\n",work()),tot--){
+		int x=ni-1,y=ni-1;
+		prob[x][y]=nextfrac();
 		seg::alt(seg::rt,x);
 	}
 	return 0;
