c31eb78777291d76b2af9fec3ad5ae3afc1357d5
Kerry Su <i_sukai@live.com>
2017-12-27 16:44:03 +0800

    Init. ETT.

--- EZOJ/Contests/1370/C.cpp
+++ EZOJ/Contests/1370/C.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#define NDEBUG
 #include <cassert>
 #include <cctype>
-#include <map>
+#include <cstdlib>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -20,132 +19,281 @@ template<class T>inline T next_num(){
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 const int N=400010;
+struct Val{
+	int val,cnt;
+	inline Val operator + (int x){
+		return (Val){val+x,cnt};
+	}
+	inline Val operator - (int x){
+		return (Val){val-x,cnt};
+	}
+	inline friend bool operator < (const Val &a,const Val &b){
+		return a.val<b.val;
+	}
+	inline friend bool operator > (const Val &a,const Val &b){
+		return a.val>b.val;
+	}
+	inline friend Val min(const Val &a,const Val &b){
+		return a<b?a:b<a?b:(Val){a.val,a.cnt+b.cnt};
+	}
+	inline friend Val max(const Val &a,const Val &b){
+		return a>b?a:b>a?b:(Val){a.val,a.cnt+b.cnt};
+	}
+};
+struct info{
+	int len,top,sect;
+	Val l,r,cur;
+	inline Val eval(int x){
+		return max(l-x,r+x);
+	}
+	inline void chroot(int pos){
+		top=pos;
+		cur=eval(pos);
+	}
+	inline pair<int,int>groot(int lenl,int lenr){
+		assert(len==lenl+lenr-(sect<<1));
+		int fir=lenl-sect;
+		return make_pair(min(top,fir),sect+max(top-fir,0));
+	}
+	inline void gcomb(info &a,info &b){
+		sect=min(a.len-a.top,b.top);
+		len=a.len+b.len-(sect<<1);
+		int fir=a.len-sect;
+		l=max(a.l,b.eval(sect)+fir);
+		r=max(a.eval(fir)-fir,b.r+sect-fir);
+		assert(a.top<=fir);
+		assert(b.top>=sect);
+		if(a.top<fir){
+			assert(b.top==sect);
+			chroot(a.top);
+		}else if(b.top>sect){
+			assert(a.top==sect);
+			chroot(b.top);
+		}else{
+			chroot(a.top);
+		}
+	}
+};
 namespace T{
+	const int N=400010<<1;
 	struct Node;
 	typedef Node* node;
+	node null;
 	struct Node{
-		union{
-			struct{
-				node lson,rson;
-			};
-			node son[2];
-		};
-		node fa;
-		int size;
-		bool rev;
-		map<int,int>m;
-		int hei[2],cnt[2];
-		int thei,tcnt;
-		inline void putinfo(node x){
-			if(x->cnt[0]==0)return;
-			m[x->hei[0]]+=x->cnt[0];
-			map<int,int>::iterator it=--m.end();
-			thei=it->first+1;
-			tcnt=it->second;
-		}
-		inline void delinfo(node x){
-			if(x->cnt[0]==0)return;
-			m[x->hei[0]]-=x->cnt[0];
-			map<int,int>::iterator it;
-			for(;it=--m.end(),it->second==0;m.erase(it));
-			thei=it->first+1;
-			tcnt=it->second;
-		}
-		inline void putrev(){
-			swap(hei[0],hei[1]);
-			swap(cnt[0],cnt[1]);
-			swap(lson,rson);
-			rev=!rev;
+		//treap info
+		node fa,lson,rson;
+		int size,prior;
+		//chain info
+		node pre,nxt;
+		//edge info
+		int fr,to;
+		//route info
+		info i,lm,all;
+		bool tag;
+		inline void putroot(int x){
+			all.chroot(x);
+			tag=true;
 		}
 		inline void down(){
-			if(rev){
-				rev=false;
-				lson->putrev();
-				rson->putrev();
+			if(!tag)return;
+			tag=false;
+			if(rson==null){
+				lm=all;
+			}else{
+				pair<int,int>rt=all.groot(lm.len,rson->all.len);
+				lm.chroot(rt.first);
+				rson->putroot(rt.second);
 			}
+			if(lson==null){
+				i=lm;
+			}else{
+				pair<int,int>rt=all.groot(lson->all.len,i.len);
+				lson->putroot(rt.first);
+				i.chroot(rt.second);
+			}
+			assert(i.len==1);
 		}
 		inline void up(){
+			assert(!tag);
 			size=lson->size+1+rson->size;
-			for(int d=0;d<2;d++,swap(lson,rson)){
-				hei[d]=lson->hei[d];
-				cnt[d]=lson->cnt[d];
-				int lsize=lson->size;
-				if(thei+lsize>hei[d]){
-					hei[d]=thei+lsize;
-					cnt[d]=0;
-				}
-				if(thei+lsize==hei[d]){
-					cnt[d]+=tcnt;
-				}
-				if(rson->cnt[d]){
-					lsize++;
-					if(rson->hei[d]+lsize>hei[d]){
-						hei[d]=rson->hei[d]+lsize;
-						cnt[d]=0;
-					}
-					if(rson->hei[d]+lsize==hei[d]){
-						cnt[d]+=rson->cnt[d];
-					}
-				}
+			if(lson==null){
+				lm=i;
+			}else{
+				lm.gcomb(lson->all,i);
+				lson->fa=this;
+			}
+			if(lson==null){
+				all=lm;
+			}else{
+				all.gcomb(lm,rson->all);
+				rson->fa=this;
 			}
 		}
-		inline int side(){
-			return fa->lson==this?0:fa->rson==this?1:-1;
-		}
-		inline void rot(){
-			bool d=!side();
-			son[d]->fa=fa,fa->son[!d]=son[d],son[d]=fa;
-			int g=fa->side();
-			if(~g){
-				fa->fa->son[g]=this;
-			}
-			fa=fa->fa,son[d]->fa=this;
-			son[d]->up(),up();
-		}
-		inline void splay();
-	}pool[N],Null;
-	node null=&Null;
-	inline ostream & operator << (ostream & out,node b){
-		if(b==null){
-			out<<"null";
+	}Null,pool[N];
+	node pt[N],*n=pt;
+	inline node coup(const node &x){
+		return pool+((x-pool)^1);
+	}
+	inline node nn(node x=null){
+		return **n=*x,(*n)->prior=rand(),*n++;
+	}
+	inline void del(node x){
+		*--n=x;
+	}
+	inline node mg(node u,node v){
+		if(u==null||v==null)return u!=null?u:v;
+		if(u->prior>v->prior){
+			u->down();
+			u->rson=mg(u->rson,v);
+			u->up();
+			return u;
 		}else{
-			out<<(b-pool);
-		}
-		return out;
-	}
-	void draw(node x){
-		if(x!=null){
-			draw(x->fa),x->down();
+			v->down();
+			v->lson=mg(u,v->lson);
+			v->up();
+			return v;
 		}
 	}
-	inline void Node::splay(){
-		draw(this);
-		for(int d,g;d=side(),~d;){
-			g=fa->side();
-			g==d?fa->rot(),rot():g==!d?rot(),rot():rot();
-		}
-	}
-	inline void acc(node x){
-		for(node s=null;x!=null;s=x,x=x->fa){
-			x->splay();
-			x->putinfo(x->rson);
-			x->delinfo(s);
-			x->rson=s;
+	inline void sp(node x,int rnk,node &l,node &r){
+		if(x==null)return l=null,r=null,void();
+		x->down();
+		if(rnk<=x->lson->size){
+			r=x;
+			sp(x->lson,rnk,l,x->lson);
+			x->up();
+		}else{
+			l=x;
+			sp(x->rson,rnk-x->lson->size-1,x->rson,r);
 			x->up();
 		}
 	}
-	inline node nn(node x=null){
-		static node n=pool;
-		return *n=*x,n++;
+	inline node gnode(node x,int rnk){
+		x->down();
+		if(rnk<=x->lson->size)return gnode(x->lson,rnk);
+		if(rnk==x->lson->size+1)return x;
+		return gnode(x->rson,rnk-x->lson->size-1);
+	}
+	node currt;
+	int grank(node x,bool flag=true){
+		int rank=0;
+		if(x->fa==null){
+			currt=x;
+		}else{
+			rank=grank(x->fa,x->fa->rson==x);
+		}
+		return rank+(flag?x->lson->size+1:0);
 	}
 	inline void init(){
+		null=&Null;
 		memset(null,0,sizeof(Null));
 		null->fa=null->lson=null->rson=null;
+		null->pre=null->nxt=null;
+		for(int i=0;i<N;i++){
+			pt[i]=pool+i;
+		}
 	}
 }
-T::node node[N];
+T::node chain[N];//out edge
+inline int gedge(int x){//end edge rank
+	using namespace T;
+	node rtl,rtr;
+	int rnk=grank(chain[x]);
+	sp(currt,rnk-1,rtl,rtr);
+	int len=rnk=0;
+	for(node p=rtr;p!=null;p=p->rson){
+		p->down();
+		if(p->lson!=null){
+			if(p->lson->all.top>len){
+				p=p->lson;
+				continue;
+			}
+			rnk+=p->lson->size;
+			len-=p->lson->all.top;
+			len+=p->lson->all.len-p->lson->all.top;
+		}
+		if(p->i.top>len)break;
+		rnk++;
+		len+=p->i.top?-1:1;
+		assert(len>=0);
+	}
+	assert(len==0);
+	rnk+=rtl->size;
+	mg(rtl,rtr)->fa=null;
+	return rnk;
+}//currt ready
+inline void chroot(int x){
+	using namespace T;
+	if(chain[x]==null)return;//no son no fa
+	int rnk=gedge(x);
+	node p=gnode(currt,rnk);
+	if(p->i.top){//has son
+		p=pool[(p-pool)^1].nxt;
+		if(p->i.top){//has fa
+			rnk=grank(coup(p));
+		}else return;//no fa
+	}
+	node rtl,rtr;
+	sp(currt,rnk,rtl,rtr);
+	mg(rtr,rtl)->fa=null;
+}//currt ready
+inline void insafter(T::node x,T::node n){
+	n->pre=x,n->nxt=x->nxt;
+	n->pre->nxt=n->nxt->pre=n;
+}
+inline void lnk(int fa,int x){
+	using namespace T;
+	node fx=nn(),xf=nn(),p;
+	fx->fr=fa,fx->to=x;
+	xf->fr=x,xf->to=fa;
+	if(chain[x]==T::null){
+		chain[x]=xf;
+		xf->pre=xf->nxt=xf;
+		p=mg(fx,xf);
+	}else{
+		grank(chain[x]);
+		insafter(coup(gnode(currt,currt->size)),xf);
+		p=mg(mg(fx,currt),xf);
+	}
+	if(chain[fa]==T::null){
+		chain[fa]=fx;
+		fx->pre=fx->nxt=fx;
+	}else{
+		node rtl,rtr;
+		int rnk=gedge(fa);
+		sp(currt,rnk,rtl,rtr);
+		insafter(coup(gnode(rtl,rtl->size)),fx);
+		p=mg(mg(rtl,p),rtr);
+	}
+	p->fa=null;
+}
+inline void delnode(T::node x){
+	if(x->nxt==x){
+		chain[x->fr]=T::null;
+	}else{
+		x->pre->nxt=x->nxt;
+		x->nxt->pre=x->pre;
+		if(chain[x->fr]==x){
+			chain[x->fr]=x->nxt;
+		}
+	}
+	T::del(x);
+}
+inline void cut(int x){
+	using namespace T;
+	int rnk=gedge(x);
+	node rtl,rtm,rtr;
+	sp(currt,rnk,rtl,rtr);
+	node p;
+	sp(rtr,1,p,rtr);
+	rtl->fa=null;
+	rnk=grank(coup(p));
+	delnode(p);
+	delnode(coup(p));
+	sp(rtl,rnk,rtl,rtm);
+	sp(rtl,rtl->size-1,rtl,p);
+	mg(mg(rtl,rtm),rtr)->fa=null;
+}
 int fa[N];
-int ans=0;
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("darkforest.in","r",stdin);
@@ -153,59 +301,34 @@ int main(){
 #endif
 	int n=ni,tot=ni;
 	T::init();
-	{
-		T::node tmp=T::nn();
-		tmp->cnt[0]=1;
-		tmp->hei[0]=-1;
-		for(int i=1;i<=n;i++){
-			node[i]=T::nn();
-			node[i]->putinfo(tmp);
-			node[i]->up();
-		}
+	for(int i=1;i<=n;i++){
+		chain[i]=T::null;
 	}
 	for(int i=1;i<=n;i++){
-		T::node f=node[fa[i]=ni];
-		if(fa[i]!=0){
-			T::acc(f);
-			f->splay();
-			node[i]->splay();
-			node[i]->fa=f;
-			f->rson=node[i];
-			f->up();
+		fa[i]=ni;
+		if(fa[i]){
+			lnk(fa[i],i);
 		}
 	}
+	int ans=0;
 	for(int i=1;i<=n;i++){
 		if(fa[i]==0){
-			T::acc(node[i]);
-			node[i]->splay();
-			ans+=node[i]->cnt[0];
+			if(chain[i]==T::null){
+				ans++;
+			}else{
+				T::grank(chain[i]);
+				assert(T::currt->all.len==0&&T::currt->all.top==0);
+				ans+=T::currt->all.eval(0).cnt;
+			}
 		}
 	}
 	while(printf("%d\n",ans),tot--){
 		if(ni==1){
-			T::node x=node[ni],y=node[ni];
-			T::acc(x);
-			x->splay();
-			ans-=x->cnt[0];
-			x->putrev();//chroot x
-			T::acc(y);
-			y->splay();
-			ans-=y->cnt[0];
-			x->fa=y;
-			y->rson=x;
-			y->up();
-			ans+=y->cnt[0];
+			int x=ni,y=ni;
+			chroot(x);
+			lnk(y,x);
 		}else{
-			T::node x=node[ni];
-			T::acc(x);
-			x->splay();
-			if(x->lson!=T::null){
-				ans-=x->cnt[0];
-				ans+=x->lson->cnt[0];
-				x->lson=x->lson->fa=T::null;
-				x->up();
-				ans+=x->cnt[0];
-			}
+			cut(ni);
 		}
 	}
 	return 0;
