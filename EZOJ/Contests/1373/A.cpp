f7beea053a295517521c749535523ad19b031bd6
Kerry Su <i_sukai@live.com>
2018-01-01 16:13:42 +0800

    Accepted. 235ms.

--- EZOJ/Contests/1373/A.cpp
+++ EZOJ/Contests/1373/A.cpp
@@ -1,8 +1,8 @@
+#include <iostream>
 #include <cstdio>
 #include <cstring>
 #include <cassert>
 #include <cctype>
-#include <queue>
 #include <vector>
 using namespace std;
 typedef long long lint;
@@ -18,16 +18,70 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int N=5010;
-int fa[N];
-int c[N],d[N];
-struct pii{
-	int v,w;
-	inline friend bool operator < (const pii &a,const pii &b){
-		return a.v!=b.v?a.v>b.v:a.w<b.w;
+const int N=5010,V=32768,logN=14;
+int fa[N],v[N],w[N];
+struct Query{
+	int v,id;
+};
+vector<Query>q[N];
+int mx=0,ans[N];
+namespace T{
+	const int E=N;
+	int to[E],bro[E],head[N],e=0;
+	int size[N],bal[N];
+	int pool[logN][V],(*ps)[V]=pool,*f[N];
+	inline void init(){
+		memset(head,-1,sizeof(head));
+		memset(size,0,sizeof(size));
+		memset(f,0,sizeof(f));
 	}
-}item[N];
-vector<pii>vec[N];
+	inline void ae(int u,int v){
+		to[e]=v,bro[e]=head[u],head[u]=e++;
+	}
+	void gcenter(int x,int &g,int n){
+		size[x]=1,bal[x]=0;
+		for(int i=head[x],v;~i;i=bro[i]){
+			if(f[v=to[i]]==0){
+				gcenter(v,g,n);
+				size[x]+=size[v];
+				apmax(bal[x],size[v]);
+			}
+		}
+		apmax(bal[x],n-size[x]);
+		if(g==0||bal[x]<bal[g]){
+			g=x;
+		}
+	}
+	inline void dp(int f[],int nf[],int v,int w){
+		memcpy(nf,f,(mx+1)<<2);
+		assert(v);
+		for(int i=v;i<=mx;i++){
+			apmax(nf[i],max(nf[i-1],nf[i-v]+w));
+		}
+	}
+	void pdiv(int rt,int n){
+		int x=0;
+		gcenter(rt,x,n);
+		f[x]=*ps++;
+		if(x==rt){
+			dp(f[fa[x]],f[x],v[x],w[x]);
+		}else{
+			pdiv(rt,size[rt]-size[x]);
+		}
+		for(vector<Query>::iterator it=q[x].begin();it!=q[x].end();it++){
+			ans[it->id]=f[x][it->v];
+		}
+		for(int i=head[x],v;~i;i=bro[i]){
+			if(f[v=to[i]]){
+				dp(f[x],f[v],::v[v],w[v]);
+			}else{
+				pdiv(v,size[v]);
+			}
+		}
+		ps--;
+	}
+}
+int c[N];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("shop.in","r",stdin);
@@ -35,48 +89,24 @@ int main(){
 #endif
 	int n=ni,m=ni;
 	for(int i=1;i<=n;i++){
-		item[i]=(pii){ni,ni};
+		v[i]=ni,w[i]=ni;
 	}
-	for(int i=1;i<=n;i++){
-		fa[i]=i==1?0:ni;
+	T::init();
+	fa[1]=0;
+	for(int i=2;i<=n;i++){
+		T::ae(fa[i]=ni,i);
 	}
-	int mx=0;
 	for(int i=1;i<=m;i++){
 		c[i]=ni;
 		apmax(mx,c[i]);
 	}
-	vec[0].push_back((pii){0,0});
-	for(int i=1;i<=n;i++){
-		priority_queue<pii>q;
-		for(vector<pii>::iterator it=vec[fa[i]].begin(),ti=vec[fa[i]].end();it!=ti;it++){
-			q.push(*it);
-		}
-		int last=-1;
-		while(!q.empty()){
-			pii t=q.top();
-			q.pop();
-			assert(t.v<=mx);
-			if(t.w<=last)continue;
-			last=t.w;
-			vec[i].push_back(t);
-			if(t.v+item[i].v<=mx){
-				q.push((pii){t.v+item[i].v,t.w+item[i].w});
-			}
-		}
-	}
 	for(int i=1;i<=m;i++){
-		int tmp=ni;
-		vector<pii>&vc=vec[tmp];
-		int l=0,r=vc.size()-1;
-		while(l<r){
-			int m=((l+r)>>1)+1;
-			if(vc[m].v<=c[i]){
-				l=m;
-			}else{
-				r=m-1;
-			}
-		}
-		printf("%d\n",vc[l].w);
+		q[ni].push_back((Query){c[i],i});
 	}
-	return 0;
+	T::f[0]=*T::ps++;
+	memset(T::f[0],0,mx<<2);
+	T::pdiv(1,n);
+	for(int i=1;i<=m;i++){
+		printf("%d\n",ans[i]);
+	}
 }
