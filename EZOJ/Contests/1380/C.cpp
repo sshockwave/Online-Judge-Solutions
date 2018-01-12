94b3ecd6acebe4f956016234faa8d2bd9ad1a151
Kerry Su <i_sukai@live.com>
2018-01-12 21:38:58 +0800

    Accepted. 2735ms.

--- EZOJ/Contests/1380/C.cpp
+++ EZOJ/Contests/1380/C.cpp
@@ -20,12 +20,12 @@ template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 const int N=4010,M=200010,INF=0x7f7f7f7f;
 const lint LINF=0x7f7f7f7f7f7f7f7fll;
 namespace G{
-	const int N=::N+::M,E=(N+::M*2)*2;
-	int to[E],bro[E],head[N],e=0,s,t;
+	const int N=::N,E=(N+::M)*2;
+	int to[E],bro[E],head[N],e,s,t;
 	lint cap[E];
 	inline void init(int n){
-		memset(head+1,-1,(n+1)<<2);
-		s=n+1,t=n+2;
+		memset(head+1,-1,(n+2)<<2);
+		e=0,s=n+1,t=n+2;
 	}
 	inline void ae(int u,int v,lint c){
 		to[e]=v,bro[e]=head[u],cap[e]=c,head[u]=e++;
@@ -33,14 +33,14 @@ namespace G{
 	inline void add(int u,int v,lint c){
 		ae(u,v,c),ae(v,u,0);
 	}
-	int dis[N],que[N];
+	int dis[N],que[N],cur[N];
 	inline void bfs(){
 		memset(dis+1,127,t<<2);
 		int qh=0,qt=0;
 		dis[s]=0,que[qt++]=s;
 		while(qh<qt){
 			int x=que[qh++];
-			for(int i=head[x],v;~i;i=bro[i]){
+			for(int i=cur[x]=head[x],v;~i;i=bro[i]){
 				if(cap[i]&&dis[v=to[i]]==INF){
 					dis[v]=dis[x]+1,que[qt++]=v;
 				}
@@ -50,14 +50,12 @@ namespace G{
 	lint aug(int x,lint a){
 		if(x==t)return a;
 		lint r=a,d;
-		for(int i=head[x],v;r&&~i;i=bro[i]){
+		for(int &i=cur[x],v;~i;i=bro[i]){
 			if(cap[i]&&dis[v=to[i]]==dis[x]+1){
 				d=aug(v,min(cap[i],r));
 				cap[i]-=d,cap[i^1]+=d,r-=d;
+				if(r==0)break;
 			}
-		}
-		if(r){
-			dis[x]=INF;
 		}
 		return a-r;
 	}
@@ -72,34 +70,46 @@ int a[N],b[M];
 struct Edge{
 	int u,v;
 }e[M];
-inline lint check(int x){
-	G::init(n+m);
-	for(int i=1;i<=n;i++){
-		G::add(i,G::t,(lint)a[i]*x);
-	}
+lint indeg[N],oudeg[N];
+inline lint calc(int w){
+	memset(indeg+1,0,n<<3);
+	memset(oudeg+1,0,n<<3);
+	G::init(n);
 	lint sum=0;
 	for(int i=1;i<=m;i++){
-		sum+=b[i];
-		G::add(G::s,i+n,b[i]);
-		G::add(i+n,e[i].u,LINF);
-		G::add(i+n,e[i].v,LINF);
+		indeg[e[i].u]+=b[i];
+		indeg[e[i].v]+=b[i];
+		G::ae(e[i].u,e[i].v,b[i]);
+		G::ae(e[i].v,e[i].u,b[i]);
+		sum+=b[i]<<1;
 	}
-	sum-=G::dinic();
-	return sum;
+	for(int i=1;i<=n;i++){
+		oudeg[i]=(lint)a[i]*w;
+		lint d=min(indeg[i],oudeg[i]);
+		sum-=d,indeg[i]-=d,oudeg[i]-=d;
+		if(indeg[i]){
+			G::add(G::s,i,indeg[i]);
+		}
+		if(oudeg[i]){
+			G::add(i,G::t,oudeg[i]);
+		}
+	}
+	return sum-G::dinic();
 }
 inline int Main(){
 	n=ni,m=ni;
 	int l=0,r=0;
 	for(int i=1;i<=n;i++){
-		a[i]=ni;
+		a[i]=ni<<1;
 	}
 	for(int i=1;i<=m;i++){
 		e[i]=(Edge){ni,ni};
-		r+=b[i]=ni;
+		b[i]=ni;
+		r+=b[i]<<1;
 	}
 	while(l<r){
 		int m=((l+r)>>1)+1;
-		if(check(m)>0){//TODO
+		if(calc(m)>0){
 			l=m;
 		}else{
 			r=m-1;
