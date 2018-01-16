d45815829b43049a5c464703f2e69350748e12ed
Kerry Su <i_sukai@live.com>
2018-01-16 10:13:07 +0800

    Accepted. 2015ms.

--- EZOJ/Contests/1382/C.cpp
+++ EZOJ/Contests/1382/C.cpp
@@ -4,6 +4,7 @@
 #define NDEBUG
 #include <cassert>
 #include <cctype>
+#include <cmath>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -18,53 +19,61 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int N=1000010,O=1004535809;
-int pri[N],ps=0,sqcnt=1;
-int phi[N],mu[N],f[N],sqf[N];
+const int N=5000010,O=1004535809;//TODO
+inline int grt(lint n){
+	lint x=sqrt(n);
+	for(;x*x<n;x++);
+	for(;x*x>n;x--);
+	return x;
+}
+int pri[N],ps=0;
+int mu[N];
+int mnpw[N],mnp[N];
+int f[N],g[N],h[N];
 bool np[N];
-int g[N];
 inline void sieve(int n){
 	memset(np,0,sizeof(np));
-	phi[1]=1,mu[1]=1,f[1]=1,sqf[1]=1;
+	mu[1]=1;
+	memset(h,0,sizeof(h));
+	for(lint i=1,sqi;sqi=i*i,sqi<n;i++){
+		for(lint j=1,sqj;sqj=j*j,sqi+sqj<=n;j++){
+			(h[sqi+sqj]+=i)%=O;
+		}
+	}
+	g[1]=1;
 	for(int i=2;i<=n;i++){
 		if(!np[i]){
 			pri[ps++]=i;
-			phi[i]=i-1;
 			mu[i]=-1;
-			sqf[i]=i;
+			mnpw[i]=i;
+			mnp[i]=i;
 		}
-		if(sqf[i]==1){
-			sqcnt++;
-			assert(sqcnt*sqcnt==i);
-			f[i]=phi[sqcnt]*sqcnt;
-		}else{
-			f[i]=(lint)f[i/sqf[i]]*sqf[i]%O;
-		}
+		g[i]=((lint)g[i/mnp[i]]*mnp[i]+g[i/mnpw[i]])%O;
 		for(int j=0,p,t;j<ps&&(p=pri[j],t=i*p,t<=n);j++){
 			np[t]=true;
+			mnp[t]=p;
 			if(i%p){
-				phi[t]=phi[i]*(p-1);
+				mnpw[t]=p;
 				mu[t]=-mu[i];
-				sqf[t]=sqf[i]*p;
 			}else{
-				phi[t]=phi[i]*p;
+				mnpw[t]=mnpw[i]*p;
 				mu[t]=0;
-				sqf[t]=sqf[i/p];
 				break;
 			}
 		}
 	}
+	memset(f,0,sizeof(f));
 	for(int i=1,sqi;sqi=i*i,sqi<=n;i++){
-		for(int j=1,sqj;sqj=j*j,sqi+sqj<=n;j++){
-			int p=sqi+sqj;
-			for(int k=p;k<=n;k+=p){
-				g[k]=(g[k]+i)%O;
-			}
+		if(mu[i]==0)continue;
+		int tmp=(lint)(O+mu[i])*i%O;
+		for(int j=1,k=sqi;k<=n;j++,k+=sqi){
+			f[k]=(f[k]+(lint)h[j]*tmp%O)%O;
 		}
 	}
 	for(int i=1;i<=n;i++){
 		f[i]=(f[i-1]+f[i])%O;
 		g[i]=(g[i-1]+g[i])%O;
+		h[i]=(h[i-1]+h[i])%O;
 	}
 }
 inline int _n2(lint n){
@@ -77,23 +86,35 @@ inline int _n2(lint n){
 	}
 	return m*n%O;
 }
-inline int F(lint n){
-	if(n<N)return f[n];
+lint overall;
+int b_h[N];
+inline int H(lint n){
+	if(n<N)return h[n];
+	int &bans=b_h[overall/n];
+	if(bans!=-1)return bans;
 	lint ans=0;
 	for(lint i=1,sqi;sqi=i*i,sqi<=n;i++){
-		ans+=(lint)mu[i]*i*_n2(n/sqi);
+		ans+=i*grt(n-sqi)%O;
 	}
-	return (ans%O+O)%O;
+	return bans=ans%O;
+}
+int b_f[N];
+inline int F(lint n){
+	if(n<N)return f[n];
+	int &bans=b_f[overall/n];
+	if(bans!=-1)return bans;
+	lint ans=0;
+	for(lint i=1,sqi;sqi=i*i,sqi<=n;i++){
+		ans+=(lint)mu[i]*i*H(n/sqi)%O;
+	}
+	return bans=(ans%O+O)%O;
 }
 inline int G(lint n){
 	if(n<N)return g[n];
 	lint ans=0;
-	for(lint i=1,sqi;sqi=i*i,sqi<=n;i++){
-		lint cur=0;
-		for(lint j=1,sqj;sqj=j*j,sqi+sqj<=n;j++){
-			cur+=n/(sqi+sqj);
-		}
-		ans+=cur%O*i%O;
+	for(lint l=1,r,d;l<=n;l=r+1){
+		r=n/(d=n/l);
+		ans+=(_n2(r)-_n2(l-1))*d%O;
 	}
 	return ans%O;
 }
@@ -103,9 +124,11 @@ int main(){
 	freopen("div.out","w",stdout);
 #endif
 	sieve(N-1);
-	lint n=next_num<lint>();
+	lint n=overall=next_num<lint>();
 	lint ans=0;
 	int cur,last=0;
+	memset(b_f,-1,sizeof(b_f));
+	memset(b_h,-1,sizeof(b_h));
 	for(lint l=1,r,d;l<=n;l=r+1,last=cur){
 		r=n/(d=n/l);
 		cur=F(r);
@@ -116,7 +139,7 @@ int main(){
 	for(lint l=1,r,d;l<=n;l=r+1,last=cur){
 		r=n/(d=n/l);
 		cur=_n2(r);
-		ans+=d*(cur+O-last)%O;
+		ans+=(cur+O-last)%O*d%O;
 	}
 	printf("%lld\n",ans%O);
 	return 0;
