b36b1e2bb120373c9b2e18e755022c2813233115
Kerry Su <i_sukai@live.com>
2018-01-17 11:49:51 +0800

    Accepted. 2680ms.

--- EZOJ/Contests/1382/B.cpp
+++ EZOJ/Contests/1382/B.cpp
@@ -30,15 +30,26 @@ inline int fpow(int x,int n){
 inline int inv(int x){
 	return fpow(x,O-2);
 }
+#define arr(x) static int* x=new int[N]
 namespace poly{
-	int sh,n,invn;
+	int sh,n,mxn=0,invnum[N];
 	int o[SH][N>>1],io[SH][N>>1];
 	inline void clr(int a[],int x){
 		memset(a+x,0,(n-x)<<2);
 	}
+	inline void cpy(int a[],const int b[],int n){
+		memcpy(a,b,n<<2);
+	}
 	inline void init(int _n){
 		for(sh=0;(1<<sh)<_n;sh++);
-		n=1<<sh,invn=inv(n);
+		n=1<<sh;
+		if(n<=mxn)return;
+		mxn=n;
+		invnum[1]=1;
+		for(int i=2;i<=n;i++){
+			invnum[i]=(lint)(O-O/i)*invnum[O%i]%O;
+			assert((lint)invnum[i]*i%O==1);
+		}
 		for(int i=0;i<sh;i++){
 			int half=1<<i,full=half<<1;
 			lint wn=fpow(3,(O-1)/full),iwn=inv(wn);
@@ -68,61 +79,117 @@ namespace poly{
 		}
 		if(d==-1){
 			for(int i=0;i<n;i++){
-				a[i]=(lint)a[i]*invn%O;
+				a[i]=(lint)a[i]*invnum[n]%O;
 			}
 		}
 	}
-}
-int mlen;
-int a[4][N];
-void fpow(int n){
-	if(n==0){
-		memset(a,0,sizeof(a));
-		a[0][0]=a[3][0]=1;
-		return;
-	}
-	fpow(n>>1);
-	for(int i=0;i<4;i++){
-		poly::ntt(a[i]);
-	}
-	lint b[2][2];
-	for(int i=0;i<poly::n;i++){
-		for(int j=0;j<4;j++){
-			b[j>>1][j&1]=a[j][i],a[j][i]=0;
+	const int* inv(const int a[],int n){
+		arr(b);arr(ta);
+		if(n==1){
+			assert(a[0]);
+			b[0]=::inv(a[0]);
+			return b;
 		}
-		a[0][i]=(b[0][0]*b[0][0]+b[0][1]*b[1][0])%O;
-		a[1][i]=(b[0][0]*b[0][1]+b[0][1]*b[1][1])%O;
-		a[2][i]=(b[1][0]*b[0][0]+b[1][1]*b[1][0])%O;
-		a[3][i]=(b[1][0]*b[0][1]+b[1][1]*b[1][1])%O;
-	}
-	for(int i=0;i<4;i++){
-		poly::ntt(a[i],-1);
-		poly::clr(a[i],mlen+1);
-	}
-	if(n&1){
-		for(int i=mlen;i>=0;i--){
-			a[1][i]=(a[0][i]<<1)%O;
-			a[3][i]=(a[2][i]<<1)%O;
-			if(i){
-				a[0][i]=(a[0][i]+a[1][i-1])%O;
-				a[1][i]=(a[1][i]+a[1][i-1])%O;
-				a[2][i]=(a[2][i]+a[3][i-1])%O;
-				a[3][i]=(a[3][i]+a[3][i-1])%O;
-			}
+		assert(n>1);
+		inv(a,n>>1);
+		init(n<<1);
+		clr(b,n>>1),ntt(b);
+		cpy(ta,a,n),clr(ta,n),ntt(ta);
+		for(int i=0;i<poly::n;i++){
+			b[i]=(O-(lint)ta[i]*b[i]%O*b[i]%O+(b[i]<<1))%O;
 		}
+		ntt(b,-1);
+		return b;
+	}
+	inline const int* log(const int a[],int n){
+		arr(b);arr(c);
+		assert(a[0]==1);
+		cpy(c,inv(a,n),n);
+		for(int i=1;i<n;i++){
+			b[i-1]=(lint)a[i]*i%O;
+		}
+		init(n<<1);
+		clr(b,n-1),ntt(b);
+		clr(c,n),ntt(c);
+		for(int i=0;i<poly::n;i++){
+			b[i]=(lint)b[i]*c[i]%O;
+		}
+		ntt(b,-1);
+		for(int i=n-1;i>=1;i--){
+			b[i]=(lint)b[i-1]*invnum[i]%O;
+		}
+		b[0]=0;
+		return b;
+	}
+	const int* exp(const int a[],int n){
+		arr(b);arr(c);
+		if(n==1){
+			assert(a[0]==0);
+			b[0]=1;
+			return b;
+		}
+		assert(n>1);
+		exp(a,n>>1);
+		memset(b+(n>>1),0,(n>>1)<<2);
+		assert(b[0]==1);
+		const int* lg=log(b,n);
+		init(n<<1);
+		clr(b,n),ntt(b);
+		for(int i=0;i<n;i++){
+			c[i]=(a[i]+O-lg[i])%O;
+		}
+		assert(c[0]==0);
+		c[0]=1;
+		clr(c,n),ntt(c);
+		for(int i=0;i<poly::n;i++){
+			b[i]=(lint)b[i]*c[i]%O;
+		}
+		ntt(b,-1);
+		return b;
+	}
+	const int* fpow(const int a[],int n,int e){
+		assert(a[0]==1);//this task
+		arr(b);
+		cpy(b,log(a,n),n);
+		for(int i=0;i<n;i++){
+			b[i]=(lint)b[i]*e%O;
+		}
+		return exp(b,n);
 	}
 }
+int a[N],f[N];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("ball.in","r",stdin);
 	freopen("ball.out","w",stdout);
 #endif
-	int n=ni;
-	mlen=ni;
+	int n=ni,m=ni;
+	int mlen=min(n,m);
 	poly::init((mlen<<1)+1);
-	fpow(n);
+	int sn=poly::n>>1;
+	memset(a,0,sizeof(a));
+	a[0]=1,a[1]=6,a[2]=1;
+	poly::cpy(a,poly::fpow(a,sn,inv(2)),sn);
+	poly::cpy(f,a,sn);
+	f[0]=(f[0]+1)%O,f[1]=(f[1]+1)%O;
+	for(int i=0;i<sn;i++){
+		const static int inv2=inv(2);
+		f[i]=(lint)f[i]*inv2%O;
+	}
+	poly::cpy(f,poly::fpow(f,sn,n+1),sn);
+	poly::cpy(a,poly::inv(a,sn),sn);
+	poly::init(sn<<1);
+	poly::clr(a,sn),poly::ntt(a);
+	poly::clr(f,sn),poly::ntt(f);
+	for(int i=0;i<poly::n;i++){
+		f[i]=(lint)f[i]*a[i]%O;
+	}
+	poly::ntt(f,-1);
 	for(int i=1;i<=mlen;i++){
-		printf("%d ",(a[0][i]+a[2][i])%O);
+		printf("%d ",f[i]);
+	}
+	for(int i=mlen+1;i<=m;i++){
+		putchar('0'),putchar(' ');
 	}
 	putchar('\n');
 	return 0;
