2c914bc15d0d84b7da42d9bcc646d2c0f97ca79e
Kerry Su <i_sukai@live.com>
2018-01-17 09:10:13 +0800

    Accepted. 671ms.

--- EZOJ/Contests/1383/B.cpp
+++ EZOJ/Contests/1383/B.cpp
@@ -17,62 +17,90 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int N=1000010,D=100;
-namespace PAM{
-	int fail[N],len[N],nxt[N][D];
-	inline int nn(){
-		static int n=1;
-		return ++n;
+const int N=10000010,D=100;
+namespace H{
+	const int O=998244353;
+	inline int fpow(int x,int n){
+		int a=1;
+		for(;n;n>>=1,x=(lint)x*x%O){
+			if(n&1){
+				a=(lint)a*x%O;
+			}
+		}
+		return a;
 	}
-	inline void init(){
-		fail[0]=1;
-		len[1]=-1;
+	inline int inv(int x){
+		return fpow(x,O-2);
+	}
+	const int invnum=inv(100);
+	inline int shift(int x,char c){
+		return (lint)(x+O-c)*invnum%O;
+	}
+	inline int revsh(int x,char c){
+		return ((lint)x*100+c)%O;
+	}
+	inline int push(int x,char c,int pw){
+		return (x+(lint)pw*c)%O;
+	}
+	inline int pop(int x,char c,int pw){
+		return (x+O-(lint)pw*invnum%O*c%O)%O;
 	}
 }
 char str[N<<1];
 struct state{
-	int l,r;
-	int lnode,rnode;
-	int ans;
+	int l,r,ans,pw;
+	int l1,l2,r1,r2;
 	inline state ins_left(char c)const{
 		str[l-1]=c;
-		int p=lnode;
-		using namespace PAM;
-		for(;l+len[p]>r||str[l+len[p]]!=c;p=fail[p]);
-		if(nxt[p][c]==0){
-			int q=fail[p];
-			for(;l+len[q]>r||str[l+len[q]]!=c;q=fail[q]);
-			q=nxt[q][c];
-			nxt[p][c]=nn();
-			fail[nxt[p][c]]=q;
-			len[nxt[p][c]]=len[p]+2;
+		state nxt=(state){l-1,r,ans};
+		if(l+ans<=r&&str[l-1]==str[l+ans]&&l1==l2){//check ans
+			nxt.ans=ans+2;
+			nxt.pw=(lint)pw*10000%H::O;
+			nxt.l1=nxt.l2=H::revsh(H::push(l1,c,pw),c);
+			nxt.r1=H::revsh(H::revsh(r1,str[r-ans]),str[r-ans-1]);
+			nxt.r2=H::push(H::push(r2,str[r-ans],pw),str[r-ans-1],(lint)pw*100%H::O);
 		}
-		p=nxt[p][c];
-		int q=rnode;
-		if(len[p]==r-l+2){
-			q=p;
+		if(nxt.ans==ans&&str[l-1]==str[l+ans-1]&&H::pop(l1,c,pw)==H::shift(l2,c)){//check ans-1
+			nxt.ans=ans+1;
+			nxt.pw=(lint)pw*100%H::O;
+			nxt.l1=nxt.l2=H::revsh(l1,c);
+			nxt.r1=H::revsh(r1,str[r-ans]);
+			nxt.r2=H::push(r2,str[r-ans],pw);
 		}
-		return (state){l-1,r,p,q,max(ans,len[p])};
+		if(nxt.ans==ans){//nothing changed
+			nxt.pw=pw;
+			nxt.l1=H::revsh(H::pop(l1,str[l+ans-1],pw),c);
+			nxt.l2=H::shift(H::push(l2,c,pw),str[l+ans-1]);
+			nxt.r1=r1;
+			nxt.r2=r2;
+		}
+		return nxt;
 	}
 	inline state ins_right(char c)const{
 		str[r+1]=c;
-		int p=rnode;
-		using namespace PAM;
-		for(;r-len[p]<l||str[r-len[p]]!=c;p=fail[p]);
-		if(nxt[p][c]==0){
-			int q=fail[p];
-			for(;r-len[q]<l||str[r-len[q]]!=c;q=fail[q]);
-			q=nxt[q][c];
-			nxt[p][c]=nn();
-			fail[nxt[p][c]]=q;
-			len[nxt[p][c]]=len[p]+2;
+		state nxt=(state){l,r+1,ans};
+		if(r-ans>=l&&str[r-ans]==str[r+1]&&r1==r2){//check ans
+			nxt.ans=ans+2;
+			nxt.pw=(lint)pw*10000%H::O;
+			nxt.r1=nxt.r2=H::revsh(H::push(r1,c,pw),c);
+			nxt.l1=H::push(H::push(l1,str[l+ans],pw),str[l+ans+1],(lint)pw*100%H::O);
+			nxt.l2=H::revsh(H::revsh(l2,str[l+ans]),str[l+ans+1]);
 		}
-		p=nxt[p][c];
-		int q=lnode;
-		if(len[p]==r-l+2){
-			q=p;
+		if(nxt.ans==ans&&str[r-ans+1]==str[r+1]&&H::pop(r2,c,pw)==H::shift(r1,c)){//check ans-1
+			nxt.ans=ans+1;
+			nxt.pw=(lint)pw*100%H::O;
+			nxt.r1=nxt.r2=H::revsh(r2,c);
+			nxt.l1=H::push(l1,str[l+ans],pw);
+			nxt.l2=H::revsh(l2,str[l+ans]);
 		}
-		return (state){l,r+1,q,p,max(ans,len[p])};
+		if(nxt.ans==ans){//nothing changed
+			nxt.pw=pw;
+			nxt.l1=l1;
+			nxt.l2=l2;
+			nxt.r1=H::shift(H::push(r1,c,pw),str[r-ans+1]);
+			nxt.r2=H::revsh(H::pop(r2,str[r-ans+1],pw),c);
+		}
+		return nxt;
 	}
 }stk[N];
 int ss=0;
@@ -84,20 +112,16 @@ int main(){
 #endif
 	int tot=ni;
 	fread(s,1,N*3,stdin);
-	s[tot*3]=0;
-	stk[ss]=(state){N,N-1,0,0,0}; PAM::init();
+	stk[ss]=(state){N,N-1,0,1,0,0,0,0};
 	lint ans=0;
 	for(int i=0,j=0;i<tot;i++,j+=3){
 		int op=s[j]-'0';
 		int num=((s[j+1]-'0')*10+(s[j+2]-'0')+stk[ss].ans)%100;
-		if(op==1){
-			stk[ss+1]=stk[ss].ins_left(num);
-			ss++;
-		}else if(op==2){
-			stk[ss+1]=stk[ss].ins_right(num);
-			ss++;
-		}else{
+		if(op==3){
 			ss-=num;
+		}else{
+			stk[ss+1]=op==1?stk[ss].ins_left(num):stk[ss].ins_right(num);
+			ss++;
 		}
 		ans+=stk[ss].ans;
 	}
