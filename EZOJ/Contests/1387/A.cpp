91648a0d1c89dd74624c5eb626b137b1941241ca
Kerry Su <i_sukai@live.com>
2018-01-23 01:51:00 +0000

    Accepted. 1456ms.

--- EZOJ/Contests/1387/A.cpp
+++ EZOJ/Contests/1387/A.cpp
@@ -4,7 +4,6 @@
 #include <cassert>
 #include <cctype>
 #include <algorithm>
-#include <queue>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -20,68 +19,132 @@ template<class T>inline T next_num(){
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 const int N=100010;
-lint len[N],sum[N];
-int cost[N];
-bool vis[N];
-struct cmp{
-	inline bool operator () (const int &a,const int &b){
-		return cost[a]<cost[b];
-	}
-};
-int n;
-lint alltime;
-bool flag=true;
-inline int Main(){
-	for(int i=1;i<=n;i++){
-		sum[i]=sum[i-1]+len[i-1];
-	}
-	int ans=0;
-	if(flag){
-		for(int i=0,j=0;i<n;i++){
-			lint rest=alltime-sum[i]*2-(i+1);
-			if(rest<0)break;
-			for(;j<n&&sum[n]-sum[j]+n-j>=rest;j++);
-			if(j<n){
-				apmax(ans,i+1+n-j);
-			}else break;
+namespace seg{
+	const int N=100010*20;
+	struct Node;
+	typedef Node* node;
+	struct Node{
+		node lson,rson;
+		int cnt;
+		lint sum;
+		inline void up(){
+			sum=lson->sum+rson->sum;
 		}
-		return ans;
+	}pool[N],Null;
+	node null,nd;
+	int n;
+	inline node nn(node x=null){
+		return *nd=*x,nd++;
 	}
+	inline void init(int _n){
+		memset(null=&Null,0,sizeof(Null));
+		null->lson=null->rson=null;
+		nd=pool;
+		n=_n;
+	}
+	node add(node x,int p,lint v,int l=0,int r=n-1){
+		x=nn(x);
+		x->cnt++;
+		x->sum+=v;
+		if(l!=r){
+			int m=(l+r)>>1;
+			if(p<=m){
+				x->lson=add(x->lson,p,v,l,m);
+			}else{
+				x->rson=add(x->rson,p,v,m+1,r);
+			}
+		}
+		return x;
+	}
+	lint ask(node x,int k,int l=0,int r=n-1){
+		if(x->cnt==k)return x->sum;
+		assert(k<=x->cnt);
+		int m=(l+r)>>1;
+		if(k<=x->lson->cnt)return ask(x->lson,k,l,m);
+		else return x->lson->sum+ask(x->rson,k-x->lson->cnt,m+1,r);
+	}
+}
+namespace cov{
+	int st[N],f[N],qt;
+	inline void init(){
+		qt=0;
+		st[qt]=0,f[qt]=-1,qt++;
+	}
+	inline int ask(int x){
+		return f[upper_bound(st,st+qt,x)-1-st];
+	}
+	inline void cov(int x,int v){
+		for(;x<=st[qt-1];qt--);
+		st[qt]=x,f[qt]=v,qt++;
+	}
+}
+lint sum[N];
+lint alltime;
+seg::node rt[N];
+int cost[N],rnk[N];
+inline int work(const int len[],lint ans[],int n){
+	cov::init(),seg::init(n);
 	for(int i=0;i<n;i++){
-		if(sum[i]*2>alltime){
+		sum[i+1]=sum[i]+len[i];
+		if(sum[i+1]>alltime){
+			n=i+1;
 			break;
 		}
-		typedef priority_queue<int,vector<int>,cmp>que;
-		que q;
-		lint nowtime=sum[i]*2;
-		int cnt=0;
-		for(int j=i;j>=0;j--){
-			q.push(j);
-			nowtime+=cost[j];
-			cnt++;
-		}
-		while(!q.empty()&&nowtime>alltime){
-			nowtime-=cost[q.top()];
-			q.pop();
-			cnt--;
-		}
-		apmax(ans,cnt);
-		for(int j=n-1;j>=0;j--){
-			q.push(j);
-			nowtime+=cost[j];
-			cnt++;
-			while(!q.empty()&&sum[n]-sum[j]+nowtime>alltime){
-				nowtime-=cost[q.top()];
-				q.pop();
-				cnt--;
+	}
+	ans[0]=0;
+	for(int i=0;i<n;i++){
+		rt[i]=seg::add(i?rt[i-1]:seg::null,rnk[i],cost[i]);
+		int l=1,r=i+1;
+		while(l<r){
+			int m=(l+r)>>1;
+			int f=cov::ask(m);
+			if(sum[f]+seg::ask(rt[f],m)>sum[i]+seg::ask(rt[i],m)){
+				r=m;
+			}else{
+				l=m+1;
 			}
-			if(sum[n]-sum[j]+nowtime>alltime){
-				break;
-			}
-			apmax(ans,cnt);
+		}
+		cov::cov(l,i);
+	}
+	for(int i=1,j=1;i<=n;i++){
+		using namespace cov;
+		for(;j+1<qt&&st[j+1]<=i;j++);
+		ans[i]=sum[f[j]]+seg::ask(rt[f[j]],i);
+		if(ans[i]>alltime){
+			n=i;
+			break;
 		}
 	}
+	return n;
+}
+int len[N];
+lint cw[N],ccw[N];
+int n;
+inline int Main(){
+	for(int i=0;i<n;i++){
+		len[i]<<=1;
+	}
+	int mx1=work(len,cw,n);
+	for(int i=0;i<n;i++){
+		len[i]>>=1;
+	}
+	reverse(len,len+n),reverse(cost,cost+n),reverse(rnk,rnk+n);
+	len[n]=len[0];
+	int mx2=work(len+1,ccw,n);
+	reverse(len,len+n),reverse(cost,cost+n),reverse(rnk,rnk+n);
+	ccw[0]=-len[n-1];
+	int ans=0;
+	for(int i=0,j=mx2;i<=mx1;i++){
+		for(;j>=0&&cw[i]+ccw[j]+len[n-1]>alltime;j--);
+		if(j>=0){
+			apmax(ans,i+j);
+		}else break;
+	}
 	return ans;
+}
+int lst[N];
+inline bool lcmp(int a,int b){
+	return cost[a]<cost[b];
 }
 int main(){
 #ifndef ONLINE_JUDGE
@@ -89,17 +152,15 @@ int main(){
 	freopen("decalcomania.out","w",stdout);
 #endif
 	n=ni,alltime=next_num<lint>();
-	len[0]=0;
 	for(int i=0;i<n;i++){
-		len[i]=ni,cost[i]=ni;
-		if(cost[i]!=1){
-			flag=false;
-		}
+		len[i]=ni,cost[i]=ni,lst[i]=i;
+	}
+	sort(lst,lst+n,lcmp);
+	for(int i=0;i<n;i++){
+		rnk[lst[i]]=i;
 	}
 	int ans=Main();
-	reverse(len,len+n);
-	reverse(cost+1,cost+n);
-	apmax(ans,Main());
-	printf("%d\n",ans);
+	reverse(len,len+n),reverse(cost+1,cost+n),reverse(rnk+1,rnk+n);
+	printf("%d\n",max(ans,Main()));
 	return 0;
 }
