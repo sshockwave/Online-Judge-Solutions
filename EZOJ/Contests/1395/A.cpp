4de36b088babe1bdf3f13a35b7822dab9636938a
Kerry Su <i_sukai@live.com>
2018-03-14 19:22:14 +0800

    Accepted. 4797ms.

--- EZOJ/Contests/1395/A.cpp
+++ EZOJ/Contests/1395/A.cpp
@@ -32,30 +32,51 @@ inline int inv(int x){
 }
 int m,a,n;
 namespace sol1{
-	const int N=1000010;
-	int f[N];
-	inline int work(int m){
-		f[0]=1;
-		for(int i=1;i<=m&&i<=n;i++){
-			f[i]=(lint)f[i-1]*a%O;
-		}
-		if(n<=m)return f[n];
-		for(int i=m+1;i<=n;i++){
-			lint cur=0;
-			for(int j=1;j<=m;j++){
-				cur+=f[i-j];
+	const int N=3000010;
+	namespace gmath{
+		int fac[N],invfac[N];
+		int pwa[N],pw1a[N];
+		inline void main(int n){
+			fac[0]=1;
+			pwa[0]=pw1a[0]=1;
+			for(int i=1;i<=n;i++){
+				fac[i]=(lint)fac[i-1]*i%O;
+				pwa[i]=(lint)pwa[i-1]*a%O;
+				pw1a[i]=(lint)pw1a[i-1]*(O+1-a)%O;
 			}
-			f[i]=cur%O*(a-1)%O;
+			invfac[n]=inv(fac[n]);
+			for(int i=n;i>=1;i--){
+				invfac[i-1]=(lint)invfac[i]*i%O;
+			}
 		}
-		return f[n];
+		inline int C(int n,int k){
+			return (lint)fac[n]*invfac[k]%O*invfac[n-k]%O;
+		}
+	}
+	inline int calc(int n,int m){
+		lint ans=0;
+		using namespace gmath;
+		for(int j=0,mj=0;j+mj<=n;j++,mj+=m){
+			ans+=(lint)C(n-mj,j)*pw1a[j]%O*pwa[n-mj-j];
+			const static lint OO=(lint)O*O;
+			if(ans>OO){
+				ans-=OO;
+			}
+		}
+		return ans%O;
 	}
 	inline int Main(){
-		lint cur=0,w=1;
-		for(int i=1;i<=m;i++){
-			w=w*MUL%O;
-			cur+=work(i)*w%O;
+		gmath::main(n);
+		lint ans=0,pw=1;
+		for(int i=1;i<=m&&i<=n;i++){//get m
+			pw=pw*MUL%O;
+			ans+=(calc(n-1,i)-calc(n-i-1,i))*pw%O;
 		}
-		return cur%O;
+		ans=ans%O*a%O;
+		if(m>n){//from n+1 to m
+			ans+=(lint)gmath::pwa[n]*(fpow(MUL,m+1)-fpow(MUL,n+1))%O*inv(MUL-1)%O;
+		}
+		return (ans%O+O)%O;
 	}
 }
 namespace sol2{
