e8d06477d3f651840c3a0c7d0e738911b3217ec0
Kerry Su <i_sukai@live.com>
2018-03-15 13:58:34 +0800

    RE. 80pts.

--- EZOJ/Contests/1395/B.cpp
+++ EZOJ/Contests/1395/B.cpp
@@ -3,8 +3,10 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <cstdlib>
 using namespace std;
 typedef long long lint;
+typedef unsigned long long ull;
 #define cout cerr
 #define ni (next_num<int>())
 template<class T>inline T next_num(){
@@ -17,42 +19,238 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int N=2010,D=4;
-namespace trie{
-	int son[N][D],n;
-	int fa[N],thisc[N];
-	inline int nn(){
-		return ++n;
+const int N=80010,D=4,INF=0x7f7f7f7f;
+namespace valtreap{
+	const int N=::N*30;
+	struct Node;
+	typedef Node* node;
+	node pt[N],*n=pt,null;
+	struct Node{
+		node lson,rson;
+		int pri;
+		int val,size;
+		inline void up(){
+			size=lson->size+1+rson->size;
+		}
+	}pool[N],Null;
+	inline node nn(node x=null){
+		assert(n<pt+N);
+		return **n=*x,n[0]->pri=rand(),n++[0];
 	}
-	int lcp[N][N];
+	inline void del(node x){
+		*(--n)=x;
+	}
+	void del_all(node x){
+		if(x==null)return;
+		del(x);
+		del_all(x->lson);
+		del_all(x->rson);
+	}
 	inline void init(){
-		memset(son,0,sizeof(son));
-		memset(lcp,0,sizeof(lcp));
-	}
-	inline void work(int n){
-		lcp[1][n]=lcp[n][1]=0;
-		for(int i=2;i<=n;i++){
-			lcp[i][n]=lcp[n][i]=thisc[i]==thisc[n]?lcp[fa[i]][fa[n]]+1:0;
+		memset(null=&Null,0,sizeof(Null));
+		null->lson=null->rson=null;
+		for(int i=0;i<N;i++){
+			pt[i]=pool+i;
 		}
 	}
-	int que[N];
-	void bfs(){
-		int qh=0,qt=0;
-		que[qt++]=1;
-		thisc[1]=-1;
-		while(qh<qt){
-			int x=que[qh++];
-			lcp[x][1]=lcp[1][x]=0;
-			for(int i=1;i<qh;i++){
-				int y=que[i];
-				lcp[x][y]=lcp[y][x]=thisc[x]==thisc[y]?lcp[fa[x]][fa[y]]+1:0;
-			}
-			for(int i=0;i<D;i++){
-				if(son[x][i]){
-					que[qt++]=son[x][i];
-				}
+	void sp(node x,int val,node &lhs,node &rhs){
+		if(x==null)return lhs=rhs=null,void();
+		if(x->val<val){
+			lhs=x;
+			sp(x->rson,val,x->rson,rhs);
+		}else{
+			rhs=x;
+			sp(x->lson,val,lhs,x->lson);
+		}
+		x->up();
+	}
+	void ins(node &x,node n){
+		if(x==null){
+			x=n;
+			x->lson=x->rson=null;
+		}else if(x->pri<n->pri){
+			sp(x,n->val,n->lson,n->rson);
+			x=n;
+		}else{
+			ins(n->val<x->val?x->lson:x->rson,n);
+		}
+		x->up();
+	}
+	void ins_all(node x,node &y){
+		if(x==null)return;
+		ins(y,nn(x));
+		ins_all(x->lson,y);
+		ins_all(x->rson,y);
+	}
+	int ask(node x,int val){//<=val
+		int ans=0;
+		while(x!=null){
+			if(val<x->val){
+				x=x->lson;
+			}else if(val>x->val){
+				ans+=x->lson->size+1;
+				x=x->rson;
+			}else{
+				ans+=x->lson->size+1;
+				break;
 			}
 		}
+		return ans;
+	}
+}
+namespace strtreap{
+	struct Node;
+	typedef Node* node;
+	node null,rt;
+	struct Node{
+		/* Treap */
+		node lson,rson;
+		node lend,rend;
+		int pri;
+		/* Chain */
+		node pre,nxt;
+		/* String */
+		int c;
+		node str;
+		int len;
+		ull mark;
+		/* Info */
+		int v,lcp;
+		int size,mn;
+		valtreap::node val;
+		inline void lit_up(){
+			size=lson->size+1+rson->size;
+			lend=lson!=null?lson->lend:this;
+			rend=rson!=null?rson->rend:this;
+			assert(nxt->mark-pre->mark>=100000);
+			mark=pre->mark+((nxt->mark-pre->mark)>>1);
+			mn=min(lcp,min(lson->mn,rson->mn));
+		}
+		inline void up(){
+			lit_up();
+			valtreap::del_all(val);
+			valtreap::node t=valtreap::nn();
+			t->val=v;
+			valtreap::ins(val=valtreap::null,t);
+			valtreap::ins_all(lson->val,val);
+			valtreap::ins_all(rson->val,val);
+		}
+	}pool[N],Null;
+	inline void init(){
+		memset(null=&Null,0,sizeof(Null));
+		null->lson=null->rson=null;
+		null->lend=null->rend=null;
+		null->pre=null,null->nxt=pool;
+		null->str=null;
+		null->lcp=null->mn=INF;
+		null->val=valtreap::null;
+	}
+	inline node nn(node x=null){
+		static node n=pool+1;
+		return *n=*x,n->pri=rand(),n++;
+	}
+	int ask_lcp(node x,node a,node b){
+		if(x==null)return INF;
+		if(a->mark>b->mark){
+			swap(a,b);
+		}
+		if(a->mark<=x->lend->mark&&x->rend->mark<=b->mark)return x->mn;
+		if(x->mark>b->mark)return ask_lcp(x->lson,a,b);
+		if(x->mark<a->mark)return ask_lcp(x->rson,a,b);
+		int ans=x->lcp;
+		if(a->mark<x->mark){
+			apmin(ans,ask_lcp(x->lson,a,b));
+		}
+		if(b->mark>x->mark){
+			apmin(ans,ask_lcp(x->rson,a,b));
+		}
+		return ans;
+	}
+	inline bool operator < (const Node &a,const Node &b){
+		return a.c!=b.c?a.c<b.c:a.str->mark<b.str->mark;
+	}
+	void sp(node x,node y,node &lhs,node &rhs){//didn't take care of pre and nxt
+		if(x==null)return lhs=rhs=null,void();
+		valtreap::del_all(x->val),x->val=valtreap::null;
+		if(*x<*y){
+			lhs=x,sp(x->rson,y,x->rson,rhs);
+		}else{
+			rhs=x,sp(x->lson,y,lhs,x->lson);
+		}
+		x->up();
+	}
+	void draw(node x){
+		if(x!=null){
+			draw(x->rson);
+			x->lit_up();
+		}
+	}
+	//[null,pool]
+	void ins(node &x,node n){//put pre & nxt in n
+		if(x==null){
+			x=n;
+		}else if(x->pri<n->pri){
+			sp(x,n,n->lson,n->rson);
+			x=n;
+			if(n->lson!=null){
+				n->pre=n->lson->rend;
+			}
+			if(n->rson!=null){
+				n->nxt=n->rson->lend;
+			}
+		}else{
+			valtreap::node v=valtreap::nn();
+			v->val=n->v;
+			valtreap::ins(x->val,v);
+			if(*n<*x){
+				n->nxt=x;
+				ins(x->lson,n);
+			}else{
+				n->pre=x;
+				ins(x->rson,n);
+			}
+			x->lit_up();
+			return;
+		}
+		assert(x->lcp==INF);
+		if(x->pre!=null){
+			x->pre->nxt=x;
+		}
+		if(x->nxt!=null){
+			x->nxt->pre=x;
+		}
+		x->up();
+		if(x->pre!=null){
+			x->pre->lcp=x->pre->c==x->c?ask_lcp(rt,x->pre->str,x->str->pre)+1:0;
+			if(x->pre==x->lson->rend){
+				draw(x->lson);
+			}
+		}
+		if(x->nxt!=pool){
+			x->lcp=x->nxt->c==x->c?ask_lcp(rt,x->str,x->nxt->str->pre)+1:0;
+		}
+		x->lit_up();
+	}
+	int ans;
+	bool ask_lef(node x,node y,int l,int r){//does not include y
+		if(x==null)return true;
+		if(y->mark<=x->lend->mark)return true;
+		if(y->mark<=x->mark)return ask_lef(x->lson,y,l,r);
+		if(y->mark>x->rend->mark&&x->mn>=l)return ans+=valtreap::ask(x->val,r),true;
+		if(!ask_lef(x->rson,y,l,r))return false;
+		if(x->lcp<l)return false;
+		ans+=x->v<=r;
+		return ask_lef(x->lson,y,l,r);
+	}
+	bool ask_rig(node x,node y,int l,int r){//does not include y too
+		if(x==null)return true;
+		if(y->mark>x->mark)return ask_rig(x->rson,y,l,r);
+		if(y->mark==x->mark)return x->lcp>=l&&ask_rig(x->rson,y,l,r);
+		if(y->mark<x->lend->mark&&x->mn>=l)return ans+=valtreap::ask(x->val,r),true;
+		if(!ask_rig(x->lson,y,l,r))return false;
+		ans+=x->v<=r;
+		if(x->lcp<l)return false;
+		return ask_rig(x->rson,y,l,r);
 	}
 }
 inline int gchar(){
@@ -60,45 +258,60 @@ inline int gchar(){
 	while(!isalpha(c=getchar()));
 	return c=='A'?0:c=='T'?1:c=='C'?2:3;
 }
-int pval[N];
-using trie::n;
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("gene.in","r",stdin);
 	freopen("gene.out","w",stdout);
 #endif
-	n=ni;
-	int tot=ni;
+	int n=ni,tot=ni;
+	valtreap::init();
+	strtreap::init();
 	for(int i=1;i<=n;i++){
-		pval[i]=ni;
+		using namespace strtreap;
+		node x=nn();
+		x->v=ni;
 	}
-	trie::init();
 	for(int i=1;i<n;i++){
-		int u=ni,v=ni,c=gchar();
-		trie::son[u][c]=v;
-		trie::fa[v]=u;
-		trie::thisc[v]=c;
+		int u=ni;
+		using namespace strtreap;
+		node x=pool+ni;
+		x->str=pool+u;
+		x->c=gchar();
 	}
-	trie::bfs();
-	for(int ans=1;tot--;){
+	{
+		using namespace strtreap;
+		pool->mark=-1;
+		rt=pool+1;
+		rt->str=rt;
+		rt->c=-1;
+		rt->up();
+		for(int i=2;i<=n;i++){
+			ins(rt,pool+i);
+			pool[i].len=pool[i].str->len+1;
+		}
+	}
+	for(int last=1;tot--;){
+		using namespace strtreap;
 		if(ni==0){//ask
-			int u=ni^ans,l=ni,r=ni;
-			int cur=0;
-			for(int i=1;i<=n;i++){
-				cur+=trie::lcp[u][i]>=l&&pval[i]<=r;
+			node x=pool+(ni^last);
+			int l=ni,r=ni;
+			ans=0;
+			ask_lef(rt,x,l,r);
+			ask_rig(rt,x,l,r);
+			ans+=x->v<=r&&x->len>=l;
+			if(ans){
+				last=ans;
 			}
-			if(cur){
-				ans=cur;
-			}
-			printf("%d\n",cur);
+			printf("%d\n",ans);
 		}else{//grow
-			int x=trie::nn();
-			trie::fa[x]=ni^ans;
-			pval[x]=ni;
-			trie::thisc[x]=gchar();
-			trie::son[trie::fa[x]][trie::thisc[x]]=x;
-			trie::work(x);
+			node x=nn();
+			x->str=pool+(ni^last);
+			x->len=x->str->len+1;
+			x->v=ni;
+			x->c=gchar();
+			ins(rt,x);
 		}
 	}
 	return 0;
 }
+
