cc2d8c043bba5ca9afc03d1412d18b6118d2b18e
Kerry Su <i_sukai@live.com>
2018-03-19 09:09:21 +0800

    Accepted. 842ms.

--- EZOJ/Contests/1397/A.cpp
+++ EZOJ/Contests/1397/A.cpp
@@ -18,55 +18,93 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-template<class T>inline T gcd(const T &a,const T &b){return b?gcd(b,a%b):a;}
-template<class T>inline lint lcm(const T &a,const T &b){return (lint)a/gcd(a,b)*b;}
-const int N=510,O=232792561;
+const int N=18,K=20,O=232792561;
+inline int fpow(int x,int n){
+	int a=1;
+	for(;n;n>>=1,x=(lint)x*x%O){
+		if(n&1){
+			a=(lint)a*x%O;
+		}
+	}
+	return a;
+}
+inline int inv(int x){
+	return fpow(x,O-2);
+}
 inline void apadd(int &a,const int &b){
 	a=(a+b)%O;
 }
-lint n;
-int k;
-namespace brute1{
-	int f[N][N][20];
-	int lst[N];
-	inline int main(){
-		int ls=0;
-		for(int i=1;i<=n;i++){
-			if(n%i==0){
-				lst[++ls]=i;
-			}
+inline void apmul(int &a,const int &b){
+	a=(lint)a*b%O;
+}
+int pri[N],cnt[N],n,k;
+int f[1<<N][K],h[1<<N],bitcnt[1<<N];
+void dfs(int x,int s,int j){
+	if(x==n){
+		int *f=::f[s],g[K];
+		memcpy(g,f,sizeof(g));
+		memset(f,0,sizeof(g));
+		for(int i=0;i<k;i++){
+			apadd(f[i],g[i]);
+			apadd(f[(i+j)%k],g[i]);
 		}
-		memset(f,0,sizeof(f));
-		f[0][1][0]=1;
-		for(int i=1;i<=ls;i++){
-			int x=lst[i];
-			for(int j=1;j<=ls;j++){
-				int y=lst[j];
-				for(int t=0;t<k;t++){
-					apadd(f[i][lcm(x,y)][(t+x)%k],f[i-1][y][t]);
-				}
-			}
-			for(int j=1;j<=ls;j++){
-				int y=lst[j];
-				for(int t=0;t<k;t++){
-					apadd(f[i][y][t],f[i-1][y][t]);
-				}
-			}
-		}
-		return f[ls][n][0];
+		return;
+	}
+	for(int i=0;i<=cnt[x];i++,j=j*pri[x]%k){
+		dfs(x+1,s|((i==cnt[x])<<x),j);
 	}
 }
 inline int Main(){
-	n=next_num<lint>(),k=ni;
-	if(n<=1000)return brute1::main();
-	srand(time(0));
-	return rand()%O;
+	{
+		lint all=next_num<lint>();
+		n=0,k=ni;
+		for(int i=2;i<=100&&all>1;i++){
+			int e=0;
+			for(;all%i==0;all/=i,e++);
+			if(e){
+				pri[n]=i,cnt[n]=e,n++;
+			}
+		}
+	}
+	int sn=1<<n;
+	memset(f,0,sizeof(f));
+	for(int i=0;i<sn;i++){
+		f[i][0]=1;
+	}
+	dfs(0,0,1);
+	lint ans=0;
+	for(int tot=k,wn=fpow(71,(O-1)/k),w=1;tot--;w=(lint)w*wn%O){
+		for(int i=0;i<sn;i++){
+			int *f=::f[i],&a=h[i]=0;
+			for(int j=k-1;j>=0;j--){
+				a=((lint)a*w+f[j])%O;
+			}
+		}
+		for(int i=0;i<n;i++){
+			for(int s=0,ts=1<<(n-1);s<ts;s++){
+				int hi=s>>i<<i,s2=(s^hi)|(hi<<1);
+				apmul(h[s2|(1<<i)],h[s2]);
+			}
+		}
+		for(int i=0;i<sn;i++){
+			if((n-bitcnt[i])&1){
+				ans-=h[i];
+			}else{
+				ans+=h[i];
+			}
+		}
+	}
+	return (ans%O+O)*inv(k)%O;
 }
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("lcm.in","r",stdin);
 	freopen("lcm.out","w",stdout);
 #endif
+	bitcnt[0]=0;
+	for(int i=1;i<(1<<N);i++){
+		bitcnt[i]=bitcnt[i^(i&-i)]+1;
+	}
 	for(int tot=ni;tot--;printf("%d\n",Main()));
 	return 0;
 }
