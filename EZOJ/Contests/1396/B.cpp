d54de9579a6c7a0d1eabca03e957b67c6467d7c5
Kerry Su <i_sukai@live.com>
2018-03-19 17:39:06 +0800

    Accepted. 179ms.

--- EZOJ/Contests/1396/B.cpp
+++ EZOJ/Contests/1396/B.cpp
@@ -17,8 +17,8 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int N=2010,O=1000000007;
-inline int fpow(int x,int n){
+const int N=500010,O=1000000007;
+inline int fpow(int x,lint n){
 	int a=1;
 	for(;n;n>>=1,x=(lint)x*x%O){
 		if(n&1){
@@ -43,39 +43,64 @@ namespace gmath{
 		}
 	}
 	inline int C(int n,int k){
-		if(k>n)return 0;
+		assert(k<=n);
 		return (lint)fac[n]*invfac[k]%O*invfac[n-k]%O;
 	}
 }
-int f[N];
-inline int gf(int n,int k){
-	if(n<=k)return f[n];
-	lint ans=0;
-	for(int j=0;j<=k;j++){
-		using gmath::C;
-		lint cur=(lint)C(n-j-1,k-j)%O*f[j]%O*C(n,j)%O;
-		if((k-j)&1){
-			ans-=cur;
-		}else{
-			ans+=cur;
+namespace newton{
+	int pre[N],suf[N];
+	inline int main(int f[],int k,lint n){
+		pre[0]=1;
+		for(int i=1;i<=k;i++){
+			pre[i]=(lint)pre[i-1]*(n-i+1)%O;
 		}
+		suf[k]=1;
+		for(int i=k-1;i>=0;i--){
+			suf[i]=(lint)suf[i+1]*(n-i-1)%O;
+		}
+		lint ans=0;
+		for(int i=0;i<=k;i++){
+			using namespace gmath;
+			int cur=(lint)pre[i]*suf[i]%O*invfac[i]%O*invfac[k-i]%O*f[i]%O;
+			if((k-i)&1){
+				ans-=cur;
+			}else{
+				ans+=cur;
+			}
+		}
+		return (ans%O+O)%O;
 	}
-	return ans%O;
 }
+int f[N],g[N];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("four.in","r",stdin);
 	freopen("four.out","w",stdout);
 #endif
-	int n=ni,k=ni,q=ni;
+	lint n=next_num<lint>();
+	int k=ni,q=ni;
 	for(int i=0;i<=k;i++){
 		f[i]=ni;
 	}
-	gmath::main(n);
-	lint ans=0,pw=1;
-	for(int i=0;i<=n;i++,pw=pw*q%O){
-		ans+=pw*gf(i,k)%O;
+	gmath::main(k+1);
+	int a=0,b=1;
+	lint ta=0,tb=0;//ta+tb*x==0
+	f[k+1]=newton::main(f,k,k+1);
+	lint invq=inv(q);
+	for(int i=0;i<=k+1;i++){
+		a=(a*invq+f[i])%O,b=b*invq%O;
+		lint c=gmath::C(k+1,i);
+		if((k+1-i)&1){
+			ta+=a*c%O,tb+=b*c%O;
+		}else{
+			ta-=a*c%O,tb-=b*c%O;
+		}
 	}
-	printf("%lld\n",(ans%O+O)%O);
+	assert(tb%O);
+	g[0]=((O-ta%O)*inv((tb%O+O)%O)%O*invq+f[0])%O;
+	for(int i=1;i<=k;i++){
+		g[i]=(g[i-1]*invq+f[i])%O;
+	}
+	printf("%lld\n",((lint)newton::main(g,k,n%O)*fpow(q,n)+(f[0]+O-g[0]))%O);
 	return 0;
 }
