88abb56a1ef67ad621a27defa07e9676e96b572b
Kerry Su <i_sukai@live.com>
2018-03-23 16:17:56 +0800

    Accepted. 375ms.

--- EZOJ/Contests/1400/B.cpp
+++ EZOJ/Contests/1400/B.cpp
@@ -18,112 +18,121 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int INF=0x7f7f7f7f;
-int n,m;
+const int N=10010,INF=0x7f7f7f7f;
+int deg[N];
+bool vis[N];
+int ecnt;
+bool hasin;
 namespace G{
-	const int N=10010,E=100010;
-	int fr[E],to[E],bro[E],idx[E],head[N],e=0;
+	const int E=(100010+N)<<1;
+	int to[E],bro[E],val[E],cap[E],head[N],e=0,s,t,n;
 	inline void init(){
 		memset(head,-1,sizeof(head));
 	}
-	inline void ae(int u,int v,int i){
-		fr[e]=u,to[e]=v,bro[e]=head[u],idx[e]=i,head[u]=e++;
+	inline void ae(int u,int v,int w,int c){
+		to[e]=v,bro[e]=head[u],val[e]=w,cap[e]=c,head[u]=e++;
 	}
-}
-namespace task1{
-	const int N=18;
-	int f[N][1<<N];//[at][S]
-	int bitcnt[1<<N];
-	vector<int>sset[N+1];
-	inline int main(){
-		memset(f,127,sizeof(f));
-		int sn=1<<m;
-		for(int i=0;i<sn;i++){
-			bitcnt[i]=i?bitcnt[i^(i&-i)]+1:0;
-			sset[bitcnt[i]].push_back(i);
-		}
-		for(int i=0;i<n;i++){
-			f[0][0]=1;
-		}
-		for(int b=0;b<m;b++){
-			for(vector<int>::iterator it=sset[b].begin();it!=sset[b].end();it++){
-				int s=*it;
-				int curans=INF;
-				for(int x=0;x<n;x++){
-					apmin(curans,f[x][s]);
-				}
-				for(int x=0;x<n;x++){
-					apmin(f[x][s],curans+1);
-				}
-				for(int e=0;e<G::e;e++){
-					if(((s>>(G::idx[e]-1))&1)==0){
-						apmin(f[G::to[e]-1][s|(1<<(G::idx[e]-1))],f[G::fr[e]-1][s]);
+	inline void add(int u,int v,int w,int c){
+		ae(u,v,w,c),ae(v,u,-w,0);
+	}
+	int dis[N],que[N];
+	bool inque[N];
+	inline void spfa(){
+		memset(dis+1,127,n*sizeof(dis[0]));
+		memset(inque+1,0,n*sizeof(inque[0]));
+		int qh=0,qt=0;
+		dis[s]=0;
+		inque[s]=true,que[qt++]=s;
+		while(qh!=qt){
+			int x=que[qh++];
+			if(qh==N){
+				qh=0;
+			}
+			for(int i=head[x],v;~i;i=bro[i]){
+				if(cap[i]&&dis[v=to[i]]>dis[x]+val[i]){
+					dis[v]=dis[x]+val[i];
+					if(!inque[v]){
+						inque[v]=true,que[qt++]=v;
+						if(qt==N){
+							qt=0;
+						}
 					}
 				}
 			}
+			inque[x]=false;
 		}
-		int ans=INF;
-		for(int i=0;i<n;i++){
-			apmin(ans,f[i][sn-1]);
-		}
-		return ans;
 	}
-}
-namespace task3{
-	using G::N;
-	bool vis[N];
-	int cur;
-	int cnt;
-	void dfs(int x){
-		if(vis[x])return;
-		cnt++;
-		vis[x]=true;
-		using namespace G;
-		bool r=false;
-		for(int i=head[x];~i;i=bro[i]){
-			r^=1;
-			dfs(to[i]);
-		}
-		cur+=r;
-	}
-	inline int main(){
-		memset(vis,0,sizeof(vis));
-		int ans=0;
-		for(int i=1;i<=n;i++){
-			if(!vis[i]){
-				cur=0,cnt=0;
-				dfs(i);
-				ans+=max(cnt>1?1:0,cur>>1);
+	int tag[N],tim=0;
+	inline int aug(int x,int a){
+		if(x==t||a==0)return a;
+		tag[x]=tim;
+		int r=a,d;
+		for(int i=head[x],v;~i;i=bro[i]){
+			if(cap[i]&&dis[v=to[i]]==dis[x]+val[i]&&tag[v]<tim){
+				d=aug(v,min(cap[i],r));
+				cap[i]-=d,cap[i^1]+=d,r-=d;
+				if(r==0)break;
 			}
 		}
-		return ans;
+		return a-r;
 	}
-}
-namespace task4{
+	inline int mcmf(){
+		int cost=0;
+		for(;spfa(),dis[t]<0;tim++,cost+=dis[t]*aug(s,INF));
+		return cost;
+	}
+	void dfs(int x){
+		if(vis[x])return;
+		vis[x]=true;
+		hasin|=deg[x]!=0;
+		for(int i=head[x];~i;i=bro[i]){
+			ecnt++;
+			dfs(to[i]);
+		}
+	}
 }
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("draw.in","r",stdin);
 	freopen("draw.out","w",stdout);
 #endif
-	n=ni,m=ni;
-	bool dir=true,undir=true;
-	G::init();
+	int n=ni,m=ni;
+	G::init(),G::s=n+1,G::t=n+2,G::n=n+2;
 	for(int i=1;i<=m;i++){
-		int u=ni,v=ni,c=ni;
-		dir&=c==0;
-		undir&=c==1;
-		if(c){
-			G::ae(u,v,i);
-			G::ae(v,u,i);
-		}else{
-			G::ae(u,v,i);
+		int u=ni,v=ni;
+		deg[u]++,deg[v]--;
+		G::ae(u,v,0,0);
+		G::ae(v,u,0,ni);
+	}
+	int ans=0;
+	for(int i=1;i<=n;i++){
+		if(deg[i]>0){
+			G::add(i,G::t,-1,deg[i]>>1);
+			if(deg[i]&1){
+				G::add(i,G::t,0,1);
+			}
+			ans+=deg[i];
+		}else if(deg[i]<0){
+			G::add(G::s,i,-1,(-deg[i])>>1);
+			if((-deg[i])&1){
+				G::add(G::s,i,0,1);
+			}
 		}
 	}
-	if(undir){
-		printf("%d\n",task3::main());
-	}else{
-		printf("%d\n",task1::main());
+	ans+=G::mcmf();
+	for(int i=G::head[G::s];~i;i=G::bro[i]){
+		deg[G::to[i]]-=G::cap[i^1]*G::val[i];
 	}
+	for(int i=G::head[G::t];~i;i=G::bro[i]){
+		deg[G::to[i]]-=G::cap[i]*G::val[i^1];
+	}
+	vis[G::s]=vis[G::t]=true;
+	for(int i=1;i<=n;i++){
+		if(vis[i])continue;
+		ecnt=0,hasin=false;
+		G::dfs(i);
+		ans+=ecnt&&!hasin;
+	}
+	printf("%d\n",ans);
 	return 0;
 }
