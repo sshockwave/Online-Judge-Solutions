9f0e99fe190a3f58b4b98ad224797805c4f7946c
Kerry Su <i_sukai@live.com>
2018-04-03 21:05:07 +0800

    Accepted. 273ms.

--- EZOJ/Contests/1407/C.cpp
+++ EZOJ/Contests/1407/C.cpp
@@ -3,8 +3,9 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
-#include <cstdlib>
 #include <cmath>
+#include <algorithm>
+#include <set>
 using namespace std;
 typedef double db;
 typedef long long lint;
@@ -20,7 +21,11 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int N=20010,D=5,X=10010;
+const int N=20010,D=8,X=10010;
+const db EPS=1e-7;
+inline int sgn(db x){
+	return x>EPS?1:x<EPS?-1:0;
+}
 int dim;
 struct Pt{
 	db a[D];
@@ -54,30 +59,101 @@ struct Pt{
 		return out;
 	}
 }pt[N];
-inline Pt work(int n){
-	Pt pos;
-	for(int i=0;i<dim;i++){
-		pos.a[i]=rand()%X;
+struct Circ{
+	Pt o;
+	db r;
+	inline bool has(const Pt &x){
+		return (x-o).d2()<r*r+EPS;
 	}
-	for(db step=1;step>1e-6;step*=0.9){
-		Pt vec;
-		int cnt=0;
-		db mxv=0;
+};
+namespace gcen{
+	db eqn[D][D];
+	inline void gauss(int n){
 		for(int i=1;i<=n;i++){
-			Pt dt=pt[i]-pos;
-			db cur=dt.d2();
-			if(cur>mxv+1e-6){
-				vec=dt,cnt=1,mxv=cur;
-			}else if(cur>=mxv-1e-6){
-				vec=vec+dt,cnt++;
+			int j=i;
+			for(;sgn(eqn[j][i])==0;j++);
+			if(j!=i){
+				for(int k=i;k<=n+1;k++){
+					swap(eqn[i][k],eqn[j][k]);
+				}
+			}
+			db r=1/eqn[i][i];
+			for(int k=i;k<=n+1;k++){
+				eqn[i][k]*=r;
+			}
+			for(j=i+1;j<=n;j++){
+				if(sgn(eqn[j][i])==0)continue;
+				db r=eqn[j][i];
+				for(int k=i;k<=n+1;k++){
+					eqn[j][k]-=r*eqn[i][k];
+				}
 			}
 		}
-		for(int i=0;i<dim;i++){
-			vec.a[i]=vec.a[i]/cnt*step;
+		for(int i=n;i>=1;i--){
+			for(int j=i-1;j>=1;j--){
+				if(sgn(eqn[j][i])){
+					db r=eqn[j][i];
+					eqn[j][i]=0,eqn[j][n+1]-=r*eqn[i][n+1];
+				}
+			}
 		}
-		pos=pos+vec;
 	}
-	return pos;
+	Pt lst[D],vec[D];
+	int n;
+	inline void putvec(db eqn[],int d,db mul){
+		eqn[n+1]+=mul*lst[n].a[d];
+		for(int i=1;i<n;i++){
+			eqn[i]+=vec[i].a[d]*mul;
+		}
+	}
+	inline Circ main(const set<int>&s){
+		if(s.empty()){
+			Circ ans;
+			for(int i=0;i<dim;i++){
+				ans.o.a[i]=0;
+			}
+			ans.r=0;
+			return ans;
+		}
+		n=0;
+		for(set<int>::const_iterator it=s.begin();it!=s.end();it++){
+			lst[++n]=pt[*it];
+		}
+		for(int i=1;i<n;i++){
+			vec[i]=lst[i]-lst[n];
+		}
+		for(int i=1;i<=n;i++){//for lst[i]
+			for(int j=1;j<n;j++){
+				eqn[i][j]=0;
+			}
+			eqn[i][n]=1,eqn[i][n+1]=-lst[i].d2();
+			for(int j=0;j<dim;j++){
+				putvec(eqn[i],j,lst[i].a[j]*2);
+			}
+		}
+		gauss(n);
+		Circ ans;
+		ans.o=lst[n];
+		for(int i=1;i<n;i++){
+			db v=-eqn[i][n+1];
+			for(int j=0;j<dim;j++){
+				ans.o.a[j]+=vec[i].a[j]*v;
+			}
+		}
+		ans.r=sqrt((ans.o-lst[1]).d2());
+		return ans;
+	}
+}
+set<int>s;
+Circ ria(int n){
+	Circ ans=gcen::main(s);
+	for(int i=1;i<=n;i++){
+		if(ans.has(pt[i]))continue;
+		s.insert(i);
+		ans=ria(i-1);
+		s.erase(i);
+	}
+	return ans;
 }
 int main(){
 #ifndef ONLINE_JUDGE
@@ -91,20 +167,10 @@ int main(){
 			scanf("%lf",pt[i].a+j);
 		}
 	}
-	Pt ans;
-	db tval=1e100;
-	for(int tot=3;tot--;){
-		Pt cur=work(n);
-		db cval=0;
-		for(int i=1;i<=n;i++){
-			apmax(cval,sqrt((pt[i]-cur).d2()));
-		}
-		if(cval<tval){
-			ans=cur,tval=cval;
-		}
-	}
+	random_shuffle(pt+1,pt+n+1);
+	Circ ans=ria(n);
 	for(int i=0;i<dim;i++){
-		printf("%.10lf ",ans.a[i]);
+		printf("%.10lf ",ans.o.a[i]);
 	}
 	putchar('\n');
 	return 0;
