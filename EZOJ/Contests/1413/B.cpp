dfa3154ac847d95a608deacd61b9b18044d142c1
Kerry Su <i_sukai@live.com>
2018-04-12 16:45:50 +0800

    Accepted. 1271ms.

--- EZOJ/Contests/1413/B.cpp
+++ EZOJ/Contests/1413/B.cpp
@@ -20,7 +20,10 @@ template<class T>inline T next_num(){
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-const int N=610,O=1000000007;
+const int N=610,S=N*2,O=1000000007;
+template<class T1,class T2>inline void apadd(T1 &a,const T2 &b){
+	a=(a+b)%O;
+}
 inline int fpow(int x,int n){
 	int a=1;
 	for(;n;n>>=1,x=(lint)x*x%O){
@@ -33,206 +36,120 @@ inline int fpow(int x,int n){
 inline int inv(int x){
 	return fpow(x,O-2);
 }
-int n;
+namespace gmath{
+	int invnum[S];
+	inline void main(int n){
+		invnum[1]=1;
+		for(int i=2;i<=n;i++){
+			invnum[i]=O-(lint)O/i*invnum[O%i]%O;
+			assert((lint)invnum[i]*i%O==1);
+		}
+	}
+}
+struct Info:vector<int>{
+	typedef vector<int>vi;
+	typedef iterator iter;
+	vi sum;
+	int cache;
+	inline void calc(){
+		cache=0;
+		for(iter it1=sum.begin()+1,it2=begin();it2!=end();it1++,it2++){
+			apadd(cache,(lint)*it1**it2);
+		}
+	}
+	int lend,rend,n;
+	inline void init(int tn){
+		n=rend-lend+1;
+		lint curv=1;
+		push_back(0);
+		for(int i=0;i<=tn;i++){
+			sum.push_back(curv);
+			curv=curv*(n+i)%O*gmath::invnum[i+1]%O;
+		}
+	}
+	inline void mul2(){
+		for(iter it=begin();it!=end();it++){
+			*it=(*it<<1)%O;
+		}
+	}
+	inline void add(int v){
+		apadd(at(0),v);
+	}
+	inline void pre_int(){
+		insert(begin(),0);
+	}
+	inline void suf_int(){
+		int s=0;
+		for(int i=0,ti=size();i<ti;i++){
+			int &f=at(i);
+			apadd(s,(lint)f*sum[i+1]);
+			if(i){
+				apadd(at(i-1),f);
+			}else{
+				apadd(s,f);
+			}
+			f=O-f;
+		}
+		insert(begin(),s);
+	}
+}f[S];
 int lval[N],rval[N];
 char tp[N];
-namespace solve1{
-	const int S=100010;
-	int f[S];
-	inline int main(){
-		memset(f,0,sizeof(f));
-		int mx=0;
-		for(int i=1;i<=n;i++){
-			apmax(mx,rval[i]);
-			if(tp[i]=='>'){
-				lint sum=1;
-				for(int j=0;j<lval[i];j++){
-					sum+=f[j];
-				}
-				sum%=O;
-				for(int j=lval[i];j<=rval[i];j++){
-					sum=f[j]=(f[j]+sum)%O;
-				}
-			}else if(tp[i]=='<'){
-				lint sum=1;
-				for(int j=mx;j>rval[i];j--){
-					sum+=f[j];
-				}
-				sum%=O;
-				for(int j=rval[i];j>=lval[i];j--){
-					sum=f[j]=(f[j]+sum)%O;
-				}
-			}else{
-				for(int j=lval[i];j<=rval[i];j++){
-					f[j]=((f[j]<<1)+1)%O;
-				}
-			}
-		}
-		lint ans=0;
-		for(int i=0;i<=mx;i++){
-			ans+=f[i];
-		}
-		return ans%O;
-	}
-}
-const int S=N*2;
-int m=0;
-int lend[S],rend[S];
 int vlst[S];
-namespace solveeq{
-	int f[S];
-	inline int main(){
-		memset(f,0,sizeof(f));
-		for(int i=1;i<=n;i++){
-			for(int j=1;j<=m&&rend[j]<=rval[i];j++){
-				if(lval[i]<=lend[j]){
-					f[j]=((f[j]<<1)+1)%O;
-				}
-			}
-		}
-		int ans=0;
-		for(int i=1;i<=m;i++){
-			ans=(ans+(lint)f[i]*(rend[i]-lend[i]+1))%O;
-		}
-		return ans;
-	}
-}
-namespace solvegeq{
-	int invnum[N];
-	struct info{
-		typedef vector<int>vi;
-		typedef vi::iterator iter;
-		vi sum,val;
-		inline int eval(){
-			int ans=0;
-			for(iter it1=sum.begin(),it2=val.begin();it2!=val.end();it1++,it2++){
-				ans=(ans+(lint)*it1**it2)%O;
-			}
-			return ans;
-		}
-		int lend,rend,n;
-		inline void init(){
-			n=rend-lend+1;
-			lint curv=1;
-			int tn=n+1;
-			val.push_back(0);
-			val.push_back(0);
-			for(int i=0;i<=tn;i++){
-				sum.push_back(curv);
-				curv=curv*(n+i+1)%O*invnum[i+1]%O;
-			}
-		}
-		inline void mul2(){
-			for(iter it=val.begin();it!=val.end();it++){
-				*it=(*it<<1)%O;
-			}
-		}
-		inline void sh(){
-			int sz=val.size();
-			val.push_back(0);
-			for(int i=sz;i>=1;i--){
-				val[i]=val[i-1];
-			}
-			val[0]=0;
-		}
-	}f[S];
-	inline int main(){
-		invnum[1]=1;
-		for(int i=2;i<=n+2;i++){
-			invnum[i]=(O-(lint)O/i*invnum[O%i])%O;
-			assert((lint)invnum[i]*i%O);
-		}
-		for(int i=1;i<=m;i++){
-			f[i].lend=lend[i];
-			f[i].rend=rend[i];
-			f[i].init();
-		}
-		for(int i=1;i<=n;i++){
-			if(tp[i]=='>'){
-				lint sum=1;
-				int j=1;
-				for(;lend[j]<=lval[i];j++){
-					sum+=f[j].eval();
-				}
-				sum%=O;
-				for(;rend[j]<=rval[i];j++){
-					f[j].sh();
-					(f[j].val[1]+=sum)%=O;
-				}
-			}else for(int j=1;j<=m&&rend[j]<=rval[i];j++){
-				if(lval[i]<=lend[j]){
-					f[j].mul2();
-					++f[j].val[1];
-				}
-			}
-		}
-		lint ans=0;
-		for(int i=1;i<=m;i++){
-			ans+=f[i].eval();
-		}
-		return ans%O;
-	}
-}
-namespace solveleq{
-	inline int main(){
-		reverse(lval+1,lval+n+1);
-		reverse(rval+1,rval+n+1);
-		reverse(tp+1,tp+n+1);
-		for(int i=1;i<=n;i++){
-			if(tp[i]=='<'){
-				tp[i]='>';
-			}
-		}
-		return solvegeq::main();
-	}
-}
-inline string space(){
-	ifstream fin("/proc/self/status");
-	return string(istreambuf_iterator<char>(fin),istreambuf_iterator<char>());
-}
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("plane.in","r",stdin);
 	freopen("plane.out","w",stdout);
 #endif
-	n=ni;
-	char flag='=';
-	int mx=0;
-	for(int i=1;i<=n;i++){
-		lval[i]=ni,rval[i]=ni;
-		apmax(mx,rval[i]);
-		char &c=tp[i];
-		while(c=getchar(),c!='<'&&c!='>'&&c!='=');
-		if(c!='='){
-			if(flag=='='||flag==c){
-				flag=c;
-			}else{
-				flag=0;
-			}
-		}
-	}
-	if(mx<=200000||flag==0){
-		printf("%d\n",solve1::main());
-		return 0;
-	}
+	int n=ni,m=0;
+	gmath::main(n+2);
 	{
 		int ls=0;
 		for(int i=1;i<=n;i++){
+			lval[i]=ni,rval[i]=ni;
+			char &c=tp[i];
+			while(c=getchar(),c!='<'&&c!='>'&&c!='=');
 			vlst[++ls]=lval[i]-1;
 			vlst[++ls]=rval[i];
 		}
 		sort(vlst+1,vlst+ls+1);
 		for(int i=2;i<=ls;i++){
 			if(vlst[i-1]==vlst[i])continue;
-			++m,lend[m]=vlst[i-1]+1,rend[m]=vlst[i];
+			++m,f[m].lend=vlst[i-1]+1,f[m].rend=vlst[i],f[m].init(n+1);
 		}
 	}
-	if(flag=='<'){
-		printf("%d\n",solveleq::main());
-	}else if(flag=='>'){
-		printf("%d\n",solvegeq::main());
-	}else{
-		printf("%d\n",solveeq::main());
+	for(int i=1;i<=n;i++){
+		if(tp[i]=='>'){
+			lint sum=1;
+			int j=1;
+			for(;f[j].rend<lval[i];j++){
+				sum+=f[j].cache;
+			}
+			sum%=O;
+			for(;j<=m&&f[j].rend<=rval[i];j++){
+				f[j].pre_int(),f[j].add(sum);
+				apadd(sum,f[j].cache),f[j].calc();
+			}
+		}else if(tp[i]=='<'){
+			lint sum=1;
+			int j=m;
+			for(;f[j].lend>rval[i];j--){
+				sum+=f[j].cache;
+			}
+			sum%=O;
+			for(;j>=1&&f[j].lend>=lval[i];j--){
+				f[j].suf_int(),f[j].add(sum);
+				apadd(sum,f[j].cache),f[j].calc();
+			}
+		}else for(int j=1;j<=m&&f[j].rend<=rval[i];j++){
+			if(lval[i]<=f[j].lend){
+				f[j].mul2(),f[j].add(1),f[j].calc();
+			}
+		}
 	}
-	return 0;
+	lint ans=0;
+	for(int i=1;i<=m;i++){
+		ans+=f[i].cache;
+	}
+	printf("%lld\n",ans%O);
 }
