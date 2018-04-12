4597b9c9fe524a9b876ba3cff04e282ba2a528d0
Kerry Su <i_sukai@live.com>
2018-04-12 20:24:51 +0800

    Accepted. 6780ms.

--- EZOJ/Contests/1413/A.cpp
+++ EZOJ/Contests/1413/A.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#define NDEBUG
 #include <cassert>
 #include <cctype>
 #include <fstream>
@@ -19,102 +20,103 @@ template<class T>inline T next_num(){
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 const int N=3000010,M=30000010,D=4,O=1000000007;
+char s[N];
 namespace sam{
 	const int N=::N*2*2;
 	struct Node;
 	typedef Node* node;
-	node null;
 	struct Node{
 		node lnk,go[D];
-		node head,bro;
-		int tlen[D];//bef trans d shortened to
+		node son[D];
 		int len;
+		int pos;
 		int cnt;//count of appearances
 		lint sum;
-		inline node ext(int);
-	}pool[N],Null;
+		int d;
+		inline node ext(int,int,int);
+	}pool[N];
 	node pol=pool;
-	inline node nn(node x=null){
-		static node &n=pol;
-		return *n=*x,n++;
-	}
-	inline void init(){
-		memset(null=&Null,0,sizeof(Null));
-		null->lnk=null;
-		null->head=null->bro=null;
-		for(int i=0;i<D;i++){
-			null->go[i]=null;
-		}
+	inline node nn(){
+		return pol++;
 	}
 	node ini;
-	inline node Node::ext(int c){
+	inline node Node::ext(int c,int pos,int d){
 		node p=this,np=nn();
 		np->len=p->len+1;
-		for(;p!=null&&p->go[c]==null;p=p->lnk){
+		np->pos=pos;
+		np->d=d;
+		for(;p&&p->go[c]==0;p=p->lnk){
 			p->go[c]=np;
 		}
-		if(p==null)return np->lnk=ini,np;
+		if(p==0)return np->lnk=ini,np;
 		node q=p->go[c];
 		if(q->len==p->len+1)return np->lnk=q,np;
-		node nq=nn(q);
+		node nq=nn();
+		nq->lnk=q->lnk;
+		memcpy(nq->go,q->go,sizeof(nq->go));
+		nq->pos=pos;
+		nq->d=d;
 		nq->len=p->len+1;
 		q->lnk=np->lnk=nq;
-		for(;p!=null&&p->go[c]==q;p=p->lnk){
+		for(;p!=0&&p->go[c]==q;p=p->lnk){
 			p->go[c]=nq;
 		}
 		return np;
 	}
 	node que[N];
-	int qh=0,qt=0;
-	inline void ae(node u,node v){
-		v->bro=u->head,u->head=v;
-	}
-	inline void bfs(){
-		for(node i=pool;i<pol;i++){
-			if(i->lnk==null){
-				que[qt++]=i;
-			}else{
-				ae(i->lnk,i);
-			}
+	int cnt[N];
+	inline void buildtree(int n){
+		for(node i=pool+1;i<ini;i++){
+			i->lnk->son[(int)s[i->pos-i->lnk->len]]=i;
+			++cnt[i->len];
 		}
-		while(qh<qt){
-			node x=que[qh++];
-			for(node i=x->head;i!=null;i=i->bro){
-				que[qt++]=i;
-			}
-			for(int j=0;j<D;j++){
-				if(x->go[j]==null){
-					if(x->lnk==null){
-						x->go[j]=x;
-						x->tlen[j]=-1;
-					}else{
-						x->go[j]=x->lnk->go[j];
-						x->tlen[j]=x->lnk->tlen[j];
-					}
-				}else{
-					x->tlen[j]=x->len;
-				}
-			}
+		for(node i=ini+1;i<pol;i++){
+			i->lnk->son[(int)s[i->pos+i->lnk->len]]=i;
+			++cnt[i->len];
+		}
+		cnt[0]+=2;
+		for(int i=1;i<=n;i++){
+			cnt[i]+=cnt[i-1];
+		}
+		for(node i=pool;i<pol;i++){
+			que[--cnt[i->len]]=i;
 		}
 	}
 	inline void buildsum(){
-		for(int i=qt-1;i>=0;i--){
-			node x=que[i];
-			if(x->lnk!=null){
-				x->lnk->cnt+=x->cnt;
-			}
+		for(node *i=que+(pol-pool)-1,*ti=que+2;i>=ti;--i){
+			(*i)->lnk->cnt+=(*i)->cnt;
 		}
-		for(int i=0;i<qt;i++){
-			node x=que[i];
+		for(node *i=que+2,*ti=que+(pol-pool);i<ti;++i){
+			node x=*i;
 			x->sum=(x->sum+x->lnk->sum+(lint)(x->len-x->lnk->len)*x->cnt)%O;
 		}
 	}
 }
-char s[M];
-sam::node pre[N],suf[N];
 inline int charmap(char c){
-	return c=='A'?0:c=='T'?1:c=='G'?2:3;
+	return c=='A'?0:c=='T'?1:c=='C'?2:3;
 }
+char t[M];
+inline void work1(sam::node x){
+	int i=0;
+	using namespace sam;
+	for(node y;t[i]&&(y=x->son[charmap(t[i])])!=0;){
+		x=y;
+		for(;t[i]&&i<x->len&&charmap(t[i])==s[x->pos-i];++i);
+		if(i<x->len)break;
+	}
+	++x->cnt,x->sum+=i-x->len;
+}
+inline void work2(sam::node x){
+	int i=0;
+	using namespace sam;
+	for(node y;t[i]&&(y=x->son[charmap(t[i])])!=0;){
+		x=y;
+		for(;t[i]&&i<x->len&&charmap(t[i])==s[x->pos+i];++i);
+		if(i<x->len)break;
+	}
+	++x->cnt,x->sum+=i-x->len;
+}
+sam::node pre[N],suf[N];
 inline string space(){
 	ifstream fin("/proc/self/status");
 	return string(istreambuf_iterator<char>(fin),istreambuf_iterator<char>());
@@ -126,34 +128,21 @@ int main(){
 #endif
 	scanf("%s",s+1);
 	int n=strlen(s+1);
-	sam::init();
 	sam::node prest=pre[0]=sam::ini=sam::nn();
 	for(int i=1;i<=n;i++){
-		pre[i]=pre[i-1]->ext(charmap(s[i]));
+		s[i]=charmap(s[i]);
+	}
+	for(int i=1;i<=n;i++){
+		pre[i]=pre[i-1]->ext(s[i],i,1);
 	}
 	sam::node sufst=suf[n+1]=sam::ini=sam::nn();
 	for(int i=n;i>=1;i--){
-		suf[i]=suf[i+1]->ext(charmap(s[i]));
+		suf[i]=suf[i+1]->ext(s[i],i,-1);
 	}
-	sam::bfs();
+	sam::buildtree(n);
 	for(int i=1,m=ni;i<=m;i++){
-		scanf("%s",s);
-		sam::node p=prest,q=sufst;
-		using sam::null;
-		int len=0;
-		for(;s[len];len++);
-		int plen=0,qlen=0;
-		for(int j=len-1;j>=0;j--){
-			int c=charmap(s[j]);
-			plen=min(plen,p->tlen[c])+1,p=p->go[c];
-			qlen=min(qlen,q->tlen[c])+1,q=q->go[c];
-		}
-		if(p!=prest){
-			++p->cnt,p->sum+=plen-p->len;
-		}
-		if(q!=sufst){
-			++q->cnt,q->sum+=qlen-q->len;
-		}
+		scanf("%s",t);
+		work1(prest),work2(sufst);
 	}
 	sam::buildsum();
 	int ans=0;
