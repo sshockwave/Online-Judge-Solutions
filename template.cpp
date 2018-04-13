cb24ff18ccbbb2d477560866e036b3eeb42f457e
Kerry Su <i_sukai@live.com>
2018-04-13 14:28:04 +0800

    Change the name of 'model.cpp' to fit its definition.

--- template.cpp
+++ template.cpp
@@ -10,10 +10,10 @@ typedef long long lint;
 template<class T>inline T next_num(){
 	T i=0;char c;
 	while(!isdigit(c=getchar())&&c!='-');
-	bool flag=c=='-';
-	flag?c=getchar():0;
+	bool neg=c=='-';
+	neg?c=getchar():0;
 	while(i=i*10-'0'+c,isdigit(c=getchar()));
-	return flag?-i:i;
+	return neg?-i:i;
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
