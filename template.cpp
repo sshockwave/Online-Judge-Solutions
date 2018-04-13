5f41873a1f1bb888d7f77765d555dea3ffdbf6e9
Kerry Su <i_sukai@live.com>
2018-04-13 14:28:53 +0800

    Update IO file name to 'template'.

--- template.cpp
+++ template.cpp
@@ -19,8 +19,8 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 int main(){
 #ifndef ONLINE_JUDGE
-	freopen("model.in","r",stdin);
-	freopen("model.out","w",stdout);
+	freopen("template.in","r",stdin);
+	freopen("template.out","w",stdout);
 #endif
 	return 0;
 }
