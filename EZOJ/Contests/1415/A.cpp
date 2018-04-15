0a736445f7c543a47fe3002f4d065c9230392d35
Kerry Su <i_sukai@live.com>
2018-04-15 19:57:06 +0800

    Accepted. 1868ms.

--- EZOJ/Contests/1415/A.cpp
+++ EZOJ/Contests/1415/A.cpp
@@ -18,34 +18,24 @@ template<class T>inline T next_num(){
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 template<class T>inline T cabs(const T &x){return x>=0?x:-x;}
-const int N=100,L=1000010,INF=0x7f7f7f7f;
-int f[N][20];
-inline int work(const char s[],const char t[],const int lens,const int lent){
-	if(cabs(lens-lent)>8)return 0;
-	memset(f[0],127,sizeof(f[0]));
-	f[0][8]=0;
-	for(int i=0;i<lens;i++){
-		memset(f[i+1],127,sizeof(f[i+1]));
-		bool flag=false;
-		for(int j=max(0,8-i),tj=min(16,lent-i+8);j<=tj;j++){
-			int F=f[i][j];
-			if(F>8)continue;
-			flag=true;
-			int a=i,b=i+j-8;
-			if(a<lens&&b<lent){//match
-				apmin(f[i+1][j],F+(s[a+1]!=t[b+1]));
-			}
-			if(b<lent&&j<16){//add char
-				apmin(f[i][j+1],F+1);
-			}
-			if(a<lens&&j>0){//del char
-				apmin(f[i+1][j-1],F+1);
-			}
-		}
-		if(!flag)return 0;
+const int N=210,L=1000010+N,INF=0x7f7f7f7f;
+int ans;
+void dfs(char a[],char b[],int ra,int rb,int step){
+	if(step+cabs(ra-rb)>=ans)return;
+	for(;a[0]&&a[0]==b[0];++a,++b);
+	if(a[0]==0&&b[0]==0){
+		ans=step;
+		return;
 	}
-	int F=f[lens][lent-lens+8];
-	return F<=8?F:0;
+	if(a[0]){
+		dfs(a+1,b,ra-1,rb,step+1);
+	}
+	if(b[0]){
+		dfs(a,b+1,ra,rb-1,step+1);
+	}
+	if(a[0]&&b[0]){
+		dfs(a+1,b+1,ra-1,rb-1,step+1);
+	}
 }
 char pool[L],*s[N];
 int cnt[10];
@@ -63,7 +53,9 @@ int main(){
 	}
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<i;j++){
-			cnt[work(s[i]-1,s[j]-1,s[i+1]-s[i]-1,s[j+1]-s[j]-1)]++;
+			ans=9;
+			dfs(s[i],s[j],s[i+1]-s[i]-1,s[j+1]-s[j]-1,0);
+			++cnt[ans];
 		}
 	}
 	for(int i=1;i<=8;i++){
