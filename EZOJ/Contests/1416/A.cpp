e0e013f2e8d0e8b4543e910886c946fffbece00a
Kerry Su <i_sukai@live.com>
2018-04-17 14:26:36 +0800

    Accepted. 1770ms.

--- EZOJ/Contests/1416/A.cpp
+++ EZOJ/Contests/1416/A.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <cctype>
 #include <vector>
+#include <algorithm>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -19,8 +20,72 @@ template<class T>inline T next_num(){
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 const int N=100010;
+namespace seg{
+	const int N=::N<<1;
+	struct Node;
+	typedef Node* node;
+	struct Node{
+		node lson,rson;
+		int l,m,r;
+		int mn,cnt,dt;
+		inline void dn(){
+			if(dt){
+				lson->mn+=dt,lson->dt+=dt;
+				rson->mn+=dt,rson->dt+=dt;
+				dt=0;
+			}
+		}
+		inline void up(){
+			assert(dt==0);
+			mn=min(lson->mn,rson->mn);
+			cnt=0;
+			if(lson->mn==mn){
+				cnt+=lson->cnt;
+			}
+			if(rson->mn==mn){
+				cnt+=rson->cnt;
+			}
+		}
+	}pool[N];
+	node build(int l,int r){
+		static node n=pool;
+		node x=n++;
+		x->l=l,x->m=(l+r)>>1,x->r=r;
+		if(l==r){
+			x->mn=0,x->cnt=1,x->dt=0;
+		}else{
+			x->lson=build(l,x->m);
+			x->rson=build(x->m+1,r);
+			x->up();
+		}
+		return x;
+	}
+	void add(node x,int l,int r,int v){
+		if(x->l==l&&x->r==r){
+			x->mn+=v,x->dt+=v;
+			return;
+		}
+		x->dn();
+		if(r<=x->m){
+			add(x->lson,l,r,v);
+		}else if(l>x->m){
+			add(x->rson,l,r,v);
+		}else{
+			add(x->lson,l,x->m,v);
+			add(x->rson,x->m+1,r,v);
+		}
+		x->up();
+	}
+	int ask(node x,int l,int r){
+		if(x->mn>0)return 0;
+		if(x->l==l&&x->r==r)return x->cnt;
+		x->dn();
+		if(r<=x->m)return ask(x->lson,l,r);
+		if(l>x->m)return ask(x->rson,l,r);
+		return ask(x->lson,l,x->m)+ask(x->rson,x->m+1,r);
+	}
+}
 int n;
-int factcnt[N],factls[N],*fact[N];
 namespace T{
 	const int E=::N<<1;
 	int to[E],bro[E],head[N],e=0;
@@ -35,33 +100,62 @@ namespace T{
 	inline void add(int u,int v){
 		ae(u,v),ae(v,u);
 	}
-	lint ans=0;
-	int has2=0,has3=0;
-	void dfs(int x,int fa){
-		if(x==1||cnt[x]||(((x&1)==0)&&has2)||(x%3==0&&has3))return;
-		if(fa){
-			ans++;
-		}
-		if(fa&&bro[head[x]]==-1)return;
-		if(x==2){
-			has2++;
-		}else if(x==3){
-			has3++;
-		}else for(int i=0;i<factcnt[x];++i){
-			++cnt[fact[x][i]];
-		}
+	int fa[N],son[N],size[N],dep[N],top[N],dfn[N],dfe[N],idx[N],tim=0;
+	void dfs1(int x){
+		size[x]=1;
+		dep[x]=dep[fa[x]]+1;
 		for(int i=head[x],v;~i;i=bro[i]){
-			if((v=to[i])!=fa){
-				dfs(v,x);
+			if((v=to[i])!=fa[x]){
+				fa[v]=x;
+				dfs1(v);
+				size[x]+=size[v];
+				if(size[v]>size[son[x]]){
+					son[x]=v;
+				}
 			}
 		}
-		if(x==2){
-			has2--;
-		}else if(x==3){
-			has3--;
-		}else for(int i=0;i<factcnt[x];++i){
-			--cnt[fact[x][i]];
+	}
+	void dfs2(int x){
+		idx[dfn[x]=++tim]=x;
+		top[x]=son[fa[x]]==x?top[fa[x]]:x;
+		if(son[x]){
+			dfs2(son[x]);
+			for(int i=head[x],v;~i;i=bro[i]){
+				if((v=to[i])!=fa[x]&&v!=son[x]){
+					dfs2(v);
+				}
+			}
 		}
+		dfe[x]=tim;
+	}
+	inline int fly(int x,int n){
+		for(int d;d=dep[x]-dep[fa[top[x]]],d<=n;x=fa[top[x]],n-=d);
+		return idx[dfn[x]-n];
+	}
+}
+struct Query{
+	int y,l,r,v;
+	inline friend bool operator < (const Query &a,const Query &b){
+		return a.y<b.y;
+	}
+}qry[N*40];
+int qs=0;
+inline void add_op(int x1,int x2,int y1,int y2){
+	if(x1>x2||y1>y2)return;
+	qry[++qs]=(Query){y1,x1,x2,1};
+	qry[++qs]=(Query){y2+1,x1,x2,-1};
+}
+inline void add_op(int x,int y){
+	using namespace T;
+	if(dfn[x]>dfn[y]){
+		swap(x,y);
+	}
+	if(dfn[x]<=dfn[y]&&dfn[y]<=dfe[x]){
+		int z=fly(y,dep[y]-dep[x]-1);
+		add_op(1,dfn[z]-1,dfn[y],dfe[y]);
+		add_op(dfn[y],dfe[y],dfe[z]+1,n);
+	}else{
+		add_op(dfn[x],dfe[x],dfn[y],dfe[y]);
 	}
 }
 int main(){
@@ -74,29 +168,23 @@ int main(){
 	for(int i=1;i<n;i++){
 		T::add(ni,ni);
 	}
+	T::dfs1(1),T::dfs2(1);
 	for(int i=1;i<=n;i++){
-		for(int j=i;j<=n;j+=i){
-			++factcnt[j];
-			if(j>i){
-				++factcnt[i];
-			}
+		for(int j=i<<1;j<=n;j+=i){
+			add_op(i,j);
 		}
-		factls[i]=0;
-		fact[i]=new int[factcnt[i]];
 	}
-	for(int i=1;i<=n;i++){
-		for(int j=i;j<=n;j+=i){
-			fact[j][factls[j]++]=i;
-			if(j>i){
-				fact[i][factls[i]++]=j;
-			}
+	sort(qry+1,qry+qs+1);
+	seg::node rt=seg::build(1,n);
+	lint ans=0;
+	for(int v=1,j=1;v<=n;v++){
+		for(;j<=qs&&qry[j].y==v;j++){
+			seg::add(rt,qry[j].l,qry[j].r,qry[j].v);
 		}
-		assert(factls[i]==factcnt[i]);
+		if(v>1){
+			ans+=seg::ask(rt,1,v-1);
+		}
 	}
-	for(int i=1;i<=n;i++){
-		T::dfs(i,0);
-	}
-	assert((T::ans&1)==0);
-	printf("%lld\n",T::ans>>1);
+	printf("%lld\n",ans);
 	return 0;
 }
