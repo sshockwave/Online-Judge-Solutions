c7f02c2ee4be22015e1899e90b2954889e65dfe9
Kerry Su <i_sukai@live.com>
2018-04-21 16:37:18 +0800

    Accepted. 3373ms.

--- EZOJ/Contests/1419/B.cpp
+++ EZOJ/Contests/1419/B.cpp
@@ -20,113 +20,62 @@ template<class T>inline void mset(T a,int v,int n){memset(a,v,n*sizeof(a[0]));}
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 const int N=410,INF=0x7f7f7f7f;
-namespace G2{
-	const int N=::N*::N*2,E=N*2;
-	int to[E],bro[E],head[N],n,e=0;
-	inline void init(int _n){
-		mset(head+1,-1,n=_n);
-	}
-	inline void ae(int u,int v){
-		to[e]=v,bro[e]=head[u],head[u]=e++;
-	}
-	int deg[N];
-	int que[N],qh=0,qt=0;
-	inline void topsort();
-}
 namespace G{
 	const int N=::N*::N*2;
 	int edg[N][2],n=1;
 	inline int nn(){
 		return ++n;
 	}
-	int dfn[N],low[N],bln[N],sz[N],tim=0;
-	int stk[N],ss=0;
-	bool instk[N];
-	void tarjan(int x){
-		dfn[x]=low[x]=++tim;
-		assert(!instk[x]);
-		stk[ss++]=x,instk[x]=true;
-		for(int i=0;i<2&&edg[x][i];i++){
-			int v=edg[x][i];
-			if(dfn[v]==0){
-				tarjan(v);
-				apmin(low[x],low[v]);
-			}else if(instk[v]){
-				apmin(low[x],dfn[v]);
-			}
-		}
-		if(dfn[x]==low[x]){
-			int v;
-			sz[x]=0;
-			do{
-				v=stk[--ss];
-				bln[v]=x;
-				++sz[x];
-				instk[v]=false;
-			}while(x!=v);
-		}
-	}
 	int ans[N];
-	int que[N],vis[N],vtim=0;
-	inline void bfs(int s){
-		if(sz[s]>1)return;
-		assert(sz[bln[s]]==1);
-		int qh=0,qt=0;
-		que[qt++]=s,vis[s]=++vtim;
+	int que[N],qh,qt,vis[N],vtim=0;
+	bool okay[N];
+	inline void reset(){
+		qh=qt=0,++vtim;
+	}
+	inline bool pshque(int x){
+		if(!okay[x])return false;
+		que[qt++]=x,vis[x]=vtim;
+		return true;
+	}
+	inline bool bfs(){
 		while(qh<qt){
 			int x=que[qh++];
 			for(int i=0;i<2&&edg[x][i];i++){
 				int v=edg[x][i];
 				if(vis[v]<vtim){
-					if(ans[v]==INF)return;
-					if((v^s)==1)return;
-					assert(sz[bln[v]]<INF);
-					que[qt++]=v,vis[v]=vtim;
+					if(!pshque(v))return false;
 				}
 			}
 		}
-		ans[s]=qt;
+		return true;
+	}
+	int deg[N];
+	int revedg[N][2];
+	inline void add_rev(int u,int v){
+		if(u){
+			revedg[u][1]=revedg[u][0],revedg[u][0]=v;
+			++deg[v];
+		}
 	}
 	inline void main(){
-		mset(dfn+1,0,n);
-		for(int i=2;i<=n;i++){
-			if(dfn[i]==0){
-				tarjan(i);
+		reset();
+		mset(deg+1,0,n);
+		for(int i=1;i<=n;i++){
+			add_rev(edg[i][0],i);
+			add_rev(edg[i][1],i);
+			if(deg[i]==0){
+				que[qt++]=i;
 			}
 		}
-		G2::init(n);
-		for(int i=2;i<=n;i++){
-			if(edg[i][0]){
-				if(bln[edg[i][0]]!=bln[i]){
-					G2::ae(bln[i],bln[edg[i][0]]);
+		mset(okay+1,0,n);
+		while(qh<qt){
+			int x=que[qh++];
+			okay[x]=true;
+			for(int i=0;i<2&&revedg[x][i];i++){
+				int v=revedg[x][i];
+				if(--deg[v]==0){
+					que[qt++]=v;
 				}
-				if(edg[i][1]&&bln[edg[i][1]]!=bln[i]){
-					G2::ae(bln[i],bln[edg[i][1]]);
-				}
-			}
-		}
-		G2::topsort();
-		mset(ans+1,127,n);
-		for(int i=G2::qt-1;i>=0;i--){
-			bfs(G2::que[i]);
-		}
-	}
-}
-inline void G2::topsort(){
-	mset(deg+1,0,n);
-	for(int i=0;i<e;i++){
-		++deg[to[i]];
-	}
-	for(int x=1;x<=n;x++){
-		if(G::bln[x]==x&&deg[x]==0){
-			que[qt++]=x;
-		}
-	}
-	while(qh<qt){
-		int x=que[qh++];
-		for(int i=head[x],v;~i;i=bro[i]){
-			if(--deg[v=to[i]]==0){
-				que[qt++]=v;
 			}
 		}
 	}
@@ -177,6 +126,21 @@ int main(){
 		}
 	}
 	G::main();
+	mset(G::ans+1,127,G::n);
+	for(int i=1;i<=n;i++){
+		G::reset();
+		for(int j=1;j<=m;j++){
+			if(G::pshque(nd[i][j][1])&&G::bfs()){
+				G::ans[nd[i][j][1]]=G::qt;
+			}else break;
+		}
+		G::reset();
+		for(int j=m;j>=1;j--){
+			if(G::pshque(nd[i][j][3])&&G::bfs()){
+				G::ans[nd[i][j][3]]=G::qt;
+			}else break;
+		}
+	}
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=m;j++){
 			int a=min(G::ans[nd[i][j][0]],G::ans[nd[i][j][2]]);
