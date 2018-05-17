295c2e935f19a9877d38571ff51f3901916f5552
Kerry Su <i_sukai@live.com>
2018-05-17 16:15:19 +0800

    Init.

--- EZOJ/Contests/1429/C.cpp
+++ EZOJ/Contests/1429/C.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <queue>
 using namespace std;
 typedef long long lint;
 #define cout cerr
@@ -18,131 +19,158 @@ template<class T>inline T next_num(){
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 template<class T>inline void mset(T a,int v,int n){memset(a,v,n*sizeof(a[0]));}
-const int N=100010,L=5;
+const int N=100010,L=4;
 const lint LINF=0x7f7f7f7f7f7f7f7fll;
-int pval[N][L];
-int len;
+int pval[N][L],len;
+namespace seg{
+	const int N=::N<<1;
+	struct Node;
+	typedef Node* node;
+	struct Node{
+		node lson,rson;
+		int l,m,r;
+		lint dis[L][L];
+		inline lint eval(){
+			lint ans=-LINF;
+			for(int i=0;i<len;i++){
+				apmax(ans,dis[i][len-1]);
+			}
+			return ans;
+		}
+		inline void up(){
+			for(int i=0;i<len;i++){
+				for(int j=0;j<len;j++){
+					lint &ans=dis[i][j]=-LINF;
+					for(int k=0;k<len;k++){
+						if(lson->dis[i][k]>-LINF&&rson->dis[k][j]>-LINF){
+							apmax(ans,lson->dis[i][k]+rson->dis[k][j]);
+						}
+					}
+				}
+			}
+		}
+	}pool[N];
+	node build(int l,int r);
+}
 namespace T{
 	const int E=::N;
 	int to[E],bro[E],head[N],e=0;
 	inline void ae(int u,int v){
 		to[e]=v,bro[e]=head[u],head[u]=e++;
 	}
-	int fa[N];
+	int fa[N],son[N],size[N],top[N];
+	int dfn[N],idx[N];
+	seg::node rt[N];
+	struct state{
+		int x,t;
+		lint v;
+		inline friend bool operator < (const state &a,const state &b){
+			return a.v<b.v;
+		}
+	};
+	priority_queue<state>q[N][L];
+	int qtag[N];
+	lint sumv[N];
+	void calc(seg::node x,int p){
+		if(x->l!=x->r)return calc(dfn[p]<=x->m?x->lson:x->rson,p),x->up();
+		assert(x->l==dfn[p]);
+		lint (*const f)[L]=x->dis;
+		for(int i=0;i<len;i++){
+			for(int j=0;j<len;j++){
+				f[i][j]=-LINF;
+			}
+			priority_queue<state>&Q=q[p][i];
+			for(;!Q.empty()&&Q.top().t<qtag[Q.top().x];Q.pop());
+			if(!Q.empty()){
+				lint t=Q.top().v;
+				for(int j=0;j<len;j++){
+					apmax(f[i][j],t+sumv[p]);
+				}
+			}
+			if(i){
+				apmax(f[i][i-1],sumv[p]);
+			}else for(int j=0;j<len;j++){
+				apmax(f[i][j],sumv[p]);
+			}
+			for(int j=0;j<len;j++){
+				if(f[i][j]>-LINF){
+					f[i][j]+=pval[p][i];
+				}
+			}
+		}
+	}
+	lint prv[N];
+	inline void putque(int x){
+		assert(x!=1);
+		++qtag[x];
+		lint tmp=rt[x]->eval();
+		sumv[fa[x]]+=tmp-prv[x];
+		prv[x]=tmp;
+		for(int i=0;i+1<len;i++){
+			if(rt[x]->dis[i][len-1]>-LINF){
+				q[fa[x]][i+1].push((state){x,qtag[x],rt[x]->dis[i][len-1]-tmp});
+			}
+		}
+	}
 	inline void init(int n){
 		mset(head+1,-1,n);
+		fa[1]=0;
 		for(int i=2;i<=n;i++){
 			ae(fa[i]=ni,i);
 		}
-	}
-	inline bool ischain(int n){
-		for(int i=2;i<=n;i++){
-			if(fa[i]!=i-1)return false;
-		}
-		return true;
-	}
-	lint f[N][L];
-	void dfs(int x){
-		f[x][1]=pval[x][1];
-		for(int i=2;i<=len;i++){
-			f[x][i]=-LINF;
-		}
-		for(int i=head[x],v;~i;i=bro[i]){
-			dfs(v=to[i]);
-			lint t=-LINF;
-			for(int j=1;j<=len;j++){
-				apmax(t,f[v][j]);
+		mset(size+1,0,n);
+		mset(son+1,0,n);
+		for(int i=n;i>=2;i--){
+			size[fa[i]]+=++size[i];
+			if(size[i]>fa[i][son][size]){
+				son[fa[i]]=i;
 			}
-			lint allsum=f[x][1]-pval[x][1];
-			for(int j=1;j<=len;j++){
-				lint t2=f[x][j];
-				f[x][j]=-LINF;
-				if(t2>-LINF){
-					apmax(f[x][j],t2+t);
+		}
+		++size[1];
+		dfn[1]=1;
+		for(int x=1;x<=n;x++){
+			top[x]=x[fa][son]==x?x[fa][top]:x;
+			idx[dfn[x]]=x;
+			if(son[x]){
+				dfn[son[x]]=dfn[x]+1;
+				int tim=dfn[x]+size[son[x]];
+				for(int i=head[x],v;~i;i=bro[i]){
+					if((v=to[i])!=son[x]){
+						dfn[v]=tim+1;
+						tim+=size[dfn[v]];
+					}
 				}
-				if(j>1){
-					apmax(f[x][j],allsum+f[v][j-1]+pval[x][j]);
-				}
+			}else{
+				rt[top[x]]=seg::build(dfn[top[x]],dfn[x]);
 			}
+		}
+		mset(sumv+1,0,n);
+		mset(prv+1,0,n);
+		for(int x=n;x>=1;x--){
+			rt[x]=rt[top[x]];
+			calc(rt[x],x);
+			if(x==top[x]&&x!=1){
+				putque(x);
+			}
+		}
+	}
+	void upd(int x){
+		calc(rt[x],x);
+		x=top[x];
+		if(x!=1){
+			putque(x),upd(fa[x]);
 		}
 	}
 }
-namespace brute3{
-	const int D=4;
-	namespace seg{
-		const int N=::N<<1;
-		struct Node;
-		typedef Node* node;
-		struct Node{
-			node lson,rson;
-			int l,m,r;
-			lint dis[D][D];
-			inline void getdis(){
-				for(int i=0;i<len;i++){
-					for(int j=0;j<len;j++){
-						dis[i][j]=-LINF;
-					}
-					if(i<len-1){
-						dis[i][i+1]=pval[l][i+2];
-					}
-					dis[i][0]=pval[l][1];
-				}
-			}
-			inline void up(){
-				for(int i=0;i<len;i++){
-					for(int j=0;j<len;j++){
-						lint &ans=dis[i][j]=-LINF;
-						for(int k=0;k<len;k++){
-							if(rson->dis[i][k]>-LINF&&lson->dis[k][j]>-LINF){
-								apmax(ans,rson->dis[i][k]+lson->dis[k][j]);
-							}
-						}
-					}
-				}
-			}
-		}pool[N];
-		node build(int l,int r){
-			static node n=pool;
-			node x=n++;
-			x->l=l,x->m=(l+r)>>1,x->r=r;
-			if(l==r){
-				x->getdis();
-			}else{
-				x->lson=build(l,x->m);
-				x->rson=build(x->m+1,r);
-				x->up();
-			}
-			return x;
-		}
-		void upd(node x,int p){
-			if(x->l==x->r){
-				x->getdis();
-			}else{
-				upd(p<=x->m?x->lson:x->rson,p);
-				x->up();
-			}
-		}
+seg::node seg::build(int l,int r){
+	static node n=pool;
+	node x=n++;
+	x->l=l,x->m=(l+r)>>1,x->r=r;
+	if(l!=r){
+		x->lson=build(l,x->m);
+		x->rson=build(x->m+1,r);
 	}
-	inline int main(int n,int q){
-		seg::node rt=seg::build(1,n-1);
-		assert(n>1);
-		for(int i=1;i<=q;i++){
-			int x=ni;
-			for(int j=1;j<=len;j++){
-				pval[x][j]=ni;
-			}
-			if(x<n){
-				seg::upd(rt,x);
-			}
-			lint ans=-LINF;
-			for(int t=0;t<len;t++){
-				apmax(ans,rt->dis[0][t]);
-			}
-			ans+=pval[n][1];
-			printf("%lld\n",ans);
-		}
-		return 0;
-	}
+	return x;
 }
 int main(){
 #ifndef ONLINE_JUDGE
@@ -153,22 +181,17 @@ int main(){
 	len=ni;
 	T::init(n);
 	for(int i=1;i<=n;i++){
-		for(int j=1;j<=len;j++){
+		for(int j=0;j<len;j++){
 			pval[i][j]=ni;
 		}
 	}
-	if(T::ischain(n)&&n>1)return brute3::main(n,q);
 	for(int i=1;i<=q;i++){
 		int x=ni;
-		for(int j=1;j<=len;j++){
-			pval[x][j]=ni;
+		for(int j=0;j<len;j++){
+			pval[i][j]=ni;
 		}
-		T::dfs(1);
-		lint t=-LINF;
-		for(int j=1;j<=len;j++){
-			apmax(t,T::f[1][j]);
-		}
-		printf("%lld\n",t);
+		T::upd(x);
+		printf("%lld\n",T::rt[1]->eval());
 	}
 	return 0;
 }
