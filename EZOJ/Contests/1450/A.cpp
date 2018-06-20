8f85609b1c908a67ac528b3aa1b245cf50d12509
Kerry Su <i_sukai@live.com>
2018-06-20 15:45:56 +0800

    [AC] 0ms.

--- EZOJ/Contests/1450/A.cpp
+++ EZOJ/Contests/1450/A.cpp
@@ -19,59 +19,54 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 template<class T>inline void mset(T a,int v,int n){memset(a,v,n*sizeof(a[0]));}
 const int O=998244353;
-const int N=110;
-inline lint fpow(lint x,int n){
-	lint a=1;
-	for(;n;n>>=1,x*=x){
-		if(n&1){
-			a*=x;
-		}
-	}
-	return a;
+lint ex_gcd(lint a,lint b,lint &x,lint &y){//ax+by==d
+	if(b==0)return x=1,y=0,a;
+	const lint d=ex_gcd(b,a%b,y,x);
+	return y-=a/b*x,d;
 }
-lint f[N];
-lint a,b,c;
-lint cnt;
-inline bool check(){
-	lint t=0;
-	for(int i=100;i>=0;i--){
-		t=t*b+f[i];
-		if(t>c)return false;
-	}
-	return t==c;
-}
-void dfs(int e,lint rest){
-	if(e==0){
-		f[0]=rest;
-		cnt+=check();
-		return;
-	}
-	lint t=fpow(a,e);
-	for(lint &i=f[e]=0;i*t<=rest;i++){
-		dfs(e-1,rest-i*t);
-	}
-}
-inline int Main(){
-	memset(f,0,sizeof(f));
-	cnt=0;
-	int mxe=0;
+lint a,b;
+int solve(const lint u,const lint v){//f(a)==u,f(b)==v
+	if(u==0||v==0)return u==0&&v==0;
+	if(a==1&&b==1)return u==v?-1:0;
+	if(a==b&&u!=v)return 0;
+	lint k1,k2;
+	lint d=ex_gcd(a,b,k1,k2);
+	if((u-v)%d)return 0;
+	k2=-k2;
+	k1*=(u-v)/d,k2*=(u-v)/d;//cout
 	{
-		lint pw=1;
-		for(;pw<b;pw*=a,mxe++);
+		assert(a*k1-b*k2==u-v);
+		lint q=0;
+		if(k1<0){
+			apmax(q,(-k1+b/d-1)/(b/d));
+		}
+		if(k2<0){
+			apmax(q,(-k2+a/d-1)/(a/d));
+		}
+		k1+=b/d*q,k2+=a/d*q;
+		assert(a*k1-b*k2==u-v);
 	}
-	dfs(mxe,b);
-	return cnt%O;
+	for(;k1>=b/d&&k2>=a/d;k1-=b/d,k2-=a/d);
+	lint ans=0;
+	for(;k1*a<=u&&k2*b<=v;k1+=b/d,k2+=a/d){
+		const int t=solve(k1,k2);
+		if(t<0)return -1;
+		ans+=t;
+	}
+	return ans;
 }
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("polynomial.in","r",stdin);
 	freopen("polynomial.out","w",stdout);
 #endif
+	lint c;
 	for(;scanf("%lld%lld%lld",&a,&b,&c)!=EOF;){
-		if(a==1&&b==1){
+		const int ans=solve(b,c);
+		if(ans<0){
 			puts("infinity");
 		}else{
-			printf("%d\n",Main());
+			printf("%d\n",ans);
 		}
 	}
 	return 0;
