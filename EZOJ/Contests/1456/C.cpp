20ecb81fd9cb2c05b4f08ed1b4511fe37689cd17
Kerry Su <i_sukai@live.com>
2018-06-29 16:18:34 +0800

    [AC] 50ms O(r^2\log r)
    Assuming the described structure is a matroid.
    There are in total O(r) edges, and for each of them reconstruct the tree and conduct a bfs.

--- EZOJ/Contests/1456/C.cpp
+++ EZOJ/Contests/1456/C.cpp
@@ -21,78 +21,120 @@ template<class T>inline T next_num(){
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 template<class T>inline void mset(T a,int v,int n){memset(a,v,n*sizeof(a[0]));}
-const int N=310,D=10010;
-const int logN=64;
-namespace num_disc{
+const int N=310,D=10010,E=N<<1;
+namespace disc{
 	const int N=::N*4;
 	int *num[N],n;
 	inline void init(){
 		n=0;
 	}
-	inline void psh(int &a){
-		num[++n]=&a;
+	inline void psh(int &x){
+		num[++n]=&x;
 	}
 	inline bool ncmp(int *a,int *b){
 		return *a<*b;
 	}
-	inline void main(){
+	inline int main(){
+		int v=-1,j=0;
 		sort(num+1,num+n+1,ncmp);
-		int v=-1,j=-1;
 		for(int i=1;i<=n;i++){
 			*num[i]=v!=*num[i]?v=*num[i],++j:j;
 		}
+		return j;
 	}
 }
-struct pii{
-	int a,b,c,d;
-}item[N];
-struct LB{
-	ull a[logN];
-	inline void init(){
-		mset(a,0,logN);
+struct Edge{
+	int u,v;
+}edg[E];
+bool deci[E];
+namespace T{
+	const int N=::N*4,E=::E>>1<<1;
+	int to[E],bro[E],eid[E],head[N],e;
+	inline void init(int n){
+		mset(head+1,-1,n);
+		e=0;
 	}
-	inline bool ins(ull x){
-		for(int i=logN-1;i>=0&&x;i--){
-			if(((x>>i)&1)==0)continue;
-			if(a[i]){
-				x^=a[i];
-			}else{
-				a[i]=x;
-				return true;
+	inline void ae(int u,int v,int i){
+		to[e]=v,bro[e]=head[u],eid[e]=i,head[u]=e++;
+	}
+	inline void add(int u,int v,int i){
+		ae(u,v,i),ae(v,u,i);
+	}
+	int fa[N],rt[N],con[N],dep[N];
+	void dfs(int x,int r){
+		rt[x]=r;
+		dep[x]=dep[fa[x]]+1;
+		for(int i=head[x],v;~i;i=bro[i]){
+			if((v=to[i])==fa[x])continue;
+			fa[v]=x;
+			con[v]=eid[i];
+			dfs(v,r);
+		}
+	}
+	int que[N],qh,qt;
+	int pre[::E];
+	inline bool psh(int i){
+		if(rt[edg[i].u]!=rt[edg[i].v]){
+			for(;~i;deci[i]=true,deci[i^1]=false,i=pre[i]);
+			return true;
+		}
+		que[qt++]=i;
+		return false;
+	}
+	int djfa[N];
+	int gnxt(int x){
+		return djfa[x]?djfa[x]=gnxt(djfa[x]):x;
+	}
+	inline bool bfs(int curid){
+		{//build tree
+			init(disc::n);
+			for(int i=0;i<curid;i++){
+				if(deci[i]){
+					T::add(edg[i].u,edg[i].v,i);
+				}
+			}
+			mset(fa+1,0,disc::n);
+			mset(con+1,-1,disc::n);//cout
+			for(int i=1;i<=disc::n;i++){
+				djfa[i]=0;
+				if(fa[i]==0){
+					dfs(i,i);
+				}
+			}
+		}
+		qh=0,qt=0;
+		pre[curid]=-1,que[qt++]=curid;
+		deci[curid]=false;
+		if(psh(curid))return true;
+		for(;qh<qt;qh++){
+			const int ex=que[qh];
+			int u=gnxt(edg[ex].u),v=gnxt(edg[ex].v);
+			for(;u!=v;u=gnxt(u)){
+				if(dep[u]<dep[v]){
+					swap(u,v);
+				}
+				assert(deci[con[u]]);
+				pre[con[u]^1]=ex;
+				if(psh(con[u]^1))return true;
+				djfa[u]=fa[u];
 			}
 		}
 		return false;
 	}
-}lb[N];
-int ans;
-ull bitmp[D];
-void dfs(int x,int cnt){
-	if(cnt-x>ans)return;
-	if(x==0)return ans=cnt,void();
-	const pii &cur=item[x];
-	lb[x-1]=lb[x];
-	int icnt=0;
-	icnt+=lb[x-1].ins(bitmp[cur.a]^bitmp[cur.b]);
-	icnt+=lb[x-1].ins(bitmp[cur.c]^bitmp[cur.d]);
-	if(icnt==0)return dfs(x-1,cnt+icnt-1);
-	dfs(x-1,cnt+icnt-1);
-	lb[x-1]=lb[x];
-	dfs(x-1,cnt);
 }
 inline int Main(){
 	const int n=ni;
-	num_disc::init();
-	for(int i=1;i<=n;i++){
-		item[i]=(pii){ni,ni,ni,ni};
-		num_disc::psh(item[i].a);
-		num_disc::psh(item[i].b);
-		num_disc::psh(item[i].c);
-		num_disc::psh(item[i].d);
+	disc::init();
+	for(int i=0;i<n*2;i++){
+		edg[i]=(Edge){ni+1,ni+1};
+		disc::psh(edg[i].u);
+		disc::psh(edg[i].v);
 	}
-	num_disc::main();
-	lb[n].init();
-	ans=n;
-	dfs(n,n);
+	disc::main();
+	int ans=0;
+	for(int i=0;i<n*2;i+=2){
+		ans+=T::bfs(i)||T::bfs(i+1);
+	}
 	return ans;
 }
 int main(){
@@ -100,9 +142,6 @@ int main(){
 	freopen("coin.in","r",stdin);
 	freopen("coin.out","w",stdout);
 #endif
-	for(int i=0;i<64;i++){
-		bitmp[i]=1ull<<i;
-	}
 	for(int tot=ni;tot--;printf("%d\n",Main()*2));
 	return 0;
 }
