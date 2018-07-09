366c6dfef4099d843835ec707691f9661028438e
Kerry Su <i_sukai@live.com>
2018-07-09 17:48:53 +0800

    [AC] 1425ms

--- EZOJ/Contests/1463/C.cpp
+++ EZOJ/Contests/1463/C.cpp
@@ -7,84 +7,79 @@ using namespace std;
 typedef long long lint;
 #define cout cerr
 #define ni (next_num<int>())
-namespace io {
+namespace IO {
 	const int SIZE = (1 << 21) + 1;
-	char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55]; int f, qr;
-	// getchar
+	char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
+	int f, qr=0;
 #define gc() (iS == iT ? (iT = (iS = ibuf) + fread (ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS ++)) : *iS ++)
-	// print the remaining part
-	inline void flush () {
-		fwrite (obuf, 1, oS - obuf, stdout);
-		oS = obuf;
+	inline void flush(){
+		fwrite(obuf,1,oS-obuf,stdout);
+		oS=obuf;
 	}
-	// putchar
-	inline void putc (char x) {
-		*oS ++ = x;
-		if (oS == oT) flush ();
+	inline void putc(char x){
+		*oS++=x;
+		if(oS==oT)flush();
 	}
-	// input a signed integer
-	template <class I>
-		inline void gi (I &x) {
-			for (f = 1, c = gc(); c < '0' || c > '9'; c = gc()) if (c == '-') f = -1;
-			for (x = 0; c <= '9' && c >= '0'; c = gc()) x = x * 10 + (c & 15); x *= f;
-		}
-	// print a signed integer
-	template <class I>
-		inline void print (I x) {
-			if (!x) putc ('0'); if (x < 0) putc ('-'), x = -x;
-			while (x) qu[++ qr] = x % 10 + '0',  x /= 10;
-			while (qr) putc (qu[qr --]);
-		}
-	//no need to call flush at the end manually!
-	struct Flusher_ {~Flusher_(){flush();}}io_flusher_;
+	template<class I>inline void print(I x){
+		if(x){
+			for(;x;qu[++qr]=x%10+'0',x/=10);
+			for(;qr;putc(qu[qr--]));
+		}else putc('0');
+	}
+	struct Flusher{~Flusher(){flush();}}__flusher__;
 }
 template<class T>inline T next_num(){
 	T i=0;char c;
-	using namespace io;
-	while(!isdigit(c=gc())&&c!='-');
-	bool neg=c=='-';
-	neg?c=gc():0;
+	using namespace IO;
+	for(;!isdigit(c=gc())&&c!='-';);
 	while(i=i*10-'0'+c,isdigit(c=gc()));
-	return neg?-i:i;
+	return i;
 }
 template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
-template<class T>inline void mset(T a[],int v,int n){memset(a,v,sizeof(a[0])*n);}
-const int N=3010;
-int vis[N][N],tim=0;
-int x0,y0,x1,y1;
-inline bool valid(int i,int j){
-	return i>=x0&&i<=x1&&j>=y0&&j<=y1&&((i&j)==0)&&vis[i][j]<tim;
+template<class T>inline void mset(T a,int v,int n){memset(a,v,n*sizeof(a[0]));}
+inline bool mat(const int x,const int y){
+	return (x&y)==0;
 }
-void dfs(int x,int y){
-	vis[x][y]=tim;
-	const static int dx[]={-1,0,1,0},dy[]={0,1,0,-1};
-	for(int d=0;d<4;d++){
-		const int tx=x+dx[d],ty=y+dy[d];
-		if(valid(tx,ty)){
-			dfs(tx,ty);
-		}
+void calc(const int r,const int x,int &cnt_nd,int &cnt_cm){//excludes r
+	if(r==0)return cnt_nd=cnt_cm=0,void();
+	calc(r>>1,x>>1,cnt_nd,cnt_cm);
+	if(x&1){
+		cnt_cm=cnt_nd;
+	}else{
+		cnt_nd<<=1;
 	}
-}
-inline lint Main(){
-	++tim;
-	x0=ni-1,y0=ni-1,x1=ni-1,y1=ni-1;
-	int ans=0;
-	for(int i=x0;i<=x1;i++){
-		for(int j=y0;j<=y1;j++){
-			if(valid(i,j)){
-				++ans;
-				dfs(i,j);
+	if(r&1){
+		if(mat(x,r-1)){
+			++cnt_nd;
+			if(r==1||!mat(x,r-2)){
+				++cnt_cm;
 			}
 		}
 	}
+	assert(cnt_nd<=r);
+}
+inline int calc(int x,int l,int r){
+	int ans=0;
+	int cnt_nd,cnt_cm;
+	calc(r+1,x,cnt_nd,cnt_cm);
+	ans+=cnt_cm;
+	if(l){
+		calc(l,x,cnt_nd,cnt_cm);
+		ans-=cnt_cm;
+		ans+=mat(x,l-1)&&mat(x,l);
+	}
 	return ans;
+}
+inline int Main(){
+	const int x0=ni-1,y0=ni-1,x1=ni-1,y1=ni-1;
+	return calc(x0,y0,y1)+calc(y0,x0,x1)-mat(x0,y0);
 }
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("wall.in","r",stdin);
 	freopen("wall.out","w",stdout);
 #endif
-	for(int tot=ni;tot--;io::print(Main()),io::putc('\n'));
+	for(int tot=ni;tot--;IO::print(Main()),IO::putc('\n'));
 	return 0;
 }
