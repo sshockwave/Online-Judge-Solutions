be616e00ffe6f03412fd2d27d05932cc242da350
Kerry Su <i_sukai@live.com>
2018-07-13 16:36:27 +0800

    [AC] 1236ms

--- UOJ/387.cpp
+++ UOJ/387.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#define NDEBUG
 #include <cassert>
 #include <cctype>
-#include <set>
+#include <queue>
 using namespace std;
-typedef set<int>si;
+typedef vector<int>vi;
 typedef long long lint;
 #define cout cerr
 #define ni (next_num<int>())
@@ -23,75 +22,31 @@ template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 template<class T>inline void mset(T a[],int v,int n){memset(a,v,n*sizeof(T));}
 template<class T>inline void mcpy(T a[],T b[],int n){memcpy(a,b,n*sizeof(T));}
 const int N=100010;
-int fa[N];
+int fa[N],dep[N],hei[N];
+vi son[N];
 int cnt[N];
-int dep[N];
-int pos[N];
-int nx[N];
-int m;
-int soncnt[N];
-struct soncnt_cmp{
-	inline bool operator () (int a,int b){
-		return soncnt[a]!=soncnt[b]?soncnt[a]<soncnt[b]:a<b;
+struct hei_cmp{
+	inline bool operator () (const int &a,const int &b){
+		return hei[a]-dep[a]<hei[b]-dep[b];
 	}
 };
-set<int,soncnt_cmp>lst[N];
-int dj_nx(int x){
-	if(lst[x].size()==(size_t)m){
-		apmax(nx[x],x+1);
-	}
-	return nx[x]!=x?nx[x]=dj_nx(nx[x]):x;
-}
-inline void alt_son(int x,int v){
-	if(v==0)return;
-	if(x==0)return;
-	lst[pos[x]].erase(x);
-	soncnt[x]+=v;
-	lst[pos[x]].insert(x);
-}
-void del(int x){
-	assert(x);
-	if(x==0)return;
-	if(fa[x]&&pos[fa[x]]==pos[x]-1){
-		alt_son(fa[x],-1);
-	}
-	lst[pos[x]].erase(x);
-}
-void ins_at(int x,int _i){
-	//when you enter this function,
-	//fa didn't count x
-	pos[x]=_i;
-	assert(dep[x]<=pos[x]);
-	assert(pos[fa[x]]<=pos[x]);
-	assert(lst[pos[x]].size()<(size_t)m);
-	if(fa[x]&&pos[fa[x]]==pos[x]){
-		assert(lst[pos[x]-1].size()==(size_t)m);
-		const int y=*lst[pos[x]-1].begin();
-		assert(soncnt[y]==0);
-		del(y);
-		del(fa[x]);
-		ins_at(y,pos[x]);
-		ins_at(fa[x],pos[x]-1);
-	}
-	assert(lst[pos[x]].size()<(size_t)m);
-	assert(pos[fa[x]]<pos[x]);
-	lst[pos[x]].insert(x);
-	if(fa[x]&&pos[fa[x]]==pos[x]-1){
-		alt_son(fa[x],1);
-	}
-}
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("todotree.in","r",stdin);
 	freopen("todotree.out","w",stdout);
 #endif
-	const int n=ni;
-	m=ni;
+	const int n=ni,m=ni;
 	mset(cnt+1,0,n);
 	dep[0]=0;
-	++cnt[dep[1]=dep[fa[1]=0]+1];
-	for(int i=2;i<=n;i++){
-		++cnt[dep[i]=dep[fa[i]=ni]+1];
+	for(int i=1;i<=n;i++){
+		fa[i]=i!=1?ni:0;
+		dep[i]=dep[fa[i]]+1;
+		hei[i]=dep[i];
+		++cnt[dep[i]];
+		son[fa[i]].push_back(i);
+	}
+	for(int i=n;i>=1;i--){
+		apmax(hei[fa[i]],hei[i]);
 	}
 	int ans=0;
 	int sum=0;
@@ -102,24 +57,24 @@ int main(){
 		}
 	}
 	printf("%d\n",ans);
+	priority_queue<int,vi,hei_cmp>q;
+	q.push(1);
 	for(int i=1;i<=ans;i++){
-		nx[i]=i;
-	}
-	ins_at(1,1);
-	mset(soncnt+1,0,n);
-	for(int x=2;x<=n;x++){
-		ins_at(x,dj_nx(max(pos[fa[x]],dep[x])));
-		/*for(int i=2;i<=x;i++){
-			//cout
-			assert(pos[fa[i]]<pos[i]);
-		}*/
-	}
-	for(int i=1;i<=ans;i++){
-		printf("%d ",int(lst[i].size()));
-		for(si::iterator it=lst[i].begin(),ti=lst[i].end();it!=ti;++it){
-			printf("%d ",*it);
+		printf("%d ",min<int>(m,q.size()));
+		vi nxt;
+		for(int t=1;t<=m&&!q.empty();t++){
+			const int x=q.top();
+			q.pop();
+			printf("%d ",x);
+			for(vi::iterator it=son[x].begin(),ti=son[x].end();it!=ti;++it){
+				nxt.push_back(*it);
+			}
+		}
+		for(vi::iterator it=nxt.begin(),ti=nxt.end();it!=ti;++it){
+			q.push(*it);
 		}
 		putchar('\n');
 	}
+	assert(q.empty());
 	return 0;
 }
