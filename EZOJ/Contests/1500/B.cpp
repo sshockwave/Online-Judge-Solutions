f1ec66ca36838ce1c52e621d1834e49e20681ac9
Kerry Su <i_sukai@live.com>
2018-10-10 12:39:16 +0800

    [AC] EZOJ/Contests/B

--- EZOJ/Contests/1500/B.cpp
+++ EZOJ/Contests/1500/B.cpp
@@ -22,14 +22,10 @@ template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 template<class T>inline void mset(T a[],int v,int n){memset(a,v,n*sizeof(T));}
 template<class T>inline void mcpy(T a[],T b[],int n){memcpy(a,b,n*sizeof(T));}
 template<class T>inline T gcd(const T &a,const T &b){return b?gcd(b,a%b):a;}
+const db pi=M_PI;
 inline db Main(){
 	const int n=ni,m=ni;
-	if(n==m)return 1;
-	const int g=gcd(n,m);
-	if(n>m){
-		return tan((db)M_PI/m)/tan((db)M_PI/n);
-	}
-	return sin((db)M_PI/m)/sin((db)M_PI/n);
+	return cos(pi/(m/gcd(n,m)*n))/sin(pi/n)*tan(pi/m);
 }
 int main(){
 #ifndef ONLINE_JUDGE
