0f11abfeb8b32eebf971a96509cac5ded7c448eb
Kerry Su <i_sukai@live.com>
2018-12-27 15:17:30 +0800

    [100] half-plane intersection 85ms

--- EZOJ/Contests/1538/a.cpp
+++ EZOJ/Contests/1538/a.cpp
@@ -24,26 +24,7 @@ template<class T>inline void mcpy(T a[],T b[],int n){memcpy(a,b,n*sizeof(T));}
 template<class T>inline T sqr(const T &x){return x*x;}
 template<class T>inline T cabs(const T &x){return x>=0?x:-x;}
 template<class T>inline T gcd(const T &a,const T &b){return b?gcd(b,a%b):a;}
-const int N=200010;
-const db EPS=1e-7;
-inline bool isint(db x){
-	return cabs(x-round(x))<=EPS;
-}
-inline db cfloor(db x){
-	return floor(x+EPS);
-}
-inline db cceil(db x){
-	return ceil(x-EPS);
-}
-inline lint ex_euclid(lint a,lint b,lint c,lint n){
-	lint ans=0;
-	ans+=(lint)a/c*n*(n+1)/2,a%=c;
-	ans+=(lint)b/c*n,b%=c;
-	if(a==0)return ans;
-	lint m=(a*n+b)/c;
-	ans+=m*n-ex_euclid(c,-b-1,a,m);
-	return ans;
-}
+const int N=200010,INF=0x7f7f7f7f;
 struct Pt{
 	int x,y;
 	inline lint d2()const{return sqr<lint>(x)+sqr<lint>(y);}
@@ -54,36 +35,20 @@ struct Pt{
 		out<<"("<<b.x<<","<<b.y<<")";
 		return out;
 	}
+	inline lint eval(lint p)const{
+		return p*x+y;
+	}
 };
-struct Line{
-	Pt a,vec;
-	inline lint getval(int l,int r){
-		lint ans=0;
-		ans+=ex_euclid(vec.y,-(lint)a.x*vec.y,vec.x,r);
-		if(l){
-			ans-=ex_euclid(vec.y,-(lint)a.x*vec.y,vec.x,l-1);
-		}
-		ans+=(lint)a.y*(r-l+1);
-		return ans;
-	}
-	inline friend ostream & operator << (ostream & out,const Line &b){
-		out<<"["<<b.a<<">>"<<b.vec<<"]";
-		return out;
-	}
-}segm[N<<1],que[N<<1];
-inline db getsect(Line a,Line b){
-	Pt u=b.a-a.a;
-	db v1=(db)dot(u,a.vec)/a.vec.d2();
-	db v2=(db)crs(a.vec,u)/a.vec.d2()/((db)crs(b.vec,a.vec)/dot(b.vec,a.vec));
-	return v1+v2;
+Pt stk1[N],stk2[N];
+inline bool turn_left(Pt a,Pt b,Pt c){
+	return crs(b-a,c-a)>0;
 }
-inline void itsect(Line a,Line b,db &x,db &y){
-	db r=getsect(a,b);
-	x=a.a.x+a.vec.x*r;
-	y=a.a.y+a.vec.y*r;
+inline bool turn_right(Pt a,Pt b,Pt c){
+	return crs(b-a,c-a)<0;
 }
-inline bool hid_down(Line a,Line b,Line c){
-	return getsect(b,a)>=getsect(b,c);
+inline db itsect(Pt a,Pt b){
+	assert(a.x!=b.x);
+	return -(db)(a.y-b.y)/(a.x-b.x);
 }
 int main(){
 #ifndef ONLINE_JUDGE
@@ -91,49 +56,38 @@ int main(){
 	freopen("a.out","w",stdout);
 #endif
 	const int n=ni;
+	int ss1=0,ss2=0;
 	for(int i=1;i<=n;i++){
-		const int l=ni,r=ni;
-		segm[i]=(Line){(Pt){l,l},(Pt){i-1,i-2}};
-		segm[i+n]=(Line){(Pt){r,r},(Pt){-(i-1),-(i-2)}};
+		{
+			const Pt p={i,ni};
+			for(;ss1>=2&&!turn_right(stk1[ss1-2],stk1[ss1-1],p);ss1--);
+			stk1[ss1++]=p;
+		}
+		{
+			const Pt p={i,ni};
+			for(;ss2>=2&&!turn_left(stk2[ss2-2],stk2[ss2-1],p);ss2--);
+			stk2[ss2++]=p;
+		}
 	}
-	int qh=0,qt=0;
-	for(int i=1;i<=n*2;i++){
-		for(;qt-qh>=2&&hid_down(que[qt-2],que[qt-1],segm[i]);qt--);
-		que[qt++]=segm[i];
-	}
-	for(;qt-qh>3;){
-		if(hid_down(que[qt-1],que[qh],que[qh+1])){
-			qh++;
-		}else if(hid_down(que[qt-2],que[qt-1],que[qh])){
-			qt--;
-		}else break;
-	}
+	int st=-INF;
 	lint ans=0;
-	for(int i=qh;i<qt;i++){
-		const int pr=i>qh?i-1:(qt-1),nx=i+1<qt?i+1:qh;
-		db p1x,p1y,p2x,p2y;
-		itsect(que[i],que[pr],p1x,p1y);
-		itsect(que[i],que[nx],p2x,p2y);
-		const Pt &vec=que[i].vec;
-		if(i==qh){
-			if(vec.x==0){
-				ans+=cfloor(p1y)-cceil(p2y)+1;
-			}else if(isint(p1x)&&isint(p1y)){
-				ans+=1;
-			}
+	for(int i=0,j=ss2-1;i<ss1||j>=0;){
+		const Pt &a=stk1[i],&b=stk2[j];
+		int ed1=i+1<ss1?floor(itsect(a,stk1[i+1])):INF;
+		int ed2=j-1>=0?floor(itsect(b,stk2[j-1])):INF;
+		int ed=min(ed1,ed2);
+		if(a.x<b.x){
+			apmax(st,ceil(itsect(a,b)));
+		}else if(a.x>b.x){
+			apmin(ed,floor(itsect(a,b)));
 		}
-		if(vec.x>0){
-			int x1=cceil(p1x)+isint(p1x),x2=cfloor(p2x);
-			if(x1>x2)continue;
-			ans-=que[i].getval(x1,x2);
-			x1=cceil((db)(x1-que[i].a.x)/que[i].vec.x);
-			x2=cceil((db)(x2-que[i].a.x)/que[i].vec.x);
-			ans+=x2-x1+1;
-		}else if(vec.x<0){
-			int x1=cceil(p2x)+isint(p2x),x2=cfloor(p1x);
-			if(x1>x2)continue;
-			ans+=que[i].getval(x1,x2);
+		if(st<=ed){
+			ans+=(lint)(ed-st+1)*(b.eval(st)-a.eval(st)+1+b.eval(ed)-a.eval(ed)+1)/2;
 		}
+		ed=min(ed1,ed2);
+		i+=ed1==ed;
+		j-=ed2==ed;
+		st=ed+1;
 	}
 	printf("%lld\n",ans);
 	return 0;
