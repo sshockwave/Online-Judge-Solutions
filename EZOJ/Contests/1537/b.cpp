8aa24083587c9398a96d269cbc57f6d412571027
Kerry Su <i_sukai@live.com>
2018-12-30 16:23:19 +0800

    [100] segment tree

--- EZOJ/Contests/1537/b.cpp
+++ EZOJ/Contests/1537/b.cpp
@@ -21,18 +21,10 @@ template<class T1,class T2>inline void apmax(T1 &a,const T2 &b){if(a<b)a=b;}
 template<class T1,class T2>inline void apmin(T1 &a,const T2 &b){if(b<a)a=b;}
 template<class T>inline void mset(T a[],int v,int n){memset(a,v,n*sizeof(T));}
 template<class T>inline void mcpy(T a[],T b[],int n){memcpy(a,b,n*sizeof(T));}
+template<class T>inline T sqr(const T &x){return x*x;}
+template<class T>inline T cabs(const T &x){return x>=0?x:-x;}
+template<class T>inline T gcd(const T &a,const T &b){return b?gcd(b,a%b):a;}
 const int N=120010;
-int a[N],pos[N];
-struct Intv{
-	int l,r;
-	inline friend Intv operator + (const Intv &a,const Intv &b){
-		return (Intv){min(a.l,b.l),max(a.r,b.r)};
-	}
-	inline friend bool operator == (const Intv &a,const Intv &b){
-		return a.l==b.l&&a.r==b.r;
-	}
-}ext[N];
-int dep[N];
 namespace seg{
 	const int N=::N<<1;
 	struct Node;
@@ -40,89 +32,86 @@ namespace seg{
 	struct Node{
 		node lson,rson;
 		int l,m,r;
-		Intv ext;
-		inline void getinfo(){
-			ext=::ext[m];
+		int mn,mncnt;
+		int dt,hitcnt;
+		lint sum;
+		inline void addv(int d,int h){
+			mn+=d;
+			dt+=d,hitcnt+=h;
+			sum+=(lint)mncnt*h;
+		}
+		inline void dn(){
+			if(dt||hitcnt){
+				int v=min(lson->mn,rson->mn);
+				lson->addv(dt,lson->mn==v?hitcnt:0);
+				rson->addv(dt,rson->mn==v?hitcnt:0);
+				dt=hitcnt=0;
+			}
 		}
 		inline void up(){
-			ext=lson->ext+rson->ext;
+			assert(dt==0&&hitcnt==0);
+			mn=min(lson->mn,rson->mn);
+			mncnt=(lson->mn==mn?lson->mncnt:0)+(rson->mn==mn?rson->mncnt:0);
+			sum=lson->sum+rson->sum;
 		}
 	}pool[N];
 	node build(int l,int r){
 		static node n=pool;
 		const node x=n++;
 		x->l=l,x->m=(l+r)>>1,x->r=r;
-		if(l==r){
-			x->getinfo();
-		}else{
+		x->mn=x->dt=x->hitcnt=0;
+		x->mncnt=r-l+1;
+		x->sum=0;
+		if(l!=r){
 			x->lson=build(l,x->m);
 			x->rson=build(x->m+1,r);
-			x->up();
 		}
 		return x;
 	}
-	Intv ask_go(node x,int l,int r){
-		if(x->l==l&&x->r==r)return x->ext;
-		if(r<=x->m)return ask_go(x->lson,l,r);
-		if(l>x->m)return ask_go(x->rson,l,r);
-		return ask_go(x->lson,l,x->m)+ask_go(x->rson,x->m+1,r);
-	}
-	void upd(node x,int p){
-		if(x->l==x->r){
-			x->getinfo();
+	void alt_add(const node x,int l,int r,int v){
+		if(l>r)return;
+		if(x->l==l&&x->r==r)return x->addv(v,0);
+		x->dn();
+		if(r<=x->m){
+			alt_add(x->lson,l,r,v);
+		}else if(l>x->m){
+			alt_add(x->rson,l,r,v);
 		}else{
-			upd(p<=x->m?x->lson:x->rson,p);
-			x->up();
+			alt_add(x->lson,l,x->m,v);
+			alt_add(x->rson,x->m+1,r,v);
 		}
+		x->up();
 	}
-}
-Intv qry[N];
-lint ans[N];
-/*
-namespace T{
-	vi vec[N];
-	void build(int l,int r,int d){
-		if(l==r)return;
-		dep[l]=d;
-		const Intv &cur=ext[l];
-		assert(cur.r<=r);
-		if(cur.l<l||cur.r==r){
-			build(l,l,d+1),build(l+1,r,d+1);
+	void alt_hit(const node x,int l,int r){
+		if(x->mn>0)return;
+		if(x->l==l&&x->r==r)return x->addv(0,1);
+		x->dn();
+		if(r<=x->m){
+			alt_hit(x->lson,l,r);
+		}else if(l>x->m){
+			alt_hit(x->rson,l,r);
 		}else{
-			build(l,cur.r,d+1),build(cur.r+1,r,d+1);
+			alt_hit(x->lson,l,x->m);
+			alt_hit(x->rson,x->m+1,r);
 		}
+		x->up();
 	}
-	treap::node work(int l,int r){
-		using namespace treap;
-		if(l==r)return null;
-		const Intv &cur=ext[l];
-		assert(cur.r<=r);
-		int m=cur.l<l||cur.r==r?l:cur.r;
-		const node lson=work(l,m),rson=work(m+1,r);
-		for(vi::iterator it=vec[l].begin(),ti=vec[l].end();it!=ti;++it){
-			int ql=qry[*it].l,qr=qry[*it].r;
-			ans[*it]=ask_r(lson,m-ql+1)+ask_l(rson,qr-m);
-		}
-		const node x=mg(lson,rson);
-		add_l(x),add_r(x);
-		return x;
+	lint ask(const node x,int l,int r){
+		if(x->l==l&&x->r==r)return x->sum;
+		x->dn();
+		if(r<=x->m)return ask(x->lson,l,r);
+		if(l>x->m)return ask(x->rson,l,r);
+		return ask(x->lson,l,x->m)+ask(x->rson,x->m+1,r);
 	}
-}*/
-lint all_ans=0;
-inline lint n2(int n){
-	return (lint)n*(n+1)/2;
 }
-void solve(int l,int r){
-	if(l==r)return;
-	bool blk=false;
-	for(;l<r;){
-		const Intv &cur=ext[l];
-		if(cur.r<r){
-			//cout
-		}
-	}
-	ans+=blk;
-}
+struct Query{
+	int l;
+	lint ans;
+}qry[N];
+vi q[N];
+int perm[N];
+int stk1[N],ss1=0;
+int stk2[N],ss2=0;
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("b.in","r",stdin);
@@ -130,49 +119,32 @@ int main(){
 #endif
 	const int n=ni;
 	for(int i=1;i<=n;i++){
-		a[i]=ni;
+		perm[i]=ni;
 	}
-	if(n==1){
-		for(int tot=ni;tot--;){
-			puts("1");
+	int tot=ni;
+	for(int i=1;i<=tot;i++){
+		qry[i]=(Query){ni,0};
+		q[ni].push_back(i);
+	}
+	const seg::node rt=seg::build(1,n);
+	stk1[0]=stk2[0]=0;
+	for(int i=1;i<=n;i++){
+		for(;ss1&&perm[stk1[ss1]]>perm[i];ss1--){
+			seg::alt_add(rt,stk1[ss1-1]+1,stk1[ss1],perm[stk1[ss1]]-perm[i]);
 		}
-		return 0;
-	}
-	seg::node rt=seg::build(1,n-1);
-	for(;;){
-		bool haschange=false;
-		for(int i=1;i<n;i++){
-			Intv cur=ext[i];
-			Intv nxt=seg::ask_go(rt,cur.l,cur.r-1);
-			if(!(cur==nxt)){
-				haschange=true;
-				seg::upd(rt,i);
-			}
+		stk1[++ss1]=i;
+		for(;ss2&&perm[stk2[ss2]]<perm[i];ss2--){
+			seg::alt_add(rt,stk2[ss2-1]+1,stk2[ss2],perm[i]-perm[stk2[ss2]]);
 		}
-		if(!haschange)break;
-	}
-	solve(1,n),ans_all+=n;
-	for(int tot=ni;tot--;){
-		int l=ni,r=ni;
-		if(l==1&&r==n){
-			printf("%lld\n",all_ans);
-			continue;
+		stk2[++ss2]=i;
+		seg::alt_add(rt,1,i-1,-1);
+		seg::alt_hit(rt,1,i);
+		for(vi::iterator it=q[i].begin(),ti=q[i].end();it!=ti;++it){
+			qry[*it].ans=seg::ask(rt,qry[*it].l,i);
 		}
 	}
-	/*
-	T::build(1,n,1);
-	for(int i=1;i<n;i++){
-		seg::upd(rt,i);
+	for(int i=1;i<=tot;i++){
+		printf("%lld\n",qry[i].ans);
 	}
-	const int q=ni;
-	for(int i=1;i<=q;i++){
-		qry[i]=(Intv){ni,ni};
-		T::vec[seg::ask_pos(qry[i].l,qry[i].r-1)].push_back(i);
-	}
-	T::work(1,n);
-	for(int i=1;i<=q;i++){
-		printf("%lld\n",ans[i]);
-	}
-	*/
 	return 0;
 }
