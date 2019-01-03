295fe093765acaa99fd270487b30775873507da6
Kerry Su <i_sukai@live.com>
2019-01-03 20:34:38 +0800

    [100] segment tree 1780ms

--- EZOJ/Contests/1543/B.cpp
+++ EZOJ/Contests/1543/B.cpp
@@ -24,148 +24,130 @@ template<class T>inline T sqr(const T &x){return x*x;}
 template<class T>inline T cabs(const T &x){return x>=0?x:-x;}
 template<class T>inline T gcd(const T &a,const T &b){return b?gcd(b,a%b):a;}
 const int N=100010;
+const lint LINF=0x7f7f7f7f7f7f7f7fll;
+namespace dj{
+	int fa[N];
+	inline void init(int n){
+		mset(fa+1,0,n);
+	}
+	inline int rt(int x){
+		return fa[x]?fa[x]=rt(fa[x]):x;
+	}
+}
+namespace seg{
+	const int N=::N<<1;
+	struct Node;
+	typedef Node* node;
+	struct Info{
+		int x;
+		lint w;
+		inline friend bool operator != (const Info &a,const Info &b){
+			return dj::rt(a.x)!=dj::rt(b.x);
+		}
+		inline friend bool operator < (const Info &a,const Info &b){
+			return a.w<b.w;
+		}
+	};
+	const Info null=(Info){0,LINF};
+	struct Node{
+		node lson,rson;
+		int l,m,r;
+		Info v1,v2;
+		lint dt;
+		inline void addv(lint v){
+			dt+=v;
+			v1.w+=v;
+			if(v2.w<LINF){
+				v2.w+=v;
+			}
+		}
+		inline void up(){
+			v1=min(lson->v1,rson->v1);
+			const Info v_l=lson->v1!=v1?lson->v1:lson->v2!=v1?lson->v2:null;
+			const Info v_r=rson->v1!=v1?rson->v1:rson->v2!=v1?rson->v2:null;
+			v2=min(v_l,v_r);
+			int t=dt;
+			dt=0;
+			addv(t);
+		}
+	}pool[N];
+	node n;
+	node build(int l,int r){
+		const node x=n++;
+		x->l=l,x->m=(l+r)>>1,x->r=r;
+		x->dt=0;
+		if(l==r){
+			x->v1=(Info){x->m,0},x->v2=null;
+		}else{
+			x->lson=build(l,x->m);
+			x->rson=build(x->m+1,r);
+			x->up();
+		}
+		return x;
+	}
+	void alt_add(const node x,int l,int r,lint w){
+		if(x->l==l&&x->r==r)return x->addv(w);
+		if(r<=x->m){
+			alt_add(x->lson,l,r,w);
+		}else if(l>x->m){
+			alt_add(x->rson,l,r,w);
+		}else{
+			alt_add(x->lson,l,x->m,w);
+			alt_add(x->rson,x->m+1,r,w);
+		}
+		x->up();
+	}
+}
+struct Event{
+	int y,l,r,w;
+	inline friend bool operator < (const Event &a,const Event &b){
+		return a.y<b.y;
+	}
+}ev[N<<2];
 struct Edge{
-	int x1,x2;
-	int y1,y2;
-	int w;
+	int u,v;
+	lint w;
 	inline friend bool operator < (const Edge &a,const Edge &b){
 		return a.w<b.w;
 	}
-}edg[N*3];
-int n,e;
-namespace solve1{
-	const int N=1010,E=N*N/2;
-	struct Edge2{
-		int u,v;
-		lint w;
-		inline friend bool operator < (const Edge2 &a,const Edge2 &b){
-			return a.w<b.w;
-		}
-	}edg2[E];
-	namespace dj{
-		int fa[N];
-		inline void init(int n){
-			mset(fa+1,0,n);
-		}
-		inline int rt(int x){
-			return fa[x]?fa[x]=rt(fa[x]):x;
-		}
-	}
-	lint val[N][N];
-	inline lint main(){
-		dj::init(n);
-		for(int i=1;i<=e;i++){
-			val[edg[i].x1][edg[i].y1]+=edg[i].w;
-			val[edg[i].x1][edg[i].y2+1]-=edg[i].w;
-			val[edg[i].x2+1][edg[i].y1]-=edg[i].w;
-			val[edg[i].x2+1][edg[i].y2+1]+=edg[i].w;
-		}
-		for(int i=1;i<=n;i++){
-			for(int j=2;j<=n;j++){
-				val[i][j]+=val[i][j-1];
-			}
-		}
-		for(int i=2;i<=n;i++){
-			for(int j=1;j<=n;j++){
-				val[i][j]+=val[i-1][j];
-			}
-		}
-		int es=0;
-		for(int i=1;i<=n;i++){
-			for(int j=i+1;j<=n;j++){
-				edg2[++es]=(Edge2){i,j,val[i][j]};
-			}
-		}
-		sort(edg2+1,edg2+es+1);
-		lint ans=0;
-		for(int i=1;i<=es;i++){
-			int u=dj::rt(edg2[i].u),v=dj::rt(edg2[i].v);
-			if(u!=v){
-				dj::fa[u]=v;
-				ans+=edg2[i].w;
-			}
-		}
-		return ans;
-	}
-}
-namespace solve3{
-	namespace dj{
-		int fa[N];
-		inline void init(int n){
-			mset(fa+1,0,n);
-		}
-		inline int rt(int x){
-			return fa[x]?fa[x]=rt(fa[x]):x;
-		}
-	}
-	lint ans=0;
-	int nxt[N];
-	inline int getnxt(int x){
-		if(nxt[x]==x&&x<n&&dj::rt(x)==dj::rt(x+1)){
-			nxt[x]=x+1;
-		}
-		return nxt[x]!=x?nxt[x]=getnxt(nxt[x]):x;
-	}
-	int curw;
-	inline void mg(int u,int v){
-		u=dj::rt(u),v=dj::rt(v);
-		if(u!=v){
-			ans+=curw;
-			dj::fa[u]=v;
-		}
-	}
-	inline void mg_intv(int l,int r){
-		for(;getnxt(l)!=getnxt(r);mg(l,getnxt(l)+1));
-	}
-	int int_l[N],int_r[N];
-	inline lint main(){
-		for(int i=1;i<=n;i++){
-			nxt[i]=i;
-			int_l[i]=int_r[i]=-1;
-		}
-		dj::init(n);
-		for(int i=1;i<=e;i++){
-			int_l[edg[i].x1]=edg[i].y1;
-			int_r[edg[i].x2]=edg[i].y2;
-		}
-		for(int i=1;i<=n;i++){
-			if(int_l[i]==-1){
-				if(i+1<=n){
-					edg[++e]=(Edge){i,i,i+1,n,0};
-				}
-			}else{
-				if(i+1<=int_l[i]-1){
-					edg[++e]=(Edge){i,i,i+1,int_l[i]-1,0};
-				}
-				if(int_r[i]+1<=n){
-					edg[++e]=(Edge){i,i,int_r[i]+1,n,0};
-				}
-			}
-		}
-		sort(edg+1,edg+e+1);
-		for(int i=1;i<=e;i++){
-			curw=edg[i].w;
-			mg_intv(edg[i].x1,edg[i].x2);
-			mg_intv(edg[i].y1,edg[i].y2);
-			mg(edg[i].x1,edg[i].y1);
-		}
-		return ans;
-	}
-}
+}edg[N];
 int main(){
 #ifndef ONLINE_JUDGE
 	freopen("rect.in","r",stdin);
 	freopen("rect.out","w",stdout);
 #endif
-	n=ni,e=ni;
-	for(int i=1;i<=e;i++){
-		edg[i]=(Edge){ni,ni,ni,ni,ni};
+	const int n=ni;
+	int es=0;
+	for(int tot=ni;tot--;){
+		const int x1=ni,x2=ni,y1=ni,y2=ni,w=ni;
+		ev[++es]=(Event){y1,x1,x2,w};
+		ev[++es]=(Event){y2+1,x1,x2,-w};
+		ev[++es]=(Event){x1,y1,y2,w};
+		ev[++es]=(Event){x2+1,y1,y2,-w};
 	}
+	sort(ev+1,ev+es+1);
+	int ccnt=n;
+	dj::init(n);
 	lint ans=0;
-	if(n<=1000&&e<=1000){
-		ans=solve1::main();
-	}else{
-		ans=solve3::main();
+	for(;ccnt>1;){
+		seg::n=seg::pool;
+		const seg::node rt=seg::build(1,n);
+		for(int i=1,j=1;i<=n;i++){
+			for(;j<=es&&ev[j].y<=i;j++){
+				seg::alt_add(rt,ev[j].l,ev[j].r,ev[j].w);
+			}
+			seg::Info v=dj::rt(rt->v1.x)!=dj::rt(i)?rt->v1:rt->v2;
+			edg[i]=(Edge){i,v.x,v.w};
+		}
+		sort(edg+1,edg+n+1);
+		for(int i=1;i<=n;i++){
+			int u=dj::rt(edg[i].u),v=dj::rt(edg[i].v);
+			if(u!=v){
+				--ccnt;
+				ans+=edg[i].w;
+				dj::fa[u]=v;
+			}
+		}
 	}
 	printf("%lld\n",ans);
 	return 0;
